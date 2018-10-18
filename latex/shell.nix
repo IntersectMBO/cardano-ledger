f71c9f105997962d57d6c378ded67ebff7a72ea4
Jared Corduan <jaredcorduan@gmail.com>
2018-10-18 08:56:31 -0400

    code review changes

--- latex/shell.nix
+++ latex/shell.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
                       stmaryrd lm-math amsmath extarrows cleveref semantic xcolor
 
                       # bclogo and dependencies
-                      bclogo mdframed xkeyval etoolbox needspace pgf
+                      bclogo mdframed xkeyval etoolbox needspace
 
                       # font libraries `mathpazo` seems to depend on palatino, but it isn't pulled.
                       mathpazo palatino microtype
