25090fbd1e47ac39608ee220854122ad57227f69
Matthias Güdemann <matthias.gudemann@iohk.io>
2018-10-22 19:04:44 +0200

    Use `StakePool` instead of of set of keys in `stPools`

--- hs/src/Delegation/StakePool.hs
+++ hs/src/Delegation/StakePool.hs
@@ -4,6 +4,8 @@ module Delegation.StakePool
   ) where
 
 import           Data.Map        (Map)
+import           Data.Ratio
+import           Numeric.Natural
 
 import           Coin            (Coin)
 import           Keys
@@ -11,12 +13,14 @@ import           Keys
 -- |A stake pool.
 data StakePool = StakePool
                    { poolPubKey  :: VKey
-                   , poolPledges :: Map VKey Coin
+                   , poolPledges :: Map VKey Coin -- TODO not updated currently
                    , poolCost    :: Coin
-                   , poolMargin  :: Float -- TODO is float okay?
+                   , poolMargin  :: Ratio Natural -- TODO is float okay?
+                                                  -- how about positive rationals?
                    , poolAltAcnt :: Maybe HashKey
                    } deriving (Show, Eq, Ord)
 
+-- TODO: Does this need the sum of unspent UTxO for the delegator?
 -- |The delegation of one stake key to another.
 data Delegation = Delegation { delegator :: VKey
                              , delegatee :: VKey }
