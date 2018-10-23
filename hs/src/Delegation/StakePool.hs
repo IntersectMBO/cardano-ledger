029af9cad53536242b63e0e3c59c1506dfe173bc
Matthias Güdemann <matthias.gudemann@iohk.io>
2018-10-23 12:43:01 +0200

    Remove answered TODOs

--- hs/src/Delegation/StakePool.hs
+++ hs/src/Delegation/StakePool.hs
@@ -15,12 +15,10 @@ data StakePool = StakePool
                    { poolPubKey  :: VKey
                    , poolPledges :: Map VKey Coin -- TODO not updated currently
                    , poolCost    :: Coin
-                   , poolMargin  :: Ratio Natural -- TODO is float okay?
-                                                  -- how about positive rationals?
+                   , poolMargin  :: Ratio Natural
                    , poolAltAcnt :: Maybe HashKey
                    } deriving (Show, Eq, Ord)
 
--- TODO: Does this need the sum of unspent UTxO for the delegator?
 -- |The delegation of one stake key to another.
 data Delegation = Delegation { delegator :: VKey
                              , delegatee :: VKey }
