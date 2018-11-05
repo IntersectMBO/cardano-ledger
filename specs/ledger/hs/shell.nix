8042dccb1a2225a500cf0fd17305e5994902c9c9
Michael Bishop <cleverca22@gmail.com>
2018-11-05 08:37:45 -0400

    [DEVOPS-1118] get cs-ledger building with nix (#92)

--- specs/ledger/hs/shell.nix
+++ specs/ledger/hs/shell.nix
@@ -1,10 +1,5 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation rec {
-  name = "env";
-  env = buildEnv { name = name; paths = buildInputs; };
-  buildInputs = [
-    cabal-install
-    haskellPackages.ghcid
-    haskell.compiler.ghc861
-  ];
-}
+let
+  pkgs = import ../../../pkgs.nix;
+in pkgs.lib.overrideDerivation ((import ./.).env) (old: {
+  nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.cabal-install pkgs.haskell.packages.ghc861.ghcid ];
+})
