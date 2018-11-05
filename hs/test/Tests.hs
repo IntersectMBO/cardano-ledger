3b152eb4d76b335e6ba059b04aa05a812cb424ed
Matthias Güdemann <matthias.gudemann@iohk.io>
2018-11-05 16:34:32 +0100

    Stylish changes

--- hs/test/Tests.hs
+++ hs/test/Tests.hs
@@ -7,12 +7,12 @@ import qualified Data.Set                as Set
 import           Numeric.Natural         (Natural)
 
 import           Test.Tasty
-import           Test.Tasty.HUnit
 import           Test.Tasty.Hedgehog
+import           Test.Tasty.HUnit
 
 import           Hedgehog
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
+import qualified Hedgehog.Gen            as Gen
+import qualified Hedgehog.Range          as Range
 
 import           Coin
 import           Keys
