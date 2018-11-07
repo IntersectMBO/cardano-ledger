035a5e3edb0c150fed11fc25ac94562cfc7aa245
Rupert Horlick <rupert.horlick@iohk.io>
2018-11-07 16:27:38 -0400

    [#115] Add witness to UTxO validation

--- test/Test/Cardano/Chain/Txp/Validation.hs
+++ test/Test/Cardano/Chain/Txp/Validation.hs
@@ -34,52 +34,79 @@ import Cardano.Chain.Block
 import Cardano.Chain.Epoch.File
   (ParseError, parseEpochFile)
 import Cardano.Chain.Genesis
-  (readGenesisData)
+  (GenesisData (..), GenesisProtocolConstants (..), readGenesisData)
 import Cardano.Chain.Slotting
   (SlotId)
 import Cardano.Chain.Txp
-  (UTxO, UTxOValidationError, genesisUtxo, txpTxs, updateUTxO)
+  (TxPayload (..), UTxO, UTxOValidationError, genesisUtxo, updateUTxOWitness)
+import Cardano.Crypto
+  (ProtocolMagic)
 
 import Test.Cardano.Chain.Epoch.File
   (getEpochFiles)
 
 
+-- | These tests perform transaction validation over mainnet epoch files
+--
+--   We have chosen to split each epoch file into its own 'Property', because
+--   this leads to a clearer log of progress during testing. This requires an
+--   'IORef' to synchronise the 'UTxO' between epochs, as 'Property's do not
+--   return values.
 tests :: IO Bool
 tests = do
+
+  -- Get 'GenesisData' from the mainnet JSON configuration
   genesisData <- either (panic . sformat build) identity
     <$> runExceptT (readGenesisData "test/mainnet-genesis.json")
-  let utxo = genesisUtxo genesisData
-  utxoRef <- newIORef utxo
+
+  -- Extract mainnet 'ProtocolMagic'
+  let pm = gpcProtocolMagic $ gdProtocolConsts genesisData
+
+  -- Create an 'IORef' containing the genesis 'UTxO'
+  utxoRef <- newIORef $ genesisUtxo genesisData
+
+  -- Get a list of epoch files to perform validation on
   files   <- take 15 <$> getEpochFiles
+
+  -- Validate the transactions of each epoch file in a single 'Property' and
+  -- check them all sequentially
   let
     properties :: [(PropertyName, Property)]
-    properties = zip (fromString <$> files) (epochValid utxoRef <$> files)
+    properties = zip (fromString <$> files) (epochValid pm utxoRef <$> files)
   checkSequential $ Group "Test.Cardano.Chain.Txp.Validation" properties
+
 
 data Error
   = ErrorParseError ParseError
   | ErrorUTxOValidationError SlotId UTxOValidationError
   deriving (Eq, Show)
 
-epochValid :: IORef UTxO -> FilePath -> Property
-epochValid utxoRef fp = withTests 1 . property $ do
+
+-- | Check that a single epoch's transactions are valid by folding over 'Blund's
+epochValid :: ProtocolMagic -> IORef UTxO -> FilePath -> Property
+epochValid pm utxoRef fp = withTests 1 . property $ do
   utxo <- liftIO $ readIORef utxoRef
   let stream = parseEpochFile fp
-  result <- (liftIO . runResourceT . runExceptT) (foldUTxO utxo stream)
+  result <- (liftIO . runResourceT . runExceptT) (foldUTxO pm utxo stream)
   newUtxo <- evalEither result
   liftIO $ writeIORef utxoRef newUtxo
 
+
+-- | Fold transaction validation over a 'Stream' of 'Blund's
 foldUTxO
-  :: UTxO
+  :: ProtocolMagic
+  -> UTxO
   -> Stream (Of Blund) (ExceptT ParseError ResIO) ()
   -> ExceptT Error ResIO UTxO
-foldUTxO utxo blunds = S.foldM_
-  foldUTxOBlund
+foldUTxO pm utxo blunds = S.foldM_
+  (foldUTxOBlund pm)
   (pure utxo)
   pure
   (hoist (withExceptT ErrorParseError) blunds)
 
-foldUTxOBlund :: UTxO -> Blund -> ExceptT Error ResIO UTxO
-foldUTxOBlund utxo (block, _) =
+
+-- | Fold 'updateUTxO' over the transactions in a single 'Blund'
+foldUTxOBlund :: ProtocolMagic -> UTxO -> Blund -> ExceptT Error ResIO UTxO
+foldUTxOBlund pm utxo (block, _) =
   withExceptT (ErrorUTxOValidationError $ blockSlot block)
-    $ foldM updateUTxO utxo (txpTxs $ blockTxPayload block)
+    $ foldM (updateUTxOWitness pm) utxo (unTxPayload $ blockTxPayload block)
