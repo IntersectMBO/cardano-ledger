f927afcc77531a5fc610a4d85e5d66135ff3ff37
Jared Corduan <jaredcorduan@gmail.com>
2018-11-09 22:06:10 -0500

    refund pools only at epoch

--- docs/accounting/accounting/test/Test.hs
+++ docs/accounting/accounting/test/Test.hs
@@ -7,6 +7,7 @@ import Test.Tasty.HUnit
 
 import qualified Data.Map as Map
 import Lens.Micro ((&), (.~))
+import Numeric.Natural (Natural)
 
 import Accounting
 
@@ -25,22 +26,30 @@ conservationOfLovelace =
     ls <- forAll gledgerstate
     total (foldl applyAction ls actions) === fortyFiveBillionAda
 
-gcoin :: Coin -> Coin -> Gen Coin
-gcoin (Coin cmin) (Coin cmax) = Coin <$> Gen.integral (Range.linear cmin cmax)
+gnat :: Natural -> Natural -> Gen Natural
+gnat lower upper = Gen.integral (Range.linear lower upper)
 
-gslot :: Gen Slot
-gslot = Slot <$> Gen.integral (Range.linear 0 1000)
+gcoin :: Coin -> Coin -> Gen Coin
+gcoin (Coin cmin) (Coin cmax) = Coin <$> gnat cmin cmax
 
 gcert :: Gen Cert
 gcert = Gen.choice
-  [ KeyReg <$> Gen.integral (Range.linear 0 1000)
-  , PoolReg <$> Gen.integral (Range.linear 0 1000) ]
+  [ KeyReg <$> gnat 0 1000
+  , PoolReg <$> gnat 0 1000
+  , KeyDeReg <$> gnat 0 1000
+  , PoolRetire <$> gnat 0 1000 <*> (Duration <$> gnat 0 1000)]
 
 gobligations :: Gen (Cert, Slot)
 gobligations = do
   c <- gcert
-  s <- gslot
-  return (c, s)
+  s <- gnat 0 1000
+  return (c, Slot s)
+
+gretiring :: Slot -> Gen (Natural, Slot)
+gretiring cslot = do
+  certId <- gnat 0 1000
+  d <- gnat 0 1000
+  return (certId, cslot +* Duration d)
 
 gpconsts :: Gen PrtclConsts
 gpconsts = PrtclConsts
@@ -55,10 +64,11 @@ gledgerstate :: Gen LedgerState
 gledgerstate = do
   -- generate obligations and the current slot
   obl <- Gen.map (Range.linear 0 1000) gobligations
-  sl <- gslot
+  sl <- gnat 0 1000
   let
     creations = Map.elems obl
-    cslot = sl + (if Prelude.null creations then 0 else maximum creations)
+    minSlot = if Prelude.null creations then Slot 0 else maximum creations
+    cslot = Slot sl + minSlot
 
   -- protocol constants
   pc <- gpconsts
@@ -77,14 +87,15 @@ gledgerstate = do
   -- set the circulation to the remainder of the fort five billion ada
   let circ = fortyFiveBillionAda - (deps + fee + rpool + treas + reward + reserv)
 
+  -- set the retiring pools
+  ret <- Gen.map (Range.linear 0 1000) (gretiring cslot)
 
-  return (LedgerState circ deps treas reserv reward rpool fee obl pc cslot cslot)
+  return (LedgerState circ deps treas reserv reward rpool fee obl ret pc cslot cslot)
 
 gaction :: Gen Accounting.Action
 gaction = Gen.choice
   [ ActTxBody <$> gcoin (Coin 0) (Coin 1000)
-  , ActAddCert <$> gcert
-  , ActDelCert <$> gcert
+  , ActCert <$> gcert
   , ActWithdrawal <$> gcoin (Coin 0) (Coin 1000000)
   , ActEpochNoVote <$> Gen.realFloat (Range.exponentialFloat 0 1)
   , ActEpochWithVote <$> gpconsts <*> Gen.realFloat (Range.exponentialFloat 0 1)
@@ -103,14 +114,18 @@ examplePc = PrtclConsts
 
 exampleLS :: LedgerState
 exampleLS = LedgerState {
-    _circulation = Coin 29999999999999800
-  , _deposits = Coin 200
+    _circulation = Coin 29999999999998800
+  , _deposits = Coin 1200
   , _treasury = Coin 0
   , _reserves = Coin 14999999999999900
   , _rewards = Coin 100
   , _rewardPool = Coin 0
   , _fees = Coin 0
-  , _obligations = Map.singleton (KeyReg 0) (Slot 3)
+  , _obligations = Map.fromList
+      [ (KeyReg 0, Slot 3)
+      , (PoolReg 1, Slot 5)
+      , (PoolReg 2, Slot 6)]
+  , _retiring = Map.singleton 1 (Slot 100)
   , _pconsts = examplePc
   , _slot = Slot 100
   , _lastEpoch = Slot 50
@@ -118,21 +133,24 @@ exampleLS = LedgerState {
 
 epochLS :: LedgerState
 epochLS = LedgerState {
-    _circulation = Coin 29999999999999800
-  , _deposits = Coin 198
+    _circulation = Coin 29999999999998800
+  , _deposits = Coin 694
   , _treasury = Coin 300000018432
   , _reserves = Coin 14998499999973276
-  , _rewards = Coin 900000055396
-  , _rewardPool = Coin 299999952898
+  , _rewards = Coin 900000055892
+  , _rewardPool = Coin 299999952906
   , _fees = Coin 0
-  , _obligations = Map.singleton (KeyReg 0) (Slot 3)
+  , _obligations = Map.fromList
+      [ (KeyReg 0, Slot 3)
+      , (PoolReg 2, Slot 6)]
+  , _retiring = Map.empty
   , _pconsts = examplePc
   , _slot = Slot 100
   , _lastEpoch = Slot 100
   }
 
 testObligation :: Assertion
-testObligation = obligation exampleLS @?= Coin 198
+testObligation = obligation exampleLS @?= Coin 1190
 
 testPreservation :: LedgerState -> Assertion
 testPreservation ls = total ls @?= fortyFiveBillionAda
@@ -142,34 +160,57 @@ testTxPres = testPreservation $ applyAction exampleLS (ActTxBody $ Coin 2)
 
 testTxCalc :: Assertion
 testTxCalc = applyAction exampleLS (ActTxBody $ Coin 2) @?=
-  (exampleLS & circulation .~ Coin 29999999999999798
+  (exampleLS & circulation .~ Coin 29999999999998798
              & fees .~ Coin 2)
 
-testAddCertPres :: Assertion
-testAddCertPres = testPreservation $ applyAction exampleLS (ActAddCert $ KeyReg 1)
+testAddKeyCertPres :: Assertion
+testAddKeyCertPres = testPreservation $ applyAction exampleLS (ActCert $ KeyReg 1)
 
-testAddCertCalc :: Assertion
-testAddCertCalc = applyAction exampleLS (ActAddCert $ KeyReg 1) @?=
-  (exampleLS & circulation .~ Coin 29999999999999600
-             & deposits .~ Coin 400
-             & obligations .~ Map.fromList [(KeyReg 0, Slot 3), (KeyReg 1, Slot 100)])
+testAddKeyCertCalc :: Assertion
+testAddKeyCertCalc = applyAction exampleLS (ActCert $ KeyReg 10) @?=
+  (exampleLS & circulation .~ Coin 29999999999998600
+             & deposits .~ Coin 1400
+             & obligations .~ Map.fromList [ (KeyReg 0, Slot 3)
+                                           , (PoolReg 1, Slot 5)
+                                           , (PoolReg 2, Slot 6)
+                                           , (KeyReg 10, Slot 100)])
 
-testDelCertPres :: Assertion
-testDelCertPres = testPreservation $ applyAction exampleLS (ActDelCert $ KeyReg 0)
+testAddPoolCertPres :: Assertion
+testAddPoolCertPres = testPreservation $ applyAction exampleLS (ActCert $ PoolReg 1)
 
-testDelCertCalc :: Assertion
-testDelCertCalc = applyAction exampleLS (ActDelCert $ KeyReg 0) @?=
-  (exampleLS & circulation .~ Coin 29999999999999998
-             & deposits .~ Coin 1
+testAddPoolCertCalc :: Assertion
+testAddPoolCertCalc = applyAction exampleLS (ActCert $ PoolReg 11) @?=
+  (exampleLS & circulation .~ Coin 29999999999998300
+             & deposits .~ Coin 1700
+             & obligations .~ Map.fromList [ (KeyReg 0, Slot 3)
+                                           , (PoolReg 1, Slot 5)
+                                           , (PoolReg 2, Slot 6)
+                                           , (PoolReg 11, Slot 100)])
+
+testDelKeyCertPres :: Assertion
+testDelKeyCertPres = testPreservation $ applyAction exampleLS (ActCert $ KeyDeReg 0)
+
+testDelKeyCertCalc :: Assertion
+testDelKeyCertCalc = applyAction exampleLS (ActCert $ KeyDeReg 0) @?=
+  (exampleLS & circulation .~ Coin 29999999999998998
+             & deposits .~ Coin 1001
              & fees .~ Coin 1
-             & obligations .~ Map.empty)
+             & obligations .~ Map.fromList [ (PoolReg 1, Slot 5)
+                                           , (PoolReg 2, Slot 6)])
+
+testRetirePoolPres :: Assertion
+testRetirePoolPres = testPreservation $ applyAction exampleLS (ActCert $ PoolRetire 0 (Duration 5))
+
+testRetirePoolCalc :: Assertion
+testRetirePoolCalc = applyAction exampleLS (ActCert $ PoolRetire 1 (Duration 5)) @?=
+  (exampleLS & retiring .~ Map.singleton 1 (Slot 105))
 
 testWithdrawalPres :: Assertion
 testWithdrawalPres = testPreservation $ applyAction exampleLS (ActWithdrawal $ Coin 5)
 
 testWithdrawalCalc :: Assertion
 testWithdrawalCalc = applyAction exampleLS (ActWithdrawal $ Coin 5) @?=
-  (exampleLS & circulation .~ Coin 29999999999999805
+  (exampleLS & circulation .~ Coin 29999999999998805
              & rewards .~ Coin 95)
 
 testEpochPres :: Assertion
@@ -184,8 +225,8 @@ testFasterDecayPres = testPreservation $ applyAction exampleLS (ActEpochWithVote
 
 testFasterDecayCalc :: Assertion
 testFasterDecayCalc = applyAction exampleLS (ActEpochWithVote pc 0.75) @?=
-  (epochLS & deposits .~ Coin 197
-           & reserves .~ Coin 14998499999973277
+  (epochLS & deposits .~ Coin 690
+           & reserves .~ Coin 14998499999973280
            & pconsts .~ pc)
   where pc = examplePc {_decayRate = 0.0002}
 
@@ -195,15 +236,15 @@ testSlowerDecayPres = testPreservation $ applyAction exampleLS (ActEpochWithVote
 
 testSlowerDecayCalc :: Assertion
 testSlowerDecayCalc = applyAction exampleLS (ActEpochWithVote pc 0.75) @?=
-  (epochLS & deposits .~ Coin 199
-           & reserves .~ Coin 14998499999973275
+  (epochLS & deposits .~ Coin 697
+           & reserves .~ Coin 14998499999973273
            & pconsts .~ pc)
   where pc = examplePc {_decayRate = 0.00005}
 
 testDelCertAfterVote :: Assertion
 testDelCertAfterVote = total (foldl applyAction ls actions) @?= fortyFiveBillionAda
   where
-    actions = [ ActAddCert (KeyReg 147)
+    actions = [ ActCert (KeyReg 147)
               , ActNextSlot
               , ActEpochWithVote
                   PrtclConsts
@@ -215,7 +256,7 @@ testDelCertAfterVote = total (foldl applyAction ls actions) @?= fortyFiveBillion
                     , _rho = 0.0
                     }
                   0.0
-              , ActDelCert (KeyReg 147)
+              , ActCert (KeyDeReg 147)
               , ActTxBody (Coin 0)
               ]
     ls = LedgerState
@@ -227,6 +268,7 @@ testDelCertAfterVote = total (foldl applyAction ls actions) @?= fortyFiveBillion
            , _rewardPool = Coin 0
            , _fees = Coin 0
            , _obligations = Map.empty
+           , _retiring = Map.empty
            , _pconsts =
                PrtclConsts
                  { _keyRegDep = Coin 0
@@ -247,11 +289,17 @@ unitTests = testGroup "Unit Tests"
   , testCase "transaction preservation" testTxPres
   , testCase "transaction calculation" testTxCalc
 
-  , testCase "add cert preservation" testAddCertPres
-  , testCase "add cert" testAddCertCalc
+  , testCase "add key cert preservation" testAddKeyCertPres
+  , testCase "add key cert" testAddKeyCertCalc
 
-  , testCase "delete cert preservation" testDelCertPres
-  , testCase "delete cert" testDelCertCalc
+  , testCase "add pool cert preservation" testAddPoolCertPres
+  , testCase "add pool cert" testAddPoolCertCalc
+
+  , testCase "delete key cert preservation" testDelKeyCertPres
+  , testCase "delete key cert" testDelKeyCertCalc
+
+  , testCase "retire pool cert preservation" testRetirePoolPres
+  , testCase "retire pool cert" testRetirePoolCalc
 
   , testCase "reward withdrawal preservation" testWithdrawalPres
   , testCase "reward withdrawal" testWithdrawalCalc
