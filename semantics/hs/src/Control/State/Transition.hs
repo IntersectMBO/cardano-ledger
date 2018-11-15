6ff39ac5ed0042a2c83f690c26e518a0daa1ea2d
Nicholas Clarke <nick@topos.org.uk>
2018-11-15 15:47:20 +0100

    Updates to the STS framework.
    - Various functions for applying rules and testing predicates.
    - Allow the results of `SubTrans` transitions to be shared more easily.

--- semantics/hs/src/Control/State/Transition.hs
+++ semantics/hs/src/Control/State/Transition.hs
@@ -4,15 +4,23 @@
 {-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE StandaloneDeriving    #-}
+{-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 -- | Small step state transition systems.
 module Control.State.Transition where
 
-import Control.Lens
+import           Control.Lens
+import           Data.Bifunctor (first)
+import           Data.Maybe     (catMaybes)
 
 -- | State transition system.
-class (Show (PredicateFailure a))
+class ( Eq (PredicateFailure a)
+      , Show (PredicateFailure a)
+      )
   => STS a where
   -- | Type of the state which the system transitions between.
   type State a :: *
@@ -35,32 +43,66 @@ initialRules = filter isInitial rules
   isInitial (Rule _ (Base _)) = True
   isInitial _                 = False
 
+initialStates
+  :: forall s. STS s
+  => [State s]
+initialStates = catMaybes $ applyBase <$> rules @s
+  where
+    applyBase (Rule _ (Base b)) = Just b
+    applyBase _                 = Nothing
+
+-- | Rules applying transitions
+transitionRules
+  :: STS a
+  => [Rule a]
+transitionRules = filter (not . isInitial) rules
+  where
+    isInitial (Rule _ (Base _)) = True
+    isInitial _                 = False
+
 -- | The union of the components of the system available for making judgments.
 type JudgmentContext sts = (Environment sts, State sts, Signal sts)
 
 -- | A transition under a state transition system
 data Transition sts where
   Transition
-    :: (   Environment sts
-        -> State sts
-        -> Signal sts
+    :: (   JudgmentContext sts
         -> State sts
        )
     -> Transition sts
 
 -- | Apply a transition
 transition
-  :: Transition sts -> Environment sts -> State sts -> Signal sts -> State sts
+  :: Transition sts
+  -> JudgmentContext sts
+  -> State sts
 transition (Transition f) = f
 
 -- | Embed one STS within another.
 class (STS sub, STS super) => Embed sub super where
-  -- | Extract the state of the subsystem as a component of the super-system.
-  stateLens :: Lens' (State super) (State sub)
+  -- | Wrap a predicate failure of the subsystem in a failure of the super-system.
+  wrapFailed :: [PredicateFailure sub] -> PredicateFailure super
 
 data PredicateResult sts
   = Passed
   | Failed (PredicateFailure sts)
+
+deriving instance (Eq (PredicateFailure sts)) => Eq (PredicateResult sts)
+
+gatherFailures
+  :: [PredicateResult sts]
+  -> [PredicateFailure sts]
+gatherFailures xs = catMaybes $ prToMaybe <$> xs
+  where
+    prToMaybe Passed     = Nothing
+    prToMaybe (Failed x) = Just x
+
+data EmbeddedTransition sub sts where
+  EmbeddedTransition
+    :: Embed sub sts
+    => Getter (JudgmentContext sts) (JudgmentContext sub)
+    -> Rule sub
+    -> EmbeddedTransition sub sts
 
 -- | The antecedent to a transition rule.
 data Antecedent sts where
@@ -68,18 +110,34 @@ data Antecedent sts where
   -- | This rule is predicated upon a transition under a (sub-)system.
   SubTrans
     :: Embed sub sts
-    => Getter (JudgmentContext sts) (Environment sub)
-    -> Getter (JudgmentContext sts) (Signal sub)
-    -> Rule sub
+    => EmbeddedTransition sub sts
     -> Antecedent sts
 
   Predicate
-    :: (   Environment sts
-        -> State sts
-        -> Signal sts
+    :: (   JudgmentContext sts
         -> PredicateResult sts
        )
     -> Antecedent sts
+
+-- | Check the antecedent
+checkAntecedent
+  :: JudgmentContext sts
+  -> Antecedent sts
+  -> PredicateResult sts
+checkAntecedent jc ant = case ant of
+  SubTrans (EmbeddedTransition gjc rule) -> case applyRule (jc ^. gjc) rule of
+    Right _  -> Passed
+    Left pfs -> Failed $ wrapFailed pfs
+
+-- | Get the result from a transition under a subsystem, regardless of whether
+-- validation passed.
+subTransResult
+  :: Embed sub super
+  => JudgmentContext super
+  -> EmbeddedTransition sub super
+  -> State sub
+subTransResult jc (EmbeddedTransition jcg rule) = fst $ applyRuleIndifferently (jc ^. jcg) rule
+
 
 -- | The consequent to a transition rule.
 data Consequent sts where
@@ -88,6 +146,15 @@ data Consequent sts where
   -- | The consequent describes a valid transition between two states.
   Extension :: Transition sts -> Consequent sts
 
+applyConsequent
+  :: STS s
+  => JudgmentContext s
+  -> Consequent s
+  -> State s
+applyConsequent jc con = case con of
+  Base st'                 -> st'
+  Extension (Transition f) -> f jc
+
 -- | A rule within a transition system.
 data Rule sts where
   Rule
@@ -95,6 +162,25 @@ data Rule sts where
     -> Consequent sts
     -> Rule sts
 
---------------------------------------------------------------------------------
--- Testing state validity
---------------------------------------------------------------------------------
+applyRule
+  :: STS s
+  => JudgmentContext s
+  -> Rule s
+  -> Either [PredicateFailure s] (State s)
+applyRule jc (Rule ants con) = case checkAntecedent jc <$> ants of
+  xs | all (== Passed) xs -> Right $ applyConsequent jc con
+  xs                      -> Left $ gatherFailures xs
+
+-- | Apply a rule even if its predicates fail.
+--
+--   If the rule successfully applied, the list of predicate failures will be
+--   empty.
+applyRuleIndifferently
+  :: STS s
+  => JudgmentContext s
+  -> Rule s
+  -> (State s, [PredicateFailure s])
+applyRuleIndifferently jc (Rule ants con) = (res, prs)
+  where
+    prs = gatherFailures $ checkAntecedent jc <$> ants
+    res = applyConsequent jc con
