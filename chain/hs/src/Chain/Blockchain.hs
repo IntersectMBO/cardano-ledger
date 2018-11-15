de6b026b894b29eb622f35d3e6968b4ab657c23b
Marko Dimjašević <marko.dimjasevic@iohk.io>
2018-11-15 13:25:30 +0100

    Reorganises code into more modules

--- chain/hs/src/Chain/Blockchain.hs
+++ chain/hs/src/Chain/Blockchain.hs
@@ -14,77 +14,23 @@ import           Crypto.Hash (hashlazy)
 import           Data.ByteString.Lazy.Char8 (pack)
 import           UTxO (Hash)
 
-newtype VKey = MkVKey Word deriving (Eq, Ord)
--- data VKeyGen -- not sure how to encode VKeyGen such that it is a subset
--- of the VKey type. Therefore, find some other way of ensuring this invariant.
+import           Chain.GenesisBlock (genesisBlock)
+import           Delegation.Interface
+                   (
+                     DSIState
+                   , delegates
+                   , initDSIState
+                   , newCertsRule
+                   , updateCerts
+                   )
+import           Types (VKey, Sig, Data, HCert, Interf, BC, Slot, Block(..), BlockIx(..))
 
-data Sig
-data Data
-
-newtype BlockIx = MkBlockIx Word deriving (Eq, Ord)
-
-data Block
-  -- a genesis block
-  = GBlock {
-      gbKeys :: [VKey]
-    , gbHash :: Hash -- ^ Hash of itself
-    }
-  -- a non-genesis block
-  | RBlock {
-      rbHash   :: Hash -- ^ Hash of the predecessor block
-    , rbIx     :: BlockIx -- ^ Index of the block
-    , rbSigner :: VKey -- ^ Block signer
-    , rbCerts  :: Set HCert -- ^ New certificates posted to the blockchain
-    , rbData   :: Data -- ^ Body of the block
-    , rbSig    :: Sig -- ^ Cryptographic signature of the block
-    }
 
 -- | Computes the hash of a block
 hashBlock :: Block -> Hash
 hashBlock b@(RBlock {}) = hashlazy (pack . show $ i) where
   MkBlockIx i = rbIx b
 hashBlock b@(GBlock {}) = gbHash b
-
--- | Verification keys located in the genesis block
-initVKeys :: [VKey]
-initVKeys = undefined -- TODO(md): this is to be imported or implemented
-
-genesisBlock :: Block
-genesisBlock = GBlock { gbKeys = initVKeys, gbHash = hashlazy (pack . show $ 0) }
-
-newtype Slot = MkSlot Word deriving (Eq, Ord)
-
--- | A heavyweight delegation certificate
-data HCert
-
---------------------------------------------------------------------------------
---  Delegation interface
---------------------------------------------------------------------------------
-
-data DSIState -- TODO(md): to be imported as an abstract signature
-
--- NOTE: To be defined elsewhere as part of the delegation interface between
--- the ledger layer and the blockchain layer.
---
--- For a given delegation state, it returns a mapping from delegatee keys
--- to delegator keys. If a key is not present in the key set of the returned
--- map, it has no right to sign a block in the current slot
-delegates :: DSIState -> Map.Map VKey VKey
-delegates = undefined
-
-initDSIState :: DSIState
-initDSIState = undefined -- TODO: This is to be imported from somewhere
-
--- TODO(md): This rule is to be implemented on the ledger layer's side
-newCertsRule :: Rule Interf
-newCertsRule = undefined
-
--- TODO(md): The ledger spec should implement this as part of the delegation
--- interface. This function is closely related to *newCertsRule*
-updateCerts :: Slot -> Set HCert -> DSIState -> DSIState
-updateCerts = undefined
-
---------------------------------------------------------------------------------
 
 -- | Size of the block sliding window
 newtype K = MkK Word deriving (Eq, Ord)
@@ -102,8 +48,6 @@ verify = undefined
 -- the given key (indirectly) signed in the block sliding window of size K
 type KeyToQMap = Map.Map VKey (Queue BlockIx)
 
--- | Delegation interface transition system
-data Interf
 
 instance STS Interf where
   type State Interf = DSIState
@@ -115,9 +59,6 @@ instance STS Interf where
     deriving Show
 
   rules = [newCertsRule]
-
--- | Blockchain extension transition system
-data BC
 
 -- | Remove the oldest entry in the queues in the range of the map if it is
 --   more than *K* blocks away from the given block index
---
+++ chain/hs/src/Delegation/Interface.hs
@@ -0,0 +1,47 @@
+-- | Provides types and functions for the delegation interface
+-- between the ledger layer and the blockchain layer
+module Delegation.Interface
+  (
+    DSIState
+  , delegates
+  , initDSIState
+  , newCertsRule
+  , updateCerts
+  )
+  where
+
+import           Chain.GenesisBlock (initVKeys)
+import           Control.State.Transition
+import qualified Data.Map.Strict as Map
+import           Data.Set (Set)
+import           Types
+
+
+-- TODO: to be implemented
+-- | A delegation interface state, as described in the ledger layer
+-- specification written by Damian Nadales.
+data DSIState
+
+-- | For a given delegation state, it returns a mapping from a delegatee key
+-- to a delegator key. If a key is not present in the key set of the returned
+-- map, it has no right to sign a block in the current slot
+delegates :: DSIState -> Map.Map VKey VKey
+delegates = undefined
+
+-- | Computes an initial delegation interface state from a set of
+-- verification keys
+initDSIStateFromKeys :: Set VKey -> DSIState
+initDSIStateFromKeys certs = undefined
+
+-- | The initial delegation interface state
+initDSIState :: DSIState
+initDSIState = initDSIStateFromKeys initVKeys
+
+-- | Defines when new certificates can be added to the ledger's state
+newCertsRule :: Rule Interf
+newCertsRule = undefined
+
+-- TODO(md): The ledger spec should implement this as part of the delegation
+-- interface. This function is closely related to *newCertsRule*
+updateCerts :: Slot -> Set HCert -> DSIState -> DSIState
+updateCerts = undefined
