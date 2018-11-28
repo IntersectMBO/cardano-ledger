a80f60ecaea185758a08833effc220ebd5d357c6
Michael Bishop <cleverca22@gmail.com>
2018-11-28 14:59:46 -0400

    add required job

--- release.nix
+++ release.nix
@@ -1,3 +1,9 @@
-{
+let
+  pkgs = import ./pkgs.nix;
+in pkgs.lib.fix (self: {
+  required = pkgs.lib.hydraJob (pkgs.releaseTools.aggregate {
+    name = "cardano-chain-required-checks";
+    constituents = [ self.byronLedgerSpec ];
+  });
   byronLedgerSpec = import ./specs/ledger/latex {};
-}
+})
