99e79b5f9e02dcf8135fa414a18f61d47034088d
Damian Nadales <damian.nadales@iohk.io>
2018-11-28 17:59:31 -0300

    Change the link descriptions in the hydra build artifacts and update the required constitutents

--- release.nix
+++ release.nix
@@ -1,10 +1,12 @@
 let
   pkgs = import ./pkgs.nix;
 in pkgs.lib.fix (self: {
+
   required = pkgs.lib.hydraJob (pkgs.releaseTools.aggregate {
     name = "cardano-chain-required-checks";
-    constituents = [ self.byronLedgerSpec ];
+    constituents = with self; [ byronLedgerSpec byronChainSpec semanticsSpec ];
   });
+
   byronLedgerSpec = import ./specs/ledger/latex {};
   byronChainSpec = import ./specs/chain/latex {};
   semanticsSpec = import ./specs/semantics/latex {};
