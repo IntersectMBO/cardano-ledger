13217fb1b567510165c8c8648b77f60a9bee23a9
Michael Bishop <cleverca22@gmail.com>
2018-11-30 14:28:44 -0400

    [DEVOPS-1033] include the gitrev in a dummy derivation
    this forces hydra to create a new eval for every rev, even if nothing has changed

--- release.nix
+++ release.nix
@@ -1,10 +1,12 @@
+{ chain ? { outPath = ./.; rev = "abcdef"; } }:
 let
   pkgs = import ./pkgs.nix;
 in pkgs.lib.fix (self: {
+  forceNewEval = pkgs.writeText "forceNewEval" chain.rev;
 
   required = pkgs.lib.hydraJob (pkgs.releaseTools.aggregate {
     name = "cardano-chain-required-checks";
-    constituents = with self; [ byronLedgerSpec byronChainSpec semanticsSpec ];
+    constituents = with self; [ byronLedgerSpec byronChainSpec semanticsSpec forceNewEval ];
   });
 
   byronLedgerSpec = import ./specs/ledger/latex {};
