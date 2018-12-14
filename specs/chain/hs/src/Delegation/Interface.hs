ff762cdc51e5e31dc46823ddd55357ff9bb0d7c3
Marko Dimjašević <marko.dimjasevic@iohk.io>
2018-12-14 15:51:23 +0100

    Ties together the blockchain and the ledger layers via the delegation interface
    * This closes issue #193
    * It uses the new rewritten free monad-based STS interface

--- specs/chain/hs/src/Delegation/Interface.hs
+++ specs/chain/hs/src/Delegation/Interface.hs
@@ -1,47 +1,45 @@
 -- | Provides types and functions for the delegation interface
 -- between the ledger layer and the blockchain layer
 module Delegation.Interface
-  ( DSIState
-  , delegates
-  , initDSIState
-  , newCertsRule
-  , updateCerts
+  ( delegates
+  , maybeMapKeyForValue
+  , mapKeyForValue
+  , initDIState
   )
 where
 
+import Control.Lens
 import Chain.GenesisBlock (initVKeys)
 import Control.State.Transition
+import Data.Maybe (fromJust, listToMaybe)
 import qualified Data.Map.Strict as Map
 import Data.Set (Set)
+import Ledger.Core (VKey, Slot)
+import Ledger.Delegation (DCert, DIState, VKeyGen, DELEG, DSEnv, delegationMap)
 import Types
 
 
--- TODO: to be implemented
--- | A delegation interface state, as described in the ledger layer
--- specification written by Damian Nadales.
-data DSIState
-
--- | For a given delegation state, it returns a mapping from a delegatee key
--- to a delegator key. If a key is not present in the key set of the returned
+-- | For a given delegation state, it returns a mapping from a delegator key
+-- to a delegatee key. If a key is not present in the value set of the returned
 -- map, it has no right to sign a block in the current slot
-delegates :: DSIState -> Map.Map VKey VKey
-delegates = undefined
+delegates :: DIState -> Map.Map VKeyGen VKey
+delegates s = s ^. delegationMap
+
+-- | Returns a key from a map for a given value.
+maybeMapKeyForValue :: (Eq a, Ord k) => a -> Map.Map k a -> Maybe k
+maybeMapKeyForValue v = listToMaybe . map fst . Map.toList . Map.filter (== v)
+
+-- | Unsafely returns a key from a map for a given value. It assumes there is
+-- exactly one key mapping to the given value. If there is no such key, it will
+-- result in a runtime exception.
+mapKeyForValue :: (Eq a, Ord k) => a -> Map.Map k a -> k
+mapKeyForValue v = fromJust . maybeMapKeyForValue v
 
 -- | Computes an initial delegation interface state from a set of
 -- verification keys
-initDSIStateFromKeys :: Set VKey -> DSIState
-initDSIStateFromKeys certs = undefined
+initDIStateFromKeys :: Set VKeyGen -> DIState
+initDIStateFromKeys certs = undefined
 
 -- | The initial delegation interface state
-initDSIState :: DSIState
-initDSIState = initDSIStateFromKeys initVKeys
-
--- | Defines when new certificates can be added to the ledger's state
-newCertsRule :: TransitionRule Interf
-newCertsRule = undefined
-
--- | Updates the delegation interface state with a set of heavyweight
--- delegation certificates that arrived in a block issued in the given
--- slot
-updateCerts :: Slot -> Set HCert -> DSIState -> DSIState
-updateCerts = undefined
+initDIState :: DIState
+initDIState = initDIStateFromKeys initVKeys
