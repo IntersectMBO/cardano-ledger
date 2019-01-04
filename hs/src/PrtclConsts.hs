65a02d122476398ec10ff2dd9c85e76146eb7df0
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-01-04 16:24:13 +0100

    Extend `PrtclConsts`

--- hs/src/PrtclConsts.hs
+++ hs/src/PrtclConsts.hs
@@ -1,38 +1,45 @@
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module PrtclConsts
-    (
-     PrtclConsts(..)
+  ( PrtclConsts(..)
     -- lenses
-    , minfeeA
-    , minfeeB
-    , keyDeposit
-    , poolDeposit
-    , minRefund
-    , decayRate
-    ) where
+  , minfeeA
+  , minfeeB
+  , keyDeposit
+  , poolDeposit
+  , minRefund
+  , decayRate
+  , movingAvgWeight
+  , movingAvgExp
+  , poolConsts
+  ) where
 
-import           Data.Ratio      (Rational)
+import           Data.Ratio      (Ratio, Rational)
 import           Numeric.Natural (Natural)
 
 import           Coin            (Coin (..))
 
-import Lens.Micro.TH (makeLenses)
+import           Lens.Micro.TH   (makeLenses)
 
-data PrtclConsts =
-  PrtclConsts
+data PrtclConsts = PrtclConsts
   { -- |The linear factor for the minimum fee calculation
-    _minfeeA     :: Natural
+    _minfeeA         :: Natural
     -- |The constant factor for the minimum fee calculation
-  , _minfeeB     :: Natural
+  , _minfeeB         :: Natural
     -- |The amount of a key registration deposit
-  , _keyDeposit  :: Coin
+  , _keyDeposit      :: Coin
     -- |The amount of a pool registration deposit
-  , _poolDeposit :: Coin
+  , _poolDeposit     :: Coin
     -- |The minimum percent refund guarantee
-  , _minRefund   :: Rational
+  , _minRefund       :: Rational
     -- |The deposit decay rate
-  , _decayRate   :: Rational
+  , _decayRate       :: Rational
+    -- |Moving average weight.
+  , _movingAvgWeight :: Ratio Natural
+    -- |Moving average exponent.
+  , _movingAvgExp    :: Ratio Natural
+    -- |Pool constants
+  , _poolConsts      :: (Ratio Natural, Natural)
   } deriving (Show, Eq)
 
 makeLenses ''PrtclConsts
