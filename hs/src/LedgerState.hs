a9523354beca7b510925bf24f00ba07bd5f6d729
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-02-06 15:02:15 +0100

    Add STS rule `SNAP`

--- hs/src/LedgerState.hs
+++ hs/src/LedgerState.hs
@@ -1214,6 +1214,34 @@ newPpTransition = do
           else (us ^. deposits, as ^. reserves, pp)
   pure (us & deposits .~ oblg', as & reserves .~ reserves', pp')
 
+data SNAP
+instance STS SNAP where
+    type State SNAP = (SnapShots, UTxOState)
+    type Signal SNAP = ()
+    type Environment SNAP = (Epoch, PParams, DState, PState, BlocksMade)
+    data PredicateFailure SNAP = FailureSNAP
+                  deriving (Show, Eq)
+
+    initialRules = [ pure ( emptySnapShots
+                          , UTxOState (UTxO Map.empty) (Coin 0) (Coin 0)) ]
+    transitionRules = [ snapTransition ]
+
+snapTransition :: TransitionRule SNAP
+snapTransition = do
+  TRC((eNew, pparams, d, p, blocks), (s, u), _) <- judgmentContext
+  let pooledStake = poolDistr (u ^. utxo) d p
+  let slot = slotFromEpoch eNew
+  let oblg = obligation pparams (d ^. stKeys) (p ^. stPools) slot
+  let decayed = (u ^. deposits) - oblg
+  pure ( s & pstakeMark .~ pooledStake
+           & pstakeSet  .~ (s ^. pstakeMark)
+           & pstakeGo   .~ (s ^. pstakeSet)
+           & poolsSS    .~ (p ^. pParams)
+           & blocksSS   .~ blocks
+           & feeSS      .~ (u ^. fees) + decayed
+       , u & deposits   .~ oblg
+           & fees       %~ (+) decayed)
+
 data EPOCH
 instance STS EPOCH where
     type State EPOCH = (UTxOState, AccountState, DWState)
