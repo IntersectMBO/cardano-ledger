633cce01676575a1ccb69f35b3a9e84ed7a3d762
Nicholas Clarke <nick@topos.org.uk>
2019-02-13 16:41:25 +0100

    Add: Infrastructure for nix/hydra build. (#237)
    * Add: Infrastructure for nix/hydra build.
    
    Currently some of the specs are not building owing to git issues. Need to work
    out what is causing the problem here and what to do about this.
    
    * Clean: Fix tabs/spaces

---
+++ nix/.stack.nix/delegation.nix
@@ -0,0 +1,56 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.8";
+      identifier = { name = "delegation"; version = "0.1.0.0"; };
+      license = "NONE";
+      copyright = "";
+      maintainer = "formal.methods@iohk.io";
+      author = "IOHK Formal Metheds Team";
+      homepage = "";
+      url = "";
+      synopsis = "";
+      description = "Delegation Executable Model";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs.base)
+          (hsPkgs.bytestring)
+          (hsPkgs.containers)
+          (hsPkgs.cryptonite)
+          (hsPkgs.memory)
+          (hsPkgs.multiset)
+          (hsPkgs.tasty)
+          (hsPkgs.tasty-hunit)
+          (hsPkgs.hedgehog)
+          (hsPkgs.tasty-hedgehog)
+          (hsPkgs.tasty-hedgehog-coverage)
+          (hsPkgs.text)
+          (hsPkgs.small-steps)
+          (hsPkgs.microlens)
+          (hsPkgs.microlens-th)
+          ];
+        };
+      tests = {
+        "delegation-test" = {
+          depends = [
+            (hsPkgs.base)
+            (hsPkgs.tasty)
+            (hsPkgs.tasty-hunit)
+            (hsPkgs.tasty-hedgehog)
+            (hsPkgs.tasty-hedgehog-coverage)
+            (hsPkgs.hedgehog)
+            (hsPkgs.delegation)
+            (hsPkgs.cryptonite)
+            (hsPkgs.containers)
+            (hsPkgs.multiset)
+            (hsPkgs.text)
+            (hsPkgs.microlens)
+            ];
+          };
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault .././../hs; }
