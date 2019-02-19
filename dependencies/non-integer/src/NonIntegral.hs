bfa1fa010cb8dde81194b8c080b7e7ca113625e5
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-02-19 14:18:32 +0100

    Corrections to support `Data.Fixed`
    - also fix bound which incremented bounds exponentially but factors linearly
    - `Data.Fixed` has `Enum` that increments in smallest precision steps, not 1

--- dependencies/non-integer/src/NonIntegral.hs
+++ dependencies/non-integer/src/NonIntegral.hs
@@ -16,7 +16,8 @@ scaleExp x = (ceiling x, x / fromIntegral (ceiling x :: Integer))
 a *** b
   | a == 0 = if b == 0 then 1 else 0
   | a == 1 = 1
-  | otherwise = exp' $ b * ln' a
+  | otherwise = exp' $ l
+    where l = b * ln' a
 
 -- | compute e^x using continued fractions. For x < 0 compute 1/e^(-x). Scale to
 -- x' \in [0,1] to reduce overflow risk in numerical types with limited values.
@@ -30,7 +31,7 @@ exp' x
 -- | Approximate exp(x) via continued fraction.
 fexp :: (Fractional a, Enum a, Ord a, Show a) => Int -> a -> a
 fexp maxN x =
-  cf maxN 0 Nothing 1 0 1 1 [x * a | a <- 1 : [-1,-2 ..]] (1 : [x + b | b <- [2 ..]])
+  cf maxN 0 eps Nothing 1 0 1 1 [x * a | a <- 1 : [-1,-2 ..]] (1 : [x + b | b <- [2,3 ..]])
 
 logAs :: (Num a) => a -> [a]
 logAs a = a' : a' : logAs (a + 1)
@@ -41,10 +42,10 @@ logAs a = a' : a' : logAs (a + 1)
 fln :: (Fractional a, Enum a, Ord a, Show a) => Int -> a -> a
 fln maxN x = if x < 0
              then error ("x = " ++ show x ++ "is not inside domain [0, ..) ")
-             else cf maxN 0 Nothing 1 0 0 1 (x : [a * x | a <- logAs 1]) [1 ..]
+             else cf maxN 0 eps Nothing 1 0 0 1 (x : [a * x | a <- logAs 1]) [1,2 ..]
 
 eps :: (Fractional a) => a
-eps = 1 / 10.0^(16::Int)
+eps = 1 / 10.0^(24::Int)
 
 -- | Compute continued fraction using max steps or bounded list of a/b factors.
 -- The 'maxN' parameter gives the maximum recursion depth, 'n' gives the current
@@ -66,6 +67,7 @@ cf ::
      (Fractional a, Ord a, Show a)
   => Int
   -> Int
+  -> a
   -> Maybe a
   -> a
   -> a
@@ -74,16 +76,16 @@ cf ::
   -> [a]
   -> [a]
   -> a
-cf _ _ _ _ _ aNm1 bNm1 _ [] = aNm1 / bNm1
-cf _ _ _ _ _ aNm1 bNm1 [] _ = aNm1 / bNm1
-cf maxN n lastVal aNm2 bNm2 aNm1 bNm1 (an:as) (bn:bs)
+cf _ _ _ _ _ _ aNm1 bNm1 _ [] = aNm1 / bNm1
+cf _ _ _ _ _ _ aNm1 bNm1 [] _ = aNm1 / bNm1
+cf maxN n epsilon lastVal aNm2 bNm2 aNm1 bNm1 (an:as) (bn:bs)
   | maxN == n = convergent
   | otherwise =
       case lastVal of
-        Nothing -> cf maxN (n + 1) (Just convergent) aNm1 bNm1 aN bN as bs
-        Just c' -> if abs(convergent - c') < eps
+        Nothing -> cf maxN (n + 1) epsilon (Just convergent) aNm1 bNm1 aN bN as bs
+        Just c' -> if abs(convergent - c') < epsilon
                    then convergent
-                   else cf maxN (n + 1) (Just convergent) aNm1 bNm1 aN bN as bs
+                   else cf maxN (n + 1) epsilon (Just convergent) aNm1 bNm1 aN bN as bs
   where
     ba = bn * aNm1
     aa = an * aNm2
@@ -107,7 +109,7 @@ bound ::
   -> (Integer, Integer)
 bound factor x x' x'' l u
   | x' <= x && x'' >= x = (l, u)
-  | otherwise = bound factor x (x' / factor) (x'' * factor) (2 * l) (2 * u)
+  | otherwise = bound factor x (x' / factor) (x'' * factor) (l - 1) (u + 1)
 
 -- | Bisect bounds to find the smallest integer power such that
 -- `factor^n<=x<factor^(n+1)`.
@@ -132,10 +134,9 @@ e :: (RealFrac a, Enum a, Show a) => a
 e = exp' 1
 
 -- | find n with `e^n<=x<e^(n+1)`
-findE :: (RealFrac a, Enum a, Show a) => a -> Integer
-findE x = contract eone x lower upper
+findE :: (RealFrac a, Enum a, Show a) => a -> a -> Integer
+findE eone x = contract eone x lower upper
   where
-    eone           = e
     (lower, upper) = bound eone x (1 / eone) eone (-1) 1
 
 -- | Compute natural logarithm via continued fraction, first splitting integral
@@ -143,10 +144,11 @@ findE x = contract eone x lower upper
 ln' :: (RealFrac a, Enum a, Show a) => a -> a
 ln' x = if x == 0
         then error "0 is not in domain of ln"
-        else fromIntegral n + fln 1000 x'
+        else fromIntegral n + approxln
   where (n, x') = splitLn x
+        approxln = fln 1000 x'
 
 splitLn :: (RealFrac b, Enum b, Show b) => b -> (Integer, b)
 splitLn x = (n, x')
-    where n = findE x
+    where n = findE e x
           x' = (x / exp' (fromIntegral n)) - 1 -- x / e^n > 1!
