1337b4ce2db268a6d5072b45e473bbc87b1f786d
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-03-13 13:47:45 +0100

    Fix compiler warnings

--- hs/src/BaseTypes.hs
+++ hs/src/BaseTypes.hs
@@ -11,14 +11,14 @@ import qualified Data.Fixed as FP
 data E34
 
 instance FP.HasResolution E34 where
-  resolution _ = (10^34)::Integer
+  resolution _ = (10::Integer)^(34::Integer)
 
 type Digits34 = FP.Fixed E34
 
 type FixedPoint = Digits34
 
 fpPrecision :: FixedPoint
-fpPrecision = (10^34)::FixedPoint
+fpPrecision = (10::FixedPoint)^(34::Integer)
 
 fpEpsilon :: FixedPoint
-fpEpsilon = (10^17)::FixedPoint
+fpEpsilon = (10::FixedPoint)^(17::Integer)
