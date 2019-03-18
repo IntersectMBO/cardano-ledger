fa7d892c596ccd54e852236126fc30a6e5ff0a9b
Rupert Horlick <rupert.horlick@iohk.io>
2019-03-18 13:48:19 +0000

    [#368] Move cardano-binary to cardano-base repo

--- binary/src/Cardano/Binary/Class.hs
+++
@@ -1,9 +0,0 @@
-module Cardano.Binary.Class
-  ( module X
-  )
-where
-
-import Cardano.Binary.Class.Annotated as X
-import Cardano.Binary.Class.Core as X
-import Cardano.Binary.Class.Drop as X
-import Cardano.Binary.Class.Primitive as X
