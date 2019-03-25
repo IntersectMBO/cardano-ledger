0af2897dd2cfdd408d23f08564318113059017ee
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-03-25 13:28:40 +0100

    Add `OCERT` STS transition

--- hs/src/OCert.hs
+++ hs/src/OCert.hs
@@ -1,18 +1,21 @@
 module OCert
   ( OCert(..)
-  , KESPeriod
-  ) where
+  , KESPeriod(..)
+  , slotsPerKESPeriod
+  , kesPeriod)
+where
 
-import qualified Keys            as Keys
-import qualified Slot            as Slot
+import qualified Keys         as Keys
+import qualified Slot         as Slot
 
 import           Numeric.Natural (Natural)
 
-type KESPeriod = Slot.Slot
+newtype KESPeriod = KESPeriod Natural
+  deriving (Show, Eq, Ord)
 
 data OCert = OCert
-    -- | The operational hot key
-  { ocertVkHot     :: Keys.VKey
+  { -- | The operational hot key
+    ocertVkHot     :: Keys.VKey
     -- | counter
   , ocertN         :: Natural
     -- | Start of key evolving signature period
@@ -20,3 +23,9 @@ data OCert = OCert
     -- | Signature of block operational certificate content
   , ocertSigma     :: Keys.Sig (Keys.VKey, Natural, KESPeriod)
   } deriving (Show, Eq)
+
+slotsPerKESPeriod :: Natural
+slotsPerKESPeriod = 90
+
+kesPeriod :: Slot.Slot -> KESPeriod
+kesPeriod (Slot.Slot s) = KESPeriod $ s `div` slotsPerKESPeriod
