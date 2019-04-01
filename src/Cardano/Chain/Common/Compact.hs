06cf5d3acc69371cb4877e874e19175a0649ec16
Luke Nadur <19835357+intricate@users.noreply.github.com>
2019-04-01 09:04:08 -0500

    Add compact data types which will be used to optimize UTxO
    Co-authored-by: Duncan Coutts <duncan@community.haskell.org>

---
+++ src/Cardano/Chain/Common/Compact.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+
+module Cardano.Chain.Common.Compact
+  ( CompactAddress
+  , toCompactAddress
+  , fromCompactAddress
+  )
+where
+
+import Cardano.Prelude
+
+import Cardano.Binary.Class (serialize', decodeFull')
+import qualified Data.ByteString.Short as BSS (fromShort, toShort)
+import Data.ByteString.Short (ShortByteString)
+
+import Cardano.Chain.Common.Address (Address(..))
+
+--------------------------------------------------------------------------------
+-- Compact Address
+--------------------------------------------------------------------------------
+
+-- | A compact in-memory representation for an 'Address'.
+--
+-- Convert using 'toCompactAddress' and 'fromCompactAddress'.
+--
+newtype CompactAddress = CompactAddress ShortByteString
+  deriving (Eq, Ord, Generic, Show)
+  deriving newtype HeapWords
+  deriving anyclass NFData
+
+toCompactAddress :: Address -> CompactAddress
+toCompactAddress addr =
+  CompactAddress (BSS.toShort (serialize' addr))
+
+fromCompactAddress :: CompactAddress -> Address
+fromCompactAddress (CompactAddress addr) =
+  case decodeFull' (BSS.fromShort addr) of
+    Left err      -> panic ("fromCompactAddress: impossible: " <> show err)
+    Right decAddr -> decAddr
