haoleism/cardano-ledger-specs

Various changes to UTxO models for hedgehog state machine testing

specs/ledger/hs/src/Ledger/UTxO.hs
@@ -1,20 +1,21 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
+
 module Ledger.UTxO where
 
 import Control.State.Transition
 import qualified Data.ByteArray as BA
 import Data.List (find)
+import qualified Data.List as List
 import qualified Data.Map.Strict as Map
-import Data.Maybe (isJust)
-import qualified Data.Set as Set
+import Data.Maybe (isJust, fromMaybe)
 import Ledger.Core hiding ((◁), (⋪), (∪))
-import Ledger.Signatures
 import Numeric.Natural (Natural)
 import Data.Map.Strict (Map)
-import Data.Set (Set)
 import qualified Crypto.Hash as Crypto
 import qualified Data.ByteString.Char8 as BS
 
@@ -25,64 +26,74 @@ newtype TxId = TxId { getTxId :: Hash }
 -- |The input of a UTxO.
 --
 --     * __TODO__ - is it okay to use list indices instead of implementing the Ix Type?
-data TxIn = TxIn TxId Natural deriving (Show, Eq, Ord)
+data TxIn id = TxIn id Natural deriving (Show, Eq, Ord)
 
 -- |The output of a UTxO.
-data TxOut = TxOut Addr Value deriving (Show, Eq)
+data TxOut = TxOut Addr Value deriving (Show, Eq, Ord)
 
 -- |The unspent transaction outputs.
-newtype UTxO = UTxO (Map TxIn TxOut) deriving (Show, Eq)
+newtype UTxO id = UTxO (Map (TxIn id) TxOut) deriving (Show, Eq)
 
 -- |A raw transaction
-data Tx = Tx { inputs  :: Set TxIn
-             , outputs :: [TxOut]
-             } deriving (Show, Eq)
+data Tx id = Tx { txid :: id
+                , inputs  :: [TxIn id]
+                , outputs :: [TxOut]
+                } deriving (Show, Ord)
 
--- |Compute the id of a transaction.
-txid :: Tx -> TxId
-txid = TxId . hash
+instance Eq id => Eq (Tx id) where
+  (Tx _ ins outs) == (Tx _ ins' outs') = ins == ins' && outs == outs'
+
+-- -- |Compute the id of a transaction.
+-- class IsTxId id where
+--   txid :: Tx id -> id
+
+-- instance IsTxId TxId where
+--   txid = TxId . hash
 
 -- |Compute the UTxO inputs of a transaction.
-txins :: Tx -> Set TxIn
+txins :: Tx id -> [TxIn id]
 txins = inputs
 
 -- |Compute the UTxO outputs of a transaction.
-txouts :: Tx -> UTxO
+txouts :: Ord id => Tx id -> UTxO id
 txouts tx = UTxO $ Map.fromList
   [ (TxIn transId idx, out) | (out, idx) <- zip (outputs tx) [0 ..] ]
   where transId = txid tx
 
-txfee :: Tx -> Value
-txfee = undefined
+txfee :: Tx id -> Value
+txfee _ = Value 0
 
 -- |Domain restriction
 --
 --     * __TODO__ - better symbol?
-(◁) :: Set TxIn -> UTxO -> UTxO
-ins ◁ (UTxO utxo) = UTxO $ Map.filterWithKey (\k _ -> k `Set.member` ins) utxo
+(◁) :: Ord id => [TxIn id]-> UTxO id -> UTxO id
+ins ◁ (UTxO utxo) = UTxO $ Map.filterWithKey (\k _ -> k `elem` ins) utxo
+
+(<|) :: Ord id => [TxIn id] -> UTxO id -> UTxO id
+(<|) = (◁)
 
 -- |Domain exclusion
 --
-(⋪) :: Set TxIn -> UTxO -> UTxO
+(⋪) :: Ord id => [TxIn id] -> UTxO id -> UTxO id
 ins ⋪ (UTxO utxo) =
-  UTxO $ Map.filterWithKey (\k _ -> k `Set.notMember` ins) utxo
+  UTxO $ Map.filterWithKey (\k _ -> k `notElem` ins) utxo
 
 -- |Combine two collections of UTxO.
 --
 --     * TODO - Should we return 'Maybe UTxO' so that we can return
 -- Nothing when the collections are not disjoint?
-(∪) :: UTxO -> UTxO -> UTxO
+(∪) :: Ord id => UTxO id -> UTxO id -> UTxO id
 (UTxO a) ∪ (UTxO b) = UTxO $ Map.union a b
 
 -- |Determine the total balance contained in the UTxO.
-balance :: UTxO -> Value
+balance :: UTxO id -> Value
 balance (UTxO utxo) = Map.foldl' addValues mempty utxo
   where addValues b (TxOut _ a) = b <> a
 
-instance Ledger.Core.HasHash Tx where
+instance Ledger.Core.HasHash (Tx TxId) where
   hash = Crypto.hash
 
-instance BA.ByteArrayAccess Tx where
+instance Show id => BA.ByteArrayAccess (Tx id) where
   length        = BA.length . BS.pack . show
   withByteArray = BA.withByteArray . BS.pack  . show
 
@@ -90,17 +101,21 @@ instance BA.ByteArrayAccess Tx where
 -- UTxO transitions
 ---------------------------------------------------------------------------------
 
-newtype ProtocolConstants = ProtocolConstants
-  { pcMinFee :: Tx -> Value }
+data UTxOEnv id = UTxOEnv { utxo0 :: UTxO id
+                          , pps   :: ProtocolConstants id
+                          }
+
+newtype ProtocolConstants id = ProtocolConstants
+  { pcMinFee :: Tx id -> Value }
 
 -- | UTXO transition system
-data UTXO
+data UTXO id
 
-instance STS UTXO where
-  type State UTXO = UTxO
-  type Signal UTXO = Tx
-  type Environment UTXO = ProtocolConstants
-  data PredicateFailure UTXO
+instance Ord id => STS (UTXO id) where
+  type State (UTXO id) = UTxO id
+  type Signal (UTXO id) = Tx id
+  type Environment (UTXO id) = UTxOEnv id
+  data PredicateFailure (UTXO id)
     = BadInputs
     | FeeTooLow
     | IncreasedTotalBalance
@@ -110,16 +125,16 @@ instance STS UTXO where
     [ return (UTxO Map.empty) ]
   transitionRules = [ utxoInductive ]
 
-utxoInductive :: TransitionRule UTXO
+utxoInductive :: Ord id => TransitionRule (UTXO id)
 utxoInductive = do
-  TRC (pc, utxo, tx) <- judgmentContext
+  TRC (UTxOEnv _ pc, utxo, tx) <- judgmentContext
   balance (txouts tx)
     <> txfee tx
     == balance (txins tx ◁ utxo)
     ?! IncreasedTotalBalance
   pcMinFee pc tx <= txfee tx ?! FeeTooLow
-  let unspentInputs (UTxO aUtxo) = Map.keysSet aUtxo
-  txins tx `Set.isSubsetOf` unspentInputs utxo ?! BadInputs
+  let unspentInputs (UTxO aUtxo) = Map.keys aUtxo
+  txins tx `List.isSubsequenceOf` unspentInputs utxo ?! BadInputs
   return $ (txins tx ⋪ utxo) ∪ txouts tx
 
 ---------------------------------------------------------------------------------
@@ -127,59 +142,75 @@ utxoInductive = do
 ---------------------------------------------------------------------------------
 
 -- |Proof/Witness that a transaction is authorized by the given key holder.
-data Wit = Wit VKey (Sig Tx) deriving (Show, Eq)
+data Wit id = Wit VKey (Sig (Tx id)) deriving (Show, Eq, Ord)
 
 -- |A fully formed transaction.
 --
 --     * __TODO__ - Would it be better to name this type Tx, and rename Tx to TxBody?
-data TxWits = TxWits
-              { body     :: Tx
-              , witneses :: Set Wit
-              } deriving (Show, Eq)
+data TxWits id = TxWits
+                { body      :: Tx id
+                , witnesses :: [Wit id]
+                } deriving (Show, Eq)
 
 -- |Create a witness for transaction
-makeWitness :: KeyPair -> Tx -> Wit
+makeWitness :: KeyPair -> Tx id -> Wit id
 makeWitness keys tx = Wit (vKey keys) (sign (sKey keys) tx)
 
--- | UTXO with witnessing
-data UTXOW
+makeTxWits :: Ord id => UTxO id -> Tx id -> TxWits id
+makeTxWits (UTxO utxo) tx = TxWits
+  { body      = tx
+  , witnesses = wits
+  }
+ where
+  getKey txin =
+    let
+      TxOut (Addr (VKey o)) _ =
+        fromMaybe
+            (error "witnessTransaction: Missing output for transaction input")
+          $ Map.lookup txin utxo
+    in KeyPair (SKey o) (VKey o)
+  keys = getKey <$> inputs tx
+  wits = makeWitness <$> keys <*> pure tx
 
-instance STS UTXOW where
-  type State UTXOW = UTxO
-  type Signal UTXOW = TxWits
-  type Environment UTXOW = ProtocolConstants
-  data PredicateFailure UTXOW
-    = UtxoFailure (PredicateFailure UTXO)
+-- | UTXO with witnessing
+data UTXOW id
+
+instance Ord id => STS (UTXOW id) where
+  type State (UTXOW id) = UTxO id
+  type Signal (UTXOW id) = TxWits id
+  type Environment (UTXOW id) = UTxOEnv id
+  data PredicateFailure (UTXOW id)
+    = UtxoFailure (PredicateFailure (UTXO id))
     | InsufficientWitnesses
     deriving (Eq, Show)
 
   initialRules = [ return $ UTxO Map.empty ]
   transitionRules =
     [ do
-        TRC (pc, utxo, tw) <- judgmentContext
+        TRC (env, utxo, tw) <- judgmentContext
         witnessed tw utxo ?! InsufficientWitnesses
-        res <- trans @UTXO $ TRC (pc, utxo, body tw)
+        res <- trans @(UTXO id) $ TRC (env, utxo, body tw)
         return res
     ]
 
-instance Embed UTXO UTXOW where
+instance Ord id => Embed (UTXO id) (UTXOW id) where
   wrapFailed = UtxoFailure
 
 -- |Determine if a UTxO input is authorized by a given key.
-authTxin :: VKey -> TxIn -> UTxO -> Bool
+authTxin :: Ord id => VKey -> TxIn id -> UTxO id -> Bool
 authTxin key txin (UTxO utxo) = case Map.lookup txin utxo of
-  Just (TxOut (Addr pay) _) -> hash key == pay
+  Just (TxOut (Addr pay) _) -> key == pay
   _                         -> False
 
 -- |Given a ledger state, determine if the UTxO witnesses in a given
 -- transaction are sufficient.
 -- TODO - should we only check for one witness for each unique input address?
-witnessed :: TxWits -> UTxO -> Bool
+witnessed :: Ord id => TxWits id -> UTxO id -> Bool
 witnessed (TxWits tx wits) utxo =
-  Set.size wits == Set.size ins && all (hasWitness wits) ins
+  length wits == length ins && all (hasWitness wits) ins
  where
   ins = inputs tx
-  hasWitness witnesses input =
-    isJust $ find (isWitness tx input utxo) witnesses
+  hasWitness ws input =
+    isJust $ find (isWitness tx input utxo) ws
   isWitness tx' input unspent (Wit key sig) =
     verify key tx' sig && authTxin key input unspent

IntersectMBO/cardano-ledger

byron/ledger/executable-spec/src/Ledger/UTxO.hs
@@ -1,22 +1,28 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE UndecidableInstances       #-}
+
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+
 module Ledger.UTxO where
 
 import Control.State.Transition
-import qualified Data.ByteArray as BA
-import Data.List (find)
-import qualified Data.Map.Strict as Map
-import Data.Maybe (isJust)
-import qualified Data.Set as Set
-import Ledger.Core hiding ((◁), (⋪), (∪))
-import Ledger.Signatures
-import Numeric.Natural (Natural)
-import Data.Map.Strict (Map)
-import Data.Set (Set)
 import qualified Crypto.Hash as Crypto
+import qualified Data.ByteArray as BA
 import qualified Data.ByteString.Char8 as BS
+import Data.List (find)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe, isJust)
+import qualified Data.Set as Set
+import Ledger.Core
+import Numeric.Natural (Natural)
+
 
 -- |A unique ID of a transaction, which is computable from the transaction.
 newtype TxId = TxId { getTxId :: Hash }
@@ -25,64 +31,46 @@ newtype TxId = TxId { getTxId :: Hash }
 -- |The input of a UTxO.
 --
 --     * __TODO__ - is it okay to use list indices instead of implementing the Ix Type?
-data TxIn = TxIn TxId Natural deriving (Show, Eq, Ord)
+data TxIn id = TxIn id Natural deriving (Show, Eq, Ord)
 
 -- |The output of a UTxO.
-data TxOut = TxOut Addr Value deriving (Show, Eq)
+data TxOut = TxOut Addr Value deriving (Show, Eq, Ord)
 
 -- |The unspent transaction outputs.
-newtype UTxO = UTxO (Map TxIn TxOut) deriving (Show, Eq)
+newtype UTxO id = UTxO (Map (TxIn id) TxOut) deriving (Show, Eq, Relation)
 
 -- |A raw transaction
-data Tx = Tx { inputs  :: Set TxIn
-             , outputs :: [TxOut]
-             } deriving (Show, Eq)
+data Tx id = Tx { txid    :: id
+                , inputs  :: [TxIn id]
+                , outputs :: [TxOut]
+                } deriving (Show, Ord)
 
--- |Compute the id of a transaction.
-txid :: Tx -> TxId
-txid = TxId . hash
+instance Eq id => Eq (Tx id) where
+  (Tx _ ins outs) == (Tx _ ins' outs') = ins == ins' && outs == outs'
 
 -- |Compute the UTxO inputs of a transaction.
-txins :: Tx -> Set TxIn
+txins :: Tx id -> [TxIn id]
 txins = inputs
 
 -- |Compute the UTxO outputs of a transaction.
-txouts :: Tx -> UTxO
+txouts :: Ord id => Tx id -> UTxO id
 txouts tx = UTxO $ Map.fromList
   [ (TxIn transId idx, out) | (out, idx) <- zip (outputs tx) [0 ..] ]
   where transId = txid tx
 
-txfee :: Tx -> Value
-txfee = undefined
-
--- |Domain restriction
---
---     * __TODO__ - better symbol?
-(◁) :: Set TxIn -> UTxO -> UTxO
-ins ◁ (UTxO utxo) = UTxO $ Map.filterWithKey (\k _ -> k `Set.member` ins) utxo
-
--- |Domain exclusion
---
-(⋪) :: Set TxIn -> UTxO -> UTxO
-ins ⋪ (UTxO utxo) =
-  UTxO $ Map.filterWithKey (\k _ -> k `Set.notMember` ins) utxo
-
--- |Combine two collections of UTxO.
---
---     * TODO - Should we return 'Maybe UTxO' so that we can return
--- Nothing when the collections are not disjoint?
-(∪) :: UTxO -> UTxO -> UTxO
-(UTxO a) ∪ (UTxO b) = UTxO $ Map.union a b
+-- TODO: Handle fees properly
+txfee :: Tx id -> Value
+txfee _ = Value 0
 
 -- |Determine the total balance contained in the UTxO.
-balance :: UTxO -> Value
+balance :: UTxO id -> Value
 balance (UTxO utxo) = Map.foldl' addValues mempty utxo
   where addValues b (TxOut _ a) = b <> a
 
-instance Ledger.Core.HasHash Tx where
+instance Ledger.Core.HasHash (Tx TxId) where
   hash = Crypto.hash
 
-instance BA.ByteArrayAccess Tx where
+instance Show id => BA.ByteArrayAccess (Tx id) where
   length        = BA.length . BS.pack . show
   withByteArray = BA.withByteArray . BS.pack  . show
 
@@ -90,96 +78,127 @@ instance BA.ByteArrayAccess Tx where
 -- UTxO transitions
 ---------------------------------------------------------------------------------
 
-newtype ProtocolConstants = ProtocolConstants
-  { pcMinFee :: Tx -> Value }
+data UTxOEnv id = UTxOEnv { utxo0 :: UTxO id
+                          , pps   :: ProtocolConstants id
+                          }
+
+newtype ProtocolConstants id = ProtocolConstants
+  { pcMinFee :: Tx id -> Value }
 
 -- | UTXO transition system
-data UTXO
+data UTXO id
 
-instance STS UTXO where
-  type State UTXO = UTxO
-  type Signal UTXO = Tx
-  type Environment UTXO = ProtocolConstants
-  data PredicateFailure UTXO
+instance Ord id => STS (UTXO id) where
+  type State (UTXO id) = UTxO id
+  type Signal (UTXO id) = Tx id
+  type Environment (UTXO id) = UTxOEnv id
+  data PredicateFailure (UTXO id)
     = BadInputs
     | FeeTooLow
     | IncreasedTotalBalance
     deriving (Eq, Show)
 
   initialRules =
-    [ return (UTxO Map.empty) ]
+    [ do
+        IRC env <- judgmentContext
+        return (utxo0 env)
+    ]
   transitionRules = [ utxoInductive ]
 
-utxoInductive :: TransitionRule UTXO
+utxoInductive :: Ord id => TransitionRule (UTXO id)
 utxoInductive = do
-  TRC (pc, utxo, tx) <- judgmentContext
+  TRC (UTxOEnv _ pc, utxo, tx) <- judgmentContext
   balance (txouts tx)
     <> txfee tx
-    == balance (txins tx ◁ utxo)
+    == balance (Set.fromList (txins tx) ◁ utxo)
     ?! IncreasedTotalBalance
   pcMinFee pc tx <= txfee tx ?! FeeTooLow
-  let unspentInputs (UTxO aUtxo) = Map.keysSet aUtxo
-  txins tx `Set.isSubsetOf` unspentInputs utxo ?! BadInputs
-  return $ (txins tx ⋪ utxo) ∪ txouts tx
+  let
+    unspentInputs (UTxO aUtxo) = Map.keysSet aUtxo
+    txinsSet = Set.fromList $ txins tx
+  Set.size txinsSet == length (txins tx)
+    && txinsSet `Set.isSubsetOf` unspentInputs utxo
+    ?! BadInputs
+  return $ (Set.fromList (txins tx) ⋪ utxo) ∪ txouts tx
 
 ---------------------------------------------------------------------------------
 -- UTxO transitions
 ---------------------------------------------------------------------------------
 
 -- |Proof/Witness that a transaction is authorized by the given key holder.
-data Wit = Wit VKey (Sig Tx) deriving (Show, Eq)
+data Wit id = Wit VKey (Sig (Tx id)) deriving (Show, Eq, Ord)
 
 -- |A fully formed transaction.
 --
 --     * __TODO__ - Would it be better to name this type Tx, and rename Tx to TxBody?
-data TxWits = TxWits
-              { body     :: Tx
-              , witneses :: Set Wit
-              } deriving (Show, Eq)
+data TxWits id = TxWits
+                { body      :: Tx id
+                , witnesses :: [Wit id]
+                } deriving (Show, Eq)
 
 -- |Create a witness for transaction
-makeWitness :: KeyPair -> Tx -> Wit
+makeWitness :: KeyPair -> Tx id -> Wit id
 makeWitness keys tx = Wit (vKey keys) (sign (sKey keys) tx)
 
--- | UTXO with witnessing
-data UTXOW
+makeTxWits :: Ord id => UTxO id -> Tx id -> TxWits id
+makeTxWits (UTxO utxo) tx = TxWits
+  { body      = tx
+  , witnesses = wits
+  }
+ where
+  getKey txin =
+    let
+      TxOut (Addr (VKey o)) _ =
+        fromMaybe
+            (error "makeTxWits: Missing output for transaction input")
+          $ Map.lookup txin utxo
+    in KeyPair (SKey o) (VKey o)
+  keys = getKey <$> inputs tx
+  wits = makeWitness <$> keys <*> pure tx
 
-instance STS UTXOW where
-  type State UTXOW = UTxO
-  type Signal UTXOW = TxWits
-  type Environment UTXOW = ProtocolConstants
-  data PredicateFailure UTXOW
-    = UtxoFailure (PredicateFailure UTXO)
+-- | UTXO with witnessing
+data UTXOW id
+
+instance Ord id => STS (UTXOW id) where
+  type State (UTXOW id) = UTxO id
+  type Signal (UTXOW id) = TxWits id
+  type Environment (UTXOW id) = UTxOEnv id
+  data PredicateFailure (UTXOW id)
+    = UtxoFailure (PredicateFailure (UTXO id))
     | InsufficientWitnesses
     deriving (Eq, Show)
 
-  initialRules = [ return $ UTxO Map.empty ]
+  initialRules =
+    [ do
+        IRC env <- judgmentContext
+        return (utxo0 env)
+    ]
   transitionRules =
     [ do
-        TRC (pc, utxo, tw) <- judgmentContext
+        TRC (env, utxo, tw) <- judgmentContext
         witnessed tw utxo ?! InsufficientWitnesses
-        res <- trans @UTXO $ TRC (pc, utxo, body tw)
+        res <- trans @(UTXO id) $ TRC (env, utxo, body tw)
         return res
     ]
 
-instance Embed UTXO UTXOW where
+instance Ord id => Embed (UTXO id) (UTXOW id) where
   wrapFailed = UtxoFailure
 
 -- |Determine if a UTxO input is authorized by a given key.
-authTxin :: VKey -> TxIn -> UTxO -> Bool
+authTxin :: Ord id => VKey -> TxIn id -> UTxO id -> Bool
 authTxin key txin (UTxO utxo) = case Map.lookup txin utxo of
-  Just (TxOut (Addr pay) _) -> hash key == pay
+  Just (TxOut (Addr pay) _) -> key == pay
   _                         -> False
 
 -- |Given a ledger state, determine if the UTxO witnesses in a given
 -- transaction are sufficient.
 -- TODO - should we only check for one witness for each unique input address?
-witnessed :: TxWits -> UTxO -> Bool
+witnessed :: Ord id => TxWits id -> UTxO id -> Bool
 witnessed (TxWits tx wits) utxo =
-  Set.size wits == Set.size ins && all (hasWitness wits) ins
+  length wits == length ins && all (hasWitness wits) ins
  where
   ins = inputs tx
-  hasWitness witnesses input =
-    isJust $ find (isWitness tx input utxo) witnesses
+  hasWitness ws input =
+    isJust $ find (isWitness tx input utxo) ws
   isWitness tx' input unspent (Wit key sig) =
     verify key tx' sig && authTxin key input unspent