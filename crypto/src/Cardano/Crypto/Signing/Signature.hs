input-output-hk/cardano-ledger-byron

Change `signEncoded` to take an `Encoding`

crypto/src/Cardano/Crypto/Signing/Signature.hs
@@ -137,12 +137,12 @@ sign
   -> SecretKey
   -> a
   -> Signature a
-sign pm tag sk = signEncoded pm tag sk . serialize'
+sign pm tag sk = signEncoded pm tag sk . toCBOR
 
 -- | Like 'sign' but without the 'ToCBOR' constraint
 signEncoded
-  :: ProtocolMagicId -> SignTag -> SecretKey -> ByteString -> Signature a
-signEncoded pm tag sk = coerce . signRaw pm (Just tag) sk
+  :: ProtocolMagicId -> SignTag -> SecretKey -> Encoding -> Signature a
+signEncoded pm tag sk = coerce . signRaw pm (Just tag) sk . BSL.toStrict . serializeEncoding
 
 -- | Sign a 'Raw' bytestring
 signRaw

crypto/test/Test/Cardano/Crypto/Gen.hs
@@ -54,6 +54,7 @@ import Cardano.Prelude
 import Test.Cardano.Prelude
 
 import qualified Data.ByteArray as ByteArray
+import Data.Coerce (coerce)
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
@@ -86,7 +87,7 @@ import Cardano.Crypto.Signing
   , proxySign
   , safeCreateProxyCert
   , sign
-  , signEncoded
+  , signRaw
   , toPublic
   )
 import Cardano.Crypto.Signing.Redeem
@@ -201,7 +202,7 @@ genSignature pm genA = sign pm <$> genSignTag <*> genSecretKey <*> genA
 
 genSignatureEncoded :: Gen ByteString -> Gen (Signature a)
 genSignatureEncoded genB =
-  signEncoded <$> genProtocolMagicId <*> genSignTag <*> genSecretKey <*> genB
+  coerce . signRaw <$> genProtocolMagicId <*> (Just <$> genSignTag) <*> genSecretKey <*> genB
 
 genRedeemSignature
   :: ToCBOR a => ProtocolMagicId -> Gen a -> Gen (RedeemSignature a)

IntersectMBO/cardano-ledger

crypto/src/Cardano/Crypto/Signing/Signature.hs
@@ -137,12 +137,12 @@ sign
   -> SigningKey
   -> a
   -> Signature a
-sign pm tag sk = signEncoded pm tag sk . serialize'
+sign pm tag sk = signEncoded pm tag sk . toCBOR
 
 -- | Like 'sign' but without the 'ToCBOR' constraint
 signEncoded
-  :: ProtocolMagicId -> SignTag -> SigningKey -> ByteString -> Signature a
-signEncoded pm tag sk = coerce . signRaw pm (Just tag) sk
+  :: ProtocolMagicId -> SignTag -> SigningKey -> Encoding -> Signature a
+signEncoded pm tag sk = coerce . signRaw pm (Just tag) sk . BSL.toStrict . serializeEncoding
 
 -- | Sign a 'Raw' bytestring
 signRaw

crypto/test/Test/Cardano/Crypto/Gen.hs
@@ -54,6 +54,7 @@ import Cardano.Prelude
 import Test.Cardano.Prelude
 
 import qualified Data.ByteArray as ByteArray
+import Data.Coerce (coerce)
 import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
@@ -86,7 +87,7 @@ import Cardano.Crypto.Signing
   , proxySign
   , safeCreateProxyCert
   , sign
-  , signEncoded
+  , signRaw
   , toVerification
   )
 import Cardano.Crypto.Signing.Redeem
@@ -201,7 +202,7 @@ genSignature pm genA = sign pm <$> genSignTag <*> genSigningKey <*> genA
 
 genSignatureEncoded :: Gen ByteString -> Gen (Signature a)
 genSignatureEncoded genB =
-  signEncoded <$> genProtocolMagicId <*> genSignTag <*> genSigningKey <*> genB
+  coerce . signRaw <$> genProtocolMagicId <*> (Just <$> genSignTag) <*> genSigningKey <*> genB
 
 genRedeemSignature
   :: ToCBOR a => ProtocolMagicId -> Gen a -> Gen (RedeemSignature a)