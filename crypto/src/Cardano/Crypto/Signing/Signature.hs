input-output-hk/cardano-ledger-byron

Use explicit serialiser for `verifySignature`

crypto/src/Cardano/Crypto/Signing/Signature.hs
@@ -34,6 +34,7 @@ import Cardano.Prelude
 import qualified Cardano.Crypto.Wallet as CC
 import Data.Aeson (FromJSON(..), ToJSON(..))
 import Data.ByteArray (ScrubbedBytes)
+import qualified Data.ByteString.Lazy as BSL
 import Data.Coerce (coerce)
 import Formatting (Format, bprint, build, formatToString, later, sformat, stext)
 import qualified Formatting.Buildable as B
@@ -49,6 +50,7 @@ import Cardano.Binary
   , Raw
   , ToCBOR(..)
   , serialize'
+  , serializeEncoding
   )
 import Cardano.Crypto.ProtocolMagic (ProtocolMagicId)
 import Cardano.Crypto.Signing.PublicKey (PublicKey(..))
@@ -176,15 +178,15 @@ safeSignRaw pm mbTag (SafeSigner (EncryptedSecretKey sk _) (PassPhrase pp)) x =
 
 -- | Verify a signature
 verifySignature
-  :: ToCBOR a
-  => ProtocolMagicId
+  :: (a -> Encoding)
+  -> ProtocolMagicId
   -> SignTag
   -> PublicKey
   -> a
   -> Signature a
   -> Bool
-verifySignature pm tag pk x sig =
-  verifySignatureRaw pk (signTag pm tag <> serialize' x) (coerce sig)
+verifySignature toEnc pm tag pk x sig =
+  verifySignatureRaw pk (signTag pm tag <> (BSL.toStrict . serializeEncoding $ toEnc x)) (coerce sig)
 
 -- | Verify a signature
 verifySignatureDecoded

crypto/test/Test/Cardano/Crypto/Signing/Signing.hs
@@ -9,6 +9,7 @@ import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
+import Cardano.Binary (toCBOR)
 import Cardano.Crypto.Signing (SignTag(..), sign, toPublic, verifySignature)
 
 import qualified Test.Cardano.Crypto.Dummy as Dummy
@@ -35,8 +36,8 @@ prop_sign = property $ do
   a        <- forAll genData
 
   assert
-    $ verifySignature Dummy.protocolMagicId SignForTestingOnly pk a
-    $ sign Dummy.protocolMagicId SignForTestingOnly sk a
+    $ verifySignature toCBOR Dummy.dummyProtocolMagicId SignForTestingOnly pk a
+    $ sign dummyProtocolMagicId SignForTestingOnly sk a
 
 -- | Signing fails when the wrong 'PublicKey' is used
 prop_signDifferentKey :: Property
@@ -47,8 +48,8 @@ prop_signDifferentKey = property $ do
 
   assert
     . not
-    $ verifySignature Dummy.protocolMagicId SignForTestingOnly pk a
-    $ sign Dummy.protocolMagicId SignForTestingOnly sk a
+    $ verifySignature toCBOR Dummy.dummyProtocolMagicId SignForTestingOnly pk a
+    $ sign dummyProtocolMagicId SignForTestingOnly sk a
 
 -- | Signing fails when then wrong signature data is used
 prop_signDifferentData :: Property
@@ -59,8 +60,8 @@ prop_signDifferentData = property $ do
 
   assert
     . not
-    $ verifySignature Dummy.protocolMagicId SignForTestingOnly pk b
-    $ sign Dummy.protocolMagicId SignForTestingOnly sk a
+    $ verifySignature toCBOR Dummy.dummyProtocolMagicId SignForTestingOnly pk b
+    $ sign dummyProtocolMagicId SignForTestingOnly sk a
 
 genData :: Gen [Int32]
 genData = Gen.list (Range.constant 0 50) (Gen.int32 Range.constantBounded)

IntersectMBO/cardano-ledger

crypto/src/Cardano/Crypto/Signing/Signature.hs
@@ -34,6 +34,7 @@ import Cardano.Prelude
 import qualified Cardano.Crypto.Wallet as CC
 import Data.Aeson (FromJSON(..), ToJSON(..))
 import Data.ByteArray (ScrubbedBytes)
+import qualified Data.ByteString.Lazy as BSL
 import Data.Coerce (coerce)
 import Formatting (Format, bprint, build, formatToString, later, sformat, stext)
 import qualified Formatting.Buildable as B
@@ -49,6 +50,7 @@ import Cardano.Binary
   , Raw
   , ToCBOR(..)
   , serialize'
+  , serializeEncoding
   )
 import Cardano.Crypto.ProtocolMagic (ProtocolMagicId)
 import Cardano.Crypto.Signing.VerificationKey (VerificationKey(..))
@@ -176,15 +178,15 @@ safeSignRaw pm mbTag (SafeSigner (EncryptedSigningKey sk _) (PassPhrase pp)) x =
 
 -- | Verify a signature
 verifySignature
-  :: ToCBOR a
-  => ProtocolMagicId
+  :: (a -> Encoding)
+  -> ProtocolMagicId
   -> SignTag
   -> VerificationKey
   -> a
   -> Signature a
   -> Bool
-verifySignature pm tag vk x sig =
-  verifySignatureRaw vk (signTag pm tag <> serialize' x) (coerce sig)
+verifySignature toEnc pm tag vk x sig =
+  verifySignatureRaw vk (signTag pm tag <> (BSL.toStrict . serializeEncoding $ toEnc x)) (coerce sig)
 
 -- | Verify a signature
 verifySignatureDecoded

crypto/test/Test/Cardano/Crypto/Signing/Signing.hs
@@ -9,6 +9,7 @@ import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
+import Cardano.Binary (toCBOR)
 import Cardano.Crypto.Signing (SignTag(..), sign, toVerification, verifySignature)
 
 import qualified Test.Cardano.Crypto.Dummy as Dummy
@@ -35,7 +36,7 @@ prop_sign = property $ do
   a        <- forAll genData
 
   assert
-    $ verifySignature Dummy.protocolMagicId SignForTestingOnly vk a
+    $ verifySignature toCBOR Dummy.protocolMagicId SignForTestingOnly vk a
     $ sign Dummy.protocolMagicId SignForTestingOnly sk a
 
 -- | Signing fails when the wrong 'VerificationKey' is used
@@ -47,7 +48,7 @@ prop_signDifferentKey = property $ do
 
   assert
     . not
-    $ verifySignature Dummy.protocolMagicId SignForTestingOnly vk a
+    $ verifySignature toCBOR Dummy.protocolMagicId SignForTestingOnly vk a
     $ sign Dummy.protocolMagicId SignForTestingOnly sk a
 
 -- | Signing fails when then wrong signature data is used
@@ -59,7 +60,7 @@ prop_signDifferentData = property $ do
 
   assert
     . not
-    $ verifySignature Dummy.protocolMagicId SignForTestingOnly vk b
+    $ verifySignature toCBOR Dummy.protocolMagicId SignForTestingOnly vk b
     $ sign Dummy.protocolMagicId SignForTestingOnly sk a
 
 genData :: Gen [Int32]