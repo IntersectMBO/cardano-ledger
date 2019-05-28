d64dfca3598b0d65dcba98c2372887c55c261282
Marko Dimjašević <marko.dimjasevic@iohk.io>
2019-05-28 12:02:38 +0200

    Issue #473: implements in Haskell three properties for the PVBUMP STS

--- byron/ledger/executable-spec/src/Ledger/Core/Generator.hs
+++ byron/ledger/executable-spec/src/Ledger/Core/Generator.hs
@@ -1,16 +1,20 @@
 -- | Generators for the 'Ledger.Core' values.
 module Ledger.Core.Generator
-  ( vkGen
+  ( slotGen
+  , vkGen
   , vkgenesisGen
   )
 where
 
-import Hedgehog (Gen)
-import Hedgehog.Gen (integral)
-import Hedgehog.Range (linear)
+import           Data.Word (Word64)
+import           Hedgehog (Gen)
+import qualified Hedgehog.Gen as Gen
+import           Hedgehog.Gen (integral)
+import           Hedgehog.Range (linear)
 
 import Ledger.Core
   ( Owner(Owner)
+  , Slot(Slot)
   , VKey(VKey)
   , VKeyGenesis(VKeyGenesis)
   )
@@ -20,3 +24,8 @@ vkGen = VKey . Owner <$> integral (linear 0 10000)
 
 vkgenesisGen :: Gen VKeyGenesis
 vkgenesisGen = VKeyGenesis <$> vkGen
+
+-- | Generates a slot within the given bound
+slotGen :: Word64 -> Word64 -> Gen Slot
+slotGen lower upper = do
+  Slot <$> Gen.word64 (linear lower upper)
