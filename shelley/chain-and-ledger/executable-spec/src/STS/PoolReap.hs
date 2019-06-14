08158bf0ee69b0455eefdb822cb888b7e054f064
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-06-14 16:32:25 +0200

    Adapt `POOLREAP`

--- shelley/chain-and-ledger/executable-spec/src/STS/PoolReap.hs
+++ shelley/chain-and-ledger/executable-spec/src/STS/PoolReap.hs
@@ -31,32 +31,32 @@ instance STS POOLREAP where
 
 poolReapTransition :: TransitionRule POOLREAP
 poolReapTransition = do
-  TRC (pp, (a, d, p), eNew) <- judgmentContext
-  let retired = Map.keysSet $ Map.filter (== eNew) $ p ^. retiring
-  let pr = poolRefunds pp (p ^. retiring) (firstSlot eNew)
-  let relevant = Map.restrictKeys (p ^. pParams) retired
+  TRC (pp, (a, ds, ps), e) <- judgmentContext
+  let retired = Map.keysSet $ Map.filter (== e) $ ps ^. retiring
+  let pr = poolRefunds pp (ps ^. retiring) (firstSlot e)
+  let relevant = Map.restrictKeys (ps ^. pParams) retired
   let rewardAcnts =
         Map.mapMaybeWithKey
           (\k v ->
              case Map.lookup k relevant of
                Nothing -> Nothing
                Just _  -> Just (v ^. poolRAcnt))
-          (p ^. pParams)
+          (ps ^. pParams)
   let rewardAcnts' = Map.restrictKeys rewardAcnts (Map.keysSet pr)
   let refunds' =
         Map.foldlWithKey
           (\m k addr -> Map.insert addr (pr Map.! k) m)
           Map.empty
           rewardAcnts' -- not yet restricted to a in dom(rewards)
-  let domRewards = Map.keysSet (d ^. rewards)
+  let domRewards = Map.keysSet (ds ^. rewards)
   let (refunds, unclaimed') =
         Map.partitionWithKey (\k _ -> k `Set.member` domRewards) refunds'
   let unclaimed = Map.foldl (+) (Coin 0) unclaimed'
-  let StakePools stakePools = p ^. stPools
+  let StakePools stakePools = ps ^. stPools
   pure
     ( a & treasury %~ (+) unclaimed
-    , d & rewards %~ flip Map.union refunds &
-      delegations %~ flip Map.withoutKeys retired
-    , p & stPools .~ (StakePools $ Map.withoutKeys stakePools retired) &
-      pParams %~ flip Map.withoutKeys retired &
-      retiring %~ flip Map.withoutKeys retired)
+    , ds & rewards %~ flip Map.union refunds
+         & delegations %~ flip Map.withoutKeys retired
+    , ps & stPools .~ (StakePools $ Map.withoutKeys stakePools retired)
+         & pParams %~ flip Map.withoutKeys retired
+         & retiring %~ flip Map.withoutKeys retired)
