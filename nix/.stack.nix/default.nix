bd32f108b82713bea2da1b73608086e17c82e9d4
Damian Nadales <damian.nadales@iohk.io>
2019-07-18 20:46:06 +0200

    Make `nix` happy.

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -5,6 +5,7 @@
         "libsystemd-journal" = (((hackage.libsystemd-journal)."1.4.4").revisions).default;
         "tasty-hedgehog" = (((hackage.tasty-hedgehog)."1.0.0.1").revisions).default;
         "haskell-src-exts" = (((hackage.haskell-src-exts)."1.21.0").revisions).default;
+        "generic-monoid" = (((hackage.generic-monoid)."0.1.0.0").revisions).default;
         "bimap" = (((hackage.bimap)."0.4.0").revisions).default;
         "time-units" = (((hackage.time-units)."1.0.0").revisions).default;
         "base58-bytestring" = (((hackage.base58-bytestring)."0.1.0").revisions).default;
