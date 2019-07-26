a86c48bc5f054942dc3b0e1daf321d850c9409e4
Damian Nadales <damian.nadales@iohk.io>
2019-07-26 17:05:55 +0200

    Reverted change caused by merge :/

--- cardano-ledger/src/Cardano/Chain/Block/Validation.hs
+++ cardano-ledger/src/Cardano/Chain/Block/Validation.hs
@@ -625,7 +625,7 @@ updateBlock config cvs b = do
     { cvsLastSlot     = blockSlot b
     , cvsPreviousHash = Right $! blockHashAnnotated b
     , cvsUtxo         = utxo
-    , cvsUpdateState  = updateState'
+    , cvsUpdateState  = updateState
     , cvsDelegationState = delegationState
     }
  where
