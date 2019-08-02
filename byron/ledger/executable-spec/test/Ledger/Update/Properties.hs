6734d57a3fea35e6b8b2a5683e33750275ad735b
uroboros <ryan.lemmer@iohk.io>
2019-08-02 10:02:25 +0200

    [706] Recalibrate coverage metrics

--- byron/ledger/executable-spec/test/Ledger/Update/Properties.hs
+++ byron/ledger/executable-spec/test/Ledger/Update/Properties.hs
@@ -98,7 +98,7 @@ upiregRelevantTracesAreCovered = withTests 300 $ property $ do
     "at least 30% of the update proposals increase the maximum block-size"
     (0.3 <= ratio (wrtCurrentProtocolParameters Update._maxBkSz Increases) sample)
 
-  cover 40
+  cover 30
     "at least 10% of the update proposals do not change the maximum block-size"
     (0.1 <= ratio (wrtCurrentProtocolParameters Update._maxBkSz RemainsTheSame) sample)
 
