1b8f165b1f05eb3bebcb104916e36cf37b22c319
Michael Hueschen <michaelhueschen@gmail.com>
2019-08-14 19:54:39 +0200

    [#767] Fix Nix

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -6,6 +6,8 @@
         "tasty-hedgehog" = (((hackage.tasty-hedgehog)."1.0.0.1").revisions).default;
         "Unique" = (((hackage.Unique)."0.4.7.6").revisions).default;
         "bimap" = (((hackage.bimap)."0.4.0").revisions).default;
+        "moo" = (((hackage.moo)."1.2").revisions).default;
+        "gray-code" = (((hackage.gray-code)."0.3.1").revisions).default;
         "base58-bytestring" = (((hackage.base58-bytestring)."0.1.0").revisions).default;
         "hedgehog" = (((hackage.hedgehog)."1.0").revisions).default;
         "micro-recursion-schemes" = (((hackage.micro-recursion-schemes)."5.0.2.2").revisions).default;
@@ -21,6 +23,7 @@
         cardano-prelude = ./cardano-prelude.nix;
         cardano-binary = ./cardano-binary.nix;
         cardano-crypto-class = ./cardano-crypto-class.nix;
+        goblins = ./goblins.nix;
         cardano-crypto = ./cardano-crypto.nix;
         };
       compiler.version = "8.6.5";
