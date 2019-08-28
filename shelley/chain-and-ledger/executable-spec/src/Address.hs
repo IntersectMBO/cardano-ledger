ce1155abd3625bab340e3b189f7d84ecd0853731
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-08-28 21:00:05 +0200

    Add `mkRwdAcnt` for both scripts and vkeys

--- shelley/chain-and-ledger/executable-spec/src/Address.hs
+++ shelley/chain-and-ledger/executable-spec/src/Address.hs
@@ -2,12 +2,13 @@
 
 module Address
   ( mkVKeyRwdAcnt
+  , mkRwdAcnt
   )
 where
 
 import           Cardano.Crypto.Hash (HashAlgorithm)
 
-import           Keys (KeyDiscriminator(..), DSIGNAlgorithm, KeyPair, hashKey, vKey)
+import           Keys (DSIGNAlgorithm, KeyDiscriminator (..), KeyPair, hashKey, vKey)
 import           TxData (Credential (..), RewardAcnt (..))
 
 mkVKeyRwdAcnt
@@ -17,3 +18,11 @@ mkVKeyRwdAcnt
   => KeyPair 'Regular dsignAlgo
   -> RewardAcnt hashAlgo dsignAlgo
 mkVKeyRwdAcnt keys = RewardAcnt $ KeyHashObj (hashKey $ vKey keys)
+
+mkRwdAcnt
+  :: Credential hashAlgo dsignAlgo
+  -> RewardAcnt hashAlgo dsignAlgo
+mkRwdAcnt script@(ScriptHashObj _) = RewardAcnt script
+mkRwdAcnt key@(KeyHashObj _) = RewardAcnt key
+mkRwdAcnt (GenesisHashObj _) =
+  error "cannot construct reward account with genesis key"
