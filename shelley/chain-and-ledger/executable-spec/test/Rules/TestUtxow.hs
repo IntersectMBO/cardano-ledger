adadfab9b51acbf026930a42ebd7fb7dbd49e326
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-09-12 15:38:57 +0200

    Add restricted preservance property

--- shelley/chain-and-ledger/executable-spec/test/Rules/TestUtxow.hs
+++ shelley/chain-and-ledger/executable-spec/test/Rules/TestUtxow.hs
@@ -58,3 +58,30 @@ preserveBalance = withTests (fromIntegral numberOfTests) . property $ do
         consumed u tx pp stk =
             balance u
           + keyRefunds pp stk (_body tx)
+
+-- | Preserve balance restricted to TxIns and TxOuts of the Tx
+preserveBalanceRestricted :: Property
+preserveBalanceRestricted = withTests (fromIntegral numberOfTests) . property $ do
+  t <- forAll (trace @UTXOW $ fromIntegral traceLen)
+  let
+    n :: Integer
+    n = fromIntegral $ traceLength t
+    tr = sourceSignalTargets t
+    UtxoEnv _ pp stk stp _ = _traceEnv t
+
+  when (n > 1) $
+    [] === filter (not . (createdIsConsumed pp stk stp)) tr
+
+  where createdIsConsumed pp stk stp (UTxOState u _ _ _, tx, UTxOState _ _ _ _) =
+          inps u tx == outs (_body tx) pp stk stp
+        inps u tx = balance $ (_inputs $ _body tx) <| u
+        outs tx pp stk stp =
+            balance (txouts tx)
+          + _txfee tx
+          + depositChange pp stk stp (toList $ _certs tx) tx
+
+        depositChange pp stk stp certs txb =
+            deposits pp stp certs
+          - (refunded pp stk txb + decayed pp stk txb)
+        refunded pp stk txb = keyRefunds pp stk txb
+        decayed pp stk txb  = decayedTx pp stk txb
