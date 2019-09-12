ac53113afa4460afa7d0a245377540bcc2b974f2
Luke Nadur <19835357+intricate@users.noreply.github.com>
2019-09-11 22:58:51 -0500

    Ensure that GenesisAvvmBalances Map returned by fromJSON is strict
    n.b. This is in reference to canonical-json

--- cardano-ledger/src/Cardano/Chain/Genesis/AvvmBalances.hs
+++ cardano-ledger/src/Cardano/Chain/Genesis/AvvmBalances.hs
@@ -10,8 +10,10 @@ module Cardano.Chain.Genesis.AvvmBalances
   )
 where
 
-import Cardano.Prelude
+import Cardano.Prelude hiding (toStrict)
+import Prelude (id)
 
+import Data.Map.Strict as Map
 import Text.JSON.Canonical (FromJSON(..), ToJSON(..))
 
 import Cardano.Chain.Common (Lovelace)
@@ -29,5 +31,19 @@ instance Monad m => ToJSON m GenesisAvvmBalances where
     toJSON = toJSON . unGenesisAvvmBalances
 
 instance MonadError SchemaError m => FromJSON m GenesisAvvmBalances where
-    fromJSON = fmap GenesisAvvmBalances . fromJSON
-
+    -- | Unfortunately, because @canonical-json@ doesn't utilize operations from
+    -- "Data.Map.Strict" but only those from "Data.Map.Lazy" (i.e. 'fromJSON'
+    -- will return a 'Map' that is not necessarily strict in its values), we
+    -- need to be careful in order to ensure that we're still dealing with a
+    -- 'Map' that's strict in both its keys and values.
+    --
+    -- To remedy this, we use @Map.map id@ to convert the 'Map'
+    -- to one that is now guaranteed to be strict in both its keys and values.
+    --
+    -- n.b. both the strict and lazy 'Map' modules utilize the same 'Map' data
+    -- type which is what makes something like this possible.
+    fromJSON = fmap (GenesisAvvmBalances . toStrict) . fromJSON
+     where
+      -- | /O(n)/. Ensures that all values in the given 'Map' are in WHNF.
+      toStrict :: Map k a -> Map k a
+      toStrict = Map.map id
