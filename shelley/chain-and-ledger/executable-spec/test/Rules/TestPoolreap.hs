ada1bd071b0cd32c0b1d89bbc1399cde785b7cbd
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-09-25 19:44:43 +0200

    Take Damian's review comments into account

--- shelley/chain-and-ledger/executable-spec/test/Rules/TestPoolreap.hs
+++ shelley/chain-and-ledger/executable-spec/test/Rules/TestPoolreap.hs
@@ -6,13 +6,14 @@
 
 module Rules.TestPoolreap where
 
-import           Control.Monad (when)
 import qualified Data.Set as Set (intersection, isSubsetOf, null, singleton)
+import           Data.Word (Word64)
 
 import           Hedgehog (Property, forAll, property, withTests)
 
-import           Control.State.Transition.Generator (trace)
-import           Control.State.Transition.Trace (STTriple (..), sourceSignalTargets, traceLength)
+import           Control.State.Transition.Generator (ofLengthAtLeast, trace)
+import           Control.State.Transition.Trace (pattern SourceSignalTarget, signal, source,
+                     sourceSignalTargets, target)
 
 import           MockTypes (POOLREAP)
 import           STS.PoolReap (PoolreapState (..))
@@ -27,10 +28,10 @@ import           Test.Utils (assertAll)
 -- Constants for Properties --
 ------------------------------
 
-numberOfTests :: Int
+numberOfTests :: Word64
 numberOfTests = 300
 
-traceLen :: Int
+traceLen :: Word64
 traceLen = 100
 
 -----------------------------
@@ -41,16 +42,13 @@ traceLen = 100
 -- the stake pool and retiring maps.
 removedAfterPoolreap :: Property
 removedAfterPoolreap = withTests (fromIntegral numberOfTests) . property $ do
-  t <- forAll (trace @POOLREAP $ fromIntegral traceLen)
-  let
-    n :: Integer
-    n = fromIntegral $ traceLength t
-    tr = sourceSignalTargets t
+  tr <- fmap sourceSignalTargets
+        $ forAll
+        $ trace @POOLREAP traceLen `ofLengthAtLeast` 1
 
-  when (n > 1) $
-    assertAll poolRemoved tr
+  assertAll poolRemoved tr
 
-  where poolRemoved (STTriple
+  where poolRemoved (SourceSignalTarget
                       { source = PoolreapState _ _ _ p
                       , signal = e
                       , target = PoolreapState _ _ _ p'}) =
