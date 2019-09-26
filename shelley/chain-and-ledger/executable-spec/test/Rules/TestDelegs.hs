321c2a20e531dbc3648549c68537b5cee7810a06
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-09-26 19:17:10 +0200

    Add `TestDelegs` module
    Property which checks that rewards of new state decrease via the amount spent
    via withdrawals.

---
+++ shelley/chain-and-ledger/executable-spec/test/Rules/TestDelegs.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Rules.TestDelegs where
+
+import           Data.Word (Word64)
+
+import           Hedgehog (Property, TestLimit, forAll, property, withTests)
+
+import           Control.State.Transition.Generator (ofLengthAtLeast, trace)
+import           Control.State.Transition.Trace (pattern SourceSignalTarget, source,
+                     sourceSignalTargets, target, _traceEnv)
+
+import           Coin (pattern Coin)
+import           LedgerState (_dstate, _rewards)
+import           MockTypes (DELEGS)
+import           STS.Delegs (pattern DelegsEnv)
+import           TxData (_body, _wdrls)
+
+import           Test.Utils (assertAll)
+
+------------------------------
+-- Constants for Properties --
+------------------------------
+
+numberOfTests :: TestLimit
+numberOfTests = 300
+
+traceLen :: Word64
+traceLen = 100
+
+---------------------------
+-- Properties for DELEGS --
+---------------------------
+
+-- | Check that the rewards pot decreases by the sum of withdrawals in the
+-- transaction.
+rewardsDecreasesByWithdrawals :: Property
+rewardsDecreasesByWithdrawals = withTests numberOfTests . property $ do
+  t <- forAll $ trace @DELEGS traceLen `ofLengthAtLeast` 1
+
+  let DelegsEnv _ _ _ tx = _traceEnv t
+      tr = sourceSignalTargets t
+
+  assertAll (rewardsPotdecreases $ _body tx) tr
+
+  where rewardsPotdecreases tx (SourceSignalTarget
+                              { source = d
+                              , target = d'}) =
+          let wdrls = _wdrls tx
+              rewards  = (_rewards . _dstate) d
+              rewards' = (_rewards . _dstate) d'
+              rewardsSum = foldl (+) (Coin 0) rewards
+              rewardsSum' = foldl (+) (Coin 0) rewards'
+              wdrlSum = foldl (+) (Coin 0) wdrls
+          in
+             rewardsSum >= rewardsSum'
+          && wdrlSum >= Coin 0
+          && rewardsSum == wdrlSum + rewardsSum'
