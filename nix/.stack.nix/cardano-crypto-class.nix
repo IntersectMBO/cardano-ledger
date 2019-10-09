d39df916e07325620cb6d513fe3c1d77f42e001e
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-09 16:37:37 +1100

    Nix voodoo

--- nix/.stack.nix/cardano-crypto-class.nix
+++ nix/.stack.nix/cardano-crypto-class.nix
@@ -48,8 +48,8 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/cardano-base";
-      rev = "bb262f466d659d94ed6e5500c2dc101f7c2c6b1e";
-      sha256 = "1vmp4lia4s2c20wgfbyvywsdmzg0s1m25zjw7n8a0my7cjg7n3vg";
+      rev = "c50bfdb129ae1eb319c9af3902674ef75b017fec";
+      sha256 = "06963ni4hh06ai0x795wn7bxdjwnnwhraniijzyzzl5pir5lcli6";
       });
     postUnpack = "sourceRoot+=/cardano-crypto-class; echo source root reset to \$sourceRoot";
     }
