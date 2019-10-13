f0d057c3edbfe6c0f7547f68901b6e8a752f9ca8
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-10-14 07:45:00 +1100

    Nix voodoo

--- nix/.stack.nix/delegation.nix
+++ nix/.stack.nix/delegation.nix
@@ -1,4 +1,43 @@
-{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+let
+  buildDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (build dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  sysDepError = pkg:
+    builtins.throw ''
+      The Nixpkgs package set does not contain the package: ${pkg} (system dependency).
+      
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      '';
+  pkgConfDepError = pkg:
+    builtins.throw ''
+      The pkg-conf packages does not contain the package: ${pkg} (pkg-conf dependency).
+      
+      You may need to augment the pkg-conf package mapping in haskell.nix so that it can be found.
+      '';
+  exeDepError = pkg:
+    builtins.throw ''
+      The local executable components do not include the component: ${pkg} (executable dependency).
+      '';
+  legacyExeDepError = pkg:
+    builtins.throw ''
+      The Haskell package set does not contain the package: ${pkg} (executable dependency).
+      
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+  buildToolDepError = pkg:
+    builtins.throw ''
+      Neither the Haskell package set or the Nixpkgs package set contain the package: ${pkg} (build tool dependency).
+      
+      If this is a system dependency:
+      You may need to augment the system package mapping in haskell.nix so that it can be found.
+      
+      If this is a Haskell dependency:
+      If you are using Stackage, make sure that you are using a snapshot that contains the package. Otherwise you may need to update the Hackage snapshot you are using, usually by updating haskell.nix.
+      '';
+in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
   {
     flags = { development = false; };
     package = {
@@ -13,46 +52,49 @@
       synopsis = "";
       description = "Delegation Executable Model";
       buildType = "Simple";
+      isLocal = true;
       };
     components = {
       "library" = {
         depends = [
-          (hsPkgs.base)
-          (hsPkgs.bytestring)
-          (hsPkgs.containers)
-          (hsPkgs.cryptonite)
-          (hsPkgs.hedgehog)
-          (hsPkgs.small-steps)
-          (hsPkgs.microlens)
-          (hsPkgs.microlens-th)
-          (hsPkgs.non-integer)
-          (hsPkgs.cs-ledger)
-          (hsPkgs.cardano-binary)
-          (hsPkgs.cardano-crypto-class)
-          (hsPkgs.cardano-prelude)
+          (hsPkgs."base" or (buildDepError "base"))
+          (hsPkgs."bytestring" or (buildDepError "bytestring"))
+          (hsPkgs."containers" or (buildDepError "containers"))
+          (hsPkgs."cryptonite" or (buildDepError "cryptonite"))
+          (hsPkgs."hedgehog" or (buildDepError "hedgehog"))
+          (hsPkgs."small-steps" or (buildDepError "small-steps"))
+          (hsPkgs."microlens" or (buildDepError "microlens"))
+          (hsPkgs."microlens-th" or (buildDepError "microlens-th"))
+          (hsPkgs."non-integer" or (buildDepError "non-integer"))
+          (hsPkgs."cs-ledger" or (buildDepError "cs-ledger"))
+          (hsPkgs."cardano-binary" or (buildDepError "cardano-binary"))
+          (hsPkgs."cardano-crypto-class" or (buildDepError "cardano-crypto-class"))
+          (hsPkgs."cardano-prelude" or (buildDepError "cardano-prelude"))
           ];
+        buildable = true;
         };
       tests = {
         "delegation-test" = {
           depends = [
-            (hsPkgs.base)
-            (hsPkgs.bytestring)
-            (hsPkgs.cryptonite)
-            (hsPkgs.tasty)
-            (hsPkgs.tasty-hunit)
-            (hsPkgs.tasty-hedgehog)
-            (hsPkgs.hedgehog)
-            (hsPkgs.delegation)
-            (hsPkgs.containers)
-            (hsPkgs.multiset)
-            (hsPkgs.text)
-            (hsPkgs.microlens)
-            (hsPkgs.cs-ledger)
-            (hsPkgs.cardano-binary)
-            (hsPkgs.cardano-crypto-class)
-            (hsPkgs.cardano-prelude)
-            (hsPkgs.small-steps)
+            (hsPkgs."base" or (buildDepError "base"))
+            (hsPkgs."bytestring" or (buildDepError "bytestring"))
+            (hsPkgs."cryptonite" or (buildDepError "cryptonite"))
+            (hsPkgs."tasty" or (buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (buildDepError "tasty-hunit"))
+            (hsPkgs."tasty-hedgehog" or (buildDepError "tasty-hedgehog"))
+            (hsPkgs."hedgehog" or (buildDepError "hedgehog"))
+            (hsPkgs."delegation" or (buildDepError "delegation"))
+            (hsPkgs."containers" or (buildDepError "containers"))
+            (hsPkgs."multiset" or (buildDepError "multiset"))
+            (hsPkgs."text" or (buildDepError "text"))
+            (hsPkgs."microlens" or (buildDepError "microlens"))
+            (hsPkgs."cs-ledger" or (buildDepError "cs-ledger"))
+            (hsPkgs."cardano-binary" or (buildDepError "cardano-binary"))
+            (hsPkgs."cardano-crypto-class" or (buildDepError "cardano-crypto-class"))
+            (hsPkgs."cardano-prelude" or (buildDepError "cardano-prelude"))
+            (hsPkgs."small-steps" or (buildDepError "small-steps"))
             ];
+          buildable = true;
           };
         };
       };
