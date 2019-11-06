74c03bbe82534b6f2de36caa769fd38b6e958d33
Duncan Coutts <duncan@well-typed.com>
2019-11-06 09:56:35 +0000

    Add a comment on the last remaining SafeSigner test
    It is now completely trivial.

--- crypto/test/Test/Cardano/Crypto/Signing/Safe.hs
+++ crypto/test/Test/Cardano/Crypto/Signing/Safe.hs
@@ -33,6 +33,30 @@ tests = checkParallel $$discover
 --------------------------------------------------------------------------------
 
 -- | Making a 'SafeSigner' from a 'SigningKey' preserves the 'VerificationKey'
+--
+-- Since SafeSigner was simplified, this is a completely trivial property.
+--
+-- Definitions:
+--
+-- > safeToVerification (SafeSigner sk _) = toVerification sk
+-- > noPassSafeSigner sk = SafeSigner sk emptyPassphrase
+--
+-- Theorem:
+--
+-- > safeToVerification (noPassSafeSigner sk) = toVerification sk
+--
+-- Proof:
+--
+-- >   safeToVerification (noPassSafeSigner sk)
+-- >
+-- > = { by expanding definition of noPassSafeSigner }
+-- >
+-- >   safeToVerification (SafeSigner sk emptyPassphrase)
+-- >
+-- > = { by expanding definition of safeToVerification }
+-- >
+-- >   toVerification sk
+--
 prop_safeSignerPreservesVerificationKey :: Property
 prop_safeSignerPreservesVerificationKey = property $ do
   sk <- forAll genSigningKey
