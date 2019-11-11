fb356d8423a7fa6614669d3c1725ac7669ef3cfb
Nicholas Clarke <nicholas.clarke@iohk.io>
2019-11-11 11:38:34 +0100

    API for chain extension and block validation

---
+++ shelley/chain-and-ledger/executable-spec/src/Cardano/Ledger/Shelley/API.hs
@@ -0,0 +1,9 @@
+-- | API to the Shelley ledger
+module Cardano.Ledger.Shelley.API
+  ( module X,
+  )
+where
+
+-- import qualified Cardano.Ledger.Shelley.API.Anachronism as X
+-- import qualified Cardano.Ledger.Shelley.API.Mempool as X
+import qualified Cardano.Ledger.Shelley.API.Validation as X
---
+++ shelley/chain-and-ledger/executable-spec/src/Cardano/Ledger/Shelley/API/Validation.hs
@@ -0,0 +1,110 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+-- | Interface to the block validation and chain extension logic in the Shelley
+-- API.
+module Cardano.Ledger.Shelley.API.Validation
+  ( ShelleyState,
+    mkBbodyEnv,
+    mkBheadEnv,
+    applyHeaderTransition,
+    applyBlockTransition,
+  )
+where
+
+import BlockChain
+import qualified Cardano.Crypto.DSIGN as DSIGN
+import Cardano.Ledger.Shelley.Crypto
+import Control.Arrow (left, right)
+import Control.Monad.Except
+import Control.State.Transition (TRC (..), applySTS)
+import Ledger.Core (Relation (..))
+import qualified LedgerState
+import qualified STS.Bbody as STS
+import qualified STS.Bhead as STS
+import qualified TxData as Tx
+
+-- | Type alias for the state updated by BHEAD and BBODY rules
+type ShelleyState = LedgerState.NewEpochState
+
+{-------------------------------------------------------------------------------
+  Applying blocks
+-------------------------------------------------------------------------------}
+
+mkBheadEnv ::
+  ShelleyState crypto ->
+  STS.BheadEnv crypto
+mkBheadEnv = STS.BheadEnv . LedgerState.getGKeys
+
+mkBbodyEnv ::
+  -- | @NewEpochState@ is also the state of the @BHEAD@ transition system.
+  ShelleyState crypto ->
+  STS.BbodyEnv
+mkBbodyEnv
+  LedgerState.NewEpochState
+    { LedgerState.nesOsched,
+      LedgerState.nesEs
+    } = STS.BbodyEnv
+    { STS.bbodySlots = dom nesOsched,
+      STS.bbodyPp = LedgerState.esPp nesEs,
+      STS.bbodyReserves =
+        LedgerState._reserves
+          . LedgerState.esAccountState
+          $ nesEs
+    }
+
+newtype HeaderTransitionError crypto
+  = HeaderTransitionError [STS.PredicateFailure (STS.BHEAD crypto)]
+  deriving (Eq, Show)
+
+-- | Apply the header level ledger transition.
+--
+-- This handles checks and updates that happen on a slot tick, as well as a few
+-- header level checks, such as size constraints.
+applyHeaderTransition ::
+  forall crypto m.
+  ( Crypto crypto,
+    MonadError (HeaderTransitionError crypto) m
+  ) =>
+  ShelleyState crypto ->
+  BHeader crypto ->
+  m (ShelleyState crypto)
+applyHeaderTransition state hdr =
+  liftEither
+    . left (HeaderTransitionError . join)
+    . applySTS @(STS.BHEAD crypto)
+    $ TRC (mkBheadEnv state, state, hdr)
+
+newtype BlockTransitionError crypto
+  = BlockTransitionError [STS.PredicateFailure (STS.BBODY crypto)]
+  deriving (Eq, Show)
+
+-- | Apply the block level ledger transition.
+applyBlockTransition ::
+  forall crypto m.
+  ( Crypto crypto,
+    MonadError (BlockTransitionError crypto) m,
+    DSIGN.Signable (DSIGN crypto) (Tx.TxBody crypto)
+  ) =>
+  ShelleyState crypto ->
+  Block crypto ->
+  m (ShelleyState crypto)
+applyBlockTransition state blk =
+  liftEither
+    . right (updateShelleyState state)
+    . left (BlockTransitionError . join)
+    . applySTS @(STS.BBODY crypto)
+    $ TRC (mkBbodyEnv state, bbs, blk)
+  where
+    updateShelleyState ::
+      ShelleyState crypto ->
+      STS.BbodyState crypto ->
+      ShelleyState crypto
+    updateShelleyState ss (STS.BbodyState ls bcur) =
+      LedgerState.updateNES ss bcur ls
+    bbs =
+      STS.BbodyState
+        (LedgerState.esLState $ LedgerState.nesEs state)
+        (LedgerState.nesBcur state)
