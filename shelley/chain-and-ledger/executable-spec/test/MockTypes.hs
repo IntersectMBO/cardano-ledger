adac7ae12c55c491c74bf469b2d03d6e773396be
Matthias Güdemann <matthias.gudemann@iohk.io>
2019-11-27 21:01:11 +0100

    Add `CoreKeyPairs` type

--- shelley/chain-and-ledger/executable-spec/test/MockTypes.hs
+++ shelley/chain-and-ledger/executable-spec/test/MockTypes.hs
@@ -57,6 +57,10 @@ type GenKeyHash = Keys.GenKeyHash MockCrypto
 
 type KeyPair = Keys.KeyPair 'Keys.Regular MockCrypto
 
+type CoreKeyPair = Keys.KeyPair 'Keys.Genesis MockCrypto
+
+type CoreKeyPairs = [CoreKeyPair]
+
 type VKey = Keys.VKey MockCrypto
 
 type SKey = Keys.SKey MockCrypto
