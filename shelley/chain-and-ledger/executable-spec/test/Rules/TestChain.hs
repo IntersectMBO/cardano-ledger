adaa743c59af89048ab11a39f30a4c631ca2f2ac
Matthias Güdemann <matthias.gudemann@iohk.io>
2020-01-29 16:18:52 +0100

    Remove duplicated code for `epochFromSlot`

--- shelley/chain-and-ledger/executable-spec/test/Rules/TestChain.hs
+++ shelley/chain-and-ledger/executable-spec/test/Rules/TestChain.hs
@@ -10,11 +10,9 @@ module Rules.TestChain
   , preservationOfAda)
 where
 
-import           Control.Monad.Trans.Reader (asks)
 import           Data.Word (Word64)
 import           Test.QuickCheck (Property, Testable, property, withMaxSuccess)
 
-import           BaseTypes (epochInfo)
 import           BlockChain (Block (..), bhbody, bheaderSlotNo)
 import           ConcreteCryptoTypes (CHAIN, NEWEPOCH, POOLREAP)
 import           Control.State.Transition.Trace (SourceSignalTarget (..), Trace,
@@ -25,11 +23,10 @@ import           LedgerState (pattern DPState, esAccountState, esLState, nesEs, 
                      _utxoState)
 import qualified Rules.TestNewEpoch as TestNewEpoch
 import qualified Rules.TestPoolreap as TestPoolreap
-import           Slot (EpochNo, SlotNo, epochInfoEpoch)
 import           STS.Chain (ChainState (..))
 import           STS.PoolReap (PoolreapState (..))
 
-import           Test.Utils (runShelleyBase, testGlobals)
+import           Test.Utils (epochFromSlotNo, testGlobals)
 
 
 ------------------------------
@@ -92,12 +89,6 @@ forAllChainTrace prop =
   withMaxSuccess (fromIntegral numberOfTests) . property $
     forAllTraceFromInitState testGlobals traceLen traceLen (Just mkGenesisChainState) prop
 
-epochFromSlot :: SlotNo -> EpochNo
-epochFromSlot s =
-  runShelleyBase $ do
-    ei <- asks epochInfo
-    epochInfoEpoch ei s
-
 -- | Transform CHAIN `sourceSignalTargets`s to POOLREAP ones.
 chainToPoolreapSst
   :: SourceSignalTarget CHAIN
@@ -113,7 +104,7 @@ chainToPoolreapSst (SourceSignalTarget ChainState {chainNes = nes}
                                     (accountSt nes')
                                     dstate'
                                     pstate')
-                     (epochFromSlot s)
+                     (epochFromSlotNo s)
   where
     s = (bheaderSlotNo . bhbody) bh
 
@@ -130,6 +121,6 @@ chainToNewEpochSst
 chainToNewEpochSst (SourceSignalTarget ChainState {chainNes = nes}
                                        ChainState {chainNes = nes'}
                                        (Block bh _)) =
-  SourceSignalTarget nes nes' (epochFromSlot s)
+  SourceSignalTarget nes nes' (epochFromSlotNo s)
   where
     s = (bheaderSlotNo . bhbody) bh
