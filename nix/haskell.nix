a566e33f4509f11d90a05ea73ef09a0ae0acda2e
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2020-03-02 12:23:05 +0100

    Disable doctests.

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -19,33 +19,17 @@ let
     src = haskell-nix.haskellLib.cleanGit { src = ../. ; };
     ghc = buildPackages.haskell-nix.compiler.${compiler};
     modules = [
-        # Allow reinstallation of Win32
-        { nonReinstallablePkgs =
-          [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
-            "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
-            # ghcjs custom packages
-            "ghcjs-prim" "ghcjs-th"
-            "array" "binary" "bytestring" "containers"
-            "filepath" "ghc-compact" "ghc-prim"
-            # "ghci" "haskeline"
-            "mtl" "parsec" "text" "transformers"
-            "xhtml"
-            # "stm" "terminfo"
-          ];
-        }
-        {
-          packages.cs-blockchain.configureFlags = [ "--ghc-option=-Werror" ];
-          packages.cs-ledger.configureFlags = [ "--ghc-option=-Werror" ];
-          packages.delegation.configureFlags = [ "--ghc-option=-Werror" ];
-          packages.non-integer.configureFlags = [ "--ghc-option=-Werror" ];
-          packages.small-steps.configureFlags = [ "--ghc-option=-Werror" ];
-          enableLibraryProfiling = profiling;
-          # necessary for doctests (https://github.com/input-output-hk/haskell.nix/issues/221)
-          reinstallableLibGhc = true;
-          # Make sure we use a buildPackages version of alex
-          packages.small-steps.components.tests.doctests.build-tools = [ buildPackages.haskell-nix.haskellPackages.alex ];
-          packages.cs-ledger.components.tests.doctests.build-tools = [ buildPackages.haskell-nix.haskellPackages.alex ];
-        }
+      {
+        packages.cs-blockchain.configureFlags = [ "--ghc-option=-Werror" ];
+        packages.cs-ledger.configureFlags = [ "--ghc-option=-Werror" ];
+        packages.delegation.configureFlags = [ "--ghc-option=-Werror" ];
+        packages.non-integer.configureFlags = [ "--ghc-option=-Werror" ];
+        packages.small-steps.configureFlags = [ "--ghc-option=-Werror" ];
+        enableLibraryProfiling = profiling;
+        # Disable doctests for now (waiting for https://github.com/input-output-hk/haskell.nix/pull/427):
+        packages.small-steps.components.tests.doctests.buildable = lib.mkForce false;
+        packages.cs-ledger.components.tests.doctests.buildable = lib.mkForce false;
+      }
     ];
   };
 in
