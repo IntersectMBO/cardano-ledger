8a23786425e512f928758f5e86daceadb0427d92
Alex Byaly <alexander.byaly@iohk.io>
2020-03-25 09:41:07 -0500

    Nightly tests

--- .buildkite/nightly-tests.sh
+++ .buildkite/nightly-tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-nix build -f `dirname $0`/.. haskellPackages.shelley-spec-ledger.components.tests.shelley-spec-ledger-test -o shelley-specs-ledger-tests
-# cd cardano-ledger && ../cardano-ledger-test/bin/cardano-ledger-test --scenario=QualityAssurance
+nix build -f `dirname $0`/.. haskellPackages.shelley-spec-ledger.components.tests.shelley-spec-ledger-test -o shelley-spec-ledger-test
+cd shelley/chain-and-ledger/executable-spec/ && ../../../shelley-spec-ledger-test/bin/shelley-spec-ledger-test --scenario=Nightly
