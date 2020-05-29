a815d2f9407e6cc5c4769f5a6a8fac7df4ea7a19
uroboros <ryan.lemmer@iohk.io>
2020-05-29 18:26:36 +0200

    Add more Serialisation property tests from ouroboros-network

--- shelley/chain-and-ledger/executable-spec/test/Test/Shelley/Spec/Ledger/SerializationProperties.hs
+++ shelley/chain-and-ledger/executable-spec/test/Test/Shelley/Spec/Ledger/SerializationProperties.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -15,6 +16,9 @@ module Test.Shelley.Spec.Ledger.SerializationProperties
     prop_roundtrip_Tx,
     prop_roundtrip_TxId,
     prop_roundtrip_LEDGER_PredicateFails,
+    prop_roundtrip_PrtclState,
+    prop_roundtrip_LedgerState,
+    prop_roundtrip_NewEpochState,
   )
 where
 
@@ -34,67 +38,89 @@ import Codec.CBOR.Decoding (Decoder)
 import Codec.CBOR.Encoding (Encoding)
 import Codec.CBOR.Read (deserialiseFromBytes)
 import Codec.CBOR.Write (toLazyByteString)
+import qualified Data.ByteString.Char8 as BS (pack)
 import qualified Data.ByteString.Lazy as Lazy
 import Data.Coerce (coerce)
+import Data.IP (IPv4, IPv6, toIPv4, toIPv6)
+import qualified Data.Map.Strict as Map (fromList)
 import Data.Maybe (fromJust)
 import Data.Proxy (Proxy (..))
 import Data.Ratio ((%))
+import Data.Sequence.Strict (StrictSeq)
+import qualified Data.Sequence.Strict as StrictSeq
+import Data.Word (Word8)
 import Generic.Random (genericArbitraryU)
 import Numeric.Natural (Natural)
 import Shelley.Spec.Ledger.Address (Addr (Addr))
 import Shelley.Spec.Ledger.BaseTypes
-  ( Network,
+  ( DnsName,
+    Network,
     Nonce (..),
+    Port,
+    StrictMaybe,
     UnitInterval,
+    Url,
     mkNonce,
     mkUnitInterval,
+    textToDns,
+    textToUrl,
   )
 import Shelley.Spec.Ledger.BlockChain (HashHeader (..), pattern Block)
 import Shelley.Spec.Ledger.Coin (Coin (Coin))
 import Shelley.Spec.Ledger.Credential (Credential (..), Ptr, StakeReference)
 import Shelley.Spec.Ledger.Crypto (Crypto, HASH)
+import Shelley.Spec.Ledger.Delegation.Certificates (PoolDistr (..))
+import Shelley.Spec.Ledger.EpochBoundary (BlocksMade (..), Stake (..))
 import Shelley.Spec.Ledger.Keys
   ( Hash,
     KeyHash (KeyHash),
     VKey (VKey),
     hash,
   )
+import Shelley.Spec.Ledger.LedgerState
+  ( AccountState,
+    EpochState (..),
+    OBftSlot,
+    RewardUpdate,
+    emptyRewardUpdate,
+  )
 import Shelley.Spec.Ledger.MetaData (MetaDataHash (..))
 import Shelley.Spec.Ledger.OCert (KESPeriod (..))
-import Shelley.Spec.Ledger.PParams (ProtVer)
+import Shelley.Spec.Ledger.PParams (PParams, ProtVer)
+import Shelley.Spec.Ledger.Rewards (ApparentPerformance (..))
 import qualified Shelley.Spec.Ledger.STS.Chain as STS
+import qualified Shelley.Spec.Ledger.STS.Prtcl as STS (PrtclState)
 import Shelley.Spec.Ledger.Scripts (ScriptHash (ScriptHash))
-import Shelley.Spec.Ledger.TxData (RewardAcnt (RewardAcnt), TxId (TxId), TxIn (TxIn))
+import Shelley.Spec.Ledger.TxData
+  ( PoolMetaData (PoolMetaData),
+    PoolParams (PoolParams),
+    RewardAcnt (RewardAcnt),
+    StakePoolRelay,
+    TxId (TxId),
+    TxIn (TxIn),
+  )
 import Test.QuickCheck
   ( Arbitrary,
     arbitrary,
     genericShrink,
+    listOf,
     oneof,
     shrink,
   )
 import Test.QuickCheck.Hedgehog (hedgehog)
 import qualified Test.Shelley.Spec.Ledger.ConcreteCryptoTypes as Mock
-  ( Addr,
-    BHeader,
-    Block,
-    ConcreteCrypto,
-    DELEG,
-    DELEGS,
-    DELPL,
-    HashHeader,
-    LEDGER,
-    LEDGERS,
-    POOL,
-    PPUP,
-    Tx,
-    TxId,
-    TxOut,
-    UTXO,
-    UTXOW,
+import Test.Shelley.Spec.Ledger.Generator.Core
+  ( KeySpace (KeySpace_),
+    NatNonce (..),
+    geConstants,
+    geKeySpace,
+    ksCoreNodes,
+    mkBlock,
+    mkOCert,
   )
-import Test.Shelley.Spec.Ledger.Generator.Core (KeySpace (KeySpace_), NatNonce (..), geKeySpace, ksCoreNodes, mkBlock, mkOCert)
 import Test.Shelley.Spec.Ledger.Generator.Presets (genEnv)
-import Test.Shelley.Spec.Ledger.NonTraceProperties.Generator (genStateTx)
+import Test.Shelley.Spec.Ledger.Generator.Update (genPParams)
+import Test.Shelley.Spec.Ledger.NonTraceProperties.Generator (genStateTx, genValidStateTx)
 import Test.Tasty.QuickCheck ((===), Gen, Property, choose, counterexample, elements)
 
 roundtrip ::
@@ -157,6 +183,15 @@ prop_roundtrip_TxId = roundtrip toCBOR fromCBOR
 
 prop_roundtrip_LEDGER_PredicateFails :: [STS.PredicateFailure Mock.LEDGERS] -> Property
 prop_roundtrip_LEDGER_PredicateFails = roundtrip toCBOR fromCBOR
+
+prop_roundtrip_PrtclState :: STS.PrtclState Mock.ConcreteCrypto -> Property
+prop_roundtrip_PrtclState = roundtrip toCBOR fromCBOR
+
+prop_roundtrip_LedgerState :: Mock.LedgerState -> Property
+prop_roundtrip_LedgerState = roundtrip toCBOR fromCBOR
+
+prop_roundtrip_NewEpochState :: Mock.NewEpochState -> Property
+prop_roundtrip_NewEpochState = roundtrip toCBOR fromCBOR
 
 {-------------------------------------------------------------------------------
   Generators
@@ -330,3 +365,111 @@ instance Crypto c => Arbitrary (MetaDataHash c) where
 
 instance Arbitrary (Hash.Hash ShortHash a) where
   arbitrary = genHash (Proxy @Mock.ConcreteCrypto)
+
+instance Arbitrary (STS.PrtclState Mock.ConcreteCrypto) where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Arbitrary Mock.LedgerState where
+  arbitrary = do
+    (_ledgerState, _steps, _txfee, _tx, ledgerState) <- hedgehog genValidStateTx
+    return ledgerState
+
+instance Arbitrary Mock.NewEpochState where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Crypto c => Arbitrary (BlocksMade c) where
+  arbitrary = BlocksMade <$> arbitrary
+
+instance Crypto c => Arbitrary (PoolDistr c) where
+  arbitrary =
+    PoolDistr . Map.fromList
+      <$> listOf ((,) <$> arbitrary <*> genVal)
+    where
+      genVal = (,) <$> arbitrary <*> genHash (Proxy @c)
+
+instance Arbitrary Mock.EpochState where
+  arbitrary =
+    EpochState
+      <$> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+
+instance Crypto c => Arbitrary (RewardUpdate c) where
+  arbitrary = return emptyRewardUpdate
+
+instance Arbitrary a => Arbitrary (StrictMaybe a) where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Crypto c => Arbitrary (OBftSlot c) where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Arbitrary PParams where
+  arbitrary = genPParams (geConstants genEnv)
+
+instance Arbitrary Mock.NonMyopic where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Arbitrary Mock.SnapShot where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Arbitrary Mock.SnapShots where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Arbitrary ApparentPerformance where
+  arbitrary = ApparentPerformance <$> arbitrary
+
+instance Arbitrary Mock.Stake where
+  arbitrary = Stake <$> arbitrary
+
+instance Crypto c => Arbitrary (PoolParams c) where
+  arbitrary =
+    PoolParams
+      <$> arbitrary
+      <*> genHash (Proxy @c)
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+      <*> arbitrary
+
+instance Arbitrary PoolMetaData where
+  arbitrary = (`PoolMetaData` BS.pack "bytestring") <$> arbitrary
+
+instance Arbitrary Url where
+  arbitrary = return . fromJust $ textToUrl "text"
+
+instance Arbitrary a => Arbitrary (StrictSeq a) where
+  arbitrary = StrictSeq.toStrict <$> arbitrary
+  shrink = map StrictSeq.toStrict . shrink . StrictSeq.getSeq
+
+instance Arbitrary StakePoolRelay where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
+
+instance Arbitrary Port where
+  arbitrary = fromIntegral @Word8 @Port <$> arbitrary
+
+instance Arbitrary IPv4 where
+  arbitrary = pure $ toIPv4 [192, 0, 2, 1]
+
+instance Arbitrary IPv6 where
+  arbitrary = pure $ toIPv6 [0x2001, 0xDB8, 0, 0, 0, 0, 0, 1]
+
+instance Arbitrary DnsName where
+  arbitrary = pure . fromJust $ textToDns "foo.example.com"
+
+instance Arbitrary AccountState where
+  arbitrary = genericArbitraryU
+  shrink = genericShrink
