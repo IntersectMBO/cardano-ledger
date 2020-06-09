424b8c4ce1f368ed781b31975d60e2ca1a522c64
kderme <k.dermenz@gmail.com>
2020-06-09 03:07:47 +0300

    Move Shelley Genesis tests and examples from cardano-node

---
+++ shelley/chain-and-ledger/executable-spec/test/Test/Shelley/Spec/Ledger/Genesis/Example.hs
@@ -0,0 +1,42 @@
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Shelley.Spec.Ledger.Genesis.Example
+  ( exampleShelleyGenesis
+  ) where
+
+import Cardano.Crypto.ProtocolMagic
+import Cardano.Slotting.Slot (EpochSize (..))
+import qualified Data.Map.Strict as Map
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import Shelley.Spec.Ledger.BaseTypes (Network (..), truncateUnitInterval)
+import Shelley.Spec.Ledger.Genesis (ShelleyGenesis (..), emptyGenesisStaking)
+import Shelley.Spec.Ledger.PParams (PParams' (..), emptyPParams)
+import Test.Shelley.Spec.Ledger.ConcreteCryptoTypes hiding (Addr, VerKeyVRF, KeyHash)
+
+exampleShelleyGenesis :: ShelleyGenesis ConcreteCrypto
+exampleShelleyGenesis =
+  ShelleyGenesis
+    { sgSystemStart = posixSecondsToUTCTime $ realToFrac (1234566789 :: Integer)
+    , sgNetworkMagic = 4036000900
+    , sgNetworkId = Testnet
+    , sgProtocolMagicId = ProtocolMagicId 838299499
+    , sgActiveSlotsCoeff = 6.259
+    , sgSecurityParam = 120842
+    , sgEpochLength = EpochSize 1215
+    , sgSlotsPerKESPeriod = 8541
+    , sgMaxKESEvolutions = 28899
+    , sgSlotLength =  8
+    , sgUpdateQuorum = 16991
+    , sgMaxMajorPV = 25446
+    , sgMaxLovelaceSupply = 71
+    , sgProtocolParams = emptyPParams
+        { _d = truncateUnitInterval . realToFrac $ (1.9e-2 :: Double)
+        , _maxBBSize = 239857
+        , _maxBHSize = 217569
+        }
+    , sgGenDelegs = Map.empty
+    , sgInitialFunds = Map.empty
+    , sgStaking = emptyGenesisStaking
+    }
