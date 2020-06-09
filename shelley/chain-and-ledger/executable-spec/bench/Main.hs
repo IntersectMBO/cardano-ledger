70eebaa07f3f02758a0be1f496f5e5d6a85586f1
TimSheard <tim.sheard@relational.ai>
2020-06-09 11:19:26 -0700

    save before merge NFdata branch

--- shelley/chain-and-ledger/executable-spec/bench/Main.hs
+++ shelley/chain-and-ledger/executable-spec/bench/Main.hs
@@ -1,9 +1,35 @@
+{-# LANGUAGE TypeSynonymInstances,
+             FlexibleInstances,
+             StandaloneDeriving
+#-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
 
-import Criterion.Main (bench, bgroup, defaultMain, whnf)
+import Control.DeepSeq (NFData(rnf))
+import Criterion.Main -- (bench, bgroup, defaultMain, whnf)
+
+import Test.Shelley.Spec.Ledger.ConcreteCryptoTypes(UTxOState)
+
 import Test.Shelley.Spec.Ledger.BenchmarkFunctions
   ( ledgerSpendOneUTxO,
+    ledgerSpendOneGivenUTxO,
+    initUTxO
   )
+
+-- deriving instance NFData UTxOState
+
+-- given:: Integer -> Benchmark
+given n  = (n,initUTxO n)
+many = map given [5::Integer,50,500]
+
+spend:: Integer -> UTxOState -> Benchmark
+spend n state = bench ("given "++show n) (whnf ledgerSpendOneGivenUTxO state)
+
 
 -- Our benchmark harness.
 main :: IO ()
