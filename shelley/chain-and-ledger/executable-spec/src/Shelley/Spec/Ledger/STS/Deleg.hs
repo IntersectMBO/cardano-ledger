4afcc0cdd7292d8d81c667af60d885c029d22c1e
Nicholas Clarke <nicholas.clarke@iohk.io>
2020-07-06 14:28:07 +0200

    Example precondition

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Deleg.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Deleg.hs
@@ -24,14 +24,6 @@ import Cardano.Binary
 import Cardano.Prelude (NoUnexpectedThunks (..))
 import Control.Monad.Trans.Reader (asks)
 import Control.State.Transition
-  ( STS (..),
-    TRC (..),
-    TransitionRule,
-    failBecause,
-    judgmentContext,
-    liftSTS,
-    (?!),
-  )
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import Data.Typeable (Typeable)
@@ -43,7 +35,7 @@ import Shelley.Spec.Ledger.BaseTypes
     invalidKey,
   )
 import Shelley.Spec.Ledger.Coin (Coin (..))
-import Shelley.Spec.Ledger.Core (addpair, haskey, range, removekey, (∉), (⋫))
+import Shelley.Spec.Ledger.Core (addpair, dom, haskey, range, removekey, (∉), (⋫))
 import Shelley.Spec.Ledger.Credential (Credential)
 import Shelley.Spec.Ledger.Crypto (Crypto)
 import Shelley.Spec.Ledger.Keys
@@ -133,6 +125,15 @@ instance Typeable crypto => STS (DELEG crypto) where
 
   initialRules = [pure emptyDState]
   transitionRules = [delegationTransition]
+
+  assertions =
+    [ PreCondition
+        "_stkCreds and _rewards must have the same domain"
+        ( \(TRC (_, st, _)) ->
+            dom (_stkCreds st)
+              == (Set.map getRwdCred $ dom (_rewards st))
+        )
+    ]
 
 instance NoUnexpectedThunks (PredicateFailure (DELEG crypto))
 
