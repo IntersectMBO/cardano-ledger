f4ef044c6f7971ebf71f1c884f1d9ca4c0171cb6
Nicholas Clarke <nicholas.clarke@iohk.io>
2020-08-05 13:42:01 +0200

    Need moar bits!

--- shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Utils.hs
+++ shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Utils.hs
@@ -43,7 +43,7 @@ import Cardano.Crypto.DSIGN.Class (DSIGNAlgorithm (..))
 import Cardano.Crypto.Hash
   ( Hash,
     HashAlgorithm,
-    MD5,
+    Blake2b_256,
     hashToBytes,
     hashWithSerialiser,
   )
@@ -135,7 +135,7 @@ mkSeedFromWords ::
   (Word64, Word64, Word64, Word64, Word64) ->
   Seed
 mkSeedFromWords stuff =
-  mkSeedFromBytes . hashToBytes $ hashWithSerialiser @MD5 toCBOR stuff
+  mkSeedFromBytes . hashToBytes $ hashWithSerialiser @Blake2b_256 toCBOR stuff
 
 -- | For testing purposes, generate a deterministic genesis key pair given a seed.
 mkGenKey :: DSIGNAlgorithm (DSIGN crypto) => (Word64, Word64, Word64, Word64, Word64) -> (SignKeyDSIGN (DSIGN crypto), VKey kd crypto)
