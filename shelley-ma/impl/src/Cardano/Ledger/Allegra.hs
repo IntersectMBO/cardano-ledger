267545a6f924d6b698a6d90f0fe658821474b579
Nicholas Clarke <nicholas.clarke@iohk.io>
2020-10-28 09:39:48 +0100

    Mary and Allegra top-level definitions.
    This should allow these to be used by the consensus layer.

--- shelley-ma/impl/src/Cardano/Ledger/Allegra.hs
+++ shelley-ma/impl/src/Cardano/Ledger/Allegra.hs
@@ -1,7 +1,34 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cardano.Ledger.Allegra where
 
+import Cardano.Crypto.Hash (Hash)
+import Cardano.Ledger.Crypto (Crypto, HASH)
 import Cardano.Ledger.ShelleyMA
+import Cardano.Ledger.ShelleyMA.Rules.Utxo ()
+import Cardano.Ledger.ShelleyMA.Rules.Utxow ()
+import Cardano.Ledger.ShelleyMA.Scripts ()
+import Cardano.Ledger.ShelleyMA.TxBody ()
+import Shelley.Spec.Ledger.API (ApplyBlock, ApplyTx, GetLedgerView)
+import Shelley.Spec.Ledger.Hashing (EraIndependentTxBody)
+import Shelley.Spec.Ledger.Keys (DSignable)
 
 type AllegraEra = ShelleyMAEra 'Allegra
+
+instance
+  ( Crypto c,
+    DSignable c (Hash (HASH c) EraIndependentTxBody)
+  ) =>
+  ApplyTx (AllegraEra c)
+
+instance
+  ( Crypto c,
+    DSignable c (Hash (HASH c) EraIndependentTxBody)
+  ) =>
+  ApplyBlock (AllegraEra c)
+
+instance (Crypto c) => GetLedgerView (AllegraEra c)
--- shelley-ma/impl/src/Cardano/Ledger/Mary.hs
+++ shelley-ma/impl/src/Cardano/Ledger/Mary.hs
@@ -1,7 +1,34 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cardano.Ledger.Mary where
 
+import Cardano.Crypto.Hash (Hash)
+import Cardano.Ledger.Crypto (Crypto, HASH)
 import Cardano.Ledger.ShelleyMA
+import Cardano.Ledger.ShelleyMA.Rules.Utxo ()
+import Cardano.Ledger.ShelleyMA.Rules.Utxow ()
+import Cardano.Ledger.ShelleyMA.Scripts ()
+import Cardano.Ledger.ShelleyMA.TxBody ()
+import Shelley.Spec.Ledger.API (ApplyBlock, ApplyTx, GetLedgerView)
+import Shelley.Spec.Ledger.Hashing (EraIndependentTxBody)
+import Shelley.Spec.Ledger.Keys (DSignable)
 
 type MaryEra = ShelleyMAEra 'Mary
+
+instance
+  ( Crypto c,
+    DSignable c (Hash (HASH c) EraIndependentTxBody)
+  ) =>
+  ApplyTx (MaryEra c)
+
+instance
+  ( Crypto c,
+    DSignable c (Hash (HASH c) EraIndependentTxBody)
+  ) =>
+  ApplyBlock (MaryEra c)
+
+instance (Crypto c) => GetLedgerView (MaryEra c)
