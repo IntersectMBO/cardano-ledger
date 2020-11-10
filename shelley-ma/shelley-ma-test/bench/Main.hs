1743f58c28c00c37ca3bd011c279b92e1fc2e313
Tim Sheard <tim.sheard@IOHK.io>
2020-11-10 10:38:42 -0800

    Added the skeleton benchmark Main.hs

---
+++ shelley-ma/shelley-ma-test/bench/Main.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+-- | This benchmark file is a placholder for benchmarks
+module Main where
+
+import Criterion.Main
+  ( -- bench, bgroup, nf,
+    defaultMain,
+  )
+
+main :: IO ()
+main = defaultMain []
