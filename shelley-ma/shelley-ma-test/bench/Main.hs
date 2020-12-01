985841aa7abbd39aa2acd8eeef0109ebe795c230
Nicholas Clarke <nicholas.clarke@iohk.io>
2020-12-01 12:12:42 +0100

    Add benchmarks for generation of transactions.
    Aim is to debug slow transaction round-trip test performance in
    consensus.

--- shelley-ma/shelley-ma-test/bench/Main.hs
+++ shelley-ma/shelley-ma-test/bench/Main.hs
@@ -4,10 +4,11 @@
 -- | This benchmark file is a placholder for benchmarks
 module Main where
 
+import qualified Bench.Cardano.Ledger.ShelleyMA.Serialisation.Generators as SerGen
 import Criterion.Main
   ( -- bench, bgroup, nf,
     defaultMain,
   )
 
 main :: IO ()
-main = defaultMain []
+main = defaultMain [SerGen.benchTxGeneration]
