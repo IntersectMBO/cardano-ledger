488c6d85f738649a36f6368d5f7397ea9c105d1f
Nicholas Clarke <nicholas.clarke@iohk.io>
2020-12-09 13:54:06 +0100

    Use 'Arbitrary' instances for roundtrip tests.
    There are two principal benefits to doing this:
    - We ensure we are using the same composable generators which will be
      used for higher level tests. So we should see the serialisation
      failures at the lowest level possible.
    - We get access to shrinkers, which makes it much easier to debug
      problems. For example, if I revert the fix from #2049, I now get the
      following error:
    
      ```
              Value:        FAIL
              *** Failed! Falsified (after 81 tests and 43 shrinks):
              Value 0 (fromList [(PolicyID {policyID = ScriptHash "e536a96e3101b673e62546f4f694256798af84a603b28242dfa379f4"},fromList [("\US\156\206\187\EOT\217\1
    67\205i\157\187\161\225}\252\235\218\181\225\171W\255*\ENQ{\194\231.S\174\176\&1\248i+\206\223Z\200m\ETB\163l0\217\DC490\157:u\215!\220Z\160u\161\179\207\DC2\b
    \190)\ETXo\211)\214\175\186",9223372036854775808)])])
              Failed to decode: DeserialiseFailure 107 "expected int"
      ```
    
      Before this change, the generated Value was not shrunk and was a page
      long when printed, which made finding the actual problem very
      difficult!
    
    Since we are now using `Arbitrary` generators for these tests, the
    `EraIndex` becomes redundant. It may still be useful for property-based
    testing where we do have custom generators/functionality depending on
    the era.

--- shelley-ma/shelley-ma-test/src/Test/Cardano/Ledger/ShelleyMA/Serialisation/Roundtrip.hs
+++ shelley-ma/shelley-ma-test/src/Test/Cardano/Ledger/ShelleyMA/Serialisation/Roundtrip.hs
@@ -22,9 +22,12 @@ module Test.Cardano.Ledger.ShelleyMA.Serialisation.Roundtrip where
 
 import Cardano.Binary (Annotator (..), FromCBOR, ToCBOR)
 import Cardano.Ledger.Core (SerialisableData)
+import qualified Cardano.Ledger.Core as Core
 import Cardano.Ledger.Shelley.Constraints (ShelleyBased)
 import Control.State.Transition
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Proxy (Proxy (Proxy))
+import Data.Typeable (typeRep)
 import Shelley.Spec.Ledger.API (ApplyTxError, UTXOW)
 import Test.Cardano.Ledger.EraBuffet
 import Test.Cardano.Ledger.ShelleyMA.Serialisation.Coders
@@ -32,84 +35,43 @@ import Test.Cardano.Ledger.ShelleyMA.Serialisation.Coders
     roundTripAnn,
   )
 import Test.Cardano.Ledger.ShelleyMA.Serialisation.Generators ()
-import Test.QuickCheck (counterexample, forAll, (===))
+import Test.QuickCheck (Arbitrary, Property, counterexample, (===))
 import Test.Shelley.Spec.Ledger.Generator.Metadata ()
 import Test.Shelley.Spec.Ledger.Serialisation.Generators ()
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.QuickCheck (Gen, arbitrary, testProperty)
-
--- ======================================================================
--- Witnesses to each Era
-
-data EraIndex index where
-  Mary :: EraIndex (MaryEra StandardCrypto)
-  Shelley :: EraIndex (ShelleyEra StandardCrypto)
-  Allegra :: EraIndex (AllegraEra StandardCrypto)
-
-instance Show (EraIndex e) where
-  show Mary = "Mary Era"
-  show Shelley = "Shelley Era"
-  show Allegra = "Allegra Era"
-
--- ============================================================
--- EraIndex parameterized generators for each type family
-
-genTxBody :: EraIndex e -> Gen (TxBody e)
-genTxBody Shelley = arbitrary
-genTxBody Mary = arbitrary
-genTxBody Allegra = arbitrary
-
-genScript :: EraIndex e -> Gen (Script e)
-genScript Shelley = arbitrary
-genScript Mary = arbitrary
-genScript Allegra = arbitrary
-
-genValue :: EraIndex e -> Gen (Value e)
-genValue Shelley = arbitrary
-genValue Mary = arbitrary
-genValue Allegra = arbitrary
-
-genMeta :: EraIndex e -> Gen (Metadata e)
-genMeta Mary = arbitrary
-genMeta Allegra = arbitrary
-genMeta Shelley = arbitrary
-
-genApplyTxError :: EraIndex e -> Gen (ApplyTxError e)
-genApplyTxError Shelley = arbitrary
-genApplyTxError Mary = arbitrary
-genApplyTxError Allegra = arbitrary
-
--- ==========================================================
--- EraIndex parameterized property tests
+import Test.Tasty.QuickCheck (testProperty)
 
 propertyAnn ::
-  forall e t.
-  (Eq t, Show t, ToCBOR t, FromCBOR (Annotator t)) =>
-  String ->
-  EraIndex e ->
-  (EraIndex e -> Gen t) ->
-  TestTree
-propertyAnn name i gen = testProperty ("roundtripAnn " ++ name) $ do
-  forAll (gen i) $ \x -> case roundTripAnn x of
-    Right (remaining, y) | BSL.null remaining -> x === y
-    Right (remaining, _) ->
-      counterexample
-        ("Unconsumed trailing bytes:\n" <> BSL.unpack remaining)
-        False
-    Left stuff ->
-      counterexample
-        ("Failed to decode: " <> show stuff)
-        False
+  forall t.
+  ( Eq t,
+    Show t,
+    ToCBOR t,
+    FromCBOR (Annotator t)
+  ) =>
+  t ->
+  Property
+propertyAnn x = case roundTripAnn x of
+  Right (remaining, y) | BSL.null remaining -> x === y
+  Right (remaining, _) ->
+    counterexample
+      ("Unconsumed trailing bytes:\n" <> BSL.unpack remaining)
+      False
+  Left stuff ->
+    counterexample
+      ("Failed to decode: " <> show stuff)
+      False
 
 property ::
-  forall e t.
-  (Eq t, Show t, ToCBOR t, FromCBOR t) =>
-  String ->
-  EraIndex e ->
-  (EraIndex e -> Gen t) ->
-  TestTree
-property name i gen = testProperty ("roundtrip " ++ name) $
-  forAll (gen i) $ \x -> case roundTrip x of
+  forall t.
+  ( Eq t,
+    Show t,
+    ToCBOR t,
+    FromCBOR t
+  ) =>
+  t ->
+  Property
+property x =
+  case roundTrip x of
     Right (remaining, y) | BSL.null remaining -> x === y
     Right (remaining, _) ->
       counterexample
@@ -121,25 +83,33 @@ property name i gen = testProperty ("roundtrip " ++ name) $
         False
 
 allprops ::
+  forall e.
   ( ShelleyBased e,
+    Arbitrary (Core.TxBody e),
+    Arbitrary (Core.Metadata e),
+    Arbitrary (Core.Value e),
+    Arbitrary (Core.Script e),
+    Arbitrary (ApplyTxError e),
     SerialisableData (ApplyTxError e),
     Eq (PredicateFailure (UTXOW e)),
     Show (PredicateFailure (UTXOW e))
   ) =>
-  EraIndex e ->
   TestTree
-allprops index =
+allprops =
   testGroup
-    (show index)
-    [ propertyAnn "TxBody" index genTxBody,
-      propertyAnn "Metadata" index genMeta,
-      property "Value" index genValue,
-      propertyAnn "Script" index genScript,
-      property "ApplyTxError" index genApplyTxError
+    (show $ typeRep (Proxy @e))
+    [ testProperty "TxBody" $ propertyAnn @(Core.TxBody e),
+      testProperty "Metadata" $ propertyAnn @(Core.Metadata e),
+      testProperty "Value" $ property @(Core.Value e),
+      testProperty "Script" $ propertyAnn @(Core.Script e),
+      testProperty "ApplyTxError" $ property @(ApplyTxError e)
     ]
 
 allEraRoundtripTests :: TestTree
 allEraRoundtripTests =
   testGroup
     "All Era Roundtrip Tests"
-    [allprops Shelley, allprops Allegra, allprops Mary]
+    [ allprops @(ShelleyEra StandardCrypto),
+      allprops @(AllegraEra StandardCrypto),
+      allprops @(MaryEra StandardCrypto)
+    ]
