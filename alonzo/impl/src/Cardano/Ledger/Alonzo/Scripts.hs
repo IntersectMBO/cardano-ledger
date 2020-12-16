f25b20f7feabd9ba60840ffefac105d78fc326a6
Polina Vinogradova <polina.vino@gmail.com>
2020-12-16 00:36:44 -0500

    add new alonzo params

--- alonzo/impl/src/Cardano/Ledger/Alonzo/Scripts.hs
+++ alonzo/impl/src/Cardano/Ledger/Alonzo/Scripts.hs
@@ -1,13 +1,20 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Cardano.Ledger.Alonzo.Scripts
   ( Tag (..),
     Script,
     ExUnits (..),
+    CostModel,
+    Language,
+    Prices (..)
   )
 where
 
+import Data.Map (Map)
+import Data.ByteString (ByteString)
 import Cardano.Binary (FromCBOR (fromCBOR), ToCBOR (toCBOR))
 import Cardano.Ledger.Era (Era (Crypto))
 import Cardano.Ledger.ShelleyMA.Timelocks
@@ -15,6 +22,8 @@ import Data.Coders
 import Data.Word (Word64)
 import GHC.Generics (Generic)
 import NoThunks.Class (NoThunks)
+import Control.DeepSeq (NFData (..))
+import Shelley.Spec.Ledger.Coin (Coin (..))
 
 -- | Marker indicating the part of a transaction for which this script is acting
 -- as a validator.
@@ -45,12 +54,41 @@ data ExUnits = ExUnits
   deriving (Eq, Generic, Show, Ord)
 
 instance NoThunks ExUnits
+instance NFData ExUnits
 
 instance Semigroup ExUnits where
   ExUnits a c <> ExUnits b d = ExUnits (a + b) (c + d)
 
 instance Monoid ExUnits where
   mempty = ExUnits 0 0
+
+-- Script language
+newtype Language = Language ByteString
+  deriving (Eq, Generic, Show, Ord)
+
+instance NoThunks Language
+instance NFData Language
+deriving instance ToCBOR Language
+deriving instance FromCBOR Language
+
+-- Cost Model
+newtype CostModel = CostModel (Map ByteString Integer)
+  deriving (Eq, Generic, Show, Ord)
+
+instance NoThunks CostModel
+instance NFData CostModel
+deriving instance ToCBOR CostModel
+deriving instance FromCBOR CostModel
+
+-- | Prices per execution unit
+data Prices = Prices
+  { prMem  :: !Coin,
+    prSteps :: !Coin
+  }
+  deriving (Eq, Generic, Show, Ord)
+
+instance NoThunks Prices
+instance NFData Prices
 
 --------------------------------------------------------------------------------
 -- Serialisation
@@ -78,3 +116,9 @@ instance ToCBOR ExUnits where
 
 instance FromCBOR ExUnits where
   fromCBOR = decode $ RecD ExUnits <! From <! From
+
+instance ToCBOR Prices where
+  toCBOR (Prices m s) = encode $ Rec Prices !> To m !> To s
+
+instance FromCBOR Prices where
+  fromCBOR = decode $ RecD Prices <! From <! From
