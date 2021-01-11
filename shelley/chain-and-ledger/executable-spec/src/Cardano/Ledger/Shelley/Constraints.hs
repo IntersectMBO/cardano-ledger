0636ad491c2e6a115ff61ed0acf44f3a6a2224c6
Nicholas Clarke <nicholas.clarke@iohk.io>
2021-01-11 19:28:52 +0100

    Add 'compactAddress' field to UsesTxOut.
    This is used by some of the ledger queries.

--- shelley/chain-and-ledger/executable-spec/src/Cardano/Ledger/Shelley/Constraints.hs
+++ shelley/chain-and-ledger/executable-spec/src/Cardano/Ledger/Shelley/Constraints.hs
@@ -26,6 +26,7 @@ import Data.Kind (Constraint, Type)
 import Data.Proxy (Proxy)
 import GHC.Records (HasField)
 import Shelley.Spec.Ledger.Address (Addr)
+import Shelley.Spec.Ledger.CompactAddr (CompactAddr)
 import Shelley.Spec.Ledger.Hashing
   ( EraIndependentTxBody,
     HashAnnotated (..),
@@ -66,6 +67,7 @@ class
     ToCBOR (TxOut era),
     FromCBOR (TxOut era),
     HasField "address" (TxOut era) (Addr (Crypto era)),
+    HasField "compactAddress" (TxOut era) (CompactAddr (Crypto era)),
     HasField "value" (TxOut era) (Value era)
   ) =>
   UsesTxOut era
