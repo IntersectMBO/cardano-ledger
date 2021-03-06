8491abf000d793790f3903345ce4864e83e0c182
Tim Sheard <tim.sheard@IOHK.io>
2021-03-05 19:18:45 -0800

    ormolise

--- alonzo/impl/test/test/Test/Cardano/Ledger/Alonzo/DescribeEra.hs
+++ alonzo/impl/test/test/Test/Cardano/Ledger/Alonzo/DescribeEra.hs
@@ -1,24 +1,22 @@
+module Test.Cardano.Ledger.Alonzo.DescribeEra (describeTest) where
 
-
-module Test.Cardano.Ledger.Alonzo.DescribeEra
-  ( describeTest ) where
-
-import Cardano.Ledger.DescribeEras(Checks(..),Witness(..),Evidence(..))
+import Cardano.Ledger.DescribeEras (Checks (..), Evidence (..), Witness (..))
 import Test.Tasty
 import Test.Tasty.HUnit (assertBool, testCase)
-
 
 describes :: Checks era => Witness era -> TestTree
 describes w = testCase (show w) (assertBool "Does not meets its description" (checks w))
 
 describeTest :: TestTree
-describeTest = testGroup "Era Self-Description Tests by Era"
-   [ describes (Shelley Test)
-   , describes (Allegra Test)
-   , describes (Mary Test)
-   , describes (Alonzo Test)
-   , describes (Shelley Standard)
-   , describes (Allegra Standard)
-   , describes (Mary Standard)
-   , describes (Alonzo Standard)
-   ]
+describeTest =
+  testGroup
+    "Era Self-Description Tests by Era"
+    [ describes (Shelley Test),
+      describes (Allegra Test),
+      describes (Mary Test),
+      describes (Alonzo Test),
+      describes (Shelley Standard),
+      describes (Allegra Standard),
+      describes (Mary Standard),
+      describes (Alonzo Standard)
+    ]
