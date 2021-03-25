cf4a83fd0bb57ebc2eef1e8d1fe8101c9be864d4
John Ky <john.ky@iohk.io>
2021-03-26 08:56:32 +1100

    Enable doctests.  Disable other tests

--- byron/ledger/executable-spec/test/DoctestDiscover.hs
+++ byron/ledger/executable-spec/test/DoctestDiscover.hs
@@ -1,12 +1,1 @@
-{-# LANGUAGE CPP #-}
-
-#if !MIN_VERSION_GLASGOW_HASKELL(8,10,0,0) || MIN_VERSION_GLASGOW_HASKELL(8,11,0,0)
 {-# OPTIONS_GHC -F -pgmF doctest-discover #-}
-#else
-module Main where
-
-import qualified System.IO as IO
-
-main :: IO ()
-main = IO.putStrLn "WARNING: doctest will not run on GHC 8.10.x.\nSee https://gitlab.haskell.org/ghc/ghc/-/issues/19421"
-#endif
