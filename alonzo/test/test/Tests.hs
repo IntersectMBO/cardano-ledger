0036187171c19a859a736c917d85343cbafdc295
Tim Sheard <tim.sheard@IOHK.io>
2021-05-10 19:35:39 -0700

    Added all the missing property tests to Alonzo
    Alonzo tests now run all the property tests.
    Cleaned up the code.
    Added scenarios to the alonzo tests.
    Ormolised.

--- alonzo/test/test/Tests.hs
+++ alonzo/test/test/Tests.hs
@@ -19,10 +19,26 @@ import Test.Cardano.Ledger.Alonzo.Golden as Golden
 import qualified Test.Cardano.Ledger.Alonzo.Serialisation.CDDL as CDDL
 import qualified Test.Cardano.Ledger.Alonzo.Serialisation.Tripping as Tripping
 import qualified Test.Cardano.Ledger.Alonzo.Translation as Translation
+import Test.Cardano.Ledger.Alonzo.Trials (testPropertyAdaPreservation)
 import Test.Cardano.Ledger.EraBuffet (TestCrypto)
 import Test.QuickCheck
 import Test.Shelley.Spec.Ledger.Generator.Constants (Constants (..))
+{-
+import GHC.Records(HasField(..))
+import Test.Shelley.Spec.Ledger.PropertyTests
+  ( adaPreservationChain,
+    -- propertyTests,
+  )
+import Cardano.Ledger.Alonzo.PParams(PParams' (..))
+import Test.Cardano.Ledger.Alonzo.AlonzoEraGen ()
+import Test.Shelley.Spec.Ledger.Generator.Trace.Chain
+import Test.Shelley.Spec.Ledger.Generator.Trace.Chain
+import Test.Shelley.Spec.Ledger.Generator.Trace.Ledger
+import Test.Shelley.Spec.Ledger.Generator.Utxo
+-}
+
 import Test.Tasty
+import Test.Tasty.QuickCheck
 
 -- ====================================================================================
 
@@ -30,7 +46,9 @@ tests :: TestTree
 tests =
   testGroup
     "Alonzo tests"
-    [ Tripping.tests,
+    [ -- testProperty "Property test ada preserved" (adaPreservationChain @(AlonzoEra TestCrypto)),
+      testPropertyAdaPreservation,
+      Tripping.tests,
       Translation.tests,
       CDDL.tests 5,
       Golden.goldenUTxOEntryMinAda,
--- shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Generator/Trace/Ledger.hs
+++ shelley/chain-and-ledger/shelley-spec-ledger-test/src/Test/Shelley/Spec/Ledger/Generator/Trace/Ledger.hs
@@ -71,7 +71,7 @@ import Test.Shelley.Spec.Ledger.Utils
     runShelleyBase,
   )
 import Cardano.Ledger.Era(SupportsSegWit(TxInBlock))
-
+import Debug.Trace
 -- ======================================================
 
 genAccountState :: Constants -> Gen AccountState
@@ -179,7 +179,7 @@ instance
                     (TRC (ledgerEnv, (u, dp), tx))
           pure $ case res of
             Left pf ->
-              error ("LEDGERS sigGen: " <> show pf)
+              trace ("\nPF = "++show pf) (error ("LEDGER sigGen: " <> show pf))
             Right (u', dp') ->
               (u', dp', tx : txs)
 
