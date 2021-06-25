3f6394adfe55816013bbc342dc81469261c1b644
Jared Corduan <jared.corduan@iohk.io>
2021-06-25 17:43:27 -0400

    Restrict poolmetadata hash starting at PV 5

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/SoftForks.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/SoftForks.hs
@@ -4,6 +4,7 @@
 
 module Shelley.Spec.Ledger.SoftForks
   ( validMetadata,
+    restrictPoolMetadataHash,
   )
 where
 
@@ -15,3 +16,9 @@ validMetadata ::
   pp ->
   Bool
 validMetadata pp = getField @"_protocolVersion" pp > ProtVer 2 0
+
+restrictPoolMetadataHash ::
+  (HasField "_protocolVersion" pp ProtVer) =>
+  pp ->
+  Bool
+restrictPoolMetadataHash pp = getField @"_protocolVersion" pp > ProtVer 4 0
