47c4595c76d28f2699b41ab2961ecb66cc821a00
Madeline Haraj <madeline.haraj@obsidian.systems>
2021-07-09 14:49:27 -0400

    WIP: Generate transactions from the action graph

--- cardano-ledger-test/src/Test/Cardano/Ledger/DependGraph.hs
+++ cardano-ledger-test/src/Test/Cardano/Ledger/DependGraph.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -13,9 +14,11 @@ import Control.Lens hiding (elements)
 import Control.Monad
 import Control.Monad.State
 import Control.Monad.Supply
+import Data.Foldable
 import qualified Data.Graph.Inductive as FGL
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Semigroup
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Set.NonEmpty (NESet)
@@ -23,6 +26,7 @@ import qualified Data.Set.NonEmpty as NES
 import QuickCheck.GenT
 import Test.Cardano.Ledger.ModelChain
 import Test.Cardano.Ledger.ModelChain.Address
+import Test.Cardano.Ledger.ModelChain.Value
 import Test.QuickCheck hiding (choose, elements, frequency, oneof, resize, shuffle, sized, variant)
 
 -- TODO: Handle anti-dependencies. e.g. retiring a pool precludes delegating to it
@@ -100,16 +104,18 @@ outputTo aId minOutputs maxOutputs spenders = do
   -- First decide how many and which actions to enable
   numOutputs <- choose (min minOutputs 0, max maxOutputs (Set.size spenders))
   chosenSpenders <- take numOutputs <$> shuffle (Set.toList spenders)
-  forM_ chosenSpenders $ \i ->
+  forM_ chosenSpenders $ \i -> do
     actionState_withDeps . at i . _Just . _3 . at aId .= Just Enables_Spending
-  -- Then, decide how many of those spenders should be removed from needing inputs
-  numDeleted <-
-    frequency
-      [ (4, pure 0),
-        (1, choose (0, numOutputs))
-      ]
-  chosenDeleted <- take numDeleted <$> shuffle chosenSpenders
-  actionState_needsInputs %= (`Set.difference` (Set.fromList chosenDeleted))
+    mdeps <- fmap (view _3) <$> use (actionState_withDeps . at i)
+    -- Decide whether or not this dependent is satisfied
+    case mdeps of
+      Nothing -> error "outputTo: impossible empty deps"
+      Just deps -> do
+        let numInputs = Map.size $ Map.filter (== Enables_Spending) deps
+        r <- choose (minInputs, maxInputs)
+        case r <= numInputs of
+          True -> actionState_needsInputs %= (Set.delete i)
+          False -> pure ()
 
 makeWithdrawal :: (MonadGen m, MonadState ActionState m) => Int -> Int -> Set Int -> m ()
 makeWithdrawal aId epoch withdrawals = do
@@ -119,6 +125,105 @@ makeWithdrawal aId epoch withdrawals = do
       wId <- elements (Set.toList withdrawals)
       actionState_withDeps . at wId . _Just . _3 . at aId .= Just Enables_Withdrawing
       actionState_needsDelegate . at epoch . _Just %= Set.delete wId
+
+-- TODO: Some of these actions should not be passed more than one dependent.
+chooseEnablement :: (MonadState ActionState m, MonadGen m) => Set Int -> Int -> Int -> Action -> m ()
+chooseEnablement targets _ i = \case
+  Action_Withdraw -> outputTo i 1 1 targets
+  Action_Delegate -> do
+    forM_ (Set.toList targets) $ \target -> fmap (view _1) <$> (use (actionState_withDeps . at target)) >>= \case
+      Nothing -> pure ()
+      Just targetEpoch -> makeWithdrawal i targetEpoch (Set.singleton target)
+  Action_Spend -> outputTo i (Set.size targets) (Set.size targets) targets
+  Action_RegisterPool -> do
+    forM_ (Set.toList targets) $ \target ->
+      actionState_withDeps . at target . _Just . _3 . at i .= Just Enables_PoolForDelegating
+    actionState_needsPool %= (`Set.difference` targets)
+  Action_RegisterStake -> do
+    forM_ (Set.toList targets) $ \target ->
+      actionState_withDeps . at target . _Just . _3 . at i .= Just Enables_StakeForDelegating
+    actionState_needsStake %= (`Set.difference` targets)
+
+randomEnablement :: (MonadState ActionState m, MonadGen m) => Int -> Int -> Action -> m ()
+randomEnablement epoch i = \case
+  Action_Withdraw -> do
+    -- Withdrawal produces an output that can be spent, so flip a coin if it's used
+    spenders <- use actionState_needsInputs
+    outputTo i 0 1 spenders
+  Action_Delegate -> do
+    -- Delegation can enable withdrawal of staking rewards
+    -- A particular delegation can only enable a withdrawal once per epoch
+    eligibleEpochs <- uses actionState_needsDelegate $ Map.filterWithKey (\e _ -> e >= epoch + 2)
+    iforM_ eligibleEpochs $ \e withdrawals ->
+      frequency
+        [ (1, pure ()),
+          (3, makeWithdrawal i e withdrawals)
+        ]
+  Action_Spend -> do
+    -- Spends create outputs which can then be used in later spends
+    spenders <- use actionState_needsInputs
+    outputTo i 0 4 spenders
+  Action_RegisterPool -> do
+    -- Delegations depend on pools
+    delegations <- use actionState_needsPool
+    numDelegations <- choose (0, 8)
+    poolDelegs <- take numDelegations <$> shuffle (Set.toList delegations)
+    forM_ poolDelegs $ \dId -> do
+      actionState_withDeps . at dId . _Just . _3 . at i .= Just Enables_PoolForDelegating
+    actionState_needsPool %= (`Set.difference` Set.fromList poolDelegs)
+  Action_RegisterStake -> do
+    -- Delegations depend on a staking certificate, which can only justify one
+    -- delegation.
+    delegations <- use actionState_needsStake
+    numDelegations <-
+      frequency
+        [ (1, pure 0),
+          (1 + 2 * Set.size delegations, pure 1)
+        ]
+    stakeDelegs <- take numDelegations <$> shuffle (Set.toList delegations)
+    forM_ stakeDelegs $ \dId -> do
+      actionState_withDeps . at dId . _Just . _3 . at i .= Just Enables_StakeForDelegating
+    actionState_needsStake %= (`Set.difference` Set.fromList stakeDelegs)
+
+addAction :: MonadState ActionState m => (Int -> Int -> Action -> m ()) -> Int -> Int -> Action -> m ()
+addAction enablement epoch i a = do
+  actionState_withDeps . at i .= Just (epoch, a, mempty)
+  -- Action specific logic. An action reports the dependencies it needs fulfilled
+  -- and also chooses to fulfill some dependencies that have been emitted by actions
+  -- that happen after it.
+  enablement epoch i a
+  case a of
+    Action_Withdraw -> do
+      -- Withdrawal can only occur if a delegation was made in the past
+      actionState_needsDelegate . at epoch %= Just . maybe (Set.singleton i) (Set.insert i)
+    Action_Delegate -> do
+      -- Delegation needs a staking certificate and registered pool
+      actionState_needsStake %= Set.insert i
+      actionState_needsPool %= Set.insert i
+    Action_Spend -> do
+      -- Spending requires inputs, which potentially could come from the genesis block
+      actionState_needsInputs %= Set.insert i
+    Action_RegisterPool -> pure ()
+    Action_RegisterStake -> pure ()
+
+fillDependencies :: (MonadState ActionState m, MonadSupply Int m, MonadGen m) => m ()
+fillDependencies = do
+  -- Delegations first
+  ds <- uses actionState_needsDelegate fold
+  forM_ (Set.toList ds) $ \i -> do
+    n <- supply
+    addAction (chooseEnablement (Set.singleton i)) 1 n Action_Delegate
+  -- Use one pool for all the hanging threads
+  do
+    n <- supply
+    ps <- use actionState_needsPool
+    addAction (chooseEnablement ps) 1 n Action_RegisterPool
+  -- One staking cert per delegation
+  ss <- use actionState_needsStake
+  forM_ (Set.toList ss) $ \i -> do
+    n <- supply
+    addAction (chooseEnablement (Set.singleton i)) 1 n Action_RegisterStake
+
 
 genEpoch :: (MonadGen m, MonadSupply Int m, MonadState ActionState m) => Int -> m ()
 genEpoch epoch = do
@@ -137,75 +242,159 @@ genEpoch epoch = do
           (if i > minActionsPerEpoch then 2 else 0, pure Action_Delegate),
           (24, pure Action_Spend)
         ]
-    actionState_withDeps . at i .= Just (epoch, a, mempty)
-    -- Action specific logic. An action reports the dependencies it needs fulfilled
-    -- and also chooses to fulfill some dependencies that have been emitted by actions
-    -- that happen after it.
-    case a of
-      Action_Withdraw -> do
-        -- Withdrawal produces an output that can be spent, so flip a coin if it's used
-        spenders <- use actionState_needsInputs
-        outputTo i 0 1 spenders
-
-        -- Withdrawal can only occur if a delegation was made in the past
-        actionState_needsDelegate . at epoch %= Just . maybe (Set.singleton i) (Set.insert i)
-      Action_Delegate -> do
-        -- Delegation can enable withdrawal of staking rewards
-        -- A particular delegation can only enable a withdrawal once per epoch
-        eligibleEpochs <- uses actionState_needsDelegate $ Map.filterWithKey (\e _ -> e >= epoch + 2)
-        iforM_ eligibleEpochs $ \e withdrawals ->
-          frequency
-            [ (1, pure ()),
-              (3, makeWithdrawal i e withdrawals)
-            ]
-
-        -- Delegation needs a staking certificate and registered pool
-        actionState_needsStake %= Set.insert i
-        actionState_needsPool %= Set.insert i
-      Action_Spend -> do
-        -- Spends create outputs which can then be used in later spends
-        spenders <- use actionState_needsInputs
-        outputTo i 0 4 spenders
-
-        -- Spending requires inputs, which potentially could come from the genesis block
-        actionState_needsInputs %= Set.insert i
-      Action_RegisterPool -> do
-        -- Delegations depend on pools
-        delegations <- use actionState_needsPool
-        numDelegations <- choose (0, 8)
-        poolDelegs <- take numDelegations <$> shuffle (Set.toList delegations)
-        forM_ poolDelegs $ \dId -> do
-          actionState_withDeps . at dId . _Just . _3 . at i .= Just Enables_PoolForDelegating
-        actionState_needsPool %= (`Set.difference` Set.fromList poolDelegs)
-      Action_RegisterStake -> do
-        -- Delegations depend on a staking certificate, which can only justify one
-        -- delegation.
-        delegations <- use actionState_needsStake
-        numDelegations <-
-          frequency
-            [ (1, pure 0),
-              (1 + 2 * Set.size delegations, pure 1)
-            ]
-        stakeDelegs <- take numDelegations <$> shuffle (Set.toList delegations)
-        forM_ stakeDelegs $ \dId -> do
-          actionState_withDeps . at dId . _Just . _3 . at i .= Just Enables_StakeForDelegating
-        actionState_needsStake %= (`Set.difference` Set.fromList stakeDelegs)
+    addAction randomEnablement epoch i a
   pure ()
 
-genGraph :: (MonadGen m, MonadSupply Int m, MonadState ActionState m) => m ()
-genGraph = do
+genActions :: (MonadGen m, MonadSupply Int m, MonadState ActionState m) => m ()
+genActions = do
   -- First figure out how many epochs we're aiming for.
   -- We need to do this here because delegation to a pool
   -- has to happen some number of epochs (2) before reward
   -- withdrawal.
   numEpochs <- choose (minEpochs, maxEpochs)
-  forM_ [numEpochs, numEpochs -1 .. 1] genEpoch
+  forM_ [numEpochs, numEpochs-1 .. 1] genEpoch
+  -- After random generation there might still be actions in the graph
+  -- that don't have their dependencies met. Fill those in now in the first epoch.
+  fillDependencies
   pure ()
 
-testGenGraph :: IO ActionState
-testGenGraph = do
-  s <- generate (execStateT genGraph initialActionState)
+testGenActions :: IO ActionState
+testGenActions = do
+  s <- generate (execStateT genActions initialActionState)
   pure $ s & actionState_freshNames .~ []
+
+minActions :: Int
+minActions = 1
+
+maxActions :: Int
+maxActions = 4
+
+minTxs :: Int
+minTxs = 1
+
+maxTxs :: Int
+maxTxs = 4
+
+data TransactionState era = TransactionState
+  { _transactionState_freshNames :: [Integer]
+  , _transactionState_livePaymentAddresses :: Set ModelAddress
+  , _transactionState_liveUtxos :: Map Int {- action id -} (Map ModelUTxOId (ModelTxOut era))
+  , _transactionState_txDependencies :: Map {- action id -} Int (Map ModelTxId (NESet Enables))
+  , _transactionState_genesisUtxo :: [(ModelUTxOId, ModelAddress, Coin)]
+  , _transactionState_transactionsInEpoch :: Map {- epoch -} Int (Map ModelTxId (ModelTx era))
+  }
+
+makeLenses ''TransactionState
+
+initialTransactionState :: TransactionState era
+initialTransactionState = TransactionState
+  { _transactionState_freshNames = [1..]
+  , _transactionState_livePaymentAddresses = mempty
+  , _transactionState_liveUtxos = mempty
+  , _transactionState_txDependencies = mempty
+  , _transactionState_genesisUtxo = mempty
+  , _transactionState_transactionsInEpoch = mempty
+  }
+
+freshTxId :: MonadSupply Integer m => m ModelTxId
+freshTxId = ModelTxId <$> supply
+
+freshUtxoId :: MonadSupply Integer m => m ModelUTxOId
+freshUtxoId = ModelUTxOId <$> supply
+
+freshPaymentAddress :: MonadSupply Integer m => m ModelAddress
+freshPaymentAddress = ModelAddress . ("pay_" <>) . show <$> supply
+
+freshPoolAddress :: MonadSupply Integer m => m ModelAddress
+freshPoolAddress = ModelAddress . ("pool_" <>) . show <$> supply
+
+genTransactions
+  :: ( MonadGen m
+     , MonadState (TransactionState era) m
+     , MonadSupply Integer m
+     )
+  => Set Int -> ActionGraph -> m ()
+genTransactions actionIds actionGraph = do
+  -- Here we exploit the fact that we generated our action graph in such a way that [1..]
+  -- is an antitone topological sort of the actions.
+  gr <- (\x y f -> foldlM f x y) (unActionGraph actionGraph) (Set.toDescList actionIds) $ \gr i -> do
+    let (mctx, gr') = FGL.match i gr
+    case mctx of
+      Nothing -> error "genTransactions: Passed invalid actionId"
+      Just (eIn, _, (epoch, action), eOut) -> do
+        case eIn of
+          [] -> pure ()
+          _:_ -> error "genTransactions: actionIds were not topsorted"
+        txDeps <- uses (transactionState_txDependencies . at i) (maybe mempty id)
+        myInputs <- uses (transactionState_liveUtxos . at i) (maybe mempty id)
+        -- TODO: Handle ModelValue properly
+        let Sum myTotalInput = flip foldMap myInputs $ \(ModelTxOut _ (ModelValue mv)) -> case mv of
+              ModelValue_Inject (Coin v) -> Sum v
+              _ -> Sum 0
+        case action of
+          -- Spends are the simplest. Since they represent an arbitrary exchange of inputs to outputs
+          -- each spend action is its own transaction. Other actions must be merged into
+          -- transactions with spends or require a genesis UTxO for fees
+          Action_Spend -> do
+            txId <- freshTxId
+            writeDependencies i txId eOut
+            let outs = [ aId | (Enables_Spending, aId) <- eOut ]
+            -- Generate UTxOs for my dependencies and then maybe a UTXO that lies inert
+            (leftoverValue, myUtxos) <- do
+              (lv, utxos) <- generateUtxosFor myTotalInput outs
+              case lv > 1 of
+                False -> pure (lv, utxos)
+                True -> do
+                  leftoverUtxo <- frequency
+                    [ (3, pure 0)
+                    , (1, choose (1, lv-1))
+                    ]
+                  case leftoverUtxo > 0 of
+                    False -> pure (lv, utxos)
+                    True -> do
+                      paddr <- generatePaymentAddress
+                      utxoId <- freshUtxoId
+                      pure $
+                        ( lv - leftoverUtxo
+                        , Map.insert utxoId (ModelTxOut paddr (ModelValue (ModelValue_Inject (Coin leftoverUtxo)))) utxos
+                        )
+            pure ()
+          Action_Withdraw -> pure ()
+          Action_Delegate -> pure ()
+          Action_RegisterStake -> pure ()
+          Action_RegisterPool -> pure ()
+        pure gr'
+  pure ()
+
+writeDependencies :: MonadState (TransactionState era) m => Int -> ModelTxId -> [(Enables, Int)] -> m ()
+writeDependencies aId txId eOut = do
+  let newDeps = Map.fromList $ flip fmap eOut $ \(enable, depId) -> (depId, Map.singleton txId (NES.singleton enable))
+  transactionState_txDependencies %= Map.unionWith (Map.unionWith (<>)) newDeps . Map.delete aId
+
+generatePaymentAddress
+  :: (MonadState (TransactionState era) m, MonadSupply Integer m, MonadGen m)
+  => m ModelAddress
+generatePaymentAddress = do
+  paddrs <- use transactionState_livePaymentAddresses
+  frequency $
+    [ (1, freshPaymentAddress >>= \paddr -> transactionState_livePaymentAddresses %= (Set.insert paddr) >> pure paddr)
+    , (Set.size paddrs, elements (Set.toList paddrs))
+    ]
+
+generateUtxosFor
+  :: (MonadState (TransactionState era) m, MonadSupply Integer m, MonadGen m)
+  => Integer
+  -> [Int] {- action ids -}
+  -> m (Integer, Map ModelUTxOId (ModelTxOut era))
+generateUtxosFor totalValue outs = do
+  (leftoverValue, newUtxos) <- (\x y f -> foldlM f x y) (totalValue, mempty) outs $ \(valueLeft, acc) aId -> do
+    utxoId <- freshUtxoId
+    amount <- choose (1, valueLeft `div` 4 + 1) -- TODO: Split up the value in a more sensible and robust way
+    paddr <- generatePaymentAddress
+    let newUtxo = Map.singleton aId (Map.singleton utxoId (ModelTxOut paddr (ModelValue (ModelValue_Inject (Coin amount)))))
+    pure (valueLeft - amount, Map.union newUtxo acc)
+  transactionState_liveUtxos %= Map.unionWith Map.union newUtxos
+  pure (leftoverValue, fold newUtxos)
 
 -- Orphans
 instance MonadFail Gen where
@@ -227,3 +416,13 @@ instance {-# OVERLAPPING #-} (Monad m, MonadFail m) => MonadSupply Int (StateT A
     x : _ <- use actionState_freshNames
     pure x
   exhausted = uses actionState_freshNames null
+
+instance {-# OVERLAPPING #-} (Monad m, MonadFail m) => MonadSupply Integer (StateT (TransactionState era) m) where
+  supply = do
+    x : xs <- use transactionState_freshNames
+    transactionState_freshNames .= xs
+    pure x
+  peek = do
+    x : _ <- use transactionState_freshNames
+    pure x
+  exhausted = uses transactionState_freshNames null
