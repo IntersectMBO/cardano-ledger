5bbd6a5a7fc9571f9a4f8b8e19a53ebcdf50ef47
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2021-08-24 03:33:20 +0300

    Potential work around a bug in Ord, which was fixed in #2419

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/Tx.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/Tx.hs
@@ -460,6 +460,12 @@ instance
   where
   fromCBOR = decodeWits
 
+newtype IgnoreSigOrd kr crypto = IgnoreSigOrd {unIgnoreSigOrd :: WitVKey kr crypto}
+  deriving (Eq)
+
+instance (Typeable kr, CC.Crypto crypto) => Ord (IgnoreSigOrd kr crypto) where
+  compare (IgnoreSigOrd w1) (IgnoreSigOrd w2) = compare (witKeyHash w1) (witKeyHash w2)
+
 decodeWits ::
   forall era s.
   ( FromCBOR (Annotator (Core.Script era)),
@@ -473,7 +479,13 @@ decodeWits = do
         decodeWord >>= \case
           0 ->
             decodeList fromCBOR >>= \x ->
-              pure (\ws -> ws {addrWits' = Set.fromList <$> sequence x})
+              pure
+                ( \ws ->
+                    ws
+                      { addrWits' =
+                          Set.map unIgnoreSigOrd . Set.fromList . fmap IgnoreSigOrd <$> sequence x
+                      }
+                )
           1 ->
             decodeList fromCBOR >>= \x ->
               pure (\ws -> ws {scriptWits' = keyBy (hashScript @era) <$> sequence x})
