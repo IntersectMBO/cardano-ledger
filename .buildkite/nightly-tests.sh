71a9c6059521a6dd4d55f7aaa9f670eedd5b4bfe
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2021-09-24 03:23:47 +0300

    Fix nightly builds

--- .buildkite/nightly-tests.sh
+++ .buildkite/nightly-tests.sh
@@ -3,14 +3,14 @@ set -e
 
 nix build -f `dirname $0`/.. haskellPackages.cardano-ledger-alonzo-test.components.tests.cardano-ledger-alonzo-test -o cardano-ledger-alonzo-test
 pushd eras/alonzo/test-suite/
-nix-shell ../../shell.nix --run \
-  "../../cardano-ledger-alonzo-test/bin/cardano-ledger-alonzo-test --scenario=Nightly"
+nix-shell ../../../shell.nix --run \
+  "../../../cardano-ledger-alonzo-test/bin/cardano-ledger-alonzo-test --scenario=Nightly"
 popd
 
 nix build -f `dirname $0`/.. haskellPackages.cardano-ledger-shelley-ma-test.components.tests.cardano-ledger-shelley-ma-test -o cardano-ledger-shelley-ma-test
 pushd eras/shelley-ma/test-suite/
-nix-shell ../../shell.nix --run \
-  "../../cardano-ledger-shelley-ma-test/bin/cardano-ledger-shelley-ma-test --scenario=Nightly"
+nix-shell ../../../shell.nix --run \
+  "../../../cardano-ledger-shelley-ma-test/bin/cardano-ledger-shelley-ma-test --scenario=Nightly"
 popd
 
 nix build -f `dirname $0`/.. haskellPackages.cardano-ledger-shelley-test.components.tests.cardano-ledger-shelley-test -o cardano-ledger-shelley-test
@@ -24,7 +24,7 @@ popd
 #../../../cardano-ledger-byron-test/bin/cardano-ledger-byron-test --scenario=QualityAssurance
 #popd
 #
-# Two bryon property tests are currently failing:
+# Two byron property tests are currently failing:
 #   1 ts_prop_mainnetEpochsValid
 #   2 prop_deserializeEpochs
 # See:
