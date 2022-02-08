95f0a8fcda55c494213ce1fe4b002e7843a607c6
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2022-02-08 16:39:18 +0300

    Fix nasty memory leak

--- libs/compact-map/src/Data/Compact/SplitMap.hs
+++ libs/compact-map/src/Data/Compact/SplitMap.hs
@@ -11,8 +11,8 @@ import Control.DeepSeq
 import Data.Compact.KeyMap (Key (..), KeyMap, PDoc, ppKeyMap, ppList, ppSexp)
 import qualified Data.Compact.KeyMap as KeyMap
 import qualified Data.Foldable as F
-import qualified Data.IntMap as IntMap
 import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IntMap
 import Data.Map (Map)
 import qualified Data.Map.Strict as Map
 import Data.Set (Set)
