d08db361abe7e090624b56579be6697264f3e343
Jared Corduan <jared.corduan@iohk.io>
2022-03-08 18:04:35 -0500

    txOutDataHash stop returning hashed inline datums

--- eras/babbage/impl/src/Cardano/Ledger/Babbage/TxBody.hs
+++ eras/babbage/impl/src/Cardano/Ledger/Babbage/TxBody.hs
@@ -981,12 +981,12 @@ txOutDataHash :: Era era => TxOut era -> Maybe (DataHash (Crypto era))
 txOutDataHash = \case
   TxOutCompact' {} -> Nothing
   TxOutCompactDH' _ _ dh -> Just dh
-  TxOutCompactDatum _ _ binaryData -> Just $! hashBinaryData binaryData
+  TxOutCompactDatum _ _ _ -> Nothing
   TxOutCompactRefScript _ _ datum _ ->
     case datum of
       NoDatum -> Nothing
       DatumHash dh -> Just dh
-      Datum binaryData -> Just $! hashBinaryData binaryData
+      Datum _ -> Nothing
   TxOut_AddrHash28_AdaOnly {} -> Nothing
   TxOut_AddrHash28_AdaOnly_DataHash32 _ _ _ dataHash32 -> decodeDataHash32 dataHash32
 
