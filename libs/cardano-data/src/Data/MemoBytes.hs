f3ab682784ed05d2ae25e7107615b9306e138e97
TimSheard <sheard@pdx.edu>
2022-06-27 16:19:37 -0400

    Haddock for Memobytes and SafeHash

--- libs/cardano-data/src/Data/MemoBytes.hs
+++ libs/cardano-data/src/Data/MemoBytes.hs
@@ -18,10 +18,11 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 -- | MemoBytes is an abstraction for a data type that encodes its own serialization.
---   The idea is to use a newtype around a MemoBytes non-memoizing version.
+--   The idea is to use a newtype around a MemoBytes applied to a non-memoizing type.
 --   For example:   newtype Foo = Foo (MemoBytes NonMemoizingFoo)
 --   This way all the instances for Foo (Eq,Show,Ord,ToCBOR,FromCBOR,NoThunks,Generic)
---   can be derived for free.
+--   can be derived for free. MemoBytes plays an important role in the 'SafeToHash' class
+--   introduced in the module 'Cardano.Ledger.SafeHash'
 module Data.MemoBytes
   ( MemoBytes (..),
     memoBytes,
@@ -54,6 +55,11 @@ import Prelude hiding (span)
 
 -- ========================================================================
 
+-- | Pair together a type @t@ and its serialization. Used to encode a type
+--   that is serialized over the network, and to remember the original bytes
+--   that were used to transmit it. Important since hashes are computed
+--   from the serialization of a type, and ToCBOR instances do not have unique
+--   serializations.
 data MemoBytes t = Memo {memotype :: !t, memobytes :: ShortByteString}
   deriving (NoThunks) via AllowThunksIn '["memobytes"] (MemoBytes t)
 
@@ -73,6 +79,7 @@ instance Eq (MemoBytes t) where (Memo _ x) == (Memo _ y) = x == y
 
 instance Show t => Show (MemoBytes t) where show (Memo y _) = show y
 
+-- | Turn a lazy bytestring into a short bytestring.
 shorten :: Lazy.ByteString -> ShortByteString
 shorten x = toShort (toStrict x)
 
@@ -80,15 +87,19 @@ shorten x = toShort (toStrict x)
 -- deriving via (Mem T) instance (Era era) => FromCBOR (Annotator T)
 type Mem t = Annotator (MemoBytes t)
 
+-- | Turn a MemoBytes into a string, Showing both its internal structure and its original bytes.
+--   Useful since the Show instance of MemoBytes does not display the original bytes.
 showMemo :: Show t => MemoBytes t -> String
 showMemo (Memo t b) = "(Memo " ++ show t ++ "  " ++ show b ++ ")"
 
 printMemo :: Show t => MemoBytes t -> IO ()
 printMemo x = putStrLn (showMemo x)
 
+-- | Create MemoBytes from its CBOR encoding
 memoBytes :: Encode w t -> MemoBytes t
 memoBytes t = Memo (runE t) (shorten (toLazyByteString (encode t)))
 
+-- | Try and deserialize a MemoBytes, and then reconstruct it. Useful in tests.
 roundTripMemo :: (FromCBOR t) => MemoBytes t -> Either Codec.CBOR.Read.DeserialiseFailure (Lazy.ByteString, MemoBytes t)
 roundTripMemo (Memo _t bytes) =
   case deserialiseFromBytes fromCBOR (fromStrict (fromShort bytes)) of
