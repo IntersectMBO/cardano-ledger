1d33317483c52cb0362ebc89735bce03f4637642
Joosep Jääger <joosep.jaager@iohk.io>
2022-08-09 16:36:31 +0300

    Added FromJSON instance to ConwayGenesis

--- eras/conway/impl/src/Cardano/Ledger/Conway/Genesis.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway/Genesis.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Cardano.Ledger.Conway.Genesis
   ( ConwayGenesis (..),
     extendPPWithGenesis,
@@ -6,5 +8,7 @@ where
 
 import Cardano.Ledger.Babbage.Genesis (extendPPWithGenesis)
 import Cardano.Ledger.Keys (GenDelegs)
+import Data.Aeson
 
 newtype ConwayGenesis crypto = ConwayGenesis (GenDelegs crypto)
+  deriving (FromJSON)
