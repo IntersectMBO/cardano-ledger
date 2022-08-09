96a5618d036d77ce153d6f9e9033aa70d853a0f9
Joosep Jääger <joosep.jaager@iohk.io>
2022-08-09 17:19:59 +0300

    Ormolize

--- eras/conway/impl/src/Cardano/Ledger/Conway/Genesis.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway/Genesis.hs
@@ -7,14 +7,14 @@ module Cardano.Ledger.Conway.Genesis
 where
 
 import Cardano.Ledger.Babbage.Genesis (extendPPWithGenesis)
+import Cardano.Ledger.Crypto (Crypto)
 import Cardano.Ledger.Keys (GenDelegs)
 import Data.Aeson (FromJSON (..), withObject, (.:))
 import Data.Unit.Strict (forceElemsToWHNF)
-import Cardano.Ledger.Crypto (Crypto)
 
 newtype ConwayGenesis crypto = ConwayGenesis (GenDelegs crypto)
 
 instance Crypto crypto => FromJSON (ConwayGenesis crypto) where
   parseJSON = withObject "ConwayGenesis" $ \obj ->
-    ConwayGenesis 
+    ConwayGenesis
       <$> forceElemsToWHNF obj .: "genDelegs"
