1a1e7dfcd8ccafa0dc530fd1625815c28b83b4ff
Joosep Jääger <joosep.jaager@iohk.io>
2022-08-09 17:17:45 +0300

    Implemented FromJSON manually

--- eras/conway/impl/src/Cardano/Ledger/Conway/Genesis.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway/Genesis.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Cardano.Ledger.Conway.Genesis
   ( ConwayGenesis (..),
@@ -8,7 +8,13 @@ where
 
 import Cardano.Ledger.Babbage.Genesis (extendPPWithGenesis)
 import Cardano.Ledger.Keys (GenDelegs)
-import Data.Aeson
+import Data.Aeson (FromJSON (..), withObject, (.:))
+import Data.Unit.Strict (forceElemsToWHNF)
+import Cardano.Ledger.Crypto (Crypto)
 
 newtype ConwayGenesis crypto = ConwayGenesis (GenDelegs crypto)
-  deriving (FromJSON)
+
+instance Crypto crypto => FromJSON (ConwayGenesis crypto) where
+  parseJSON = withObject "ConwayGenesis" $ \obj ->
+    ConwayGenesis 
+      <$> forceElemsToWHNF obj .: "genDelegs"
