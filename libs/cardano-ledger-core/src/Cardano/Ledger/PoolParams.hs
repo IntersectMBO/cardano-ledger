ec8b3c457a3161e48982533940ebef1cc65da0dc
TimSheard <sheard@pdx.edu>
2022-10-28 15:41:20 -0700

    moved Orphans PoolParams EpochBoundary files to core

--- libs/cardano-ledger-core/src/Cardano/Ledger/PoolParams.hs
+++ libs/cardano-ledger-core/src/Cardano/Ledger/PoolParams.hs
@@ -1,347 +1,231 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-module Cardano.Ledger.PoolParams
-  ( PoolParams (..),
-    PoolMetadata (..),
-    StakePoolRelay (..),
-    SizeOfPoolRelays (..),
-    SizeOfPoolOwners (..),
+-- |
+-- Module      : EpochBoundary
+-- Description : Functions and definitions for rules at epoch boundary.
+--
+-- This modules implements the necessary functions for the changes that can happen at epoch boundaries.
+module Cardano.Ledger.EpochBoundary
+  ( Stake (..),
+    sumAllStake,
+    sumStakePerPool,
+    SnapShot (..),
+    SnapShots (..),
+    emptySnapShot,
+    emptySnapShots,
+    poolStake,
+    obligation,
+    maxPool,
+    maxPool',
   )
 where
 
-import Cardano.Binary
-  ( Case (..),
-    FromCBOR (fromCBOR),
-    Size,
-    ToCBOR (..),
-    encodeListLen,
-    szCases,
+import Cardano.Binary (FromCBOR (..), ToCBOR (..), encodeListLen)
+import Cardano.Ledger.BaseTypes (BoundedRational (..), NonNegativeInterval)
+import Cardano.Ledger.Coin
+  ( Coin (..),
+    CompactForm (..),
+    coinToRational,
+    rationalToCoinViaFloor,
   )
-import Cardano.Ledger.Address (RewardAcnt (..))
-import Cardano.Ledger.BaseTypes
-  ( DnsName,
-    Port,
-    StrictMaybe (..),
-    UnitInterval,
-    Url,
-    invalidKey,
-    maybeToStrictMaybe,
-    strictMaybeToMaybe,
-  )
-import Cardano.Ledger.Coin (Coin (..))
-import qualified Cardano.Ledger.Crypto as CC
-import Cardano.Ledger.Keys
-  ( Hash,
-    KeyHash (..),
-    KeyRole (..),
-    VerKeyVRF,
-  )
-import Cardano.Ledger.Orphans ()
-import Cardano.Ledger.Serialization
-  ( CBORGroup (..),
-    CborSeq (..),
-    FromCBORGroup (..),
-    ToCBORGroup (..),
-    decodeNullMaybe,
-    decodeRecordNamed,
-    decodeRecordSum,
-    decodeSet,
-    decodeStrictSeq,
-    encodeFoldable,
-    encodeNullMaybe,
-    ipv4FromCBOR,
-    ipv4ToCBOR,
-    ipv6FromCBOR,
-    ipv6ToCBOR,
-  )
-import Control.DeepSeq (NFData ())
-import Data.Aeson (FromJSON (..), ToJSON (..), Value, (.!=), (.:), (.:?), (.=))
-import qualified Data.Aeson as Aeson
-import Data.Aeson.Types (Parser, explicitParseField)
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Base16 as B16
-import qualified Data.ByteString.Char8 as Char8
-import Data.Foldable (asum)
-import Data.IP (IPv4, IPv6)
-import Data.Proxy (Proxy (..))
-import Data.Sequence.Strict (StrictSeq)
-import qualified Data.Sequence.Strict as StrictSeq
-import Data.Set (Set)
-import qualified Data.Text.Encoding as Text
-import Data.Word (Word8)
+import Cardano.Ledger.Compactible
+import Cardano.Ledger.Credential (Credential)
+import qualified Cardano.Ledger.Crypto as CC (Crypto)
+import Cardano.Ledger.Keys (KeyHash, KeyRole (..))
+import Cardano.Ledger.PoolParams (PoolParams)
+import Cardano.Ledger.Serialization (decodeRecordNamedT)
+import Cardano.Ledger.Val ((<+>), (<×>))
+import Control.DeepSeq (NFData)
+import Control.Monad.Trans (lift)
+import Data.Default.Class (Default, def)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Ratio ((%))
+import Data.Sharing
+import Data.Typeable
+import Data.VMap as VMap
 import GHC.Generics (Generic)
+import GHC.Records (HasField, getField)
+import Lens.Micro (_1, _2)
 import NoThunks.Class (NoThunks (..))
+import Numeric.Natural (Natural)
 
--- ========================================================================
-
-data PoolMetadata = PoolMetadata
-  { _poolMDUrl :: !Url,
-    _poolMDHash :: !ByteString
+-- | Type of stake as map from hash key to coins associated.
+newtype Stake c = Stake
+  { unStake :: VMap VB VP (Credential 'Staking c) (CompactForm Coin)
   }
-  deriving (Eq, Ord, Generic, Show)
+  deriving (Show, Eq, NFData, Generic)
 
-deriving instance NFData PoolMetadata
+deriving newtype instance Typeable c => NoThunks (Stake c)
 
-instance ToJSON PoolMetadata where
-  toJSON pmd =
-    Aeson.object
-      [ "url" .= _poolMDUrl pmd,
-        "hash" .= (Text.decodeLatin1 . B16.encode) (_poolMDHash pmd)
-      ]
+deriving newtype instance
+  CC.Crypto c => ToCBOR (Stake c)
 
-instance FromJSON PoolMetadata where
-  parseJSON =
-    Aeson.withObject "PoolMetadata" $ \obj -> do
-      url <- obj .: "url"
-      hash <- explicitParseField parseJsonBase16 obj "hash"
-      return $ PoolMetadata url hash
+instance CC.Crypto c => FromSharedCBOR (Stake c) where
+  type Share (Stake c) = Share (VMap VB VP (Credential 'Staking c) (CompactForm Coin))
+  getShare = getShare . unStake
+  fromSharedCBOR = fmap Stake . fromSharedCBOR
 
-parseJsonBase16 :: Value -> Parser ByteString
-parseJsonBase16 v = do
-  s <- parseJSON v
-  case B16.decode (Char8.pack s) of
-    Right bs -> return bs
-    Left msg -> fail msg
+sumAllStake :: Stake c -> Coin
+sumAllStake = fromCompact . CompactCoin . VMap.foldl (\acc (CompactCoin c) -> acc + c) 0 . unStake
+{-# INLINE sumAllStake #-}
 
-instance NoThunks PoolMetadata
+-- | Get stake of one pool
+poolStake ::
+  KeyHash 'StakePool c ->
+  VMap VB VB (Credential 'Staking c) (KeyHash 'StakePool c) ->
+  Stake c ->
+  Stake c
+poolStake hk delegs (Stake stake) =
+  -- Stake $ (eval (dom (delegs ▷ setSingleton hk) ◁ stake))
+  Stake $ VMap.filter (\cred _ -> VMap.lookup cred delegs == Just hk) stake
 
-data StakePoolRelay
-  = -- | One or both of IPv4 & IPv6
-    SingleHostAddr !(StrictMaybe Port) !(StrictMaybe IPv4) !(StrictMaybe IPv6)
-  | -- | An @A@ or @AAAA@ DNS record
-    SingleHostName !(StrictMaybe Port) !DnsName
-  | -- | A @SRV@ DNS record
-    MultiHostName !DnsName
-  deriving (Eq, Ord, Generic, Show)
+sumStakePerPool ::
+  VMap VB VB (Credential 'Staking c) (KeyHash 'StakePool c) ->
+  Stake c ->
+  Map (KeyHash 'StakePool c) Coin
+sumStakePerPool delegs (Stake stake) = VMap.foldlWithKey accum Map.empty stake
+  where
+    accum !acc cred compactCoin =
+      case VMap.lookup cred delegs of
+        Nothing -> acc
+        Just kh -> Map.insertWith (<+>) kh (fromCompact compactCoin) acc
 
-instance FromJSON StakePoolRelay where
-  parseJSON =
-    Aeson.withObject "Credential" $ \obj ->
-      asum
-        [ explicitParseField parser1 obj "single host address",
-          explicitParseField parser2 obj "single host name",
-          explicitParseField parser3 obj "multi host name"
-        ]
-    where
-      parser1 = Aeson.withObject "SingleHostAddr" $ \obj ->
-        SingleHostAddr
-          <$> obj .:? "port" .!= SNothing
-          <*> obj .:? "IPv4" .!= SNothing
-          <*> obj .:? "IPv6" .!= SNothing
-      parser2 = Aeson.withObject "SingleHostName" $ \obj ->
-        SingleHostName
-          <$> obj .:? "port" .!= SNothing
-          <*> obj .: "dnsName"
-      parser3 = Aeson.withObject "MultiHostName" $ \obj ->
-        MultiHostName
-          <$> obj .: "dnsName"
+-- | Calculate total possible refunds.
+obligation ::
+  forall c pp t.
+  ( HasField "_keyDeposit" pp Coin,
+    HasField "_poolDeposit" pp Coin,
+    Foldable (t (Credential 'Staking c))
+  ) =>
+  pp ->
+  t (Credential 'Staking c) Coin ->
+  Map (KeyHash 'StakePool c) (PoolParams c) ->
+  Coin
+obligation pp rewards stakePools =
+  (length rewards <×> getField @"_keyDeposit" pp)
+    <+> (length stakePools <×> getField @"_poolDeposit" pp)
 
-instance ToJSON StakePoolRelay where
-  toJSON (SingleHostAddr port ipv4 ipv6) =
-    Aeson.object
-      [ "single host address"
-          .= Aeson.object
-            [ "port" .= port,
-              "IPv4" .= ipv4,
-              "IPv6" .= ipv6
-            ]
-      ]
-  toJSON (SingleHostName port dnsName) =
-    Aeson.object
-      [ "single host name"
-          .= Aeson.object
-            [ "port" .= port,
-              "dnsName" .= dnsName
-            ]
-      ]
-  toJSON (MultiHostName dnsName) =
-    Aeson.object
-      [ "multi host name"
-          .= Aeson.object
-            [ "dnsName" .= dnsName
-            ]
-      ]
+-- | Calculate maximal pool reward
+maxPool' ::
+  NonNegativeInterval ->
+  Natural ->
+  Coin ->
+  Rational ->
+  Rational ->
+  Coin
+maxPool' a0 nOpt r sigma pR = rationalToCoinViaFloor $ factor1 * factor2
+  where
+    z0 = 1 % fromIntegral nOpt
+    sigma' = min sigma z0
+    p' = min pR z0
+    factor1 = coinToRational r / (1 + unboundRational a0)
+    factor2 = sigma' + p' * unboundRational a0 * factor3
+    factor3 = (sigma' - p' * factor4) / z0
+    factor4 = (z0 - sigma') / z0
 
-instance NoThunks StakePoolRelay
+-- | Version of maxPool' that extracts a0 and nOpt from a PParam with the right HasField instances
+maxPool ::
+  (HasField "_a0" pp NonNegativeInterval, HasField "_nOpt" pp Natural) =>
+  pp ->
+  Coin ->
+  Rational ->
+  Rational ->
+  Coin
+maxPool pc r sigma pR = maxPool' a0 nOpt r sigma pR
+  where
+    a0 = getField @"_a0" pc
+    nOpt = getField @"_nOpt" pc
 
-instance NFData StakePoolRelay
+-- | Snapshot of the stake distribution.
+data SnapShot c = SnapShot
+  { _stake :: !(Stake c),
+    _delegations :: !(VMap VB VB (Credential 'Staking c) (KeyHash 'StakePool c)),
+    _poolParams :: !(VMap VB VB (KeyHash 'StakePool c) (PoolParams c))
+  }
+  deriving (Show, Eq, Generic)
 
-instance ToCBOR StakePoolRelay where
-  toCBOR (SingleHostAddr p ipv4 ipv6) =
+instance Typeable c => NoThunks (SnapShot c)
+
+instance NFData (SnapShot c)
+
+instance
+  CC.Crypto c =>
+  ToCBOR (SnapShot c)
+  where
+  toCBOR
+    SnapShot
+      { _stake = s,
+        _delegations = d,
+        _poolParams = p
+      } =
+      encodeListLen 3
+        <> toCBOR s
+        <> toCBOR d
+        <> toCBOR p
+
+instance CC.Crypto c => FromSharedCBOR (SnapShot c) where
+  type
+    Share (SnapShot c) =
+      (Interns (Credential 'Staking c), Interns (KeyHash 'StakePool c))
+  fromSharedPlusCBOR = decodeRecordNamedT "SnapShot" (const 3) $ do
+    _stake <- fromSharedPlusLensCBOR _1
+    _delegations <- fromSharedPlusCBOR
+    _poolParams <- fromSharedPlusLensCBOR (toMemptyLens _1 _2)
+    pure SnapShot {_stake, _delegations, _poolParams}
+
+-- | Snapshots of the stake distribution.
+data SnapShots c = SnapShots
+  { _pstakeMark :: SnapShot c, -- Lazy on purpose
+    _pstakeSet :: !(SnapShot c),
+    _pstakeGo :: !(SnapShot c),
+    _feeSS :: !Coin
+  }
+  deriving (Show, Eq, Generic)
+
+instance Typeable c => NoThunks (SnapShots c)
+
+instance NFData (SnapShots c)
+
+instance
+  CC.Crypto c =>
+  ToCBOR (SnapShots c)
+  where
+  toCBOR (SnapShots {_pstakeMark, _pstakeSet, _pstakeGo, _feeSS}) =
     encodeListLen 4
-      <> toCBOR (0 :: Word8)
-      <> encodeNullMaybe toCBOR (strictMaybeToMaybe p)
-      <> encodeNullMaybe ipv4ToCBOR (strictMaybeToMaybe ipv4)
-      <> encodeNullMaybe ipv6ToCBOR (strictMaybeToMaybe ipv6)
-  toCBOR (SingleHostName p n) =
-    encodeListLen 3
-      <> toCBOR (1 :: Word8)
-      <> encodeNullMaybe toCBOR (strictMaybeToMaybe p)
-      <> toCBOR n
-  toCBOR (MultiHostName n) =
-    encodeListLen 2
-      <> toCBOR (2 :: Word8)
-      <> toCBOR n
+      <> toCBOR _pstakeMark
+      <> toCBOR _pstakeSet
+      <> toCBOR _pstakeGo
+      <> toCBOR _feeSS
 
-instance FromCBOR StakePoolRelay where
-  fromCBOR = decodeRecordSum "StakePoolRelay" $
-    \case
-      0 ->
-        (\x y z -> (4, SingleHostAddr x y z))
-          <$> (maybeToStrictMaybe <$> decodeNullMaybe fromCBOR)
-          <*> (maybeToStrictMaybe <$> decodeNullMaybe ipv4FromCBOR)
-          <*> (maybeToStrictMaybe <$> decodeNullMaybe ipv6FromCBOR)
-      1 ->
-        (\x y -> (3, SingleHostName x y))
-          <$> (maybeToStrictMaybe <$> decodeNullMaybe fromCBOR)
-          <*> fromCBOR
-      2 -> do
-        x <- fromCBOR
-        pure (2, MultiHostName x)
-      k -> invalidKey k
+instance CC.Crypto c => FromSharedCBOR (SnapShots c) where
+  type Share (SnapShots c) = Share (SnapShot c)
+  fromSharedPlusCBOR = decodeRecordNamedT "SnapShots" (const 4) $ do
+    !_pstakeMark <- fromSharedPlusCBOR
+    _pstakeSet <- fromSharedPlusCBOR
+    _pstakeGo <- fromSharedPlusCBOR
+    _feeSS <- lift fromCBOR
+    pure SnapShots {_pstakeMark, _pstakeSet, _pstakeGo, _feeSS}
 
--- | A stake pool.
-data PoolParams c = PoolParams
-  { _poolId :: !(KeyHash 'StakePool c),
-    _poolVrf :: !(Hash c (VerKeyVRF c)),
-    _poolPledge :: !Coin,
-    _poolCost :: !Coin,
-    _poolMargin :: !UnitInterval,
-    _poolRAcnt :: !(RewardAcnt c),
-    _poolOwners :: !(Set (KeyHash 'Staking c)),
-    _poolRelays :: !(StrictSeq StakePoolRelay),
-    _poolMD :: !(StrictMaybe PoolMetadata)
-  }
-  deriving (Show, Generic, Eq, Ord)
-  deriving (ToCBOR) via CBORGroup (PoolParams c)
-  deriving (FromCBOR) via CBORGroup (PoolParams c)
+instance Default (SnapShots c) where
+  def = emptySnapShots
 
-instance NoThunks (PoolParams c)
+emptySnapShot :: SnapShot c
+emptySnapShot = SnapShot (Stake VMap.empty) VMap.empty VMap.empty
 
-deriving instance NFData (PoolParams c)
-
-instance CC.Crypto c => ToJSON (PoolParams c) where
-  toJSON pp =
-    Aeson.object
-      [ "publicKey" .= _poolId pp, -- TODO publicKey is an unfortunate name, should be poolId
-        "vrf" .= _poolVrf pp,
-        "pledge" .= _poolPledge pp,
-        "cost" .= _poolCost pp,
-        "margin" .= _poolMargin pp,
-        "rewardAccount" .= _poolRAcnt pp,
-        "owners" .= _poolOwners pp,
-        "relays" .= _poolRelays pp,
-        "metadata" .= _poolMD pp
-      ]
-
-instance CC.Crypto c => FromJSON (PoolParams c) where
-  parseJSON =
-    Aeson.withObject "PoolParams" $ \obj ->
-      PoolParams
-        <$> obj .: "publicKey" -- TODO publicKey is an unfortunate name, should be poolId
-        <*> obj .: "vrf"
-        <*> obj .: "pledge"
-        <*> obj .: "cost"
-        <*> obj .: "margin"
-        <*> obj .: "rewardAccount"
-        <*> obj .: "owners"
-        <*> obj .: "relays"
-        <*> obj .: "metadata"
-
-instance ToCBOR PoolMetadata where
-  toCBOR (PoolMetadata u h) =
-    encodeListLen 2
-      <> toCBOR u
-      <> toCBOR h
-
-instance FromCBOR PoolMetadata where
-  fromCBOR = do
-    decodeRecordNamed "PoolMetadata" (const 2) (PoolMetadata <$> fromCBOR <*> fromCBOR)
-
--- | The size of the '_poolOwners' 'Set'.  Only used to compute size of encoded
--- 'PoolParams'.
-data SizeOfPoolOwners = SizeOfPoolOwners
-
-instance ToCBOR SizeOfPoolOwners where
-  toCBOR = error "The `SizeOfPoolOwners` type cannot be encoded!"
-
--- | The size of the '_poolRelays' 'Set'.  Only used to compute size of encoded
--- 'PoolParams'.
-data SizeOfPoolRelays = SizeOfPoolRelays
-
-instance ToCBOR SizeOfPoolRelays where
-  toCBOR = error "The `SizeOfPoolRelays` type cannot be encoded!"
-
-instance
-  CC.Crypto c =>
-  ToCBORGroup (PoolParams c)
-  where
-  toCBORGroup poolParams =
-    toCBOR (_poolId poolParams)
-      <> toCBOR (_poolVrf poolParams)
-      <> toCBOR (_poolPledge poolParams)
-      <> toCBOR (_poolCost poolParams)
-      <> toCBOR (_poolMargin poolParams)
-      <> toCBOR (_poolRAcnt poolParams)
-      <> encodeFoldable (_poolOwners poolParams)
-      <> toCBOR (CborSeq (StrictSeq.fromStrict (_poolRelays poolParams)))
-      <> encodeNullMaybe toCBOR (strictMaybeToMaybe (_poolMD poolParams))
-
-  encodedGroupSizeExpr size' proxy =
-    encodedSizeExpr size' (_poolId <$> proxy)
-      + encodedSizeExpr size' (_poolVrf <$> proxy)
-      + encodedSizeExpr size' (_poolPledge <$> proxy)
-      + encodedSizeExpr size' (_poolCost <$> proxy)
-      + encodedSizeExpr size' (_poolMargin <$> proxy)
-      + encodedSizeExpr size' (_poolRAcnt <$> proxy)
-      + 2
-      + poolSize * encodedSizeExpr size' (elementProxy (_poolOwners <$> proxy))
-      + 2
-      + relaySize * encodedSizeExpr size' (elementProxy (_poolRelays <$> proxy))
-      + szCases
-        [ Case "Nothing" 1,
-          Case "Just" $ encodedSizeExpr size' (elementProxy (_poolMD <$> proxy))
-        ]
-    where
-      poolSize, relaySize :: Size
-      poolSize = size' (Proxy @SizeOfPoolOwners)
-      relaySize = size' (Proxy @SizeOfPoolRelays)
-      elementProxy :: Proxy (f a) -> Proxy a
-      elementProxy _ = Proxy
-
-  listLen _ = 9
-  listLenBound _ = 9
-
-instance
-  CC.Crypto c =>
-  FromCBORGroup (PoolParams c)
-  where
-  fromCBORGroup = do
-    hk <- fromCBOR
-    vrf <- fromCBOR
-    pledge <- fromCBOR
-    cost <- fromCBOR
-    margin <- fromCBOR
-    ra <- fromCBOR
-    owners <- decodeSet fromCBOR
-    relays <- decodeStrictSeq fromCBOR
-    md <- decodeNullMaybe fromCBOR
-    pure $
-      PoolParams
-        { _poolId = hk,
-          _poolVrf = vrf,
-          _poolPledge = pledge,
-          _poolCost = cost,
-          _poolMargin = margin,
-          _poolRAcnt = ra,
-          _poolOwners = owners,
-          _poolRelays = relays,
-          _poolMD = maybeToStrictMaybe md
-        }
+emptySnapShots :: SnapShots c
+emptySnapShots = SnapShots emptySnapShot emptySnapShot emptySnapShot (Coin 0)
