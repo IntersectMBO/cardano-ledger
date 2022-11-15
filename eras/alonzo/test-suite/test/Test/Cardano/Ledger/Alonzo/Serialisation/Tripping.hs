15bc4fd7dd7d8d1f4a49bb68337ca2b578a8be46
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2022-11-15 14:53:20 +0300

    Bring back skipped twiddling tests. They still require proper Eq instance for MemoBytes

--- eras/alonzo/test-suite/test/Test/Cardano/Ledger/Alonzo/Serialisation/Tripping.hs
+++ eras/alonzo/test-suite/test/Test/Cardano/Ledger/Alonzo/Serialisation/Tripping.hs
@@ -20,66 +20,10 @@ import Cardano.Protocol.TPraos.BHeader (BHeader)
 import Test.Cardano.Ledger.Alonzo.Serialisation.Generators ()
 import Test.Cardano.Ledger.Binary.Arbitrary ()
 import Test.Cardano.Ledger.Binary.RoundTrip
--- import Test.Cardano.Ledger.Binary.Twiddle (Twiddle (..))
 import Test.Cardano.Ledger.Shelley.ConcreteCryptoTypes
 import Test.Cardano.Ledger.ShelleyMA.Serialisation.Generators ()
 import Test.Tasty
 import Test.Tasty.QuickCheck
-
--- trippingF ::
---   (Eq src, Show src, Show target, ToCBOR src) =>
---   (src -> Either target (BSL.ByteString, src)) ->
---   src ->
---   Property
--- trippingF f x = runIdentity $ trippingM f' (===) x
---   where
---     f' a = pure (f a, toLazyByteString $ toCBOR a)
-
--- trippingM ::
---   (Monad m, Show target) =>
---   (src -> m (Either target (BSL.ByteString, src), BSL.ByteString)) ->
---   (src -> src -> Property) ->
---   src ->
---   m Property
--- trippingM f g x = do
---   res <- f x
---   pure $ case res of
---     (Right (remaining, y), _)
---       | BSL.null remaining ->
---           g x y
---     (Right (remaining, _), _) ->
---       counterexample
---         ("Unconsumed trailing bytes:\n" <> BSL.unpack remaining)
---         False
---     (Left stuff, bs) ->
---       counterexample
---         ( concat
---             [ "Failed to decode: ",
---               show stuff,
---               "\nbytes: ",
---               show (Base16.encode bs)
---             ]
---         )
---         False
-
--- trippingAnn ::
---   ( Eq t,
---     Show t,
---     ToCBOR t,
---     FromCBOR (Annotator t)
---   ) =>
---   t ->
---   Property
--- trippingAnn = trippingF roundTripAnn
-
--- trippingAnnWithTwiddling :: (Twiddle t, FromCBOR (Annotator t)) => (t -> t -> Property) -> t -> Property
--- trippingAnnWithTwiddling comp x = property $ trippingM roundTripAnnWithTwiddling comp x
-
--- tripping :: (Eq src, Show src, ToCBOR src, FromCBOR src) => src -> Property
--- tripping = trippingF roundTrip
-
--- trippingWithTwiddling :: (Twiddle src, FromCBOR src) => (src -> src -> Property) -> src -> Property
--- trippingWithTwiddling comp x = property $ trippingM roundTripWithTwiddling comp x
 
 tests :: TestTree
 tests =
@@ -87,32 +31,28 @@ tests =
     "Alonzo CBOR round-trip"
     [ testProperty "alonzo/Script" $
         roundTripAnnExpectation @(Script Alonzo) v,
-      -- skip $
-      --   testTwiddlingAnnWith @(Script Alonzo)
-      --     "alonzo/Script twiddled"
-      --     Script.contentsEq,
+      skip $
+        testProperty "alonzo/Script twiddled" $
+          roundTripAnnTwiddledProperty @(Script Alonzo),
       testProperty "alonzo/Data" $
         roundTripAnnExpectation @(Data Alonzo) v,
-      -- skip $
-      --   testTwiddlingAnnWith @(Data Alonzo)
-      --     "alonzo/Data twiddled"
-      --     Data.contentsEq,
+      skip $
+        testProperty "alonzo/Data twiddled" $
+          roundTripAnnTwiddledProperty @(Data Alonzo) v,
       testProperty "alonzo/BinaryData" $
         roundTripCborExpectation @(BinaryData Alonzo) v,
-      -- skip $
-      --   testTwiddling @(BinaryData Alonzo)
-      --     "alonzo/BinaryData twiddled",
+      skip $
+        testProperty "alonzo/BinaryData twiddled" $
+          roundTripTwiddledProperty @(BinaryData Alonzo) v,
       testProperty "alonzo/TxAuxData" $
         roundTripAnnExpectation @(ShelleyTxAuxData Alonzo) v,
       testProperty "alonzo/AlonzoTxWits" $
         roundTripAnnExpectation @(AlonzoTxWits Alonzo) v,
       testProperty "alonzo/TxBody" $
         roundTripAnnExpectation @(TxBody Alonzo) v,
-      -- skip $
-      --   testTwiddlingAnnWith
-      --     @(TxBody Alonzo)
-      --     "alonzo/TxBody twiddled"
-      --     txBodyRawEq,
+      skip $
+        testProperty "alonzo/TxBody twiddled" $
+          roundTripAnnTwiddledProperty @(TxBody Alonzo),
       testProperty "alonzo/CostModels" $
         roundTripCborExpectation @CostModels v,
       testProperty "alonzo/PParams" $
@@ -136,27 +76,4 @@ tests =
     ]
   where
     v = eraProtVerHigh @Alonzo
-    -- testTwiddlingAnnWith ::
-    --   forall a.
-    --   (Twiddle a, Arbitrary a, Show a, FromCBOR (Annotator a)) =>
-    --   TestName ->
-    --   (a -> a -> Bool) ->
-    --   TestTree
-    -- testTwiddlingAnnWith name comp =
-    --   testProperty name $ trippingAnnWithTwiddling (\x y -> counterexample (msg x y) $ comp x y)
-    --   where
-    --     msg x y = "Contents differ:\n" <> show x <> "\n" <> show y
-
-    -- testTwiddling ::
-    --   forall a.
-    --   ( Arbitrary a,
-    --     Twiddle a,
-    --     FromCBOR a,
-    --     Show a,
-    --     Eq a
-    --   ) =>
-    --   TestName ->
-    --   TestTree
-    -- testTwiddling name = testProperty name $ trippingWithTwiddling @a (===)
-
-    _skip _ = testProperty "Test skipped" True
+    skip _ = testProperty "Test skipped" True
