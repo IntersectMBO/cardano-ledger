6456064db49fba51af6d584742f4695a061fcf1c
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-01-20 02:48:39 +0300

    Start on better integration with `cardno-binary` by adding `Cardano.ledger.Binary.Plain` module

---
+++ libs/cardano-ledger-binary/src/Cardano/Ledger/Binary/Plain.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Module that re-exports everythign from `cardano-binary` package.
+--
+-- Everything that gets defined in this module should most likely be migrated to
+-- `cardano-binary` package.
+module Cardano.Ledger.Binary.Plain (
+  module Cardano.Binary,
+  decodeRecordNamed,
+  decodeRecordSum,
+  decodeListLike,
+)
+where
+
+import Cardano.Binary
+import Control.Monad (unless)
+import qualified Data.Text as Text
+
+decodeRecordNamed :: Text.Text -> (a -> Int) -> Decoder s a -> Decoder s a
+decodeRecordNamed name getRecordSize decoder =
+  decodeListLike name decoder $ \result n ->
+    matchSize ("Record " <> name) n (getRecordSize result)
+{-# INLINEABLE decodeRecordNamed #-}
+
+decodeRecordSum :: Text.Text -> (Word -> Decoder s (Int, a)) -> Decoder s a
+decodeRecordSum name decoder =
+  snd <$> do
+    decodeListLike name (decodeWord >>= decoder) $ \(size, _) n ->
+      matchSize (Text.pack "Sum " <> name) size n
+{-# INLINEABLE decodeRecordSum #-}
+
+decodeListLike ::
+  -- | Name for error reporting
+  Text.Text ->
+  -- | Decoder for the datastructure itself
+  Decoder s a ->
+  -- | In case when length was provided, act upon it.
+  (a -> Int -> Decoder s ()) ->
+  Decoder s a
+decodeListLike name decoder actOnLength = do
+  lenOrIndef <- decodeListLenOrIndef
+  result <- decoder
+  case lenOrIndef of
+    Just n -> actOnLength result n
+    Nothing -> do
+      isBreak <- decodeBreakOr
+      unless isBreak $ cborError $ DecoderErrorCustom name "Excess terms in array"
+  pure result
+{-# INLINEABLE decodeListLike #-}
