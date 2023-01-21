6c6123abc91fc00d7bf6f70a14b6ef767156ebeb
Jared Corduan <jared.corduan@iohk.io>
2023-01-21 11:18:31 -0500

    Apply suggestions from code review
    Co-authored-by: Alexey Kuleshevich <alexey.kuleshevich@iohk.io>

--- eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxWits.hs
+++ eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxWits.hs
@@ -145,7 +145,7 @@ instance Memoized Redeemers where
   type RawType Redeemers = RedeemersRaw
 
 -- | Note that 'Redeemers' are based on 'MemoBytes' since we must preserve
--- the original bytes for the 'ScriptIntegrity'.
+-- the original bytes for the 'Cardano.Ledger.Alonzo.Tx.ScriptIntegrity'.
 -- Since the 'Redeemers' exist outside of the transaction body,
 -- this is how we ensure that they are not manipulated.
 newtype Redeemers era = RedeemersConstr (MemoBytes RedeemersRaw era)
@@ -283,7 +283,7 @@ instance (Era era) => FromCBOR (Annotator (TxDatsRaw era)) where
   fromCBOR = decode $ fmap (TxDatsRaw . keyBy hashData) <$> listDecodeA From
 
 -- | Note that 'TxDats' are based on 'MemoBytes' since we must preserve
--- the original bytes for the 'ScriptIntegrity'.
+-- the original bytes for the 'Cardano.Ledger.Alonzo.Tx.ScriptIntegrity'.
 -- Since the 'TxDats' exist outside of the transaction body,
 -- this is how we ensure that they are not manipulated.
 newtype TxDats era = TxDatsConstr (MemoBytes TxDatsRaw era)
