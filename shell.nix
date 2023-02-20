02ccee9f9ba8c91c30cc13b9544c09a7ca2ed33b
Aniket Deshpande <aniket.deshpande@iohk.io>
2023-02-20 15:09:56 +0530

    Add gen-hie to CI

--- shell.nix
+++ shell.nix
@@ -28,13 +28,14 @@ let
       pkg-config
       hlint
       pyEnv
+      implicit-hie
     ];
 
     tools = {
       cabal = "3.8.1.0";
       ghcid = "0.8.8";
       fourmolu = "0.10.1.0";
-      haskell-language-server="1.9.1.0";
+      haskell-language-server = "1.9.1.0";
     };
 
     # Prevents cabal from choosing alternate plans, so that
