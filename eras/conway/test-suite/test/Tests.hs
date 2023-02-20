34fcc232527b6b82c13b2ae364b77dc42aad9cec
teodanciu <teodora.danciu@tweag.io>
2023-02-20 22:56:26 +0000

    Simplify conway tests entry point

--- eras/conway/test-suite/test/Tests.hs
+++ eras/conway/test-suite/test/Tests.hs
@@ -6,20 +6,15 @@ module Main where
 import Cardano.Ledger.Conway (Conway)
 import qualified Test.Cardano.Ledger.Conway.Serialisation.CDDL as CDDL
 import qualified Test.Cardano.Ledger.Conway.Serialisation.Roundtrip as Roundtrip
-import Test.Tasty (TestTree, askOption, testGroup)
-import Test.TestScenario (TestScenario (..), mainWithTestScenario)
+import Test.Tasty (TestTree, defaultMain, testGroup)
 
-tests :: TestTree
-tests = askOption @TestScenario $ \case
-  _ -> mainTests
+main :: IO ()
+main = defaultMain defaultTests
 
-mainTests :: TestTree
-mainTests =
+defaultTests :: TestTree
+defaultTests =
   testGroup
     "Conway tests"
     [ Roundtrip.allprops @Conway
     , CDDL.tests 5
     ]
-
-main :: IO ()
-main = mainWithTestScenario tests
