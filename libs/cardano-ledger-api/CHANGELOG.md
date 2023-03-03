f4039dd3b9e8bd84e0a923bc197bba64d3cfb798
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-03-04 01:17:34 +0300

    Improve API (Address, TxAuxData and TxBody):
    * Addition of `Cardano.Ledger.Api.Tx.Address`
    * Hide `ShelleyTxBody`, `AlonzoTxBody`, `BabbageTxBody` from `Cardano.Ledger.Api.Tx.Body`
    * Export `Withdrawals` from `Cardano.Ledger.Api.Tx.Body`
    * Export `ShelleyTxAuxData`, `AllegraTxAuxData` and `AlonzoTxAuxData` from
      `Cardano.Ledger.Api.Tx.AuxData`

--- libs/cardano-ledger-api/CHANGELOG.md
+++ libs/cardano-ledger-api/CHANGELOG.md
@@ -1,8 +1,13 @@
 # Version history for `cardano-ledger-api`
 
-## 1.0.1.0
+## 1.1.0.0
 
 * Addition of `Cardano.Ledger.Api.PParams`
+* Addition of `Cardano.Ledger.Api.Tx.Address`
+* Hide `ShelleyTxBody`, `AlonzoTxBody`, `BabbageTxBody` from `Cardano.Ledger.Api.Tx.Body`
+* Export `Withdrawals` from `Cardano.Ledger.Api.Tx.Body`
+* Export `ShelleyTxAuxData`, `AllegraTxAuxData` and `AlonzoTxAuxData` from
+  `Cardano.Ledger.Api.Tx.AuxData`
 
 ## 1.0.0.0
 
--- libs/cardano-ledger-api/src/Cardano/Ledger/Api/Tx/AuxData.hs
+++ libs/cardano-ledger-api/src/Cardano/Ledger/Api/Tx/AuxData.hs
@@ -1,6 +1,18 @@
 module Cardano.Ledger.Api.Tx.AuxData (
   EraTxAuxData (..),
+
+  -- * Shelley
+  ShelleyTxAuxData (..),
+
+  -- * Allegra
+  AllegraTxAuxData (..),
+
+  -- * Alonzo
+  AlonzoTxAuxData (..),
 )
 where
 
+import Cardano.Ledger.Allegra.TxAuxData (AllegraTxAuxData (..))
+import Cardano.Ledger.Alonzo.TxAuxData (AlonzoTxAuxData (..))
 import Cardano.Ledger.Core (EraTxAuxData (..))
+import Cardano.Ledger.Shelley.TxAuxData (ShelleyTxAuxData (..))
