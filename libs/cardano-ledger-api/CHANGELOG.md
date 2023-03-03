28a66966a7d7a243d830ed528d5800ddf2b22c9c
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-03-04 01:17:34 +0300

    Improve API (Address, TxAuxData and TxBody):
    * Addition of `Cardano.Ledger.Api.Tx.Address`
    * Hide `ShelleyTxBody`, `AlonzoTxBody`, `BabbageTxBody` from `Cardano.Ledger.Api.Tx.Body`
    * Export `Withdrawals` from `Cardano.Ledger.Api.Tx.Body`
    * Export `ShelleyTxAuxData`, `AllegraTxAuxData` and `AlonzoTxAuxData` from
      `Cardano.Ledger.Api.Tx.AuxData`

--- libs/cardano-ledger-api/CHANGELOG.md
+++ libs/cardano-ledger-api/CHANGELOG.md
@@ -1,9 +1,14 @@
 # Version history for `cardano-ledger-api`
 
-## 1.0.1.0
+## 1.1.0.0
 
 * Add `Cardano.Ledger.Api.Governance` that exposes all of the governance related types.
 * Addition of `Cardano.Ledger.Api.PParams`
+* Addition of `Cardano.Ledger.Api.Tx.Address`
+* Hide `ShelleyTxBody`, `AlonzoTxBody`, `BabbageTxBody` from `Cardano.Ledger.Api.Tx.Body`
+* Export `Withdrawals` from `Cardano.Ledger.Api.Tx.Body`
+* Export `ShelleyTxAuxData`, `AllegraTxAuxData` and `AlonzoTxAuxData` from
+  `Cardano.Ledger.Api.Tx.AuxData`
 
 ## 1.0.0.0
 
--- libs/cardano-ledger-api/src/Cardano/Ledger/Api/Tx/Body.hs
+++ libs/cardano-ledger-api/src/Cardano/Ledger/Api/Tx/Body.hs
@@ -9,9 +9,9 @@ module Cardano.Ledger.Api.Tx.Body (
   -- | Working with Timelock scripts and Plutus scripts
   module Cardano.Ledger.Api.Scripts,
   EraTxBody (..),
+  Withdrawals (..),
 
   -- * Shelley Era
-  ShelleyTxBody,
   ShelleyEraTxBody (..),
 
   -- * Allegra Era
@@ -21,21 +21,19 @@ module Cardano.Ledger.Api.Tx.Body (
   MaryEraTxBody (..),
 
   -- * Alonzo Era
-  AlonzoTxBody,
   AlonzoEraTxBody (..),
 
   -- * Babbage Era
-  BabbageTxBody,
   BabbageEraTxBody (..),
 )
 where
 
+import Cardano.Ledger.Address (Withdrawals (..))
 import Cardano.Ledger.Allegra.Core (AllegraEraTxBody (..))
-import Cardano.Ledger.Alonzo.TxBody (AlonzoEraTxBody (..), AlonzoTxBody)
+import Cardano.Ledger.Alonzo.TxBody (AlonzoEraTxBody (..))
 import Cardano.Ledger.Api.Scripts
 import Cardano.Ledger.Api.Tx.Out
-import Cardano.Ledger.Babbage.TxBody (BabbageEraTxBody (..), BabbageTxBody)
+import Cardano.Ledger.Babbage.TxBody (BabbageEraTxBody (..))
 import Cardano.Ledger.Core (EraTxBody (..))
 import Cardano.Ledger.Mary.Core (MaryEraTxBody (..))
-import Cardano.Ledger.Shelley (ShelleyTxBody)
 import Cardano.Ledger.Shelley.Core (ShelleyEraTxBody (..))
