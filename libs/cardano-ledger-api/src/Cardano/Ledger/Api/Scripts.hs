ea79b695ba8427f2895004574c1151404dd44c0a
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-03-08 00:04:05 +0300

    Export more witness related functionality from `Cardano.Ledger.Api.Tx.Wits`:
    * `WitVKey`, `BootstrapWitness`, `AlonzoEraTxWits`, `TxDats`, `unTxDats`,
      `Redeemers`, `unRedeemers`, `RdmrPtr` and `Tag`.

--- libs/cardano-ledger-api/src/Cardano/Ledger/Api/Scripts.hs
+++ libs/cardano-ledger-api/src/Cardano/Ledger/Api/Scripts.hs
@@ -2,11 +2,13 @@ module Cardano.Ledger.Api.Scripts (
   module Cardano.Ledger.Api.Scripts.Data,
   EraScript (..),
   ScriptHash,
+  CostModels (..),
   ValidityInterval (..),
 )
 where
 
 import Cardano.Ledger.Allegra.Scripts (ValidityInterval (..))
+import Cardano.Ledger.Alonzo.Scripts (CostModels (..))
 import Cardano.Ledger.Api.Scripts.Data
 import Cardano.Ledger.Core (EraScript (..))
 import Cardano.Ledger.Hashes (ScriptHash)
