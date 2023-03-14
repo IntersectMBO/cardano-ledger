c5e30045875197adac64d6ea93abb5dac166f442
Jared Corduan <jared.corduan@iohk.io>
2023-03-14 18:27:56 -0400

    enforce unique elements in CBOR sets, ver. 9
    Starting in major version 9, we now raise a decoding error if a CBOR
    set contains duplicate keys. Additionally, we allow (but do not require)
    the set tag.

--- libs/cardano-ledger-binary/test/Test/Cardano/Ledger/Binary/Failure.hs
+++ libs/cardano-ledger-binary/test/Test/Cardano/Ledger/Binary/Failure.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Test.Cardano.Ledger.Binary.Failure (spec) where
 
 import Cardano.Ledger.Binary
-import Data.Either (isLeft)
 import Data.Map (Map)
+import Data.Proxy (Proxy (Proxy))
+import Data.Set (Set)
+import Test.Cardano.Ledger.Binary.RoundTrip (Trip (..), embedTripRangeFailureExpectation)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
@@ -28,20 +31,44 @@ genDuplicateAssocListEncoding = do
     , pure $ encodeMapLenIndef <> foldMap encCBOR flatXs <> encodeBreak
     ]
 
+-- | Generate a list with at least one duplicate
+genDuplicateList :: Gen [Int]
+genDuplicateList = do
+  xs <- getNonEmpty <$> arbitrary
+  a <- elements xs -- pick an element to duplicate
+  shuffle (a : xs)
+
+-- | Generate a CBOR encoded list with at least one duplicate, with and with the set tag
+genDuplicateListEncoding :: Gen Encoding
+genDuplicateListEncoding = do
+  xs <- genDuplicateList
+  let definite = encodeListLen (fromIntegral $ Prelude.length xs) <> foldMap encCBOR xs
+      indefinite = encodeListLenIndef <> foldMap encCBOR xs <> encodeBreak
+  elements
+    [ definite
+    , encodeTag 258 <> definite
+    , indefinite
+    , encodeTag 258 <> indefinite
+    ]
+
 -- | Starting in version 9, do not accept duplicates in CBOR maps
 prop_shouldFailMapWithDupKeys :: Property
 prop_shouldFailMapWithDupKeys =
   forAllBlind genDuplicateAssocListEncoding $
-    ( \encodedMap ->
-        (property . isLeft) (decode (natVersion @9) encodedMap :: Either DecoderError (Map Int Int))
-    )
+    \mapEncoding ->
+      let trip = Trip id (decCBOR @(Map Int Int)) (dropCBOR (Proxy @(Map Int Int)))
+       in property $ embedTripRangeFailureExpectation trip (natVersion @9) maxBound mapEncoding
 
-decode :: DecCBOR a => Version -> Encoding -> Either DecoderError a
-decode version enc =
-  let encoded = serialize version enc
-   in decodeFull version encoded
+-- | Starting in version 9, do not accept duplicates in CBOR sets
+prop_shouldFailSetWithDupKeys :: Property
+prop_shouldFailSetWithDupKeys =
+  forAllBlind genDuplicateListEncoding $
+    \setEncoding ->
+      let trip = Trip id (decCBOR @(Set Int)) (dropCBOR (Proxy @(Set Int)))
+       in property $ embedTripRangeFailureExpectation trip (natVersion @9) maxBound setEncoding
 
 spec :: Spec
 spec = do
   describe "Failures" $ do
     prop "map duplicates are not allowed starting v9" prop_shouldFailMapWithDupKeys
+    prop "set duplicates are not allowed starting v9" prop_shouldFailSetWithDupKeys
