fc76ad0b2a0fd533f18ec45989786a47e6658fdd
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-05-11 23:05:25 +0300

    Add two modules for backwards compatibility

---
+++ eras/shelley/test-suite/src/Test/Cardano/Ledger/Shelley/Generator/Delegation.hs
@@ -0,0 +1,6 @@
+module Test.Cardano.Ledger.Shelley.Generator.Delegation
+  {-# DEPRECATED "Use `Test.Cardano.Ledger.Shelley.Generator.TxCert` instead" #-} (
+  module Test.Cardano.Ledger.Shelley.Generator.TxCert,
+) where
+
+import Test.Cardano.Ledger.Shelley.Generator.TxCert
---
+++ eras/shelley/test-suite/src/Test/Cardano/Ledger/Shelley/Generator/Trace/DCert.hs
@@ -0,0 +1,6 @@
+module Test.Cardano.Ledger.Shelley.Generator.Trace.DCert
+  {-# DEPRECATED "Use `Test.Cardano.Ledger.Shelley.Generator.Trace.TxCert` instead" #-} (
+  module Test.Cardano.Ledger.Shelley.Generator.Trace.TxCert,
+) where
+
+import Test.Cardano.Ledger.Shelley.Generator.Trace.TxCert
