328e3d525fe1430bc3317bbd2771b7cc6ac0e225
teodanciu <teodora.danciu@tweag.io>
2023-05-17 16:24:03 +0100

    Introduce golden translation file generation and test for Babbage

---
+++ eras/babbage/test-suite/src/Test/Cardano/Ledger/Babbage/Translation/TranslatableGen.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Babbage.Translation.TranslatableGen (
+  genTx,
+  genTxOut,
+  utxoWithTx,
+) where
+
+import Cardano.Ledger.Babbage (Babbage, BabbageEra)
+import Cardano.Ledger.Babbage.Core
+import Cardano.Ledger.Crypto
+import Lens.Micro ((&), (.~), (^.))
+import Test.Cardano.Ledger.Babbage.Arbitrary ()
+import Test.Cardano.Ledger.Babbage.Serialisation.Generators ()
+
+import Cardano.Ledger.Address (Addr (..))
+import Cardano.Ledger.Alonzo.Scripts (AlonzoScript (..), ExUnits (..), Tag (..))
+import Cardano.Ledger.Alonzo.Scripts.Data (Data (..))
+import Cardano.Ledger.Alonzo.Tx (AlonzoTx (..))
+import Cardano.Ledger.Alonzo.TxWits
+import Cardano.Ledger.Babbage.TxBody (BabbageTxBody (..), BabbageTxOut (..), Datum (..))
+import Cardano.Ledger.Binary (mkSized)
+import Cardano.Ledger.Credential (StakeReference (..))
+import Cardano.Ledger.Language (Language (..))
+import Cardano.Ledger.TxIn (TxIn (..))
+import Cardano.Ledger.UTxO (UTxO (..))
+import qualified Data.Map as Map
+import Data.Maybe.Strict
+import Data.Sequence.Strict (fromList)
+import qualified Data.Set as Set
+import qualified PlutusLedgerApi.V1 as PV1
+import Test.Cardano.Ledger.Alonzo.Translation.TranslationInstanceGen (TranslatableGen (..))
+import Test.Cardano.Ledger.Core.Arbitrary ()
+import Test.QuickCheck (
+  Arbitrary,
+  Gen,
+  arbitrary,
+  frequency,
+  listOf1,
+  oneof,
+  scale,
+  vectorOf,
+ )
+
+instance TranslatableGen Babbage where
+  tgTx l = genTx @Babbage l (genTxBody l)
+  tgUtxo = utxoWithTx @Babbage
+
+utxoWithTx ::
+  forall era.
+  ( EraTx era
+  , Arbitrary (Value era)
+  , Arbitrary (Script era)
+  , BabbageEraTxBody era
+  , TxOut era ~ BabbageTxOut era
+  ) =>
+  Language ->
+  Tx era ->
+  Gen (UTxO era)
+utxoWithTx l tx = do
+  let ins = tx ^. bodyTxL ^. inputsTxBodyL
+  let refIns = tx ^. bodyTxL ^. referenceInputsTxBodyL
+  let allIns = ins `Set.union` refIns
+  outs <- vectorOf (length allIns) (genTxOut @era l)
+  pure $ UTxO (Map.fromList $ Set.toList allIns `zip` outs)
+
+genTx ::
+  forall era.
+  ( Arbitrary (TxAuxData era)
+  , Arbitrary (Script era)
+  , AlonzoEraTxWits era
+  , AlonzoScript era ~ Script era
+  , AlonzoTxWits era ~ TxWits era
+  ) =>
+  Language ->
+  Gen (TxBody era) ->
+  Gen (AlonzoTx era)
+genTx l txbGen =
+  AlonzoTx
+    <$> txbGen
+    <*> genTxWits @era l
+    <*> arbitrary
+    <*> arbitrary
+
+genTxOut ::
+  forall era.
+  ( EraTxOut era
+  , Arbitrary (Value era)
+  , Arbitrary (Script era)
+  ) =>
+  Language ->
+  Gen (BabbageTxOut era)
+genTxOut l = do
+  addr <- genNonByronAddr @(EraCrypto era)
+  value <- scale (`div` 15) arbitrary
+  script <- case l of
+    PlutusV1 -> pure SNothing
+    _ -> arbitrary
+  datum <- case l of
+    PlutusV1 -> oneof [pure NoDatum, DatumHash <$> (arbitrary :: Gen (DataHash (EraCrypto era)))]
+    _ -> arbitrary
+  pure $ BabbageTxOut addr value datum script
+
+genTxWits ::
+  forall era.
+  ( Arbitrary (Script era)
+  , AlonzoEraTxWits era
+  , AlonzoScript era ~ Script era
+  , AlonzoTxWits era ~ TxWits era
+  ) =>
+  Language ->
+  Gen (AlonzoTxWits era)
+genTxWits = \case
+  PlutusV1 -> arbitrary
+  _ -> do
+    arbWits <- arbitrary :: Gen (AlonzoTxWits era)
+    pure $
+      arbWits
+        & rdmrsTxWitsL @era
+          .~ Redeemers (Map.singleton (RdmrPtr Spend 0) (Data (PV1.I 42), ExUnits 5000 5000))
+
+genTxBody :: forall era. Crypto era => Language -> Gen (BabbageTxBody (BabbageEra era))
+genTxBody l = do
+  let genTxOuts = fromList <$> listOf1 (mkSized (eraProtVerLow @Babbage) <$> genTxOut @(BabbageEra era) l)
+  let genTxIns = Set.fromList <$> listOf1 (arbitrary :: Gen (TxIn era))
+  BabbageTxBody
+    <$> genTxIns
+    <*> arbitrary
+    <*> ( case l of -- refinputs
+            PlutusV1 -> pure Set.empty
+            _ -> arbitrary
+        )
+    <*> genTxOuts
+    <*> arbitrary
+    <*> arbitrary
+    <*> arbitrary
+    <*> arbitrary
+    <*> arbitrary
+    <*> arbitrary
+    <*> scale (`div` 15) arbitrary
+    <*> arbitrary
+    <*> scale (`div` 15) arbitrary
+    <*> arbitrary
+    <*> arbitrary
+    <*> arbitrary
+
+genNonByronAddr :: forall era. Crypto era => Gen (Addr era)
+genNonByronAddr =
+  Addr
+    <$> arbitrary
+    <*> arbitrary
+    <*> frequency
+      [ (85, StakeRefBase <$> arbitrary)
+      , (15, pure StakeRefNull)
+      ]
---
+++ eras/babbage/test-suite/test/Test/Cardano/Ledger/Babbage/GoldenTranslation.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- | Deserializes `TranslationInstance`s from golden/translations.cbor file.
+--
+-- Each instance represents arguments passed to `ExtendedUTxO.txInfo` along with the produced result.
+-- This test checks that calling `alonzoTxInfo` with the arguments from this file, produces the same result as in the flie.
+--
+-- To regenerate the golden file (for example, if the logic in the translation changes),
+-- run the following command from the root of the repository:
+-- cabal run cardano-ledger-<era>-test:gen-golden"
+module Test.Cardano.Ledger.Babbage.GoldenTranslation (
+  tests,
+)
+where
+
+import Test.Cardano.Ledger.Babbage.Serialisation.Generators ()
+
+import Cardano.Ledger.Babbage (Babbage)
+import Paths_cardano_ledger_babbage_test (getDataFileName)
+import Test.Cardano.Ledger.Alonzo.Translation.Golden (TxInfoResultComparison (..), compareGoldenTxInfoResults)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (Assertion, assertEqual, testCase)
+
+tests :: TestTree
+tests =
+  testGroup
+    "Golden translation tests"
+    [testCase "golden/translations.cbor" $ check "golden/translations.cbor"]
+
+check :: String -> Assertion
+check file = do
+  comps <- compareGoldenTxInfoResults @Babbage (getDataFileName file)
+  mapM_
+    ( \(TxInfoResultComparison expected actual err) ->
+        assertEqual err expected actual
+    )
+    comps
