286b84a19fffb6f723c953cd3849579db13846f7
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-06-28 19:33:52 +0300

    Add TxCert API

---
+++ libs/cardano-ledger-api/src/Cardano/Ledger/Api/Tx/Cert.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE PatternSynonyms #-}
+
+module Cardano.Ledger.Api.Tx.Cert (
+  EraTxCert,
+  TxCert,
+  getVKeyWitnessTxCert,
+  getScriptWitnessTxCert,
+  pattern RegPoolTxCert,
+  pattern RetirePoolTxCert,
+
+  -- * Shelley Era
+
+  -- | Complete set of patterns for Shelley through Babbage `TxCert`:
+  --
+  -- @
+  -- `TxCert` =
+  --   `RegPoolTxCert`
+  --   `RetirePoolTxCert`
+  --   `RegTxCert`
+  --   `UnRegTxCert`
+  --   `DelegStakeTxCert`
+  --   `MirTxCert`
+  --   `GenesisDelegTxCert`
+  -- @
+  ShelleyEraTxCert,
+  pattern MirTxCert,
+  pattern GenesisDelegTxCert,
+  pattern RegTxCert,
+  pattern UnRegTxCert,
+  pattern DelegStakeTxCert,
+
+  -- * Conway Era
+
+  -- | Complete set of patterns for Conway `TxCert`:
+  --
+  -- @
+  -- `TxCert` =
+  --   `RegPoolTxCert`
+  --   `RetirePoolTxCert`
+  --   `RegTxCert`
+  --   `UnRegTxCert`
+  --   `DelegStakeTxCert`
+  --   `RegDepositTxCert`
+  --   `UnRegDepositTxCert`
+  --   `DelegTxCert`
+  --   `RegDepositDelegTxCert`
+  --   `AuthCommitteeHotKeyTxCert`
+  --   `ResignCommitteeColdTxCert`
+  --   `RegDRepTxCert`
+  --   `UnRegDRepTxCert`
+  -- @
+  ConwayEraTxCert,
+  Delegatee (..),
+  pattern RegDepositTxCert,
+  pattern UnRegDepositTxCert,
+  pattern DelegTxCert,
+  pattern RegDepositDelegTxCert,
+  pattern AuthCommitteeHotKeyTxCert,
+  pattern ResignCommitteeColdTxCert,
+  pattern RegDRepTxCert,
+  pattern UnRegDRepTxCert,
+)
+where
+
+import Cardano.Ledger.Conway.TxCert (
+  ConwayEraTxCert,
+  Delegatee (..),
+  pattern AuthCommitteeHotKeyTxCert,
+  pattern DelegTxCert,
+  pattern RegDRepTxCert,
+  pattern RegDepositDelegTxCert,
+  pattern RegDepositTxCert,
+  pattern ResignCommitteeColdTxCert,
+  pattern UnRegDRepTxCert,
+  pattern UnRegDepositTxCert,
+ )
+import Cardano.Ledger.Core (
+  EraTxCert,
+  TxCert,
+  getScriptWitnessTxCert,
+  getVKeyWitnessTxCert,
+  pattern RegPoolTxCert,
+  pattern RetirePoolTxCert,
+ )
+import Cardano.Ledger.Shelley.TxCert (
+  ShelleyEraTxCert,
+  pattern DelegStakeTxCert,
+  pattern GenesisDelegTxCert,
+  pattern MirTxCert,
+  pattern RegTxCert,
+  pattern UnRegTxCert,
+ )
