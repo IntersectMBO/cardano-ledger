dabc73fe7f2f5356e47f7487b3543c95b5d32a7b
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-07-08 13:23:00 +0300

    Bunch of debugging stuff

--- eras/conway/impl/src/Cardano/Ledger/Conway.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
@@ -11,6 +14,11 @@
 module Cardano.Ledger.Conway (
   Conway,
   ConwayEra,
+  readNewEpochState,
+  tTx,
+  mkGlobals,
+  mkEnv,
+  repro,
 )
 where
 
@@ -38,6 +46,65 @@ import qualified Data.Map.Strict as Map
 import Data.Maybe.Strict
 import qualified Data.Set as Set
 import Lens.Micro
+
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.Binary
+import Cardano.Ledger.Core
+import Cardano.Ledger.Shelley.API.Mempool
+import Cardano.Ledger.Shelley.Genesis
+import Cardano.Ledger.Shelley.LedgerState
+import Cardano.Ledger.Shelley.Rules
+import Cardano.Slotting.EpochInfo (fixedEpochInfo)
+import Cardano.Slotting.Time (mkSlotLength)
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Lazy as BSL
+
+repro :: IO (LedgerState Conway)
+repro = do
+  nes <- readNewEpochState
+  tx <- tTx
+  globals <- mkGlobals (esPp (nesEs nes))
+  let env = mkEnv nes
+      st = esLState (nesEs nes)
+  either (error . show) (pure . fst) $ applyTx globals env st tx
+
+readNewEpochState :: IO (NewEpochState Conway)
+readNewEpochState = do
+  ls <- BSL.readFile "/home/lehins/Downloads/ledger-state.42.cbor"
+  either (error . show) pure $ decodeFull (natVersion @9) ls :: IO (NewEpochState Conway)
+
+tTx :: IO (Tx Conway)
+tTx = do
+  tx <- either error (pure . BSL.fromStrict) $ B16.decode "84a300818258207c7e5fc419c2fd1a33d8f4761dd6e4cb6c43554c55378c7fccdf65f22c1c946b000181a200581d60ed0998e484ef9c9088c43ee7169cf0811cea2e52c07a3bf7b5b0cb63011b00000045d962375b021a000280a5a10081825820190e437455e5909c2af62f5e61e316324dccca282a4a6fa8c47b851e1d4137b05840f687759143c365eb459597dca30c46fdda72a37cada18da040813624a59ae6da1eed3db6292470ea40c1a55ccc228be39791d6ae8a1cb06ddbc371ee52a37803f5f6"
+  either (error . show) pure $ decodeFullAnnotator (natVersion @9) "Tx" decCBOR tx :: IO (Tx Conway)
+
+getShelleyGenesis :: IO (ShelleyGenesis StandardCrypto)
+getShelleyGenesis = do
+  either error id <$> Aeson.eitherDecodeFileStrict' "/home/lehins/Downloads/genesis.json"
+
+mkGlobals :: PParams Conway -> IO Globals
+mkGlobals pp = do
+  genesis <- getShelleyGenesis
+  pure $ mkShelleyGlobals genesis (epochInfoE genesis) majorPParamsVer
+  where
+    majorPParamsVer = pvMajor $ pp ^. ppProtocolVersionL
+    epochInfoE genesis =
+      fixedEpochInfo
+        (sgEpochLength genesis)
+        (mkSlotLength . fromNominalDiffTimeMicro . sgSlotLength $ genesis)
+
+mkEnv :: NewEpochState Conway -> LedgerEnv Conway
+mkEnv nes =
+  LedgerEnv
+    { ledgerSlotNo = SlotNo 123456
+    , ledgerIx = mkTxIxPartial 0
+    , ledgerPp = pp
+    , ledgerAccount = as
+    }
+  where
+    pp = esPp (nesEs nes)
+    as = esAccountState (nesEs nes)
 
 type Conway = ConwayEra StandardCrypto
 
@@ -82,3 +149,5 @@ instance
       referencedOuts = Map.elems $ Map.restrictKeys utxo (txBody ^. referenceInputsTxBodyL)
       outs = newOuts <> referencedOuts
   getDatum = getDatumBabbage
+
+-- cabal exec -- ghci -package cardano-ledger-core -package cardano-ledger-conway -package cardano-ledger-shelley -package cardano-ledger-babbage
