bf2dc0fdde313723b4f7a35362839920670e67ea
Joosep Jääger <joosep.jaager@iohk.io>
2023-08-09 17:49:01 +0300

    Implemented DRep refreshing

--- eras/conway/impl/src/Cardano/Ledger/Conway/Rules/GovCert.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway/Rules/GovCert.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -15,15 +16,19 @@ module Cardano.Ledger.Conway.Rules.GovCert (
   ConwayGOVCERT,
   ConwayGovCertEvent (..),
   ConwayGovCertPredFailure (..),
+  ConwayGovCertEnv (..),
 )
 where
 
 import Cardano.Ledger.BaseTypes (
+  EpochNo,
   ShelleyBase,
+  SlotNo,
+  epochInfoPure,
  )
 import Cardano.Ledger.Binary (DecCBOR (..), EncCBOR (..), encodeListLen)
 import Cardano.Ledger.Binary.Coders
-import Cardano.Ledger.CertState (DRepState (..), VState (..))
+import Cardano.Ledger.CertState (DRepState (..), VState (..), vsDRepsL)
 import Cardano.Ledger.Coin (Coin)
 import Cardano.Ledger.Conway.Core (ConwayEraPParams, ppDRepActivityL)
 import Cardano.Ledger.Conway.Era (ConwayGOVCERT)
@@ -31,19 +36,24 @@ import Cardano.Ledger.Conway.TxCert (ConwayGovCert (..))
 import Cardano.Ledger.Core (Era (EraCrypto), EraRule, PParams)
 import Cardano.Ledger.Credential (Credential)
 import Cardano.Ledger.Crypto (Crypto)
+import Cardano.Ledger.DRepDistr (drepAnchorL, drepExpiryL)
 import Cardano.Ledger.Keys (KeyRole (ColdCommitteeRole, DRepRole))
+import Cardano.Ledger.Slot (epochInfoEpoch)
 import Control.DeepSeq (NFData)
+import Control.Monad.Trans.Reader (asks)
 import Control.State.Transition.Extended (
   BaseM,
   Environment,
   Event,
   PredicateFailure,
+  Rule,
   STS,
   Signal,
   State,
   TRC (TRC),
   TransitionRule,
   judgmentContext,
+  liftSTS,
   transitionRules,
   (?!),
  )
@@ -52,8 +62,13 @@ import Data.Maybe (isNothing)
 import Data.Typeable (Typeable)
 import Data.Word (Word8)
 import GHC.Generics (Generic)
-import Lens.Micro ((^.))
+import Lens.Micro ((%~), (&), (.~), (^.))
 import NoThunks.Class (NoThunks (..))
+
+data ConwayGovCertEnv era = ConwayGovCertEnv
+  { cgcePParams :: !(PParams era)
+  , cgceCurrentSlot :: !SlotNo
+  }
 
 data ConwayGovCertPredFailure era
   = ConwayDRepAlreadyRegistered !(Credential 'DRepRole (EraCrypto era))
@@ -114,7 +129,7 @@ instance
   ( ConwayEraPParams era
   , State (EraRule "GOVCERT" era) ~ VState era
   , Signal (EraRule "GOVCERT" era) ~ ConwayGovCert (EraCrypto era)
-  , Environment (EraRule "GOVCERT" era) ~ PParams era
+  , Environment (EraRule "GOVCERT" era) ~ ConwayGovCertEnv era
   , EraRule "GOVCERT" era ~ ConwayGOVCERT era
   , Eq (PredicateFailure (EraRule "GOVCERT" era))
   , Show (PredicateFailure (EraRule "GOVCERT" era))
@@ -123,18 +138,33 @@ instance
   where
   type State (ConwayGOVCERT era) = VState era
   type Signal (ConwayGOVCERT era) = ConwayGovCert (EraCrypto era)
-  type Environment (ConwayGOVCERT era) = PParams era
+  type Environment (ConwayGOVCERT era) = ConwayGovCertEnv era
   type BaseM (ConwayGOVCERT era) = ShelleyBase
   type PredicateFailure (ConwayGOVCERT era) = ConwayGovCertPredFailure era
   type Event (ConwayGOVCERT era) = ConwayGovCertEvent era
 
   transitionRules = [conwayGovCertTransition @era]
 
+calcDRepExpiryEpoch ::
+  ( ConwayEraPParams era
+  , Eq (PredicateFailure (EraRule "GOVCERT" era))
+  , Show (PredicateFailure (EraRule "GOVCERT" era))
+  , EraRule "GOVCERT" era ~ ConwayGOVCERT era
+  ) =>
+  SlotNo ->
+  EpochNo ->
+  Rule (ConwayGOVCERT era) ctx EpochNo
+calcDRepExpiryEpoch slot drepActivity = do
+  curEpoch <- liftSTS $ do
+    ei <- asks epochInfoPure
+    epochInfoEpoch ei slot
+  pure $ curEpoch + drepActivity
+
 conwayGovCertTransition ::
-  ConwayEraPParams era => TransitionRule (ConwayGOVCERT era)
+  (ConwayEraPParams era, EraRule "GOVCERT" era ~ ConwayGOVCERT era) => TransitionRule (ConwayGOVCERT era)
 conwayGovCertTransition = do
   TRC
-    ( pp
+    ( ConwayGovCertEnv {..}
       , vState@VState {vsDReps, vsCommitteeHotKeys}
       , c
       ) <-
@@ -143,11 +173,16 @@ conwayGovCertTransition = do
     ConwayRegDRep cred _deposit mAnchor -> do
       Map.notMember cred vsDReps ?! ConwayDRepAlreadyRegistered cred
       -- TODO: check against a new PParam `drepDeposit`, once PParams are updated. -- someCheck ?! ConwayDRepIncorrectDeposit deposit
-      let drepState =
-            DRepState
-              { drepExpiry = pp ^. ppDRepActivityL
-              , drepAnchor = mAnchor
-              }
+      drepState <- do
+        expiryEpoch <-
+          calcDRepExpiryEpoch
+            cgceCurrentSlot
+            (cgcePParams ^. ppDRepActivityL)
+        pure
+          DRepState
+            { drepExpiry = expiryEpoch
+            , drepAnchor = mAnchor
+            }
       pure $ vState {vsDReps = Map.insert cred drepState vsDReps}
     ConwayUnRegDRep cred _deposit -> do
       -- TODO: check against a new PParam `drepDeposit`, once PParams are updated. -- someCheck ?! ConwayDRepIncorrectDeposit deposit
@@ -159,9 +194,17 @@ conwayGovCertTransition = do
     ConwayResignCommitteeColdKey coldK -> do
       checkColdKeyHasNotResigned coldK vsCommitteeHotKeys
       pure $ vState {vsCommitteeHotKeys = Map.insert coldK Nothing vsCommitteeHotKeys}
-    ConwayUpdateDRep cred _mAnchor -> do
+    ConwayUpdateDRep cred mAnchor -> do
       Map.notMember cred vsDReps ?! ConwayDRepNotRegistered cred
-      pure vState -- TODO: update anchor
+      expiryEpoch <-
+        calcDRepExpiryEpoch
+          cgceCurrentSlot
+          (cgcePParams ^. ppDRepActivityL)
+      let updateDRepState drepState =
+            drepState
+              & drepExpiryL .~ expiryEpoch
+              & drepAnchorL .~ mAnchor
+      pure $ vState & vsDRepsL %~ Map.update (Just . updateDRepState) cred
   where
     checkColdKeyHasNotResigned coldK vsCommitteeHotKeys =
       ((isNothing <$> Map.lookup coldK vsCommitteeHotKeys) /= Just True)
