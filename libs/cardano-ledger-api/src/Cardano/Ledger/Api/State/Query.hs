01dd49f4a29f92f42f41473843789b3459898cc8
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-08-14 18:52:32 +0200

    Add `queryGovState`, `queryDRepState`, `queryDRepStakeDistr` and `queryCommitteeState`

--- libs/cardano-ledger-api/src/Cardano/Ledger/Api/State/Query.hs
+++ libs/cardano-ledger-api/src/Cardano/Ledger/Api/State/Query.hs
@@ -6,17 +6,35 @@ module Cardano.Ledger.Api.State.Query (
   filterStakePoolDelegsAndRewards,
   queryStakePoolDelegsAndRewards,
 
+  -- * @GetGovState@
+  queryGovState,
+
+  -- * @GetConstitution@
+  queryConstitution,
+
   -- * @GetConstitutionHash@
   queryConstitutionHash,
+
+  -- * @GetDRepState@
+  queryDRepState,
+
+  -- * @GetDRepStakeDistr@
+  queryDRepStakeDistr,
+
+  -- * @GetCommitteeState@
+  queryCommitteeState,
 ) where
 
 import Cardano.Ledger.Allegra.Core (Constitution (constitutionAnchor))
+import Cardano.Ledger.CertState
 import Cardano.Ledger.Coin (Coin)
-import Cardano.Ledger.Conway.Governance (Anchor (..), AnchorData, EraGov (..))
+import Cardano.Ledger.Compactible (fromCompact)
 import Cardano.Ledger.Core
 import Cardano.Ledger.Credential (Credential)
-import Cardano.Ledger.Keys (KeyHash, KeyRole (StakePool, Staking))
+import Cardano.Ledger.DRepDistr (extractDRepDistr)
+import Cardano.Ledger.Keys (KeyHash, KeyRole (..))
 import Cardano.Ledger.SafeHash (SafeHash)
+import Cardano.Ledger.Shelley.Governance (EraGov (GovState, getConstitution))
 import Cardano.Ledger.Shelley.LedgerState
 import Cardano.Ledger.UMap (
   StakeCredentials (scRewards, scSPools),
@@ -24,6 +42,7 @@ import Cardano.Ledger.UMap (
   domRestrictedStakeCredentials,
  )
 import Data.Map (Map)
+import qualified Data.Map.Strict as Map
 import Data.Set (Set)
 import Lens.Micro
 
@@ -52,8 +71,7 @@ getDState :: NewEpochState era -> DState era
 getDState = certDState . lsCertState . esLState . nesEs
 
 queryConstitution :: EraGov era => NewEpochState era -> Maybe (Constitution era)
-queryConstitution nes =
-  getConstitution (nes ^. nesEpochStateL . esLStateL . lsUTxOStateL . utxosGovStateL)
+queryConstitution = getConstitution . queryGovState
 
 queryConstitutionHash ::
   EraGov era =>
@@ -61,3 +79,40 @@ queryConstitutionHash ::
   Maybe (SafeHash (EraCrypto era) AnchorData)
 queryConstitutionHash nes =
   anchorDataHash . constitutionAnchor <$> queryConstitution nes
+
+-- | This query returns all of the state related to governance
+queryGovState :: NewEpochState era -> GovState era
+queryGovState nes = nes ^. nesEpochStateL . esLStateL . lsUTxOStateL . utxosGovStateL
+
+-- | Query DRep state.
+queryDRepState ::
+  NewEpochState era ->
+  -- | Specify a set of DRep credentials whose state should be returned. When this set is
+  -- empty, states for all of the DReps will be returned.
+  Set (Credential 'DRepRole (EraCrypto era)) ->
+  Map (Credential 'DRepRole (EraCrypto era)) (DRepState (EraCrypto era))
+queryDRepState nes creds
+  | null creds = drepsState
+  | otherwise = drepsState `Map.restrictKeys` creds
+  where
+    drepsState = vsDReps $ certVState $ lsCertState $ esLState $ nesEs nes
+
+-- | Query DRep stake distribution. Note that this can be an expensive query because there
+-- is a chance that current distribution has not been fully computed yet.
+queryDRepStakeDistr ::
+  NewEpochState era ->
+  -- | Specify DRep Ids whose stake distribution should be returned. When this set is
+  -- empty, distributions for all of the DReps will be returned.
+  Set (DRep (EraCrypto era)) ->
+  Map (DRep (EraCrypto era)) Coin
+queryDRepStakeDistr nes creds
+  | null creds = Map.map fromCompact distr
+  | otherwise = Map.map fromCompact $ distr `Map.restrictKeys` creds
+  where
+    distr = extractDRepDistr distrPulser
+    distrPulser = vsDRepDistr $ certVState $ lsCertState $ esLState $ nesEs nes
+
+-- | Query committee members
+queryCommitteeState :: NewEpochState era -> CommitteeState era
+queryCommitteeState nes =
+  vsCommitteeState $ certVState $ lsCertState $ esLState $ nesEs nes
