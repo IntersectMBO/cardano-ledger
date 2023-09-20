7aa2c7ada1bf37718e4ece88782507589846e083
teodanciu <teodora.danciu@tweag.io>
2023-09-20 03:01:54 +0100

    Add tests for committee ratification logic

--- eras/conway/impl/test/Main.hs
+++ eras/conway/impl/test/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Conway.BinarySpec as BinarySpec
+import qualified Test.Cardano.Ledger.Conway.CommitteeRatifySpec as CommitteeRatifySpec
 import qualified Test.Cardano.Ledger.Conway.GenesisSpec as GenesisSpec
 import qualified Test.Cardano.Ledger.Conway.GovActionReorderSpec as GovActionReorderSpec
 import qualified Test.Cardano.Ledger.Conway.RatifySpec as RatifySpec
@@ -12,5 +13,6 @@ main =
     describe "Conway" $ do
       BinarySpec.spec
       RatifySpec.spec
+      CommitteeRatifySpec.spec
       GenesisSpec.spec
       GovActionReorderSpec.spec
