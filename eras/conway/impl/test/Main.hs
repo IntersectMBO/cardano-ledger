9dd1f1f97cd5fa1b50e7090ab880ced2d1bb55ac
teodanciu <teodora.danciu@tweag.io>
2023-09-20 03:11:27 +0100

    Rename `RatifySpec` to `DRepRatifySpec`
    to make the specialization clear and for uniformity with
    `CommitteeRatifySpec`

--- eras/conway/impl/test/Main.hs
+++ eras/conway/impl/test/Main.hs
@@ -3,16 +3,16 @@ module Main where
 import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Conway.BinarySpec as BinarySpec
 import qualified Test.Cardano.Ledger.Conway.CommitteeRatifySpec as CommitteeRatifySpec
+import qualified Test.Cardano.Ledger.Conway.DRepRatifySpec as DRepRatifySpec
 import qualified Test.Cardano.Ledger.Conway.GenesisSpec as GenesisSpec
 import qualified Test.Cardano.Ledger.Conway.GovActionReorderSpec as GovActionReorderSpec
-import qualified Test.Cardano.Ledger.Conway.RatifySpec as RatifySpec
 
 main :: IO ()
 main =
   ledgerTestMain $
     describe "Conway" $ do
       BinarySpec.spec
-      RatifySpec.spec
+      DRepRatifySpec.spec
       CommitteeRatifySpec.spec
       GenesisSpec.spec
       GovActionReorderSpec.spec
