9133249e7a201353c8f616afc6b1d5c57e099bfa
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-09-29 17:12:44 +0100

    Remove duplicate serializatio roundtrip tests for ConwayEra

--- eras/conway/test-suite/test/Tests.hs
+++ eras/conway/test-suite/test/Tests.hs
@@ -7,7 +7,6 @@ import Data.Proxy (Proxy (..))
 import qualified Test.Cardano.Ledger.Babbage.TxInfo as Babbage (txInfoTests)
 import Test.Cardano.Ledger.Conway.GovSnapshot (govSnapshotProps)
 import qualified Test.Cardano.Ledger.Conway.Serialisation.CDDL as CDDL
-import qualified Test.Cardano.Ledger.Conway.Serialisation.Roundtrip as Roundtrip
 import qualified Test.Cardano.Ledger.Conway.TxInfo as Conway (txInfoTests)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
@@ -18,8 +17,7 @@ defaultTests :: TestTree
 defaultTests =
   testGroup
     "Conway tests"
-    [ Roundtrip.allprops @Conway
-    , CDDL.tests 10
+    [ CDDL.tests 10
     , Babbage.txInfoTests (Proxy @Conway)
     , Conway.txInfoTests (Proxy @Conway)
     , govSnapshotProps
