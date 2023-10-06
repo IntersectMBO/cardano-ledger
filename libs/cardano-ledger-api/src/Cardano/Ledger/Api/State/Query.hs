bde159b335552120414b863dd28dec73950dba27
teodanciu <teodora.danciu@tweag.io>
2023-10-06 02:12:56 +0100

    Add new query for committee members and a model to represent the result
    Deprecate existing `queryCommitteeState` in favour of the new query

--- libs/cardano-ledger-api/src/Cardano/Ledger/Api/State/Query.hs
+++ libs/cardano-ledger-api/src/Cardano/Ledger/Api/State/Query.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.Ledger.Api.State.Query (
   -- * @GetFilteredDelegationsAndRewardAccounts@
@@ -23,9 +25,24 @@ module Cardano.Ledger.Api.State.Query (
 
   -- * @GetCommitteeState@
   queryCommitteeState,
+
+  -- * @GetCommitteeState@
+  queryCommitteeMembersState,
+  CommitteeMemberState (..),
+  CommitteeMembersState (..),
+  HotCredAuthStatus (..),
+  MemberStatus (..),
+  NextEpochChange (..),
 ) where
 
 import Cardano.Ledger.Allegra.Core (Constitution (constitutionAnchor))
+import Cardano.Ledger.Api.State.Query.CommitteeMembersState (
+  CommitteeMemberState (..),
+  CommitteeMembersState (..),
+  HotCredAuthStatus (..),
+  MemberStatus (..),
+  NextEpochChange (..),
+ )
 import Cardano.Ledger.CertState
 import Cardano.Ledger.Coin (Coin)
 import Cardano.Ledger.Compactible (fromCompact)
@@ -34,17 +51,27 @@ import Cardano.Ledger.Credential (Credential)
 import Cardano.Ledger.DRepDistr (drepExpiryL)
 import Cardano.Ledger.Keys (KeyHash, KeyRole (..))
 import Cardano.Ledger.SafeHash (SafeHash)
-import Cardano.Ledger.Shelley.Governance (EraGov (GovState, getConstitution, getDRepDistr))
+import Cardano.Ledger.Shelley.Governance (
+  EraGov (
+    GovState,
+    getCommitteeMembers,
+    getConstitution,
+    getDRepDistr
+  ),
+ )
 import Cardano.Ledger.Shelley.LedgerState
 import Cardano.Ledger.UMap (
   StakeCredentials (scRewards, scSPools),
   UMap,
   domRestrictedStakeCredentials,
  )
+import Cardano.Slotting.Slot (EpochNo)
 import Data.Map (Map)
 import qualified Data.Map.Strict as Map
+import Data.Maybe (maybeToList)
 import Data.Set (Set)
-import Lens.Micro
+import qualified Data.Set as Set
+import Lens.Micro ((%~), (&), (<&>), (^.))
 
 -- | Filter out stake pool delegations and rewards for a set of stake credentials
 filterStakePoolDelegsAndRewards ::
@@ -121,3 +148,103 @@ queryDRepStakeDistr nes creds
 queryCommitteeState :: NewEpochState era -> CommitteeState era
 queryCommitteeState nes =
   vsCommitteeState $ certVState $ lsCertState $ esLState $ nesEs nes
+{-# DEPRECATED queryCommitteeState "In favor of `queryCommitteeMembersState`" #-}
+
+-- | Query committee members
+queryCommitteeMembersState ::
+  forall era.
+  EraGov era =>
+  -- | filter by cold credentials (don't filter when empty)
+  Set (Credential 'ColdCommitteeRole (EraCrypto era)) ->
+  -- | filter by hot credentials (don't filter when empty)
+  Set (Credential 'HotCommitteeRole (EraCrypto era)) ->
+  -- | filter by status (don't filter when empty)
+  -- (useful, for discovering, for example, only active members)
+  Set MemberStatus ->
+  NewEpochState era ->
+  CommitteeMembersState (EraCrypto era)
+queryCommitteeMembersState coldCredsFilter hotCredsFilter statusFilter nes =
+  let comStateMembers =
+        csCommitteeCreds $
+          nes ^. nesEpochStateL . esLStateL . lsCertStateL . certVStateL . vsCommitteeStateL
+      comMembers = getCommitteeMembers (queryGovState nes)
+
+      filteredComMembers
+        | null coldCredsFilter = comMembers
+        | otherwise = comMembers `Map.restrictKeys` coldCredsFilter
+
+      -- Start from the committee and lookup the hot credential in the Committee State.
+      -- This will miss 'Unrecognized' members (since we're iterating over the "recognized" ones).
+      recognized :: [(Credential 'ColdCommitteeRole (EraCrypto era), CommitteeMemberState (EraCrypto era))]
+      recognized =
+        Map.assocs filteredComMembers >>= \(ck, expirationEpoch) ->
+          let mbHk = Map.lookup ck comStateMembers
+              mbCommitteeMemberState = do
+                hkStatus <- hotCredStatus mbHk
+                status <- memberStatus expirationEpoch
+                let cms =
+                      CommitteeMemberState
+                        hkStatus
+                        status
+                        (Just expirationEpoch)
+                        nextEpochChange
+                pure (ck, cms)
+           in maybeToList mbCommitteeMemberState
+
+      unrecognizedMembers = Map.difference comStateMembers comMembers
+      filteredUnrecognizedMembers
+        | null coldCredsFilter = unrecognizedMembers
+        | otherwise = Map.restrictKeys unrecognizedMembers coldCredsFilter
+
+      -- Check for Unrecognized members, by starting from the CommitteeState
+      unrecognized :: [(Credential 'ColdCommitteeRole (EraCrypto era), CommitteeMemberState (EraCrypto era))]
+      unrecognized
+        -- we only look for Unrecognized members when it is contained in the filter, or when there is no filtering
+        | Set.null statusFilter || Set.member Unrecognized statusFilter =
+            unrecognized' filteredUnrecognizedMembers
+        | otherwise = []
+
+      unrecognized' ::
+        Map (Credential 'ColdCommitteeRole (EraCrypto era)) (Maybe (Credential 'HotCommitteeRole (EraCrypto era))) ->
+        [(Credential 'ColdCommitteeRole (EraCrypto era), CommitteeMemberState (EraCrypto era))]
+      unrecognized' members =
+        Map.assocs members >>= \(ck, mbHk) ->
+          let mbCommitteeMemberState = do
+                hkStatus <- hotCredStatus (Just mbHk)
+                let cms =
+                      CommitteeMemberState
+                        hkStatus
+                        Unrecognized
+                        Nothing
+                        nextEpochChange
+                pure (ck, cms)
+           in maybeToList mbCommitteeMemberState
+   in CommitteeMembersState
+        { csCommittee = Map.fromList (recognized ++ unrecognized)
+        , csEpochNo = nes ^. nesELL - 1
+        }
+  where
+    hotCredStatus ::
+      Maybe (Maybe (Credential 'HotCommitteeRole (EraCrypto era))) ->
+      Maybe (HotCredAuthStatus (EraCrypto era))
+    hotCredStatus Nothing = Just MemberNotAuthorized
+    hotCredStatus (Just Nothing) = Just MemberResigned
+    hotCredStatus (Just (Just hk)) = filtered hotCredsFilter hk MemberAuthorized
+
+    memberStatus :: EpochNo -> Maybe MemberStatus
+    memberStatus expiry =
+      let status =
+            if (nes ^. nesELL - 1) > expiry
+              then Expired
+              else Active
+       in filtered statusFilter status id
+
+    -- TODO: implement this
+    nextEpochChange :: NextEpochChange
+    nextEpochChange = NoChangeExpected
+
+    filtered :: Ord a => Set a -> a -> (a -> b) -> Maybe b
+    filtered fl x f
+      | Set.null fl = Just (f x)
+      | Set.member x fl = Just (f x)
+      | otherwise = Nothing
