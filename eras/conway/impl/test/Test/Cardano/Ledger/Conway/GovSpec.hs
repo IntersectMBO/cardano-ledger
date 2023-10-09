dd9f1cf1c1cc6b22f4e8eddd86f332ef1ef8b0b5
Aniket Deshpande <aniket.deshpande@iohk.io>
2023-10-09 15:00:16 +0530

    Fix tests as per suggestions

--- eras/conway/impl/test/Test/Cardano/Ledger/Conway/GovSpec.hs
+++ eras/conway/impl/test/Test/Cardano/Ledger/Conway/GovSpec.hs
@@ -16,8 +16,53 @@ import Test.Cardano.Ledger.Conway.ImpTest
 spec :: Spec
 spec =
   describe "Proposals always have valid previous actions" $ do
+    itM @Conway "First ever proposal is accepted without needing a PrevGovActionId but after it is enacted, the following ones are not" $ do
+      constitutionHash <- freshSafeHash
+      let constitutionAction =
+            NewConstitution
+              SNothing
+              ( Constitution
+                  ( Anchor
+                      (fromJust $ textToUrl "constitution.0")
+                      constitutionHash
+                  )
+                  SNothing
+              )
+      gaidConstitutionProp <- submitProposal constitutionAction
+      constitutionHash' <- freshSafeHash
+      let constitutionAction' =
+            NewConstitution
+              SNothing
+              ( Constitution
+                  ( Anchor
+                      (fromJust $ textToUrl "constitution.0")
+                      constitutionHash'
+                  )
+                  SNothing
+              )
+      -- Until the first proposal is enacted all proposals with empty PrevGovActionIds are valid
+      _gaidConstitutionProp' <- submitProposal constitutionAction'
+      modifyNES $ \nes ->
+        nes
+          & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgEnactStateL . ensPrevConstitutionL
+            .~ SJust (PrevGovActionId gaidConstitutionProp) -- Add first proposal to PrevGovActionIds in enacted state
+          & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgGovSnapshotsL
+            .~ def -- Remove all proposals from snapshots, so that the lookup only succeeds for enacted state
+      constitutionHash'' <- freshSafeHash
+      -- Once a proposal with a purpose has been enacted, following proposals can no longer have empty PrevGovActionIds
+      let constitutionAction'' =
+            NewConstitution
+              SNothing
+              ( Constitution
+                  ( Anchor
+                      (fromJust $ textToUrl "constitution.0")
+                      constitutionHash''
+                  )
+                  SNothing
+              )
+      submitFailingProposal constitutionAction''
     context "Invalid proposals are rejected" $ do
-      itM @Conway "Lookup snapshots" $ do
+      itM @Conway "Lookup in snapshots for invalid PrevGovActionId" $ do
         constitutionHash <- freshSafeHash
         let constitutionAction =
               NewConstitution
@@ -32,7 +77,7 @@ spec =
         gaidConstitutionProp <- submitProposal constitutionAction
         let constitutionActionNext =
               NewConstitution
-                (SJust $ PrevGovActionId (gaidConstitutionProp {gaidGovActionIx = GovActionIx 1})) -- make it wrong
+                (SJust $ PrevGovActionId (gaidConstitutionProp {gaidGovActionIx = GovActionIx 1})) -- Expected Ix = 0
                 ( Constitution
                     ( Anchor
                         (fromJust $ textToUrl "constitution.1")
@@ -41,7 +86,23 @@ spec =
                     SNothing
                 )
         submitFailingProposal constitutionActionNext
-      itM @Conway "Lookup snapshots" $ do
+      itM @Conway "Lookup in snapshots for valid PrevGovActionId but invalid purpose" $ do
+        constitutionHash <- freshSafeHash
+        let constitutionAction =
+              NewConstitution
+                SNothing
+                ( Constitution
+                    ( Anchor
+                        (fromJust $ textToUrl "constitution.0")
+                        constitutionHash
+                    )
+                    SNothing
+                )
+        gaidConstitutionProp <- submitProposal constitutionAction
+        let noConfidenceAction =
+              NoConfidence $ SJust $ PrevGovActionId gaidConstitutionProp
+        submitFailingProposal noConfidenceAction
+      itM @Conway "Lookup enacted state for invalid PrevGovActionId" $ do
         constitutionHash <- freshSafeHash
         let constitutionAction =
               NewConstitution
@@ -57,12 +118,12 @@ spec =
         modifyNES $ \nes ->
           nes
             & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgEnactStateL . ensPrevConstitutionL
-              .~ SJust (PrevGovActionId gaidConstitutionProp) -- Add the wrong one to PrevGovActionIds
+              .~ SJust (PrevGovActionId gaidConstitutionProp) -- Add it to PrevGovActionIds in enacted state
             & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgGovSnapshotsL
-              .~ def -- Remove from snapshots
+              .~ def -- Remove from snapshots, so that the lookup only succeeds for enacted state
         let constitutionActionNext =
               NewConstitution
-                (SJust $ PrevGovActionId (gaidConstitutionProp {gaidGovActionIx = GovActionIx 1})) -- make it wrong
+                (SJust $ PrevGovActionId (gaidConstitutionProp {gaidGovActionIx = GovActionIx 1})) -- Expected Ix = 0
                 ( Constitution
                     ( Anchor
                         (fromJust $ textToUrl "constitution.1")
@@ -71,8 +132,30 @@ spec =
                     SNothing
                 )
         submitFailingProposal constitutionActionNext
+      itM @Conway "Lookup enacted state for valid PrevGovActionId but invalid purpose" $ do
+        constitutionHash <- freshSafeHash
+        let constitutionAction =
+              NewConstitution
+                SNothing
+                ( Constitution
+                    ( Anchor
+                        (fromJust $ textToUrl "constitution.0")
+                        constitutionHash
+                    )
+                    SNothing
+                )
+        gaidConstitutionProp <- submitProposal constitutionAction
+        modifyNES $ \nes ->
+          nes
+            & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgEnactStateL . ensPrevConstitutionL
+              .~ SJust (PrevGovActionId gaidConstitutionProp) -- Add it to PrevGovActionIds in enacted state
+            & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgGovSnapshotsL
+              .~ def -- Remove from snapshots, so that the lookup only succeeds for enacted state
+        let noConfidenceAction =
+              NoConfidence $ SJust $ PrevGovActionId gaidConstitutionProp
+        submitFailingProposal noConfidenceAction
     context "Valid proposals are accepted" $ do
-      itM @Conway "Lookup snapshots" $ do
+      itM @Conway "Lookup snapshots for valid PrevGovActionId and purpose" $ do
         constitutionHash <- freshSafeHash
         let constitutionAction =
               NewConstitution
@@ -96,7 +179,7 @@ spec =
                     SNothing
                 )
         submitProposal constitutionActionNext
-      itM @Conway "Lookup PrevGovActionIds" $ do
+      itM @Conway "Lookup enacted state for valid PrevGovActionId and purpose" $ do
         constitutionHash <- freshSafeHash
         let constitutionAction =
               NewConstitution
@@ -112,9 +195,9 @@ spec =
         modifyNES $ \nes ->
           nes
             & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgEnactStateL . ensPrevConstitutionL
-              .~ SJust (PrevGovActionId gaidConstitutionProp) -- Add to PrevGovActionIds
+              .~ SJust (PrevGovActionId gaidConstitutionProp) -- Add it to PrevGovActionIds in enacted state
             & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgGovSnapshotsL
-              .~ def -- Remove from snapshots
+              .~ def -- Remove from snapshots, so that the lookup only succeeds for enacted state
         let constitutionActionNext =
               NewConstitution
                 (SJust $ PrevGovActionId gaidConstitutionProp)
