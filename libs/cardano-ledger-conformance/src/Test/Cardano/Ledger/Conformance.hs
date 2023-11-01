33ed732122e14a46e4474581c32f7cb3c86d56c6
Joosep Jääger <joosep.jaager@iohk.io>
2023-11-01 13:40:10 +0200

    Added cardano-ledger-conformance

---
+++ libs/cardano-ledger-conformance/src/Test/Cardano/Ledger/Conformance.hs
@@ -0,0 +1,10 @@
+module Test.Cardano.Ledger.Conformance (
+
+) where
+
+import Lib (TxBody)
+
+-- Placeholder so that the compiler wouldn't complain about
+-- `cardano-ledger-executable-spec` being a redundant dependency
+_placeholder :: TxBody
+_placeholder = undefined
