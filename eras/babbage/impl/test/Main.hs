e2f6c3b0e30676ab65e07661a7b0db401dcd5b25
teodanciu <teodora.danciu@tweag.io>
2023-11-12 18:06:09 +0000

    Add tests for each post-alonzo era to verify CostModels deserialization
    both independently and within PParamsUpdate

--- eras/babbage/impl/test/Main.hs
+++ eras/babbage/impl/test/Main.hs
@@ -3,6 +3,8 @@
 module Main where
 
 import Cardano.Ledger.Babbage (Babbage)
+import Data.Proxy (Proxy (Proxy))
+import qualified Test.Cardano.Ledger.Alonzo.Binary.CostModelsSpec as CostModelsSpec
 import qualified Test.Cardano.Ledger.Babbage.Binary.CddlSpec as CddlSpec
 import qualified Test.Cardano.Ledger.Babbage.BinarySpec as BinarySpec
 import Test.Cardano.Ledger.Babbage.ImpTest ()
@@ -17,3 +19,5 @@ main =
       CddlSpec.spec
       describe "Imp" $ do
         ShelleyImp.spec @Babbage
+      describe "CostModels" $ do
+        CostModelsSpec.spec @Babbage Proxy
