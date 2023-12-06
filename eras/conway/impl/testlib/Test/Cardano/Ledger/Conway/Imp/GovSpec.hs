26302e6dc4ad810465268de72743a4941e908f16
teodanciu <teodora.danciu@tweag.io>
2023-12-06 13:43:22 +0000

    Regroup and rename tests in `GovSpec` for more clarity

--- eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/GovSpec.hs
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/GovSpec.hs
@@ -35,183 +35,145 @@ spec ::
   SpecWith (ImpTestState era)
 spec =
   describe "GOV" $ do
-    let submitInitConstitutionGovAction = do
-          constitutionHash <- freshSafeHash
-          let constitutionAction =
+    describe "Voting" $ do
+      context "fails for" $ do
+        it "expired gov-actions" $ do
+          modifyPParams $ ppGovActionLifetimeL .~ 2 -- Voting after the 3rd epoch should fail
+          khDRep <- setupSingleDRep
+          (govActionId, _) <- submitConstitution SNothing
+          passEpoch
+          passEpoch
+          passEpoch
+          let voter = DRepVoter $ KeyHashObj khDRep
+          submitFailingVote
+            voter
+            govActionId
+            [ inject $
+                VotingOnExpiredGovAction @era $
+                  Map.singleton govActionId voter
+            ]
+        it "non-existent gov-actions" $ do
+          khDRep <- setupSingleDRep
+          (govActionId, _) <- submitConstitution SNothing
+          let voter = DRepVoter $ KeyHashObj khDRep
+              dummyGaid = govActionId {gaidGovActionIx = GovActionIx 99} -- non-existent `GovActionId`
+          submitFailingVote
+            voter
+            dummyGaid
+            [inject $ GovActionsDoNotExist @era $ Set.singleton dummyGaid]
+
+    describe "Constitution proposals" $ do
+      context "accepted for" $
+        it "empty PrevGovId before the first constitution is enacted" $ do
+          --  Initial proposal does not need a PrevGovActionId but after it is enacted, the
+          --  following ones are not
+          _ <- submitConstitution SNothing
+          -- Until the first proposal is enacted all proposals with empty PrevGovActionIds are valid
+          void $ submitConstitution SNothing
+
+      context "rejected for" $ do
+        it "empty PrevGovId after the first constitution was enacted" $ do
+          (govActionId, _) <- submitConstitution SNothing
+          modifyNES $ \nes ->
+            nes
+              & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgEnactStateL . ensPrevConstitutionL
+                .~ SJust (PrevGovActionId govActionId) -- Add first proposal to PrevGovActionIds in enacted state
+              & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgProposalsL
+                .~ def -- Remove all proposals, so that the lookup only succeeds for enacted state.
+                -- Once a proposal with a purpose has been enacted, following proposals can no
+                -- longer have empty PrevGovActionIds
+          constitution <- newConstitution
+          let invalidNewConstitutionGovAction =
                 NewConstitution
                   SNothing
-                  ( Constitution
-                      ( Anchor
-                          (fromJust $ textToUrl "constitution.0")
-                          constitutionHash
-                      )
-                      SNothing
-                  )
-          submitGovAction constitutionAction
-    describe "Votes fail as expected" $ do
-      it "on expired gov-actions" $ do
-        modifyPParams $ ppGovActionLifetimeL .~ 2 -- Voting after the 3rd epoch should fail
-        khDRep <- setupSingleDRep
-        gaidConstitutionProp <- submitInitConstitutionGovAction
-        passEpoch
-        passEpoch
-        passEpoch
-        let voter = DRepVoter $ KeyHashObj khDRep
-        submitFailingVote voter gaidConstitutionProp $
-          [ inject $
-              VotingOnExpiredGovAction @era $
-                Map.singleton gaidConstitutionProp voter
-          ]
-      it "on non-existant gov-actions" $ do
-        khDRep <- setupSingleDRep
-        gaidConstitutionProp <- submitInitConstitutionGovAction
-        let voter = DRepVoter $ KeyHashObj khDRep
-            dummyGaid = gaidConstitutionProp {gaidGovActionIx = GovActionIx 99} -- non-existant `GovActionId`
-        submitFailingVote voter dummyGaid $
-          [inject $ GovActionsDoNotExist @era $ Set.singleton dummyGaid]
-
-    describe "Proposals always have valid previous actions" $ do
-      it "Another proposal after the first ever proposal is accepted" $ do
-        --  Initial proposal does not need a PrevGovActionId but after it is enacted, the
-        --  following ones are not
-        gaidConstitutionProp <- submitInitConstitutionGovAction
-        constitutionHash' <- freshSafeHash
-        let constitutionAction' =
-              NewConstitution
-                SNothing
-                ( Constitution
-                    ( Anchor
-                        (fromJust $ textToUrl "constitution.0")
-                        constitutionHash'
-                    )
-                    SNothing
-                )
-        -- Until the first proposal is enacted all proposals with empty PrevGovActionIds are valid
-        submitGovAction_ constitutionAction'
-        modifyNES $ \nes ->
-          nes
-            & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgEnactStateL . ensPrevConstitutionL
-              .~ SJust (PrevGovActionId gaidConstitutionProp) -- Add first proposal to PrevGovActionIds in enacted state
-            & nesEsL . esLStateL . lsUTxOStateL . utxosGovStateL . cgProposalsL
-              .~ def -- Remove all proposals, so that the lookup only succeeds for ecacted state.
-        constitutionHash'' <- freshSafeHash
-        -- Once a proposal with a purpose has been enacted, following proposals can no
-        -- longer have empty PrevGovActionIds
-        rewardAccount <- registerRewardAccount
-        let invalidNewConstitutionGovAction =
-              NewConstitution
-                SNothing
-                ( Constitution
-                    ( Anchor
-                        (fromJust $ textToUrl "constitution.0")
-                        constitutionHash''
-                    )
-                    SNothing
-                )
-            invalidNewConstitutionProposal =
-              ProposalProcedure
-                { pProcDeposit = mempty
-                , pProcReturnAddr = rewardAccount
-                , pProcGovAction = invalidNewConstitutionGovAction
-                , pProcAnchor = def
-                }
-        submitFailingProposal
-          invalidNewConstitutionProposal
-          [ inject $ InvalidPrevGovActionId invalidNewConstitutionProposal
-          ]
-      context "Invalid proposals are rejected" $ do
-        it "Invalid Index in PrevGovActionId" $ do
-          gaidConstitutionProp <- submitInitConstitutionGovAction
-          constitutionHash <- freshSafeHash
-          rewardAccount <- registerRewardAccount
-          let invalidPrevGovActionId =
-                -- Expected Ix = 0
-                PrevGovActionId (gaidConstitutionProp {gaidGovActionIx = GovActionIx 1})
-              invalidNewConstitutionGovAction =
-                NewConstitution
-                  (SJust invalidPrevGovActionId)
-                  ( Constitution
-                      ( Anchor
-                          (fromJust $ textToUrl "constitution.1")
-                          constitutionHash
-                      )
-                      SNothing
-                  )
-              invalidNewConstitutionProposal =
-                ProposalProcedure
-                  { pProcDeposit = mempty
-                  , pProcReturnAddr = rewardAccount
-                  , pProcGovAction = invalidNewConstitutionGovAction
-                  , pProcAnchor = def
-                  }
+                  constitution
+          invalidNewConstitutionProposal <- proposalWithRewardAccount invalidNewConstitutionGovAction
           submitFailingProposal
             invalidNewConstitutionProposal
             [ inject $ InvalidPrevGovActionId invalidNewConstitutionProposal
             ]
-        it "Valid PrevGovActionId but invalid purpose" $ do
-          gaidConstitutionProp <- submitInitConstitutionGovAction
-          rewardAccount <- registerRewardAccount
+        it "invalid index in PrevGovActionId" $ do
+          (govActionId, _) <- submitConstitution SNothing
+          constitution <- newConstitution
+          let invalidPrevGovActionId =
+                -- Expected Ix = 0
+                PrevGovActionId (govActionId {gaidGovActionIx = GovActionIx 1})
+              invalidNewConstitutionGovAction =
+                NewConstitution
+                  (SJust invalidPrevGovActionId)
+                  constitution
+          invalidNewConstitutionProposal <- proposalWithRewardAccount invalidNewConstitutionGovAction
+          submitFailingProposal
+            invalidNewConstitutionProposal
+            [ inject $ InvalidPrevGovActionId invalidNewConstitutionProposal
+            ]
+        it "valid PrevGovActionId but invalid purpose" $ do
+          (govActionId, _) <- submitConstitution SNothing
           let invalidNoConfidenceAction =
-                NoConfidence $ SJust $ PrevGovActionId gaidConstitutionProp
-              invalidNoConfidenceProposal =
-                ProposalProcedure
-                  { pProcDeposit = mempty
-                  , pProcReturnAddr = rewardAccount
-                  , pProcGovAction = invalidNoConfidenceAction
-                  , pProcAnchor = def
-                  }
+                NoConfidence $ SJust $ PrevGovActionId govActionId
+          invalidNoConfidenceProposal <- proposalWithRewardAccount invalidNoConfidenceAction
+
           submitFailingProposal
             invalidNoConfidenceProposal
             [ inject $ InvalidPrevGovActionId invalidNoConfidenceProposal
             ]
-      context "Valid proposals are accepted" $ do
-        it "Submit Constitution and use valid PrevGovActionId" $ do
-          curConstitution <- getsNES $ newEpochStateGovStateL . constitutionGovStateL
-          gaidConstitutionProp <- submitInitConstitutionGovAction
-          constitutionHash <- freshSafeHash
-          let constitutionActionNext =
-                NewConstitution
-                  (SJust $ PrevGovActionId gaidConstitutionProp)
-                  ( Constitution
-                      ( Anchor
-                          (fromJust $ textToUrl "constitution.1")
-                          constitutionHash
-                      )
-                      SNothing
-                  )
-          curConstitution' <- getsNES $ newEpochStateGovStateL . constitutionGovStateL
-          impAnn "Constitution has not been enacted yet" $
-            curConstitution' `shouldBe` curConstitution
-          submitGovAction_ constitutionActionNext
-        it "Enact Constitution and use valid PrevGovActionId" $ do
-          (dRep, committeeMember) <- electBasicCommittee
-          constitutionHash <- freshSafeHash
-          let constitution =
-                Constitution
-                  ( Anchor
-                      (fromJust $ textToUrl "constitution.0")
-                      constitutionHash
-                  )
-                  SNothing
-              constitutionAction =
-                NewConstitution SNothing constitution
-          gaidConstitutionProp <- submitGovAction constitutionAction
-          submitYesVote_ (DRepVoter dRep) gaidConstitutionProp
-          submitYesVote_ (CommitteeVoter committeeMember) gaidConstitutionProp
-          passEpoch
-          passEpoch
-          curConstitution <- getsNES $ newEpochStateGovStateL . constitutionGovStateL
-          impAnn "Constitution has been enacted" $
-            curConstitution `shouldBe` constitution
-          constitutionHash1 <- freshSafeHash
-          let constitutionAction1 =
-                NewConstitution
-                  (SJust $ PrevGovActionId gaidConstitutionProp)
-                  ( Constitution
-                      ( Anchor
-                          (fromJust $ textToUrl "constitution.1")
-                          constitutionHash1
-                      )
-                      SNothing
-                  )
-          submitGovAction_ constitutionAction1
+
+    describe "Constitution" $ do
+      it "submitted successfully with valid PrevGovActionId" $ do
+        curConstitution <- getsNES $ newEpochStateGovStateL . constitutionGovStateL
+        (govActionId, _) <- submitConstitution SNothing
+        curConstitution' <- getsNES $ newEpochStateGovStateL . constitutionGovStateL
+        impAnn "Constitution has not been enacted yet" $
+          curConstitution' `shouldBe` curConstitution
+        void $ submitConstitution (SJust $ PrevGovActionId govActionId)
+
+      it "submitted and enacted when voted on" $ do
+        (dRep, committeeMember) <- electBasicCommittee
+        (govActionId, constitution) <- submitConstitution SNothing
+        submitYesVote_ (DRepVoter dRep) govActionId
+        submitYesVote_ (CommitteeVoter committeeMember) govActionId
+        passEpoch
+        passEpoch
+        curConstitution <- getsNES $ newEpochStateGovStateL . constitutionGovStateL
+        impAnn "Constitution has been enacted" $
+          curConstitution `shouldBe` constitution
+
+submitConstitution ::
+  forall era.
+  ConwayEraImp era =>
+  StrictMaybe (PrevGovActionId 'ConstitutionPurpose (EraCrypto era)) ->
+  ImpTestM era (GovActionId (EraCrypto era), Constitution era)
+submitConstitution prevGovId = do
+  constitution <- newConstitution
+  let constitutionAction =
+        NewConstitution
+          prevGovId
+          constitution
+  govActionId <- submitGovAction constitutionAction
+  pure (govActionId, constitution)
+
+newConstitution :: Era era => ImpTestM era (Constitution era)
+newConstitution = do
+  constitutionHash <- freshSafeHash
+  pure $
+    Constitution
+      ( Anchor
+          (fromJust $ textToUrl "constitution.0")
+          constitutionHash
+      )
+      SNothing
+
+proposalWithRewardAccount ::
+  forall era.
+  ConwayEraImp era =>
+  GovAction era ->
+  ImpTestM era (ProposalProcedure era)
+proposalWithRewardAccount action = do
+  rewardAccount <- registerRewardAccount
+  pure
+    ProposalProcedure
+      { pProcDeposit = mempty
+      , pProcReturnAddr = rewardAccount
+      , pProcGovAction = action
+      , pProcAnchor = def
+      }
