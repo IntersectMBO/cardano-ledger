IntersectMBO/cardano-ledger

commit 45c67892540d52aa6af9972c58bb20a125ceea09

Move `tree-diff` dependency to tests together with all instances (#3893)
* Move tree-diff dependency to tests together with all instances

* Bump versions and add changelog entries

* Adjust bounds on packages

libs/cardano-ledger-core/src/Cardano/Ledger/Plutus/CostModels.hs
@@ -21,7 +21,7 @@
 
 module Cardano.Ledger.Plutus.CostModels (
   -- * Cost Model
-  CostModel,
+  CostModel (..),
   CostModelError (..),
   decodeValidAndUnknownCostModels,
   emptyCostModels,
@@ -52,7 +52,6 @@ import Cardano.Ledger.Binary (
  )
 import Cardano.Ledger.Binary.Version (natVersion)
 import Cardano.Ledger.Plutus.Language (Language (..), mkLanguageEnum, nonNativeLanguages)
-import Cardano.Ledger.TreeDiff (Expr (App), ToExpr (..))
 import Control.DeepSeq (NFData (..), deepseq)
 import Control.Monad (forM, when)
 import Control.Monad.Trans.Writer (WriterT (runWriterT))
@@ -296,9 +295,6 @@ instance Eq CostModelError where
 instance Ord CostModelError where
   CostModelError x1 <= CostModelError x2 = show x1 <= show x2
 
-instance ToExpr CostModelError where
-  toExpr (CostModelError x1) = toExpr (show x1)
-
 instance NoThunks CostModelError
 
 instance NFData CostModelError
@@ -394,9 +390,3 @@ instance ToJSON CostModels where
 -- by `Cardano.Ledger.Alonzo.PParams.getLanguageView`
 encodeCostModel :: CostModel -> Encoding
 encodeCostModel = encodeFoldableAsDefLenList encCBOR . getCostModelParams
-
-instance ToExpr CostModel where
-  toExpr (CostModel lang cmmap _) =
-    App "CostModel" [toExpr lang, toExpr cmmap, App "PV1.EvaluationContext" []]
-
-instance ToExpr CostModels

libs/cardano-ledger-core/testlib/Test/Cardano/Ledger/TreeDiff.hs
@@ -1,0 +1,244 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.TreeDiff (
+  module Test.Cardano.Ledger.Binary.TreeDiff,
+) where
+
+import Cardano.Ledger.Address
+import Cardano.Ledger.AuxiliaryData
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.CertState
+import Cardano.Ledger.Coin
+import Cardano.Ledger.Core
+import Cardano.Ledger.Credential
+import Cardano.Ledger.Crypto (Crypto)
+import Cardano.Ledger.EpochBoundary
+import Cardano.Ledger.HKD
+import Cardano.Ledger.Keys
+import Cardano.Ledger.Keys.Bootstrap
+import Cardano.Ledger.Keys.WitVKey
+import Cardano.Ledger.MemoBytes
+import Cardano.Ledger.Plutus.CostModels
+import Cardano.Ledger.Plutus.Data
+import Cardano.Ledger.Plutus.ExUnits
+import Cardano.Ledger.Plutus.Language
+import Cardano.Ledger.Plutus.TxInfo
+import Cardano.Ledger.PoolDistr
+import Cardano.Ledger.PoolParams
+import Cardano.Ledger.SafeHash
+import Cardano.Ledger.TxIn
+import Cardano.Ledger.UMap
+import Cardano.Ledger.UTxO
+import Data.Functor.Identity
+import Data.TreeDiff.OMap
+import Data.VMap hiding (fromList)
+import Test.Cardano.Ledger.Binary.TreeDiff
+import Test.Data.VMap.TreeDiff ()
+
+-- Coin
+instance ToExpr Coin
+
+instance ToExpr DeltaCoin
+
+instance ToExpr (CompactForm Coin) where
+  toExpr x = toExpr (fromCompact x)
+
+deriving newtype instance ToExpr (CompactForm DeltaCoin)
+
+-- HKD
+instance ToExpr (NoUpdate a)
+
+-- Keys
+instance Crypto c => ToExpr (VKey r c) where
+  toExpr vk =
+    Rec "VKey" $ fromList [("VKey (hashOf)", toExpr $ hashKey vk)]
+
+instance ToExpr (GenDelegs c)
+
+instance ToExpr (GenDelegPair c)
+
+instance ToExpr (KeyHash keyrole c) where
+  toExpr (KeyHash x) = App "KeyHash" [toExpr x]
+
+-- PoolDist
+instance ToExpr (PoolDistr c)
+
+instance ToExpr (IndividualPoolStake c)
+
+-- SafeHash
+instance ToExpr (SafeHash c index) where
+  toExpr x = App "SafeHash" [toExpr (extractHash x)]
+
+-- Language
+instance ToExpr Plutus
+
+instance ToExpr BinaryPlutus
+
+instance ToExpr Language
+
+-- MemoBytes
+instance ToExpr (t era) => ToExpr (MemoBytes t era)
+
+-- Core
+instance ToExpr (ScriptHash c)
+
+instance ToExpr CostModelError where
+  toExpr (CostModelError x1) = toExpr (show x1)
+
+instance ToExpr CostModel where
+  toExpr (CostModel lang cmmap _) =
+    App "CostModel" [toExpr lang, toExpr cmmap, App "EvaluationContext" []]
+
+instance ToExpr CostModels
+
+-- Keys/WitVKey
+instance Crypto c => ToExpr (WitVKey kr c)
+
+-- Keys/Bootstrap
+instance Crypto c => ToExpr (BootstrapWitness c)
+
+instance ToExpr ChainCode
+
+-- TxIn
+instance ToExpr TxIx
+
+-- BaseTypes
+instance ToExpr CertIx where
+  toExpr (CertIx x) = App "CertIx" [toExpr x]
+
+instance ToExpr UnitInterval
+
+instance ToExpr Network
+
+instance ToExpr Port
+
+instance ToExpr Url
+
+instance ToExpr Nonce where
+  toExpr NeutralNonce = App "NeutralNonce" []
+  toExpr (Nonce x) = App "Nonce" [trimExprViaShow 10 x]
+
+instance ToExpr DnsName
+
+instance (ToExpr x, ToExpr y, Integral y) => ToExpr (BoundedRatio x y)
+
+instance ToExpr NonNegativeInterval
+
+instance ToExpr (BlocksMade c)
+
+instance ToExpr ProtVer
+
+instance ToExpr (Anchor c)
+
+instance ToExpr EpochInterval
+
+-- AuxiliaryData
+instance ToExpr (AuxiliaryDataHash c)
+
+-- Plutus/ExUnits
+instance ToExpr Prices
+
+instance ToExpr ExUnits where
+  toExpr (WrapExUnits (ExUnits' x y)) = App "ExUnits" [defaultExprViaShow x, defaultExprViaShow y]
+
+-- Credential
+instance ToExpr (Credential keyrole c)
+
+instance ToExpr (StakeReference c)
+
+instance ToExpr Ptr
+
+deriving newtype instance
+  ToExpr (PParamsHKD Identity era) => ToExpr (PParams era)
+
+deriving newtype instance
+  ToExpr (PParamsHKD StrictMaybe era) => ToExpr (PParamsUpdate era)
+
+instance ToExpr (TxIn c)
+
+instance ToExpr (TxId c)
+
+-- CertState
+instance ToExpr (DRep c)
+
+instance ToExpr (DRepState c)
+
+-- Address
+instance ToExpr (Addr c)
+
+instance ToExpr (RewardAcnt era)
+
+instance ToExpr (BootstrapAddress c) where
+  toExpr = defaultExprViaShow
+
+instance ToExpr (Withdrawals c)
+
+instance ToExpr (CompactAddr c)
+
+-- PoolParams
+instance ToExpr PoolMetadata
+
+instance ToExpr (PoolParams era)
+
+instance ToExpr StakePoolRelay
+
+instance ToExpr (PoolCert c)
+
+-- UMap
+instance ToExpr RDPair
+
+instance ToExpr (UMElem c)
+
+instance ToExpr (UMap c)
+
+instance ToExpr (PlutusData era) where
+  toExpr = trimExprViaShow 30
+
+instance ToExpr (Data era)
+
+instance ToExpr (BinaryData era) where
+  toExpr _ = App "BinaryData" []
+
+instance ToExpr (Datum era) where
+  toExpr NoDatum = App "NoDatum" []
+  toExpr (DatumHash x) = App "DatumHash" [toExpr x]
+  toExpr (Datum bd) = App "Datum" [toExpr bd]
+
+-- EpochBoundary
+instance ToExpr (SnapShots c)
+
+instance ToExpr (SnapShot c)
+
+deriving newtype instance ToExpr (Stake c)
+
+instance ToExpr (CertState era)
+
+instance ToExpr (PState era)
+
+instance ToExpr (DState era)
+
+instance ToExpr (VState era)
+
+instance ToExpr (FutureGenDeleg c)
+
+instance ToExpr (InstantaneousRewards c)
+
+instance ToExpr (CommitteeState era)
+
+-- UTxO
+deriving instance (Era era, ToExpr (Script era)) => ToExpr (ScriptsProvided era)
+
+instance ToExpr (TxOut era) => ToExpr (UTxO era)
+
+instance ToExpr (TxOutSource era)

eras/allegra/impl/testlib/Test/Cardano/Ledger/Allegra/TreeDiff.hs
@@ -1,0 +1,53 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Allegra.TreeDiff (
+  module Test.Cardano.Ledger.Shelley.TreeDiff,
+) where
+
+import Cardano.Ledger.Allegra.Rules
+import Cardano.Ledger.Allegra.Scripts
+import Cardano.Ledger.Allegra.TxAuxData
+import Cardano.Ledger.Allegra.TxBody
+import Cardano.Ledger.Core
+import Cardano.Ledger.Shelley.PParams
+import Cardano.Ledger.Shelley.Rules
+import Test.Cardano.Ledger.Shelley.TreeDiff
+
+-- Scripts
+instance ToExpr ValidityInterval
+
+instance ToExpr (TimelockRaw era)
+
+instance ToExpr (Timelock era)
+
+-- TxAuxData
+instance ToExpr (AllegraTxAuxDataRaw era)
+
+instance ToExpr (AllegraTxAuxData era)
+
+-- TxBody
+instance
+  ( ToExpr ma
+  , ToExpr (TxOut era)
+  , ToExpr (TxCert era)
+  , ToExpr (Update era)
+  ) =>
+  ToExpr (AllegraTxBodyRaw ma era)
+
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (TxCert era)
+  , ToExpr (Update era)
+  ) =>
+  ToExpr (AllegraTxBody era)
+
+-- Rules/Utxo
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (Value era)
+  , ToExpr (PPUPPredFailure era)
+  ) =>
+  ToExpr (AllegraUtxoPredFailure era)

eras/alonzo/impl/testlib/Test/Cardano/Ledger/Alonzo/TreeDiff.hs
@@ -1,0 +1,133 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Alonzo.TreeDiff (
+  module Test.Cardano.Ledger.Mary.TreeDiff,
+) where
+
+import Cardano.Ledger.Alonzo.Core hiding (TranslationError)
+import Cardano.Ledger.Alonzo.PParams
+import Cardano.Ledger.Alonzo.Plutus.TxInfo
+import Cardano.Ledger.Alonzo.PlutusScriptApi
+import Cardano.Ledger.Alonzo.Rules
+import Cardano.Ledger.Alonzo.Scripts
+import Cardano.Ledger.Alonzo.Tx
+import Cardano.Ledger.Alonzo.TxAuxData
+import Cardano.Ledger.Alonzo.TxBody
+import Cardano.Ledger.Alonzo.TxWits
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.Compactible
+import Cardano.Ledger.Shelley.Rules
+import Test.Cardano.Ledger.Mary.TreeDiff
+
+-- Scripts
+instance ToExpr Tag
+
+instance ToExpr (AlonzoScript era)
+
+-- Core
+deriving newtype instance ToExpr CoinPerWord
+
+-- TxAuxData
+instance ToExpr (AlonzoTxAuxDataRaw era)
+
+instance ToExpr (AlonzoTxAuxData era)
+
+-- PParams
+deriving newtype instance ToExpr OrdExUnits
+
+instance ToExpr (AlonzoPParams StrictMaybe era)
+
+instance ToExpr (AlonzoPParams Identity era)
+
+-- TxWits
+instance ToExpr RdmrPtr
+
+instance ToExpr (RedeemersRaw era)
+
+instance ToExpr (Redeemers era)
+
+instance
+  ( Era era
+  , ToExpr (TxDats era)
+  , ToExpr (Redeemers era)
+  , ToExpr (Script era)
+  ) =>
+  ToExpr (AlonzoTxWitsRaw era)
+
+instance
+  ( Era era
+  , ToExpr (TxDats era)
+  , ToExpr (Redeemers era)
+  , ToExpr (Script era)
+  ) =>
+  ToExpr (AlonzoTxWits era)
+
+instance ToExpr (Data era) => ToExpr (TxDatsRaw era)
+
+instance ToExpr (Data era) => ToExpr (TxDats era)
+
+-- TxOut
+instance ToExpr Addr28Extra
+
+instance ToExpr DataHash32
+
+instance ToExpr (CompactForm (Value era)) => ToExpr (AlonzoTxOut era)
+
+-- TxBody
+instance
+  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
+  ToExpr (AlonzoTxBodyRaw era)
+
+instance
+  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
+  ToExpr (AlonzoTxBody era)
+
+-- Tx
+instance ToExpr IsValid
+
+instance
+  (ToExpr (TxBody era), ToExpr (TxWits era), ToExpr (TxAuxData era)) =>
+  ToExpr (AlonzoTx era)
+
+instance ToExpr (TxCert era) => ToExpr (ScriptPurpose era)
+
+-- Plutus/TxInfo
+instance ToExpr (TranslationError c)
+
+-- PlutusScriptApi
+instance ToExpr (TxCert era) => ToExpr (CollectError era)
+
+-- Rules/Utxo
+instance
+  ( ToExpr (Value era)
+  , ToExpr (TxOut era)
+  , ToExpr (PredicateFailure (EraRule "UTXOS" era))
+  ) =>
+  ToExpr (AlonzoUtxoPredFailure era)
+
+-- Rules/Utxos
+instance ToExpr FailureDescription
+
+instance ToExpr TagMismatchDescription
+
+instance
+  ( ToExpr (PPUPPredFailure era)
+  , ToExpr (TxCert era)
+  ) =>
+  ToExpr (AlonzoUtxosPredFailure era)
+
+-- Rules/Utxow
+instance
+  ( Era era
+  , ToExpr (PredicateFailure (EraRule "UTXO" era))
+  , ToExpr (TxCert era)
+  ) =>
+  ToExpr (AlonzoUtxowPredFailure era)

eras/babbage/impl/testlib/Test/Cardano/Ledger/Babbage/TreeDiff.hs
@@ -1,0 +1,64 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Babbage.TreeDiff (
+  module Test.Cardano.Ledger.Alonzo.TreeDiff,
+) where
+
+import Cardano.Ledger.Address
+import Cardano.Ledger.Alonzo.Rules
+import Cardano.Ledger.Babbage.Core
+import Cardano.Ledger.Babbage.PParams
+import Cardano.Ledger.Babbage.Rules
+import Cardano.Ledger.Babbage.TxBody
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.Compactible
+import Cardano.Ledger.Shelley.Rules
+import Test.Cardano.Ledger.Alonzo.TreeDiff
+
+-- Core
+deriving newtype instance ToExpr CoinPerByte
+
+-- PParams
+instance ToExpr (BabbagePParams StrictMaybe era)
+
+instance ToExpr (BabbagePParams Identity era)
+
+-- TxOut
+instance
+  ( ToExpr (CompactAddr (EraCrypto era))
+  , ToExpr (CompactForm (Value era))
+  , ToExpr (Script era)
+  ) =>
+  ToExpr (BabbageTxOut era)
+
+-- TxBody
+instance
+  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
+  ToExpr (BabbageTxBodyRaw era)
+
+instance
+  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
+  ToExpr (BabbageTxBody era)
+
+-- Rules/Utxo
+instance
+  ( ToExpr (AlonzoUtxoPredFailure era)
+  , ToExpr (TxOut era)
+  ) =>
+  ToExpr (BabbageUtxoPredFailure era)
+
+-- Rules/Utxow
+instance
+  ( Era era
+  , ToExpr (PredicateFailure (EraRule "UTXO" era))
+  , ToExpr (TxCert era)
+  ) =>
+  ToExpr (BabbageUtxowPredFailure era)

eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/TreeDiff.hs
@@ -1,0 +1,131 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Conway.TreeDiff (
+  module Test.Cardano.Ledger.Babbage.TreeDiff,
+) where
+
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.Conway.Core
+import Cardano.Ledger.Conway.Governance
+import Cardano.Ledger.Conway.PParams
+import Cardano.Ledger.Conway.Rules
+import Cardano.Ledger.Conway.TxBody
+import Cardano.Ledger.Conway.TxCert
+import Cardano.Ledger.Crypto
+import Cardano.Ledger.HKD
+import Data.Functor.Identity
+import Test.Cardano.Data.TreeDiff ()
+import Test.Cardano.Ledger.Babbage.TreeDiff
+
+-- Core
+instance ToExpr PoolVotingThresholds
+
+instance ToExpr DRepVotingThresholds
+
+-- Governance/Procedure
+instance ToExpr GovActionIx
+
+instance ToExpr (GovActionId c)
+
+instance ToExpr (PParamsHKD StrictMaybe era) => ToExpr (GovActionState era)
+
+instance Crypto c => ToExpr (Voter c)
+
+instance ToExpr Vote
+
+instance Era era => ToExpr (VotingProcedures era)
+
+instance ToExpr (VotingProcedure era)
+
+instance ToExpr (PParamsHKD StrictMaybe era) => ToExpr (ProposalProcedure era)
+
+instance ToExpr (Committee era)
+
+instance ToExpr (PrevGovActionId r ci)
+
+instance ToExpr (PParamsHKD StrictMaybe era) => ToExpr (GovAction era)
+
+-- Governance/Proposals
+instance ToExpr (PParamsHKD StrictMaybe era) => ToExpr (Proposals era)
+
+-- PParams
+instance ToExpr (HKD f a) => ToExpr (THKD t f a) where
+  toExpr = toExpr . unTHKD
+
+instance ToExpr (ConwayPParams Identity era)
+
+instance ToExpr (ConwayPParams StrictMaybe era)
+
+-- Governance
+instance (EraPParams era, ToExpr (PParamsHKD StrictMaybe era)) => ToExpr (PulsingSnapshot era)
+
+instance ToExpr (PrevGovActionIds era)
+
+instance ToExpr (PParamsHKD Identity era) => ToExpr (EnactState era)
+
+instance (EraPParams era, ToExpr (PParamsHKD Identity era)) => ToExpr (RatifyState era)
+
+instance (EraPParams era, ToExpr (PParamsHKD Identity era), ToExpr (PParamsHKD StrictMaybe era)) => ToExpr (ConwayGovState era)
+
+instance (EraPParams era, ToExpr (PParamsHKD StrictMaybe era), ToExpr (PParamsHKD Identity era)) => ToExpr (DRepPulsingState era) where
+  toExpr (DRComplete x y) = App "DRComplete" [toExpr x, toExpr y]
+  toExpr x@(DRPulsing (DRepPulser {})) = App "DRComplete" [toExpr a, toExpr b]
+    where
+      (a, b) = finishDRepPulser x
+
+instance ToExpr (RatifyEnv era) where
+  toExpr (RatifyEnv stake pool drep dstate ep cs) =
+    App "RatifyEnv" [toExpr stake, toExpr pool, toExpr drep, toExpr dstate, toExpr ep, toExpr cs]
+
+-- Rules/Gov
+instance (EraPParams era, ToExpr (PParamsHKD StrictMaybe era)) => ToExpr (ConwayGovPredFailure era)
+
+-- TxCert
+instance ToExpr (Delegatee c)
+
+instance ToExpr (ConwayDelegCert c)
+
+instance ToExpr (ConwayGovCert c)
+
+instance ToExpr (ConwayTxCert c)
+
+-- Rules/GovCert
+instance ToExpr (ConwayGovCertPredFailure era)
+
+-- Rules/Delegs
+instance ToExpr (ConwayDelegPredFailure era)
+
+-- TxBody
+instance (EraPParams era, ToExpr (PParamsHKD StrictMaybe era), ToExpr (TxOut era)) => ToExpr (ConwayTxBodyRaw era)
+
+deriving instance
+  (EraPParams era, ToExpr (PParamsHKD StrictMaybe era), ToExpr (TxOut era)) =>
+  ToExpr (ConwayTxBody era)
+
+-- Rules/Cert
+instance
+  ( ToExpr (PredicateFailure (EraRule "DELEG" era))
+  , ToExpr (PredicateFailure (EraRule "GOVCERT" era))
+  , ToExpr (PredicateFailure (EraRule "POOL" era))
+  ) =>
+  ToExpr (ConwayCertPredFailure era)
+
+-- Rules/Certs
+instance
+  ToExpr (PredicateFailure (EraRule "CERT" era)) =>
+  ToExpr (ConwayCertsPredFailure era)
+
+-- Rules/Ledger
+instance
+  ( ToExpr (PredicateFailure (EraRule "UTXOW" era))
+  , ToExpr (PredicateFailure (EraRule "GOV" era))
+  , ToExpr (PredicateFailure (EraRule "CERTS" era))
+  ) =>
+  ToExpr (ConwayLedgerPredFailure era)

eras/mary/impl/testlib/Test/Cardano/Ledger/Mary/TreeDiff.hs
@@ -1,0 +1,47 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Mary.TreeDiff (
+  module Test.Cardano.Ledger.Allegra.TreeDiff,
+) where
+
+import Cardano.Ledger.Core
+import Cardano.Ledger.Mary.TxBody
+import Cardano.Ledger.Mary.Value
+import Cardano.Ledger.Shelley.PParams
+import Test.Cardano.Ledger.Allegra.TreeDiff
+
+-- Value
+instance ToExpr (CompactValue c)
+
+instance ToExpr (MaryValue c)
+
+instance ToExpr (MultiAsset c)
+
+instance ToExpr (PolicyID c)
+
+instance ToExpr AssetName where
+  toExpr an = App "AssetName" [toExpr (assetNameToTextAsHex an)]
+
+deriving newtype instance ToExpr (CompactForm (MaryValue c))
+
+-- TxBody
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (TxCert era)
+  , ToExpr (Update era)
+  ) =>
+  ToExpr (MaryTxBodyRaw era)
+
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (TxCert era)
+  , ToExpr (Update era)
+  ) =>
+  ToExpr (MaryTxBody era)

eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/TreeDiff.hs
@@ -1,0 +1,190 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Shelley.TreeDiff (
+  module Test.Cardano.Ledger.TreeDiff,
+) where
+
+import Cardano.Ledger.AuxiliaryData
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.Core
+import Cardano.Ledger.Shelley.Governance
+import Cardano.Ledger.Shelley.LedgerState
+import Cardano.Ledger.Shelley.PParams
+import Cardano.Ledger.Shelley.PoolRank
+import Cardano.Ledger.Shelley.Rules
+import Cardano.Ledger.Shelley.Scripts
+import Cardano.Ledger.Shelley.Tx
+import Cardano.Ledger.Shelley.TxAuxData
+import Cardano.Ledger.Shelley.TxBody
+import Cardano.Ledger.Shelley.TxCert
+import Cardano.Ledger.Shelley.TxWits
+import Test.Cardano.Ledger.TreeDiff
+
+-- PParams
+instance ToExpr (PParamsUpdate era) => ToExpr (ProposedPPUpdates era)
+
+instance ToExpr (ShelleyPParams StrictMaybe era)
+
+instance ToExpr (ShelleyPParams Identity era)
+
+instance ToExpr (PParamsUpdate era) => ToExpr (Update era)
+
+-- Scripts
+instance ToExpr (MultiSigRaw era)
+
+instance ToExpr (MultiSig era)
+
+-- TxAuxData
+instance ToExpr Metadatum
+
+instance ToExpr (ShelleyTxAuxData era)
+
+-- Governance
+instance
+  ( ToExpr (PParamsUpdate era)
+  , ToExpr (PParams era)
+  ) =>
+  ToExpr (ShelleyGovState era)
+
+instance ToExpr (Constitution era)
+
+-- TxCert
+instance ToExpr (GenesisDelegCert c)
+instance ToExpr MIRPot
+instance ToExpr (MIRTarget c)
+instance ToExpr (MIRCert c)
+instance ToExpr (ShelleyTxCert era)
+instance ToExpr (ShelleyDelegCert c)
+
+-- TxWits
+instance (Era era, ToExpr (Script era)) => ToExpr (ShelleyTxWits era)
+
+-- Rules/Ppup
+instance ToExpr VotingPeriod
+
+instance ToExpr (ShelleyPpupPredFailure era)
+
+-- TxOut
+instance (EraTxOut era, ToExpr (Value era)) => ToExpr (ShelleyTxOut era) where
+  toExpr (ShelleyTxOut x y) = App "ShelleyTxOut" [toExpr x, toExpr y]
+
+-- TxBody
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (TxCert era)
+  , ToExpr (Update era)
+  ) =>
+  ToExpr (ShelleyTxBodyRaw era)
+
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (TxCert era)
+  , ToExpr (Update era)
+  ) =>
+  ToExpr (ShelleyTxBody era)
+
+-- PoolRank
+instance ToExpr Likelihood
+
+instance ToExpr LogWeight
+
+instance ToExpr (NonMyopic c)
+
+-- Tx
+instance
+  ( ToExpr (TxAuxData era)
+  , ToExpr (TxBody era)
+  , ToExpr (TxWits era)
+  ) =>
+  ToExpr (ShelleyTxRaw era)
+
+instance
+  ( ToExpr (TxAuxData era)
+  , ToExpr (TxBody era)
+  , ToExpr (TxWits era)
+  ) =>
+  ToExpr (ShelleyTx era)
+
+-- RewardUpdate
+
+-- | You really don't want to see what is inside this.
+instance ToExpr (PulsingRewUpdate c) where
+  toExpr _ = App "PulsingRewUpdate..." []
+
+-- LedgerState/Types
+instance ToExpr AccountState
+
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (PParams era)
+  , ToExpr (StashedAVVMAddresses era)
+  , ToExpr (GovState era)
+  ) =>
+  ToExpr (NewEpochState era)
+
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (PParams era)
+  , ToExpr (GovState era)
+  ) =>
+  ToExpr (EpochState era)
+
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (GovState era)
+  ) =>
+  ToExpr (LedgerState era)
+
+instance
+  ( ToExpr (TxOut era)
+  , ToExpr (GovState era)
+  ) =>
+  ToExpr (UTxOState era)
+
+instance ToExpr (IncrementalStake c)
+
+-- Rules/Utxo
+instance
+  ( ToExpr (PPUPPredFailure era)
+  , ToExpr (Value era)
+  , ToExpr (TxOut era)
+  ) =>
+  ToExpr (ShelleyUtxoPredFailure era)
+
+-- Rules/Pool
+instance ToExpr (ShelleyPoolPredFailure era)
+
+-- Rules/Deleg
+instance ToExpr (ShelleyDelegPredFailure era)
+
+-- Rules/Utxow
+instance
+  ( Era era
+  , ToExpr (PredicateFailure (EraRule "UTXO" era))
+  , ToExpr (AuxiliaryDataHash (EraCrypto era))
+  ) =>
+  ToExpr (ShelleyUtxowPredFailure era)
+
+-- Rules/Delpl
+instance
+  ( ToExpr (PredicateFailure (EraRule "DELEG" era))
+  , ToExpr (PredicateFailure (EraRule "POOL" era))
+  ) =>
+  ToExpr (ShelleyDelplPredFailure era)
+
+-- Rules/Delegs
+instance
+  ToExpr (PredicateFailure (EraRule "DELPL" era)) =>
+  ToExpr (ShelleyDelegsPredFailure era)
+
+-- Rules/Ledger
+instance
+  ( ToExpr (PredicateFailure (EraRule "UTXOW" era))
+  , ToExpr (PredicateFailure (EraRule "DELEGS" era))
+  ) =>
+  ToExpr (ShelleyLedgerPredFailure era)

commit fa4ad77353739ed3c31abae14a71521810b6766d

Hide `CostModel` constructor.
Fix this mistake of exporting the `CostModel` constructor, which was introduced
in #3893.

libs/cardano-ledger-core/src/Cardano/Ledger/Plutus/CostModels.hs
@@ -21,7 +21,7 @@
 
 module Cardano.Ledger.Plutus.CostModels (
   -- * Cost Model
-  CostModel (..),
+  CostModel,
   CostModelError (..),
   decodeValidAndUnknownCostModels,
   emptyCostModels,

libs/cardano-ledger-core/testlib/Test/Cardano/Ledger/TreeDiff.hs
@@ -94,11 +94,15 @@ instance ToExpr (t era) => ToExpr (MemoBytes t era)
 instance ToExpr (ScriptHash c)
 
 instance ToExpr CostModelError where
-  toExpr (CostModelError x1) = toExpr (show x1)
+  toExpr = toExpr . show
 
 instance ToExpr CostModel where
-  toExpr (CostModel lang cmmap _) =
-    App "CostModel" [toExpr lang, toExpr cmmap, App "EvaluationContext" []]
+  toExpr costModel =
+    App
+      "CostModel"
+      [ toExpr (getCostModelLanguage costModel)
+      , toExpr (getCostModelParams costModel)
+      ]
 
 instance ToExpr CostModels
 