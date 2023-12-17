IntersectMBO/cardano-ledger

commit 45c67892540d52aa6af9972c58bb20a125ceea09

Move `tree-diff` dependency to tests together with all instances (#3893)
* Move tree-diff dependency to tests together with all instances

* Bump versions and add changelog entries

* Adjust bounds on packages

eras/allegra/impl/src/Cardano/Ledger/Allegra/Scripts.hs
@@ -27,6 +27,7 @@ module Cardano.Ledger.Allegra.Scripts (
     RequireTimeExpire,
     RequireTimeStart
   ),
+  TimelockRaw (..),
   pattern TimelockConstr,
   inInterval,
   showTimelock,
@@ -75,7 +76,6 @@ import Cardano.Ledger.MemoBytes (
 import Cardano.Ledger.SafeHash (SafeToHash)
 import Cardano.Ledger.Shelley.Scripts (nativeMultiSigTag)
 import qualified Cardano.Ledger.Shelley.Scripts as Shelley
-import Cardano.Ledger.TreeDiff (ToExpr)
 import Cardano.Slotting.Slot (SlotNo (..))
 import Control.DeepSeq (NFData (..))
 import Data.ByteString.Lazy (fromStrict)
@@ -93,8 +93,6 @@ data ValidityInterval = ValidityInterval
   , invalidHereafter :: !(StrictMaybe SlotNo)
   }
   deriving (Ord, Eq, Generic, Show, NoThunks, NFData)
-
-instance ToExpr ValidityInterval
 
 encodeVI :: ValidityInterval -> Encode ('Closed 'Dense) ValidityInterval
 encodeVI (ValidityInterval f t) = Rec ValidityInterval !> To f !> To t
@@ -123,8 +121,6 @@ data TimelockRaw era
 deriving instance Era era => NoThunks (TimelockRaw era)
 
 deriving instance HashAlgorithm (HASH (EraCrypto era)) => Show (TimelockRaw era)
-
-instance ToExpr (TimelockRaw era)
 
 -- | This function deconstructs and then reconstructs the timelock script
 -- to prove the compiler that we can arbirarily switch out the eras as long
@@ -186,8 +182,6 @@ newtype Timelock era = TimelockConstr (MemoBytes TimelockRaw era)
   deriving newtype (ToCBOR, NoThunks, NFData, SafeToHash)
 
 instance Era era => EncCBOR (Timelock era)
-
-instance ToExpr (Timelock era)
 
 instance Memoized Timelock where
   type RawType Timelock = TimelockRaw

eras/allegra/impl/src/Cardano/Ledger/Allegra/TxAuxData.hs
@@ -18,6 +18,7 @@
 
 module Cardano.Ledger.Allegra.TxAuxData (
   AllegraTxAuxData (AllegraTxAuxData),
+  AllegraTxAuxDataRaw (..),
 
   -- * Deprecations
   AuxiliaryData,
@@ -54,7 +55,6 @@ import Cardano.Ledger.MemoBytes (
  )
 import Cardano.Ledger.SafeHash (HashAnnotated, SafeToHash, hashAnnotated)
 import Cardano.Ledger.Shelley.TxAuxData (Metadatum, ShelleyTxAuxData (..), validMetadatum)
-import Cardano.Ledger.TreeDiff (ToExpr (..))
 import Codec.CBOR.Decoding (
   TokenType (
     TypeListLen,
@@ -87,8 +87,6 @@ data AllegraTxAuxDataRaw era = AllegraTxAuxDataRaw
   }
   deriving (Generic, Eq)
 
-instance ToExpr (AllegraTxAuxDataRaw era)
-
 instance Crypto c => EraTxAuxData (AllegraEra c) where
   type TxAuxData (AllegraEra c) = AllegraTxAuxData (AllegraEra c)
 
@@ -107,8 +105,6 @@ instance NFData (AllegraTxAuxDataRaw era)
 newtype AllegraTxAuxData era = AuxiliaryDataWithBytes (MemoBytes AllegraTxAuxDataRaw era)
   deriving (Generic)
   deriving newtype (Eq, ToCBOR, SafeToHash)
-
-instance ToExpr (AllegraTxAuxData era)
 
 instance Memoized AllegraTxAuxData where
   type RawType AllegraTxAuxData = AllegraTxAuxDataRaw

eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxAuxData.hs
@@ -25,6 +25,7 @@ module Cardano.Ledger.Alonzo.TxAuxData (
     atadTimelock,
     atadPlutus
   ),
+  AlonzoTxAuxDataRaw (..),
   mkAlonzoTxAuxData,
   AuxiliaryDataHash (..),
   hashAlonzoTxAuxData,
@@ -73,7 +74,6 @@ import Cardano.Ledger.SafeHash (
   hashAnnotated,
  )
 import Cardano.Ledger.Shelley.TxAuxData (Metadatum, validMetadatum)
-import Cardano.Ledger.TreeDiff (ToExpr)
 import Control.DeepSeq (NFData, deepseq)
 import qualified Data.List.NonEmpty as NE
 import Data.Map (Map)
@@ -105,8 +105,6 @@ deriving via
   InspectHeapNamed "AlonzoTxAuxDataRaw" (AlonzoTxAuxDataRaw era)
   instance
     NoThunks (AlonzoTxAuxDataRaw era)
-
-instance ToExpr (AlonzoTxAuxDataRaw era)
 
 -- | Encodes memoized bytes created upon construction.
 instance Era era => EncCBOR (AlonzoTxAuxData era)
@@ -235,8 +233,6 @@ newtype AlonzoTxAuxData era = AuxiliaryDataConstr (MemoBytes AlonzoTxAuxDataRaw 
 
 instance Memoized AlonzoTxAuxData where
   type RawType AlonzoTxAuxData = AlonzoTxAuxDataRaw
-
-instance ToExpr (AlonzoTxAuxData era)
 
 instance EqRaw (AlonzoTxAuxData era)
 

eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxBody.hs
@@ -41,6 +41,7 @@ module Cardano.Ledger.Alonzo.TxBody (
     atbAuxDataHash,
     atbTxNetworkId
   ),
+  AlonzoTxBodyRaw (..),
   AlonzoTxBodyUpgradeError (..),
   AlonzoEraTxBody (..),
   ShelleyEraTxBody (..),
@@ -111,7 +112,6 @@ import Cardano.Ledger.MemoBytes (
  )
 import Cardano.Ledger.SafeHash (HashAnnotated (..), SafeToHash)
 import Cardano.Ledger.Shelley.PParams (ProposedPPUpdates (..), Update (..))
-import Cardano.Ledger.TreeDiff (ToExpr)
 import Cardano.Ledger.TxIn (TxIn (..))
 import Control.Arrow (left)
 import Control.DeepSeq (NFData (..))
@@ -163,20 +163,12 @@ deriving instance
   (Era era, Show (TxOut era), Show (TxCert era), Show (PParamsUpdate era)) =>
   Show (AlonzoTxBodyRaw era)
 
-instance
-  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
-  ToExpr (AlonzoTxBodyRaw era)
-
 newtype AlonzoTxBody era = TxBodyConstr (MemoBytes AlonzoTxBodyRaw era)
   deriving (ToCBOR, Generic)
   deriving newtype (SafeToHash)
 
 instance Memoized AlonzoTxBody where
   type RawType AlonzoTxBody = AlonzoTxBodyRaw
-
-instance
-  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
-  ToExpr (AlonzoTxBody era)
 
 data AlonzoTxBodyUpgradeError
   = -- | The TxBody contains a protocol parameter update that attempts to update

eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxWits.hs
@@ -24,10 +24,12 @@
 module Cardano.Ledger.Alonzo.TxWits (
   RdmrPtr (..),
   Redeemers (Redeemers),
+  RedeemersRaw (..),
   unRedeemers,
   nullRedeemers,
   upgradeRedeemers,
   TxDats (TxDats, TxDats'),
+  TxDatsRaw (..),
   upgradeTxDats,
   AlonzoTxWits (
     AlonzoTxWits,
@@ -43,6 +45,7 @@ module Cardano.Ledger.Alonzo.TxWits (
     txdats',
     txrdmrs'
   ),
+  AlonzoTxWitsRaw (..),
   addrAlonzoTxWitsL,
   bootAddrAlonzoTxWitsL,
   scriptAlonzoTxWitsL,
@@ -92,7 +95,6 @@ import Cardano.Ledger.Plutus.Language (Language (..), Plutus (..), guardPlutus)
 import Cardano.Ledger.SafeHash (SafeToHash (..))
 import Cardano.Ledger.Shelley.TxBody (WitVKey)
 import Cardano.Ledger.Shelley.TxWits (ShelleyTxWits (..), keyBy, shelleyEqTxWitsRaw)
-import Cardano.Ledger.TreeDiff (ToExpr)
 import Control.DeepSeq (NFData)
 import Data.Bifunctor (Bifunctor (first))
 import Data.Map.Strict (Map)
@@ -119,8 +121,6 @@ instance NoThunks RdmrPtr
 
 instance NFData RdmrPtr
 
-instance ToExpr RdmrPtr
-
 -- EncCBOR and DecCBOR for RdmrPtr is used in UTXOW for error reporting
 instance DecCBOR RdmrPtr where
   decCBOR = RdmrPtr <$> decCBOR <*> decCBOR
@@ -145,8 +145,6 @@ newtype RedeemersRaw era = RedeemersRaw (Map RdmrPtr (Data era, ExUnits))
 
 deriving instance HashAlgorithm (HASH (EraCrypto era)) => Show (RedeemersRaw era)
 
-instance ToExpr (RedeemersRaw era)
-
 instance Typeable era => EncCBOR (RedeemersRaw era) where
   encCBOR (RedeemersRaw rs) = encodeFoldableEncoder keyValueEncoder $ Map.toAscList rs
     where
@@ -167,8 +165,6 @@ newtype Redeemers era = RedeemersConstr (MemoBytes RedeemersRaw era)
   deriving newtype (Eq, Generic, ToCBOR, NoThunks, SafeToHash, Typeable, NFData)
 
 deriving instance HashAlgorithm (HASH (EraCrypto era)) => Show (Redeemers era)
-
-instance ToExpr (Redeemers era)
 
 -- =====================================================
 -- Pattern for Redeemers
@@ -240,28 +236,12 @@ instance
   ) =>
   NFData (AlonzoTxWitsRaw era)
 
-instance
-  ( Era era
-  , ToExpr (TxDats era)
-  , ToExpr (Redeemers era)
-  , ToExpr (Script era)
-  ) =>
-  ToExpr (AlonzoTxWitsRaw era)
-
 newtype AlonzoTxWits era = TxWitnessConstr (MemoBytes AlonzoTxWitsRaw era)
   deriving newtype (SafeToHash, ToCBOR)
   deriving (Generic)
 
 instance Memoized AlonzoTxWits where
   type RawType AlonzoTxWits = AlonzoTxWitsRaw
-
-instance
-  ( Era era
-  , ToExpr (TxDats era)
-  , ToExpr (Redeemers era)
-  , ToExpr (Script era)
-  ) =>
-  ToExpr (AlonzoTxWits era)
 
 instance (Era era, Script era ~ AlonzoScript era) => Semigroup (AlonzoTxWits era) where
   (<>) x y | isEmptyTxWitness x = y
@@ -295,8 +275,6 @@ newtype TxDatsRaw era = TxDatsRaw (Map (DataHash (EraCrypto era)) (Data era))
   deriving newtype (NoThunks, NFData)
 
 deriving instance HashAlgorithm (HASH (EraCrypto era)) => Show (TxDatsRaw era)
-
-instance ToExpr (Data era) => ToExpr (TxDatsRaw era)
 
 instance (Typeable era, EncCBOR (Data era)) => EncCBOR (TxDatsRaw era) where
   encCBOR (TxDatsRaw m) = encCBOR $ Map.elems m
@@ -334,8 +312,6 @@ newtype TxDats era = TxDatsConstr (MemoBytes TxDatsRaw era)
 
 instance Memoized TxDats where
   type RawType TxDats = TxDatsRaw
-
-instance ToExpr (Data era) => ToExpr (TxDats era)
 
 deriving instance HashAlgorithm (HASH (EraCrypto era)) => Show (TxDats era)
 

eras/babbage/impl/src/Cardano/Ledger/Babbage/TxBody.hs
@@ -56,6 +56,7 @@ module Cardano.Ledger.Babbage.TxBody (
     btbAuxDataHash,
     btbTxNetworkId
   ),
+  BabbageTxBodyRaw (..),
   BabbageTxBodyUpgradeError (..),
   mkBabbageTxBody,
   inputsBabbageTxBodyL,
@@ -159,7 +160,6 @@ import Cardano.Ledger.MemoBytes (
 import Cardano.Ledger.Plutus.Data (Datum (..))
 import Cardano.Ledger.SafeHash (HashAnnotated (..), SafeToHash)
 import Cardano.Ledger.Shelley.PParams (ProposedPPUpdates (ProposedPPUpdates), Update (..))
-import Cardano.Ledger.TreeDiff (ToExpr)
 import Cardano.Ledger.TxIn (TxIn (..))
 import Control.Arrow (left)
 import Control.DeepSeq (NFData)
@@ -250,10 +250,6 @@ instance
   (Era era, NFData (TxOut era), NFData (TxCert era), NFData (PParamsUpdate era)) =>
   NFData (BabbageTxBodyRaw era)
 
-instance
-  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
-  ToExpr (BabbageTxBodyRaw era)
-
 deriving instance
   (Era era, Show (TxOut era), Show (TxCert era), Show (PParamsUpdate era)) =>
   Show (BabbageTxBodyRaw era)
@@ -267,10 +263,6 @@ instance Memoized BabbageTxBody where
 deriving newtype instance
   (Era era, NFData (TxOut era), NFData (TxCert era), NFData (PParamsUpdate era)) =>
   NFData (BabbageTxBody era)
-
-instance
-  (Era era, ToExpr (TxOut era), ToExpr (TxCert era), ToExpr (PParamsUpdate era)) =>
-  ToExpr (BabbageTxBody era)
 
 inputsBabbageTxBodyL ::
   BabbageEraTxBody era => Lens' (BabbageTxBody era) (Set (TxIn (EraCrypto era)))

eras/conway/impl/src/Cardano/Ledger/Conway/TxBody.hs
@@ -43,8 +43,10 @@ module Cardano.Ledger.Conway.TxBody (
     ctbVotingProcedures,
     ctbProposalProcedures,
     ctbCurrentTreasuryValue,
-    ctbTreasuryDonation
+    ctbTreasuryDonation,
+    TxBodyConstr
   ),
+  ConwayTxBodyRaw (..),
   conwayTotalDepositsTxBody,
   conwayProposalsDeposits,
 ) where
@@ -111,7 +113,6 @@ import Cardano.Ledger.MemoBytes (
   mkMemoized,
  )
 import Cardano.Ledger.SafeHash (HashAnnotated (..), SafeToHash)
-import Cardano.Ledger.TreeDiff (ToExpr)
 import Cardano.Ledger.TxIn (TxIn (..))
 import Cardano.Ledger.Val (Val (..))
 import Control.Arrow (left)
@@ -155,7 +156,6 @@ data ConwayTxBodyRaw era = ConwayTxBodyRaw
   deriving (Generic, Typeable)
 
 deriving instance (EraPParams era, Eq (TxOut era)) => Eq (ConwayTxBodyRaw era)
-instance (EraPParams era, ToExpr (TxOut era)) => ToExpr (ConwayTxBodyRaw era)
 
 instance
   (EraPParams era, NoThunks (TxOut era)) =>
@@ -283,10 +283,6 @@ deriving newtype instance
 deriving instance
   (EraPParams era, Show (TxOut era)) =>
   Show (ConwayTxBody era)
-
-deriving instance
-  (EraPParams era, ToExpr (TxOut era)) =>
-  ToExpr (ConwayTxBody era)
 
 type instance MemoHashIndex ConwayTxBodyRaw = EraIndependentTxBody
 

eras/mary/impl/src/Cardano/Ledger/Mary/TxBody.hs
@@ -29,6 +29,7 @@ module Cardano.Ledger.Mary.TxBody (
     mtbWithdrawals,
     mtbMint
   ),
+  MaryTxBodyRaw (..),
   MaryEraTxBody (..),
   StrictMaybe (..),
   ValidityInterval (..),
@@ -71,13 +72,6 @@ import NoThunks.Class (NoThunks (..))
 
 newtype MaryTxBodyRaw era = MaryTxBodyRaw (AllegraTxBodyRaw (MultiAsset (EraCrypto era)) era)
 
-instance
-  ( ToExpr (TxOut era)
-  , ToExpr (TxCert era)
-  , ToExpr (Update era)
-  ) =>
-  ToExpr (MaryTxBodyRaw era)
-
 deriving newtype instance
   (Era era, NFData (TxOut era), NFData (TxCert era), NFData (PParamsUpdate era)) =>
   NFData (MaryTxBodyRaw era)
@@ -107,13 +101,6 @@ instance Era era => EncCBOR (MaryTxBody era)
 instance
   (Era era, Eq (PParamsUpdate era), Eq (TxOut era), Eq (TxCert era)) =>
   EqRaw (MaryTxBody era)
-
-instance
-  ( ToExpr (TxOut era)
-  , ToExpr (TxCert era)
-  , ToExpr (Update era)
-  ) =>
-  ToExpr (MaryTxBody era)
 
 instance AllegraEraTxBody era => DecCBOR (Annotator (MaryTxBodyRaw era)) where
   decCBOR = pure <$> decCBOR

commit 2d68a2b2a3e127f0a678f9cf9ef51bbeeee191cc

Stop exporting Raw consturctors
Fix a bug introduced in #3893

eras/allegra/impl/src/Cardano/Ledger/Allegra/Scripts.hs
@@ -27,7 +27,7 @@ module Cardano.Ledger.Allegra.Scripts (
     RequireTimeExpire,
     RequireTimeStart
   ),
-  TimelockRaw (..),
+  TimelockRaw,
   pattern TimelockConstr,
   inInterval,
   showTimelock,

eras/allegra/impl/src/Cardano/Ledger/Allegra/TxAuxData.hs
@@ -18,7 +18,7 @@
 
 module Cardano.Ledger.Allegra.TxAuxData (
   AllegraTxAuxData (AllegraTxAuxData),
-  AllegraTxAuxDataRaw (..),
+  AllegraTxAuxDataRaw,
 
   -- * Deprecations
   AuxiliaryData,

eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxAuxData.hs
@@ -26,7 +26,7 @@ module Cardano.Ledger.Alonzo.TxAuxData (
     atadTimelock,
     atadPlutus
   ),
-  AlonzoTxAuxDataRaw (..),
+  AlonzoTxAuxDataRaw,
   mkAlonzoTxAuxData,
   AuxiliaryDataHash (..),
   hashAlonzoTxAuxData,

eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxBody.hs
@@ -42,7 +42,7 @@ module Cardano.Ledger.Alonzo.TxBody (
     atbAuxDataHash,
     atbTxNetworkId
   ),
-  AlonzoTxBodyRaw (..),
+  AlonzoTxBodyRaw,
   AlonzoTxBodyUpgradeError (..),
   AlonzoEraTxBody (..),
   ShelleyEraTxBody (..),

eras/alonzo/impl/src/Cardano/Ledger/Alonzo/TxWits.hs
@@ -24,12 +24,12 @@
 module Cardano.Ledger.Alonzo.TxWits (
   RdmrPtr (..),
   Redeemers (Redeemers),
-  RedeemersRaw (..),
+  RedeemersRaw,
   unRedeemers,
   nullRedeemers,
   upgradeRedeemers,
   TxDats (TxDats, TxDats'),
-  TxDatsRaw (..),
+  TxDatsRaw,
   upgradeTxDats,
   AlonzoTxWits (
     AlonzoTxWits,
@@ -45,7 +45,7 @@ module Cardano.Ledger.Alonzo.TxWits (
     txdats',
     txrdmrs'
   ),
-  AlonzoTxWitsRaw (..),
+  AlonzoTxWitsRaw,
   addrAlonzoTxWitsL,
   bootAddrAlonzoTxWitsL,
   scriptAlonzoTxWitsL,

eras/babbage/impl/src/Cardano/Ledger/Babbage/TxBody.hs
@@ -48,7 +48,7 @@ module Cardano.Ledger.Babbage.TxBody (
     btbAuxDataHash,
     btbTxNetworkId
   ),
-  BabbageTxBodyRaw (..),
+  BabbageTxBodyRaw,
   BabbageTxBodyUpgradeError (..),
   babbageAllInputsTxBodyF,
   babbageSpendableInputsTxBodyF,

eras/conway/impl/src/Cardano/Ledger/Conway/TxBody.hs
@@ -43,10 +43,9 @@ module Cardano.Ledger.Conway.TxBody (
     ctbVotingProcedures,
     ctbProposalProcedures,
     ctbCurrentTreasuryValue,
-    ctbTreasuryDonation,
-    TxBodyConstr
+    ctbTreasuryDonation
   ),
-  ConwayTxBodyRaw (..),
+  ConwayTxBodyRaw,
   conwayTotalDepositsTxBody,
   conwayProposalsDeposits,
 ) where

eras/mary/impl/src/Cardano/Ledger/Mary/TxBody.hs
@@ -31,7 +31,7 @@ module Cardano.Ledger.Mary.TxBody (
     mtbWithdrawals,
     mtbMint
   ),
-  MaryTxBodyRaw (..),
+  MaryTxBodyRaw,
 )
 where
 

eras/shelley/impl/src/Cardano/Ledger/Shelley/Scripts.hs
@@ -27,7 +27,7 @@ module Cardano.Ledger.Shelley.Scripts (
   ScriptHash (..),
   nativeMultiSigTag,
   eqMultiSigRaw,
-  MultiSigRaw (..),
+  MultiSigRaw,
 )
 where
 

eras/shelley/impl/src/Cardano/Ledger/Shelley/Tx.hs
@@ -23,7 +23,7 @@ module Cardano.Ledger.Shelley.Tx (
     wits,
     auxiliaryData
   ),
-  ShelleyTxRaw (..),
+  ShelleyTxRaw,
   bodyShelleyTxL,
   witsShelleyTxL,
   auxDataShelleyTxL,

eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/TreeDiff.hs
@@ -115,7 +115,7 @@ instance ToExpr (ConwayDelegPredFailure era)
 -- TxBody
 instance (EraPParams era, ToExpr (PParamsHKD StrictMaybe era), ToExpr (TxOut era)) => ToExpr (ConwayTxBodyRaw era)
 
-deriving instance
+instance
   (EraPParams era, ToExpr (PParamsHKD StrictMaybe era), ToExpr (TxOut era)) =>
   ToExpr (ConwayTxBody era)
 