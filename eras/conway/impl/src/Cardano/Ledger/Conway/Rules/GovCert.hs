8784a453ebd11756d30531ec9667afc4817ff931
Clément Hurlin <clement.hurlin@tweag.io>
2023-12-19 21:26:53 +0100

    Add new ledger event exposing drep registration/unregistration

--- eras/conway/impl/src/Cardano/Ledger/Conway/Rules/GovCert.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway/Rules/GovCert.hs
@@ -51,6 +51,7 @@ import Control.State.Transition.Extended (
   TransitionRule,
   failBecause,
   judgmentContext,
+  tellEvent,
   transitionRules,
   (?!),
  )
@@ -126,7 +127,9 @@ instance
         pure (2, ConwayCommitteeHasPreviouslyResigned keyH)
       k -> invalidKey k
 
-newtype ConwayGovCertEvent era = GovCertEvent (Event (EraRule "GOVCERT" era))
+data ConwayGovCertEvent c =
+    DRepRegistration !(Credential 'DRepRole c)
+  | DRepUnregistration !(Credential 'DRepRole c)
 
 instance
   ( ConwayEraPParams era
@@ -144,7 +147,7 @@ instance
   type Environment (ConwayGOVCERT era) = ConwayGovCertEnv era
   type BaseM (ConwayGOVCERT era) = ShelleyBase
   type PredicateFailure (ConwayGOVCERT era) = ConwayGovCertPredFailure era
-  type Event (ConwayGOVCERT era) = ConwayGovCertEvent era
+  type Event (ConwayGOVCERT era) = ConwayGovCertEvent (EraCrypto era)
 
   transitionRules = [conwayGovCertTransition @era]
 
@@ -163,6 +166,7 @@ conwayGovCertTransition = do
     ConwayRegDRep cred deposit mAnchor -> do
       Map.notMember cred vsDReps ?! ConwayDRepAlreadyRegistered cred
       deposit == ppDRepDeposit ?! ConwayDRepIncorrectDeposit deposit ppDRepDeposit
+      tellEvent $ DRepRegistration cred
       pure
         vState
           { vsDReps =
@@ -170,6 +174,7 @@ conwayGovCertTransition = do
           }
     ConwayUnRegDRep cred deposit -> do
       checkRegistrationAndDepositAgainstPaidDeposit vsDReps cred deposit
+      tellEvent $ DRepUnregistration cred
       pure vState {vsDReps = Map.delete cred vsDReps}
     ConwayAuthCommitteeHotKey coldCred hotCred ->
       checkAndOverwriteCommitteeHotCred vState coldCred $ Just hotCred
