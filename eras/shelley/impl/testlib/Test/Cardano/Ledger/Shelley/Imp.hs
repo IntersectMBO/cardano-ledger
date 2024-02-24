31b483bf005e32bfaa3c9f5adc9d8229e0f89c5f
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2024-02-25 00:36:19 +0100

    Add Bootstrap InvalidWitnessesUTXOW predicate failure imp test

--- eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Imp.hs
+++ eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Imp.hs
@@ -1,16 +1,27 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Test.Cardano.Ledger.Shelley.Imp (spec) where
 
+import Cardano.Ledger.Core
+import Cardano.Ledger.Shelley.Rules (ShelleyUtxowPredFailure)
 import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Shelley.Imp.EpochSpec as Epoch
 import qualified Test.Cardano.Ledger.Shelley.Imp.LedgerSpec as Ledger
+import qualified Test.Cardano.Ledger.Shelley.Imp.UtxowSpec as Utxow
 import Test.Cardano.Ledger.Shelley.ImpTest (ShelleyEraImp, withImpState)
 
-spec :: forall era. ShelleyEraImp era => Spec
+spec ::
+  forall era.
+  ( ShelleyEraImp era
+  , InjectRuleFailure "LEDGER" ShelleyUtxowPredFailure era
+  ) =>
+  Spec
 spec =
   describe "ShelleyImpSpec" $ withImpState @era $ do
     Ledger.spec @era
     Epoch.spec @era
+    Utxow.spec @era
---
+++ eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Imp/UtxowSpec.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedLists #-}
+
+module Test.Cardano.Ledger.Shelley.Imp.UtxowSpec (spec) where
+
+import qualified Cardano.Chain.Common as Byron
+import Cardano.Ledger.Address (Addr (..), BootstrapAddress (..))
+import Cardano.Ledger.Coin (Coin (..))
+import Cardano.Ledger.Core
+import Cardano.Ledger.Keys.Bootstrap
+import Cardano.Ledger.SafeHash (extractHash, hashAnnotated)
+import Cardano.Ledger.Shelley.Rules (ShelleyUtxowPredFailure (..))
+import Lens.Micro
+import Test.Cardano.Ledger.Core.KeyPair (ByronKeyPair (..))
+import Test.Cardano.Ledger.Imp.Common
+import Test.Cardano.Ledger.Shelley.ImpTest
+
+spec ::
+  ( ShelleyEraImp era
+  , InjectRuleFailure "LEDGER" ShelleyUtxowPredFailure era
+  ) =>
+  SpecWith (ImpTestState era)
+spec = describe "UTXOW" $ do
+  describe "Bootstrap Witness" $
+    it "InvalidWitnessesUTXOW" $ do
+      modifyPParams (ppMaxTxSizeL .~ 1000)
+      aliceBootAddr@(BootstrapAddress aliceByronAddr) <- freshBootstapAddress
+      aliceByronKeyPair <- lookupByronKeyPair aliceBootAddr
+      txIn <- sendCoinTo (AddrBootstrap aliceBootAddr) (Coin 1000000)
+      let (aliceVKey, _) = unpackByronVKey (bkpVerificationKey aliceByronKeyPair)
+          txBody = mkBasicTxBody & inputsTxBodyL .~ [txIn]
+
+          -- below we use TxBody that was not fixed up, which means the hash for signing
+          -- will be different thus resulting in a wrong signature
+          aliceBadWitness =
+            makeBootstrapWitness
+              (extractHash (hashAnnotated txBody))
+              (bkpSigningKey aliceByronKeyPair)
+              (Byron.addrAttributes aliceByronAddr)
+
+          txBad =
+            mkBasicTx txBody
+              & (witsTxL %~ (bootAddrTxWitsL .~ [aliceBadWitness]))
+      submitFailingTx txBad [injectFailure $ InvalidWitnessesUTXOW [aliceVKey]]
