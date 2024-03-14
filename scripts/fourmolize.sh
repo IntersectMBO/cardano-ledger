d8ace02264d570fe6f98200766c3385facfae86b
Lucsanszky <daniel.lucsanszky@iohk.io>
2024-03-14 22:49:43 +0100

    Add `--changes` flag to `fourmolize.sh`
    This will make it possible to only run `fourmolu` on changes.
    When the `--changes` flag is omitted, we default to the old
    behaviour.

--- scripts/fourmolize.sh
+++ scripts/fourmolize.sh
@@ -2,6 +2,22 @@
 
 set -euo pipefail
 
-fourmolu -m inplace $(git ls-files -- '*.hs' | grep -v Setup.hs)
+if [[ $# -gt 0 ]]; then
+  case "$1" in
+    --changes)
+      files=$(git diff --diff-filter=MA --name-only origin/master HEAD -- '*.hs')
+      if [[ -n "$files" ]]; then
+        # Run fourmolu on changes compared to `master`.
+        fourmolu -m inplace $(echo "$files" | grep -v Setup.hs)
+      fi
+      ;;
+    *)
+      echo "Invalid option: $1" >&2
+      exit 1
+      ;;
+  esac
+else
+  fourmolu -m inplace $(git ls-files -- '*.hs' | grep -v Setup.hs)
+fi
 
 git diff --exit-code
