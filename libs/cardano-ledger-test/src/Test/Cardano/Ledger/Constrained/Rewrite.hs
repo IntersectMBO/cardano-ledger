ec9223b14da158b261dbff47a29f261cb9fbbfa7
teodanciu <teodora.danciu@tweag.io>
2024-03-20 10:55:08 +0000

    Fix compilation error

--- libs/cardano-ledger-test/src/Test/Cardano/Ledger/Constrained/Rewrite.hs
+++ libs/cardano-ledger-test/src/Test/Cardano/Ledger/Constrained/Rewrite.hs
@@ -672,7 +672,6 @@ firstE f (x : xs) = case f x of
 -- | Add to the dependency map 'answer' constraints such that every Name in 'before'
 --   preceeds every Name in 'after' in the order in which Names are solved for.
 mkDeps ::
-  Era era =>
   HashSet (Name era) ->
   HashSet (Name era) ->
   Map (Name era) (HashSet (Name era)) ->
