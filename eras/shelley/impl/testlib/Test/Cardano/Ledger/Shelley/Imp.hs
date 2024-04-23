6f35764046e80356ae9c08922b91429084676bda
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2024-04-22 22:42:55 -0600

    Add basic test for `ValueNotConservedUTxO`

--- eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Imp.hs
+++ eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Imp.hs
@@ -7,10 +7,11 @@
 module Test.Cardano.Ledger.Shelley.Imp (spec) where
 
 import Cardano.Ledger.Core
-import Cardano.Ledger.Shelley.Rules (ShelleyUtxowPredFailure)
+import Cardano.Ledger.Shelley.Rules (ShelleyUtxoPredFailure, ShelleyUtxowPredFailure)
 import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Shelley.Imp.EpochSpec as Epoch
 import qualified Test.Cardano.Ledger.Shelley.Imp.LedgerSpec as Ledger
+import qualified Test.Cardano.Ledger.Shelley.Imp.UtxoSpec as Utxo
 import qualified Test.Cardano.Ledger.Shelley.Imp.UtxowSpec as Utxow
 import Test.Cardano.Ledger.Shelley.ImpTest (ShelleyEraImp, withImpState)
 import qualified Test.Cardano.Ledger.Shelley.UnitTests.IncrementalStakeTest as Incremental
@@ -18,6 +19,7 @@ import qualified Test.Cardano.Ledger.Shelley.UnitTests.IncrementalStakeTest as I
 spec ::
   forall era.
   ( ShelleyEraImp era
+  , InjectRuleFailure "LEDGER" ShelleyUtxoPredFailure era
   , InjectRuleFailure "LEDGER" ShelleyUtxowPredFailure era
   ) =>
   Spec
@@ -26,5 +28,6 @@ spec = do
     Ledger.spec @era
     Epoch.spec @era
     Utxow.spec @era
+    Utxo.spec @era
   describe "ShelleyPureTests" $ do
     Incremental.spec @era
