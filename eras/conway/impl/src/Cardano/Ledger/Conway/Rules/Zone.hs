b784e492f6447a70226c6b65049b5c0470a84259
Will Gould <will.gould@iohk.io>
2024-05-14 12:15:29 +0100

    Direct translation of spec top sort

--- eras/conway/impl/src/Cardano/Ledger/Conway/Rules/Zone.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway/Rules/Zone.hs
@@ -40,9 +40,8 @@ import Control.State.Transition.Extended (
   judgmentContext,
   trans,
  )
-import Data.Foldable (toList)
 import Data.Sequence (Seq)
-import Data.Set (Set)
+import Data.Set (Set, toList)
 import Data.Void (Void)
 import Lens.Micro ((^.))
 import Lens.Micro.Type (Lens')
@@ -121,7 +120,7 @@ mkEdges l = go
   where
     go _ [] = []
     go tx (h : txs) =
-      if txIdTx tx `elem` fmap (\(TxIn x _) -> x) (toList (h ^. bodyTxL . l))
+      if txIdTx tx `elem` fmap (\(TxIn x _) -> x) (toList $ h ^. bodyTxL . l)
         then (tx, h) : go tx txs
         else go tx txs
 
@@ -138,78 +137,60 @@ mkAllEdges :: (EraTx era, ConwayEraTxBody era) => [Tx era] -> [Tx era] -> [(Tx e
 mkAllEdges [] _ = []
 mkAllEdges (h : txs) ls = mkIOEdges h ls ++ mkFREdges h ls ++ mkAllEdges txs ls
 
--- -- get a set of TxIds containing all IDs of transaction in given list tb
--- getIDs : List Tx → ℙ TxId
--- getIDs tb = foldr (λ { tx ls → ls ∪ (singleton (tx .body .txid)) }) ∅ tb
-
--- -- make edges for a given transaction
--- mkEdges : Tx → List Tx → List (Tx × Tx)
--- mkEdges _ [] = []
--- mkEdges tx (h ∷ tb) with ( ( tx .body .txid ) ∈? (fromList (map proj₁ (setToList (h .body .txins)))) )
--- ... | yes p = (tx , h) ∷ mkEdges tx tb
--- ... | no ¬p = mkEdges tx tb
-
--- -- make FR edges for a given transaction
--- mkFREdges : Tx → List Tx → List (Tx × Tx)
--- mkFREdges _ [] = []
--- mkFREdges tx (h ∷ tb) with ( ( tx .body .txid ) ∈? (fromList (map proj₁ (setToList (h .body .fulfills)))) )
--- ... | yes p = (h , tx) ∷ mkFREdges tx tb
--- ... | no ¬p = mkFREdges tx tb
-
--- -- make all edges for all transactions
--- mkAllEdges : List Tx → List Tx → List (Tx × Tx)
--- mkAllEdges [] ls = []
--- mkAllEdges (h ∷ tb) ls = mkEdges h ls ++ mkFREdges h ls ++ mkAllEdges tb ls
-
 -- -- for a given tx, and set of edges,
 -- -- returns a list of transactions ls such that for each e in ls is such that e -> tx is a dependency
 -- -- i.e. returns all ends of incoming edges
--- hasIncEdges : Tx → List (Tx × Tx) → List Tx
--- hasIncEdges tx [] = []
--- hasIncEdges tx ((e , tx') ∷ edges) with (tx .body .txid ≡ᵇ tx' .body .txid)
--- ... | true = e ∷ (hasIncEdges tx edges)
--- ... | false = (hasIncEdges tx edges)
+hasIncEdges :: EraTx era => Tx era -> [(Tx era, Tx era)] -> [Tx era]
+hasIncEdges _ [] = []
+hasIncEdges tx ((e, tx') : edges) =
+  if txIdTx tx == txIdTx tx'
+    then e : hasIncEdges tx edges
+    else hasIncEdges tx edges
 
 -- -- filters a list of transactions such that only ones with no incoming edges remain
--- nodesWithNoIncomingEdge : List Tx → List (Tx × Tx) → List Tx
--- nodesWithNoIncomingEdge [] edges = []
--- nodesWithNoIncomingEdge (tx ∷ txs) edges with (hasIncEdges tx edges)
--- ... | e ∷ dges = nodesWithNoIncomingEdge txs edges
--- ... | [] = tx ∷ nodesWithNoIncomingEdge txs edges
+nodesWithNoIncomingEdge :: EraTx era => [Tx era] -> [(Tx era, Tx era)] -> [Tx era]
+nodesWithNoIncomingEdge [] _ = []
+nodesWithNoIncomingEdge (tx : txs) edges = case hasIncEdges tx edges of
+  [] -> tx : nodesWithNoIncomingEdge txs edges
+  _ -> nodesWithNoIncomingEdge txs edges
 
 -- -- remove the first instance of a transaction in a list
--- removeTx : Tx → List Tx → List Tx
--- removeTx tx [] = []
--- removeTx tx (n ∷ ne) with (tx .body .txid ≡ᵇ n .body .txid)
--- ... | true = ne
--- ... | false = [ n ] ++ (removeTx tx ne)
+removeTx :: EraTx era => Tx era -> [Tx era] -> [Tx era]
+removeTx _ [] = []
+removeTx tx (n : ne) =
+  if txIdTx tx == txIdTx n
+    then ne
+    else n : removeTx tx ne
 
 -- -- remove a transaction from a list if it has no incoming edges
--- ifNoEdgeRemove : Tx → List (Tx × Tx) → List Tx → List Tx
--- ifNoEdgeRemove tx edges s with (hasIncEdges tx edges)
--- ... | [] = removeTx tx s
--- ... | e ∷ dges = s
+ifNoEdgeRemove :: EraTx era => Tx era -> [(Tx era, Tx era)] -> [Tx era] -> [Tx era]
+ifNoEdgeRemove tx edges s = case hasIncEdges tx edges of
+  [] -> removeTx tx s
+  _ -> s
 
--- -- given tx1, for all tx such that (tx1 , tx) in edges,
--- --             remove (tx1 , tx) from the graph
--- --             if tx has no other incoming edges then
--- --               insert tx into S
--- updateRES : Tx → List (Tx × Tx) → List Tx → ((List (Tx × Tx)) × (List Tx))
--- updateRES tx1 [] s = ([] , s)
--- updateRES tx1 ((tx , tx') ∷ em) s with (tx .body .txid ≡ᵇ tx1 .body .txid)
--- ... | true = (proj₁ (updateRES tx1 em (ifNoEdgeRemove tx em s)) , (ifNoEdgeRemove tx em s))
--- ... | false = ((tx , tx') ∷ proj₁ (updateRES tx1 em s) , s)
+-- given tx1, for all tx such that (tx1 , tx) in edges,
+--             remove (tx1 , tx) from the graph
+--             if tx has no other incoming edges then
+--               insert tx into S
+updateRES :: EraTx era => Tx era -> [(Tx era, Tx era)] -> [Tx era] -> ([(Tx era, Tx era)], [Tx era])
+updateRES _ [] s = ([], s)
+updateRES tx1 ((tx, tx') : em) s =
+  if txIdTx tx == txIdTx tx1
+    then (fst (updateRES tx1 em (ifNoEdgeRemove tx em s)), ifNoEdgeRemove tx em s)
+    else ((tx, tx') : fst (updateRES tx1 em s), s)
 
 -- -- topologically sorts a tx list
 -- -- arguments : tracking edges for agda termination check, remaining edges, remaining txs with no incoming edge (S), current sorted list (L)
 -- -- returns nothing if there are remaining edges the graph, but S is empty
--- topSortTxs : List (Tx × Tx) → List (Tx × Tx) → List Tx → List Tx → Maybe (List Tx)
--- topSortTxs e [] s srtd = just srtd
--- topSortTxs [] (r ∷ em) s srtd = nothing
--- topSortTxs e (r ∷ em) [] srtd = nothing
--- topSortTxs ((tx1 , tx2) ∷ dges) (r ∷ em) (tx ∷ rls) srtd =
---   topSortTxs dges (proj₁ updRES) (proj₂ updRES) (srtd ++ [ tx1 ])
---   where updRES = updateRES tx1 (r ∷ em) (removeTx tx1 (tx ∷ rls))
+topSortTxs ::
+  EraTx era => [(Tx era, Tx era)] -> [(Tx era, Tx era)] -> [Tx era] -> [Tx era] -> Maybe [Tx era]
+topSortTxs _ [] _ srtd = Just srtd
+topSortTxs [] _ _ _ = Nothing
+topSortTxs _ _ [] _ = Nothing
+topSortTxs ((tx1, _) : dges) (r : em) (tx : rls) srtd =
+  topSortTxs dges (fst updRES) (snd updRES) (srtd ++ [tx1])
+  where
+    updRES = updateRES tx1 (r : em) (removeTx tx1 (tx : rls))
 
 -- -- TOP SORT original
 -- -- L ← Empty list that will contain the sorted elements
