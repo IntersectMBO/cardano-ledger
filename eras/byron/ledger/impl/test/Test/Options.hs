d10c777c70a8a97118c16ae6b69df0d07eb983d4
teodanciu <teodora.danciu@tweag.io>
2024-06-17 17:11:56 +0100

    Fix compilation in byron caused by cardano-prelude upgrade

--- eras/byron/ledger/impl/test/Test/Options.hs
+++ eras/byron/ledger/impl/test/Test/Options.hs
@@ -20,10 +20,6 @@ module Test.Options (
 where
 
 import Cardano.Prelude
-#if __GLASGOW_HASKELL__ < 900
--- Do not understand why this is visible in ghc-9.2 but not on ghc-8.10.
-         hiding (Option)
-#endif
 import Hedgehog (Gen, Group (..), Property, PropertyT, TestLimit, withTests)
 import Hedgehog.Internal.Property (GroupName (..), PropertyName (..))
 import Test.Cardano.Prelude
