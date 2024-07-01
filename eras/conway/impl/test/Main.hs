3317f12b2d51bab7d7c8cafb4fca7ed9959a7ed0
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2024-07-01 12:42:00 -0600

    Stop running duplicate tests
    `Test.Cardano.Ledger.[Era].Imp` module already calls imp tests from
    previous eras.

--- eras/conway/impl/test/Main.hs
+++ eras/conway/impl/test/Main.hs
@@ -5,7 +5,6 @@ module Main where
 import Cardano.Ledger.Conway (Conway)
 import qualified Test.Cardano.Ledger.Alonzo.Binary.CostModelsSpec as CostModelsSpec
 import qualified Test.Cardano.Ledger.Alonzo.Binary.TxWitsSpec as TxWitsSpec
-import qualified Test.Cardano.Ledger.Alonzo.Imp as AlonzoImp
 import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Conway.Binary.CddlSpec as Cddl
 import qualified Test.Cardano.Ledger.Conway.Binary.Regression as Regression
@@ -14,12 +13,11 @@ import qualified Test.Cardano.Ledger.Conway.CommitteeRatifySpec as CommitteeRati
 import qualified Test.Cardano.Ledger.Conway.DRepRatifySpec as DRepRatify
 import qualified Test.Cardano.Ledger.Conway.GenesisSpec as Genesis
 import qualified Test.Cardano.Ledger.Conway.GovActionReorderSpec as GovActionReorder
-import qualified Test.Cardano.Ledger.Conway.Imp as ConwayImp
+import qualified Test.Cardano.Ledger.Conway.Imp as Imp
 import Test.Cardano.Ledger.Conway.Plutus.PlutusSpec as PlutusSpec
 import qualified Test.Cardano.Ledger.Conway.Proposals as Proposals
 import qualified Test.Cardano.Ledger.Conway.Spec as Spec
 import Test.Cardano.Ledger.Core.JSON (roundTripJsonEraSpec)
-import qualified Test.Cardano.Ledger.Shelley.Imp as ShelleyImp
 
 main :: IO ()
 main =
@@ -34,10 +32,7 @@ main =
       Genesis.spec
       GovActionReorder.spec
       roundTripJsonEraSpec @Conway
-      describe "Imp" $ do
-        AlonzoImp.spec @Conway
-        ConwayImp.spec @Conway
-        ShelleyImp.spec @Conway
+      describe "Imp" $ Imp.spec @Conway
       describe "CostModels" $ do
         CostModelsSpec.spec @Conway
       describe "TxWits" $ do
