e288431961dcdc914e01037396c3ded5e53aab68
Neil Mayhew <neil.mayhew@iohk.io>
2024-08-13 17:05:38 -0600

    Reduce the certainty of coverage tests to shorten running times

--- eras/shelley/test-suite/src/Test/Cardano/Ledger/Shelley/Rules/ClassifyTraces.hs
+++ eras/shelley/test-suite/src/Test/Cardano/Ledger/Shelley/Rules/ClassifyTraces.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -81,12 +82,14 @@ import Test.Control.State.Transition.Trace.Generator.QuickCheck (
  )
 import qualified Test.Control.State.Transition.Trace.Generator.QuickCheck as QC
 import Test.QuickCheck (
+  Confidence (certainty),
   Property,
-  checkCoverage,
+  checkCoverageWith,
   conjoin,
   cover,
   forAllBlind,
   property,
+  stdConfidence,
   withMaxSuccess,
  )
 import Test.Tasty (TestTree)
@@ -109,7 +112,7 @@ relevantCasesAreCovered n =
   where
     prop = do
       let tl = 100
-      checkCoverage $
+      checkCoverageWith stdConfidence {certainty = 1_000_000} $
         forAllBlind
           (traceFromInitState @(CHAIN era) testGlobals tl (genEnv p defaultConstants) genesisChainSt)
           relevantCasesAreCoveredForTrace
