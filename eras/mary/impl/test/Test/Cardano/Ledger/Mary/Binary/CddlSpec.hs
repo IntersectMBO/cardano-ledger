5c6333e4e75b60f82847d22c51da9c51b365b619
Nicholas Clarke <nicholas.clarke@iohk.io>
2024-08-21 15:52:28 +0200

    Define CDDL for Mary in Huddle.
    Also includes testing and cddl generation tool.

--- eras/mary/impl/test/Test/Cardano/Ledger/Mary/Binary/CddlSpec.hs
+++ eras/mary/impl/test/Test/Cardano/Ledger/Mary/Binary/CddlSpec.hs
@@ -10,14 +10,25 @@ import Test.Cardano.Ledger.Binary.Cddl (
   cddlRoundTripAnnCborSpec,
   cddlRoundTripCborSpec,
  )
+import Test.Cardano.Ledger.Binary.Cuddle
 import Test.Cardano.Ledger.Common
 import Test.Cardano.Ledger.Mary.Binary.Cddl (readMaryCddlFiles)
+import qualified Test.Cardano.Ledger.Mary.CDDL as MaryCDDL
 
 spec :: Spec
-spec =
+spec = do
   describe "CDDL" $ beforeAllCddlFile 3 readMaryCddlFiles $ do
     let v = eraProtVerLow @Mary
     cddlRoundTripCborSpec @(Value Mary) v "value"
     cddlRoundTripAnnCborSpec @(TxBody Mary) v "transaction_body"
     cddlRoundTripAnnCborSpec @(Script Mary) v "native_script"
     cddlRoundTripAnnCborSpec @(TxAuxData Mary) v "auxiliary_data"
+  newSpec
+
+newSpec :: Spec
+newSpec = describe "Huddle" $ specWithHuddle MaryCDDL.cddl 100 $ do
+  let v = eraProtVerHigh @Mary
+  huddleRoundTripCborSpec @(Value Mary) v "value"
+  huddleRoundTripAnnCborSpec @(TxBody Mary) v "transaction_body"
+  huddleRoundTripAnnCborSpec @(TxAuxData Mary) v "auxiliary_data"
+  huddleRoundTripAnnCborSpec @(Script Mary) v "native_script"
