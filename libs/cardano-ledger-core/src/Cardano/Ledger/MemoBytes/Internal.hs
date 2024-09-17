2be89c4beb6e0affc259548edfcbc64f34ce2ec7
Sean D Gillespie <sean.gillespie@iohk.io>
2024-09-17 11:47:48 -0400

    Create `Internal` modules for `TxBody`, `MemoBytes` and others

--- libs/cardano-ledger-core/src/Cardano/Ledger/MemoBytes/Internal.hs
+++ libs/cardano-ledger-core/src/Cardano/Ledger/MemoBytes/Internal.hs
@@ -18,13 +18,20 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
--- | MemoBytes is an abstraction for a data type that encodes its own serialization.
---   The idea is to use a newtype around a MemoBytes applied to a non-memoizing type.
---   For example:   newtype Foo = Foo (`MemoBytes` NonMemoizingFoo)
---   This way all the instances for @Foo (`Eq`, `Show`, `EncCBOR`, `DecCBOR`, `NoThunks`, Generic`)@
---   can be derived for free. MemoBytes plays an important role in the 'SafeToHash' class
---   introduced in the module 'Cardano.Ledger.SafeHash'
-module Cardano.Ledger.MemoBytes (
+{-# OPTIONS_HADDOCK not-home #-}
+
+-- | Provides MemoBytes internals
+--
+-- = Warning
+--
+-- This module is considered __internal__.
+--
+-- The contents of this module may change __in any way whatsoever__
+-- and __without any warning__ between minor versions of this package.
+--
+-- Authors importing this module are expected to track development
+-- closely.
+module Cardano.Ledger.MemoBytes.Internal (
   MemoBytes (Memo),
   MemoHashIndex,
   Mem,
