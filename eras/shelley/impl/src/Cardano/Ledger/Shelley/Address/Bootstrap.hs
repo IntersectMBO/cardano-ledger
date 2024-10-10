bd520ad94047bea23599b86ad496ab0ed8f37562
teodanciu <teodanciu@users.noreply.github.com>
2024-10-10 21:54:13 +0000

    Remove all existing files

--- eras/shelley/impl/src/Cardano/Ledger/Shelley/Address/Bootstrap.hs
+++
@@ -1,7 +0,0 @@
-module Cardano.Ledger.Shelley.Address.Bootstrap
-  {-# DEPRECATED "Use 'import Cardano.Ledger.Keys.Bootstrap' instead" #-} (
-  module X,
-)
-where
-
-import Cardano.Ledger.Keys.Bootstrap as X
--- libs/cardano-ledger-conformance/src/Test/Cardano/Ledger/Conformance/SpecTranslate/Conway.hs
+++
@@ -1,9 +0,0 @@
-module Test.Cardano.Ledger.Conformance.SpecTranslate.Conway () where
-
-import Test.Cardano.Ledger.Conformance.SpecTranslate.Conway.Base ()
-import Test.Cardano.Ledger.Conformance.SpecTranslate.Conway.Cert ()
-import Test.Cardano.Ledger.Conformance.SpecTranslate.Conway.Certs ()
-import Test.Cardano.Ledger.Conformance.SpecTranslate.Conway.Deleg ()
-import Test.Cardano.Ledger.Conformance.SpecTranslate.Conway.Gov ()
-import Test.Cardano.Ledger.Conformance.SpecTranslate.Conway.GovCert ()
-import Test.Cardano.Ledger.Conformance.SpecTranslate.Conway.Pool ()
