c20aabf1d6fefaf72a662039ab719f6f6b8e6ecd
neilmayhew <neilmayhew@users.noreply.github.com>
2024-10-19 02:41:32 +0000

    Remove all existing files

--- eras/alonzo/impl/src/Cardano/Ledger/Alonzo/Scripts/Data.hs
+++
@@ -1,5 +0,0 @@
-module Cardano.Ledger.Alonzo.Scripts.Data
-  {-# DEPRECATED "Use \"Cardano.Ledger.Plutus.Data\" instead" #-}
-  (module Cardano.Ledger.Plutus.Data) where
-
-import Cardano.Ledger.Plutus.Data
