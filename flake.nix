db8f26855a6c450a51f4b6466e13571d2a06128c
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2024-10-31 10:34:46 -0600

    Add a workaround the fact plutus-tx-plugin is "special"

--- flake.nix
+++ flake.nix
@@ -80,6 +80,9 @@
           '';
 
           shell = {
+            # Due to plutus-tx-plugin being a bit special, we need to augment the default package selection.
+            packages = ps: builtins.attrValues (nixpkgs.haskell-nix.haskellLib.selectLocalPackages ps) ++ [ps.plutus-tx-plugin];
+
             # force LANG to be UTF-8, otherwise GHC might choke on UTF encoded data.
             shellHook = ''
               export LANG=en_US.UTF-8
