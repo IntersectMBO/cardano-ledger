e86f536d81b4e26d4675c106a6c7ff693ab07492
Lucsanszky <daniel.lucsanszky@iohk.io>
2024-11-22 15:05:59 +0100

    Remove `Crypto` from Alonzo tests

--- eras/alonzo/impl/test/GenerateGoldenFileMain.hs
+++ eras/alonzo/impl/test/GenerateGoldenFileMain.hs
@@ -2,10 +2,10 @@
 
 module Main where
 
-import Cardano.Ledger.Alonzo (Alonzo)
+import Cardano.Ledger.Alonzo (AlonzoEra)
 import Test.Cardano.Ledger.Alonzo.Translation.Golden (generateGoldenFile)
 import Test.Cardano.Ledger.Alonzo.Translation.TranslatableGen ()
 
 -- | Generates golden translation file for Alonzo era
 main :: IO ()
-main = generateGoldenFile @Alonzo "eras/alonzo/impl/golden/translations.cbor"
+main = generateGoldenFile @AlonzoEra "eras/alonzo/impl/golden/translations.cbor"
--- eras/alonzo/impl/test/Test/Cardano/Ledger/Alonzo/Binary/CddlSpec.hs
+++ eras/alonzo/impl/test/Test/Cardano/Ledger/Alonzo/Binary/CddlSpec.hs
@@ -4,7 +4,7 @@
 module Test.Cardano.Ledger.Alonzo.Binary.CddlSpec (spec) where
 
 import Cardano.Ledger.Allegra.Scripts
-import Cardano.Ledger.Alonzo (Alonzo)
+import Cardano.Ledger.Alonzo (AlonzoEra)
 import Cardano.Ledger.Alonzo.Scripts (CostModels)
 import Cardano.Ledger.Alonzo.TxWits (AlonzoTxWits, Redeemers)
 import Cardano.Ledger.Core
@@ -20,15 +20,15 @@ import Test.Cardano.Ledger.Common
 spec :: Spec
 spec =
   describe "CDDL" $ beforeAllCddlFile 3 readAlonzoCddlFiles $ do
-    let v = eraProtVerHigh @Alonzo
-    cddlRoundTripCborSpec @(Value Alonzo) v "coin"
-    cddlRoundTripAnnCborSpec @(TxBody Alonzo) v "transaction_body"
-    cddlRoundTripAnnCborSpec @(TxAuxData Alonzo) v "auxiliary_data"
-    cddlRoundTripAnnCborSpec @(Timelock Alonzo) v "native_script"
-    cddlRoundTripAnnCborSpec @(Data Alonzo) v "plutus_data"
-    cddlRoundTripCborSpec @(TxOut Alonzo) v "transaction_output"
-    cddlRoundTripAnnCborSpec @(AlonzoTxWits Alonzo) v "transaction_witness_set"
-    cddlRoundTripCborSpec @(PParamsUpdate Alonzo) v "protocol_param_update"
-    cddlRoundTripAnnCborSpec @(Redeemers Alonzo) v "[* redeemer]"
-    cddlRoundTripAnnCborSpec @(Tx Alonzo) v "transaction"
+    let v = eraProtVerHigh @AlonzoEra
+    cddlRoundTripCborSpec @(Value AlonzoEra) v "coin"
+    cddlRoundTripAnnCborSpec @(TxBody AlonzoEra) v "transaction_body"
+    cddlRoundTripAnnCborSpec @(TxAuxData AlonzoEra) v "auxiliary_data"
+    cddlRoundTripAnnCborSpec @(Timelock AlonzoEra) v "native_script"
+    cddlRoundTripAnnCborSpec @(Data AlonzoEra) v "plutus_data"
+    cddlRoundTripCborSpec @(TxOut AlonzoEra) v "transaction_output"
+    cddlRoundTripAnnCborSpec @(AlonzoTxWits AlonzoEra) v "transaction_witness_set"
+    cddlRoundTripCborSpec @(PParamsUpdate AlonzoEra) v "protocol_param_update"
+    cddlRoundTripAnnCborSpec @(Redeemers AlonzoEra) v "[* redeemer]"
+    cddlRoundTripAnnCborSpec @(Tx AlonzoEra) v "transaction"
     cddlRoundTripCborSpec @CostModels v "costmdls"
