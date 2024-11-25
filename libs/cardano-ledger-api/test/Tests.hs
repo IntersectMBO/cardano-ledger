e5f2dc514cba52c68cacbc678bfd87ad15120d35
Aniket Deshpande <git@aniketd.mailer.me>
2024-11-25 17:29:48 +0530

    Remove `Crypto` from `cardano-ledger-api`

--- libs/cardano-ledger-api/test/Tests.hs
+++ libs/cardano-ledger-api/test/Tests.hs
@@ -3,7 +3,7 @@
 
 module Main where
 
-import Cardano.Ledger.Conway (Conway)
+import Cardano.Ledger.Conway (ConwayEra)
 import Cardano.Ledger.Core
 import qualified Test.Cardano.Ledger.Api.State.Imp.QuerySpec as ImpQuery (spec)
 import qualified Test.Cardano.Ledger.Api.State.QuerySpec as StateQuery (spec)
@@ -25,10 +25,10 @@ apiSpec =
     describe "State" $ do
       StateQuery.spec
     describe "Imp" $
-      withImpInit @(LedgerSpec Conway) $
-        forM_ (eraProtVersions @Conway) $ \v ->
+      withImpInit @(LedgerSpec ConwayEra) $
+        forM_ (eraProtVersions @ConwayEra) $ \v ->
           modifyImpInitProtVer v $ do
-            ImpQuery.spec @Conway
+            ImpQuery.spec @ConwayEra
 
 main :: IO ()
 main = ledgerTestMain apiSpec
