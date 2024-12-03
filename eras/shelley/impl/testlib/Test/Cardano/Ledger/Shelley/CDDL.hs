3974571e2c3a533845da398035a29a31ede5eabf
Aniket Deshpande <git@aniketd.mailer.me>
2024-12-03 21:42:29 +0530

    CDDL:shelley: Refactor. Add comments. Backport MIR.
    Backport `move_instantaneous_reward`.
    Add `delta_coin`.
    
    Reorganise the exports such that only the previous era CDDL needs to be
    imported in a following era to be overridden by explicitly hiding
    definitions.

--- eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/CDDL.hs
+++ eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/CDDL.hs
@@ -1,20 +1,26 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use camelCase" #-}
 {-# HLINT ignore "Evaluate" #-}
-module Test.Cardano.Ledger.Shelley.CDDL where
+
+module Test.Cardano.Ledger.Shelley.CDDL (
+  module Test.Cardano.Ledger.Core.Binary.CDDL,
+  module Test.Cardano.Ledger.Shelley.CDDL,
+) where
 
 import Codec.CBOR.Cuddle.Huddle
 import Data.Function (($))
 import Data.Word (Word64)
 import GHC.Num (Integer)
 import Test.Cardano.Ledger.Core.Binary.CDDL
+import Text.Heredoc
 
-shelley :: Huddle
-shelley = collectFrom [block, transaction, signkeyKES]
+shelleyCDDL :: Huddle
+shelleyCDDL = collectFrom [block, transaction, signkeyKES]
 
 block :: Rule
 block =
@@ -22,10 +28,8 @@ block =
     =:= arr
       [ a header
       , "transaction_bodies" ==> arr [0 <+ a transaction_body]
-      , "transaction_witness_sets"
-          ==> arr [0 <+ a transaction_witness_set]
-      , "transaction_metadata_set"
-          ==> mp [0 <+ asKey transaction_index ==> transaction_metadata]
+      , "transaction_witness_sets" ==> arr [0 <+ a transaction_witness_set]
+      , "transaction_metadata_set" ==> mp [0 <+ asKey transaction_index ==> transaction_metadata]
       ]
 
 transaction :: Rule
@@ -41,12 +45,7 @@ transaction_index :: Rule
 transaction_index = "transaction_index" =:= VUInt `sized` (2 :: Word64)
 
 header :: Rule
-header =
-  "header"
-    =:= arr
-      [ a header_body
-      , "body_signature" ==> kes_signature
-      ]
+header = "header" =:= arr [a header_body, "body_signature" ==> kes_signature]
 
 header_body :: Rule
 header_body =
@@ -65,6 +64,9 @@ header_body =
       , a protocol_version
       ]
 
+-- FIXME: A `Named Group` does not write the spec with key-value pairs but
+-- rather as a tuple of just the values, or, in other words, as data-types
+-- without the labels
 operational_cert :: Named Group
 operational_cert =
   "operational_cert"
@@ -104,20 +106,10 @@ transaction_body =
       ]
 
 transaction_input :: Rule
-transaction_input =
-  "transaction_input"
-    =:= arr
-      [ "transaction_id" ==> hash32
-      , "index" ==> VUInt
-      ]
+transaction_input = "transaction_input" =:= arr ["transaction_id" ==> hash32, "index" ==> VUInt]
 
 transaction_output :: Rule
-transaction_output =
-  "transaction_output"
-    =:= arr
-      [ a address
-      , "amount" ==> coin
-      ]
+transaction_output = "transaction_output" =:= arr [a address, "amount" ==> coin]
 
 certificate :: Rule
 certificate =
@@ -141,11 +133,8 @@ stake_deregistration =
     "stake_deregistration" =:~ grp [1, a stake_credential]
 
 stake_delegation :: Named Group
-stake_delegation =
-  "stake_delegation"
-    =:~ grp [2, a stake_credential, a pool_keyhash]
+stake_delegation = "stake_delegation" =:~ grp [2, a stake_credential, a pool_keyhash]
 
--- POOL
 pool_registration :: Named Group
 pool_registration = "pool_registration" =:~ grp [3, a pool_params]
 
@@ -155,7 +144,7 @@ pool_retirement = "pool_retirement" =:~ grp [4, a pool_keyhash, a epoch]
 genesis_key_delegation :: Named Group
 genesis_key_delegation =
   "genesis_key_delegation"
-    =:~ grp [5, a genesishash, a genesis_delegate_hash, a vrf_keyhash]
+    =:~ grp [5, a genesis_hash, a genesis_delegate_hash, a vrf_keyhash]
 
 move_instantaneous_rewards_cert :: Named Group
 move_instantaneous_rewards_cert =
@@ -164,30 +153,55 @@ move_instantaneous_rewards_cert =
 
 move_instantaneous_reward :: Rule
 move_instantaneous_reward =
-  "move_instantaneous_reward"
-    =:= arr [a (int 0 / int 1), a $ mp [0 <+ asKey stake_credential ==> coin]]
+  comment
+    [str|The first field determines where the funds are drawn from.
+        |  0 denotes the reserves,
+        |  1 denotes the treasury.
+        |If the second field is a map, funds are moved to stake credentials.
+        |Otherwise, the funds are given to the other accounting pot.
+        |NOTE:
+        |  This has been safely backported to Shelley from Alonzo.
+        |]
+    $ "move_instantaneous_reward"
+      =:= arr
+        [ a (int 0 / int 1)
+        , a (smp [0 <+ asKey stake_credential ==> delta_coin] / coin)
+        ]
+
+delta_coin :: Rule
+delta_coin =
+  comment
+    [str|This too has been introduced in Shelley as a backport from Alonzo.
+        |]
+    $ "delta_coin" =:= VInt
 
 stake_credential :: Rule
-stake_credential =
-  "stake_credential"
-    =:= arr
-      [0, a addr_keyhash]
-    / arr [1, a scripthash]
+stake_credential = "stake_credential" =:= credential
+
+credential :: Rule
+credential = "credential" =:= arr [0, a addr_keyhash] / arr [1, a script_hash]
 
 pool_params :: Named Group
 pool_params =
-  "pool_params"
-    =:~ grp
-      [ "operator" ==> pool_keyhash
-      , "vrf_keyhash" ==> vrf_keyhash
-      , "pledge" ==> coin
-      , "cost" ==> coin
-      , "margin" ==> unit_interval
-      , "reward_account" ==> reward_account
-      , "pool_owners" ==> set addr_keyhash
-      , "relays" ==> arr [0 <+ a relay]
-      , "pool_metadata" ==> (pool_metadata / VNil)
-      ]
+  comment
+    [str|        pool_keyhash: operator       
+        |                coin: pledge         
+        |                coin: cost           
+        |       unit_interval: margin         
+        |   set<addr_keyhash>: pool_owners    
+        |]
+    $ "pool_params"
+      =:~ grp
+        [ "operator" ==> pool_keyhash
+        , "vrf_keyhash" ==> vrf_keyhash
+        , "pledge" ==> coin
+        , "cost" ==> coin
+        , "margin" ==> unit_interval
+        , "reward_account" ==> reward_account
+        , "pool_owners" ==> set addr_keyhash
+        , "relays" ==> arr [0 <+ a relay]
+        , "pool_metadata" ==> (pool_metadata / VNil)
+        ]
 
 port :: Rule
 port = "port" =:= VUInt `le` 65535
@@ -203,30 +217,21 @@ dns_name = "dns_name" =:= VText `sized` (0 :: Word64, 64 :: Word64)
 
 single_host_addr :: Named Group
 single_host_addr =
-  "single_host_addr"
-    =:~ grp
-      [ 0
-      , port / VNil
-      , ipv4 / VNil
-      , ipv6 / VNil
-      ]
+  "single_host_addr" =:~ grp [0, port / VNil, ipv4 / VNil, ipv6 / VNil]
 
 single_host_name :: Named Group
 single_host_name =
-  "single_host_name"
-    =:~ grp
-      [ 1
-      , port / VNil
-      , a dns_name -- An A or AAAA DNS record
-      ]
+  comment
+    [str|dns_name: An A or AAAA DNS record
+        |]
+    $ "single_host_name" =:~ grp [1, port / VNil, a dns_name]
 
 multi_host_name :: Named Group
 multi_host_name =
-  "multi_host_name"
-    =:~ grp
-      [ 2
-      , a dns_name -- A SRV DNS record
-      ]
+  comment
+    [str|dns_name: An SRV DNS record
+        |]
+    $ "multi_host_name" =:~ grp [2, a dns_name]
 
 relay :: Rule
 relay =
@@ -250,29 +255,47 @@ update = "update" =:= arr [a proposed_protocol_parameter_updates, a epoch]
 proposed_protocol_parameter_updates :: Rule
 proposed_protocol_parameter_updates =
   "proposed_protocol_parameter_updates"
-    =:= mp [0 <+ asKey genesishash ==> protocol_param_update]
+    =:= mp [0 <+ asKey genesis_hash ==> protocol_param_update]
 
 protocol_param_update :: Rule
 protocol_param_update =
-  "protocol_param_update"
-    =:= mp
-      [ opt (idx 0 ==> VUInt) -- minfee A
-      , opt (idx 1 ==> VUInt) -- minfee B
-      , opt (idx 2 ==> VUInt) -- max block body size
-      , opt (idx 3 ==> VUInt) -- max transaction size
-      , opt (idx 4 ==> (VUInt `sized` (2 :: Word64))) -- max block header size
-      , opt (idx 5 ==> coin) -- key deposit
-      , opt (idx 6 ==> coin) -- pool deposit
-      , opt (idx 7 ==> epoch) -- maximum epoch
-      , opt (idx 8 ==> VUInt `sized` (2 :: Word64)) -- n_opt: desired number of stake pools
-      , opt (idx 9 ==> nonnegative_interval) -- pool pledge influence
-      , opt (idx 10 ==> unit_interval) -- expansion rate
-      , opt (idx 11 ==> unit_interval) -- treasury growth rate
-      , opt (idx 12 ==> unit_interval) -- decentralisation constant
-      , opt (idx 13 ==> nonce) -- extra entropy
-      , opt (idx 14 ==> arr [a protocol_version]) -- protocol version
-      , opt (idx 15 ==> coin) -- min utxo value
-      ]
+  comment
+    [str| 0: minfee A
+        | 1: minfee B
+        | 2: max block body size
+        | 3: max transaction size
+        | 4: max block header size
+        | 5: key deposit
+        | 6: pool deposit
+        | 7: maximum epoch
+        | 8: n_opt: desired number of stake pools
+        | 9: pool pledge influence
+        |10: expansion rate
+        |11: treasury growth rate
+        |12: decentralization constant
+        |13: extra entropy
+        |14: protocol version
+        |15: min utxo value
+        |]
+    $ "protocol_param_update"
+      =:= mp
+        [ opt (idx 0 ==> VUInt)
+        , opt (idx 1 ==> VUInt)
+        , opt (idx 2 ==> VUInt)
+        , opt (idx 3 ==> VUInt)
+        , opt (idx 4 ==> (VUInt `sized` (2 :: Word64)))
+        , opt (idx 5 ==> coin)
+        , opt (idx 6 ==> coin)
+        , opt (idx 7 ==> epoch)
+        , opt (idx 8 ==> VUInt `sized` (2 :: Word64))
+        , opt (idx 9 ==> nonnegative_interval)
+        , opt (idx 10 ==> unit_interval)
+        , opt (idx 11 ==> unit_interval)
+        , opt (idx 12 ==> unit_interval)
+        , opt (idx 13 ==> nonce)
+        , opt (idx 14 ==> arr [a protocol_version])
+        , opt (idx 15 ==> coin)
+        ]
 
 transaction_witness_set :: Rule
 transaction_witness_set =
@@ -298,11 +321,7 @@ transaction_metadatum_label = "transaction_metadatum_label" =:= VUInt
 transaction_metadata :: Rule
 transaction_metadata =
   "transaction_metadata"
-    =:= mp
-      [ 0
-          <+ asKey transaction_metadatum_label
-          ==> transaction_metadatum
-      ]
+    =:= mp [0 <+ asKey transaction_metadatum_label ==> transaction_metadatum]
 
 vkeywitness :: Rule
 vkeywitness = "vkeywitness" =:= arr [a vkey, a signature]
@@ -335,9 +354,7 @@ multisig_any :: Named Group
 multisig_any = "multisig_any" =:~ grp [2, a (arr [0 <+ a multisig_script])]
 
 multisig_n_of_k :: Named Group
-multisig_n_of_k =
-  "multisig_n_of_k"
-    =:~ grp [3, "n" ==> VUInt, a (arr [0 <+ a multisig_script])]
+multisig_n_of_k = "multisig_n_of_k" =:~ grp [3, "n" ==> VUInt, a (arr [0 <+ a multisig_script])]
 
 epoch :: Rule
 epoch = "epoch" =:= VUInt
@@ -345,30 +362,34 @@ epoch = "epoch" =:= VUInt
 genesis_delegate_hash :: Rule
 genesis_delegate_hash = "genesis_delegate_hash" =:= hash28
 
-genesishash :: Rule
-genesishash = "genesishash" =:= hash28
+genesis_hash :: Rule
+genesis_hash = "genesis_hash" =:= hash28
 
-scripthash :: Rule
-scripthash = "scripthash" =:= hash28
+script_hash :: Rule
+script_hash =
+  comment
+    [str|To compute a script hash, note that you must prepend
+        |a tag to the bytes of the script before hashing.
+        |The tag is determined by the language.
+        |The tags in the Conway era are:
+        |  "\x00" for multisig scripts
+        |  "\x01" for Plutus V1 scripts
+        |  "\x02" for Plutus V2 scripts
+        |  "\x03" for Plutus V3 scripts   
+        |]
+    $ "script_hash" =:= hash28
 
 metadata_hash :: Rule
 metadata_hash = "metadata_hash" =:= hash32
 
 nonce :: Rule
-nonce =
-  "nonce"
-    =:= arr [0]
-    / arr [1, a (VBytes `sized` (32 :: Word64))]
+nonce = "nonce" =:= arr [0] / arr [1, a (VBytes `sized` (32 :: Word64))]
 
---------------------------------------------------------------------------------
--- Shelley does not support some of the tagged core datastructured that we rely
+-- Shelley does not support some of the tagged core datastructures that we rely
 -- on in future eras. In order to have the "correct" common specification in
 -- core, we override them here
---------------------------------------------------------------------------------
 set :: IsType0 t0 => t0 -> GRuleCall
 set = binding $ \x -> "set" =:= arr [0 <+ a x]
 
 nonempty_set :: IsType0 t0 => t0 -> GRuleCall
-nonempty_set = binding $ \x ->
-  "nonempty_set"
-    =:= arr [1 <+ a x]
+nonempty_set = binding $ \x -> "nonempty_set" =:= arr [1 <+ a x]
