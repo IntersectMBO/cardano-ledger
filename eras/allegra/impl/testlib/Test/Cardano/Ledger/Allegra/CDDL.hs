bcd885d6d6d1d2bb301e20d57e4741f4dbd18242
Aniket Deshpande <git@aniketd.mailer.me>
2024-12-03 21:52:35 +0530

    CDDL:allegra: Refactor. Add comments. Backport MIR

--- eras/allegra/impl/testlib/Test/Cardano/Ledger/Allegra/CDDL.hs
+++ eras/allegra/impl/testlib/Test/Cardano/Ledger/Allegra/CDDL.hs
@@ -1,54 +1,48 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use camelCase" #-}
 {-# HLINT ignore "Evaluate" #-}
 
-module Test.Cardano.Ledger.Allegra.CDDL where
+module Test.Cardano.Ledger.Allegra.CDDL (
+  module Test.Cardano.Ledger.Shelley.CDDL,
+  module Test.Cardano.Ledger.Allegra.CDDL,
+) where
 
 import Codec.CBOR.Cuddle.Huddle
 import Data.Function (($))
-import Test.Cardano.Ledger.Core.Binary.CDDL
-import Test.Cardano.Ledger.Shelley.CDDL (
-  bootstrap_witness,
-  certificate,
-  header,
-  metadata_hash,
-  set,
-  transaction_index,
-  transaction_input,
-  transaction_metadatum,
-  transaction_metadatum_label,
-  transaction_output,
-  update,
-  vkeywitness,
-  withdrawals,
+import Test.Cardano.Ledger.Shelley.CDDL hiding (
+  block,
+  transaction,
+  transaction_body,
+  transaction_witness_set,
  )
+import Text.Heredoc
 
-cddl :: Huddle
-cddl = collectFrom [block, transaction]
-
---------------------------------------------------------------------------------
--- Things changed in Allegra
---------------------------------------------------------------------------------
+allegraCDDL :: Huddle
+allegraCDDL = collectFrom [block, transaction]
 
 native_script :: Rule
 native_script =
-  comment "Timelock validity intervals are half-open intervals [a, b)." $
-    "native_script"
+  comment
+    [str|Timelock validity intervals are half-open intervals [a, b).
+        |
+        |  invalid_before:
+        |    specifies the left (included) endpoint a.
+        |  
+        |  invalid_hereafter:
+        |    specifies the right (excluded) endpoint b.
+        |]
+    $ "native_script"
       =:= arr [a script_pubkey]
       / arr [a script_all]
       / arr [a script_any]
       / arr [a script_n_of_k]
       / arr [a invalid_before]
-      -- Timelock validity intervals are half-open intervals [a, b).
-      -- This field specifies the left (included) endpoint a.
       / arr [a invalid_hereafter]
-
--- Timelock validity intervals are half-open intervals [a, b).
--- This field specifies the right (excluded) endpoint b.
 
 script_pubkey :: Named Group
 script_pubkey = "script_pubkey" =:~ grp [0, a addr_keyhash]
@@ -60,9 +54,7 @@ script_any :: Named Group
 script_any = "script_any" =:~ grp [2, a (arr [0 <+ a native_script])]
 
 script_n_of_k :: Named Group
-script_n_of_k =
-  "script_n_of_k"
-    =:~ grp [3, "n" ==> int64, a (arr [0 <+ a native_script])]
+script_n_of_k = "script_n_of_k" =:~ grp [3, "n" ==> int64, a (arr [0 <+ a native_script])]
 
 invalid_before :: Named Group
 invalid_before = "invalid_before" =:~ grp [4, a VUInt]
@@ -70,37 +62,26 @@ invalid_before = "invalid_before" =:~ grp [4, a VUInt]
 invalid_hereafter :: Named Group
 invalid_hereafter = "invalid_hereafter" =:~ grp [5, a VUInt]
 
-transaction_witness_set :: Rule
-transaction_witness_set =
-  "transaction_witness_set"
-    =:= mp
-      [ opt $ idx 0 ==> arr [0 <+ a vkeywitness]
-      , opt $ idx 1 ==> arr [0 <+ a native_script]
-      , opt $ idx 2 ==> arr [0 <+ a bootstrap_witness]
-      ]
+metadata :: Rule
+metadata = "metadata" =:= mp [0 <+ asKey transaction_metadatum_label ==> transaction_metadatum]
+
+auxiliary_scripts :: Rule
+auxiliary_scripts = "auxiliary_scripts" =:= arr [0 <+ a native_script]
 
 auxiliary_data :: Rule
 auxiliary_data =
   "auxiliary_data"
-    =:= smp
-      [ 0
-          <+ asKey transaction_metadatum_label
-          ==> transaction_metadatum
-      ]
+    =:= metadata
     / sarr
-      [ "transaction_metadata"
-          ==> mp
-            [ 0
-                <+ asKey transaction_metadatum_label
-                ==> transaction_metadatum
-            ]
-      , "auxiliary_scripts" ==> arr [0 <+ a native_script]
+      [ "transaction_metadata" ==> metadata
+      , "auxiliary_scripts" ==> auxiliary_scripts
       ]
 
 transaction_body :: Rule
 transaction_body =
   comment
-    "Allegra transaction body adds the validity interval start at index 8"
+    [str|Allegra transaction body adds the validity interval start at index 8
+        |]
     $ "transaction_body"
       =:= mp
         [ idx 0 ==> set transaction_input
@@ -114,20 +95,14 @@ transaction_body =
         , opt (idx 8 ==> VUInt)
         ]
 
---------------------------------------------------------------------------------
--- Closure
---------------------------------------------------------------------------------
-
 block :: Rule
 block =
   "block"
     =:= arr
       [ a header
       , "transaction_bodies" ==> arr [0 <+ a transaction_body]
-      , "transaction_witness_sets"
-          ==> arr [0 <+ a transaction_witness_set]
-      , "auxiliary_data_set"
-          ==> mp [0 <+ asKey transaction_index ==> auxiliary_data]
+      , "transaction_witness_sets" ==> arr [0 <+ a transaction_witness_set]
+      , "auxiliary_data_set" ==> mp [0 <+ asKey transaction_index ==> auxiliary_data]
       ]
 
 transaction :: Rule
@@ -137,4 +112,13 @@ transaction =
       [ a transaction_body
       , a transaction_witness_set
       , a (auxiliary_data / VNil)
+      ]
+
+transaction_witness_set :: Rule
+transaction_witness_set =
+  "transaction_witness_set"
+    =:= mp
+      [ opt $ idx 0 ==> arr [0 <+ a vkeywitness]
+      , opt $ idx 1 ==> arr [0 <+ a native_script]
+      , opt $ idx 2 ==> arr [0 <+ a bootstrap_witness]
       ]
