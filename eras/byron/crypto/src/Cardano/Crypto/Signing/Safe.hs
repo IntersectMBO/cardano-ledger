632041b5433dfe9b32e50a15fd402944b4e35548
lehins <lehins@users.noreply.github.com>
2024-12-09 23:25:37 +0000

    Remove all existing files

--- eras/byron/crypto/src/Cardano/Crypto/Signing/Safe.hs
+++
@@ -1,8 +0,0 @@
-module Cardano.Crypto.Signing.Safe (
-  module X,
-)
-where
-
-import Cardano.Crypto.Signing.Safe.KeyGen as X
-import Cardano.Crypto.Signing.Safe.PassPhrase as X
-import Cardano.Crypto.Signing.Safe.SafeSigner as X
