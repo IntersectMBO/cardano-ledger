dc518cca743364210763dc94b6f8a56c2844e182
lehins <lehins@users.noreply.github.com>
2024-12-21 01:05:15 +0000

    Remove all existing files

--- eras/mary/impl/src/Cardano/Ledger/Mary/Core.hs
+++
@@ -1,9 +0,0 @@
-module Cardano.Ledger.Mary.Core (
-  MaryEraTxBody (..),
-  module Cardano.Ledger.Allegra.Core,
-)
-where
-
-import Cardano.Ledger.Allegra.Core
-import Cardano.Ledger.Mary.Tx ()
-import Cardano.Ledger.Mary.TxBody (MaryEraTxBody (..))
