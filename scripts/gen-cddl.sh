eb8250260ed2415309692b1c87c41918ef402d90
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2025-01-11 19:29:00 -0700

    Fix size of `TxIx` in CDDL
    Also improve the cddl generation script to not fail fast

--- scripts/gen-cddl.sh
+++ scripts/gen-cddl.sh
@@ -6,12 +6,10 @@ eras=("shelley" "allegra" "mary" "alonzo" "babbage" "conway")
 
 for era in ${eras[@]}; do
 
-  changed=$(git diff --name-only origin/master -- "eras/${era}/impl/testlib/Test/Cardano/Ledger/${era^}/CDDL.hs" | xargs)
-
-  if [[ -n "$changed" ]]; then
-    echo "Generating cddl for $era..."
-    cabal run cardano-ledger-$era:exe:huddle-cddl
-    echo "Regenerated ${era}.cddl"
-  fi
+  echo "Generating cddl for $era..."
+  cabal run cardano-ledger-$era:exe:huddle-cddl
+  echo "Regenerated ${era}.cddl"
 
 done
+
+git diff --exit-code
