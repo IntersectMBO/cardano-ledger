36e0d6f753eb88d66bd25292a20a71c77864835d
Joosep Jääger <joosep.jaager@iohk.io>
2025-01-31 13:11:58 +0200

    Bumped spec, enabled GOV Imp conformance

--- libs/cardano-ledger-conformance/src/Test/Cardano/Ledger/Conformance/ExecSpecRule/Conway/Gov.hs
+++ libs/cardano-ledger-conformance/src/Test/Cardano/Ledger/Conformance/ExecSpecRule/Conway/Gov.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -10,13 +11,11 @@
 
 module Test.Cardano.Ledger.Conformance.ExecSpecRule.Conway.Gov () where
 
-import Cardano.Ledger.BaseTypes
 import Cardano.Ledger.Conway (ConwayEra)
-import Cardano.Ledger.Conway.Core
+import Cardano.Ledger.Conway.Core (EraPParams (..))
 import Cardano.Ledger.Conway.Governance
 import Cardano.Ledger.Conway.Rules
-import Cardano.Ledger.TxIn (TxId)
-import Lens.Micro
+import Lens.Micro ((&), (.~), (^.))
 import qualified Lib as Agda
 import Test.Cardano.Ledger.Conformance
 import Test.Cardano.Ledger.Conformance.ExecSpecRule.Conway.Base ()
@@ -25,21 +24,12 @@ import Test.Cardano.Ledger.Conway.Arbitrary ()
 import Test.Cardano.Ledger.Imp.Common
 
 instance
-  Inject
-    (TxId, Proposals ConwayEra, EnactState ConwayEra)
-    (EnactState ConwayEra)
-  where
-  inject (_, _, x) = x
-
-instance
   ( NFData (SpecRep (ConwayGovPredFailure ConwayEra))
   , IsConwayUniv fn
   ) =>
   ExecSpecRule fn "GOV" ConwayEra
   where
-  type
-    ExecContext fn "GOV" ConwayEra =
-      (TxId, ProposalsSplit, EnactState ConwayEra)
+  type ExecContext fn "GOV" ConwayEra = EnactState ConwayEra
 
   environmentSpec _ = govEnvSpec
 
@@ -47,28 +37,17 @@ instance
 
   signalSpec _ = govProceduresSpec
 
-  genExecContext = do
-    txId <- arbitrary
-    proposalsSplit <- genProposalsSplit 50
-    enactState <- arbitrary
-    pure
-      ( txId
-      , proposalsSplit
-      , enactState
-      )
-
   runAgdaRule env st sig = unComputationResult $ Agda.govStep env st sig
 
-  translateInputs env@GovEnv {gePParams} st sig (txId, _proposalsSplit, enactState) = do
+  translateInputs env@GovEnv {gePParams} st sig enactState = do
     agdaEnv <- expectRight $ runSpecTransM ctx $ toSpecRep env
     agdaSt <- expectRight $ runSpecTransM ctx $ toSpecRep st
     agdaSig <- expectRight $ runSpecTransM ctx $ toSpecRep sig
     pure (agdaEnv, agdaSt, agdaSig)
     where
       ctx =
-        ( txId
-        , st
-        , enactState
-            & ensPrevGovActionIdsL .~ toPrevGovActionIds (st ^. pRootsL)
-            & ensProtVerL .~ (gePParams ^. ppProtocolVersionL)
-        )
+        enactState
+          & ensPrevGovActionIdsL
+            .~ toPrevGovActionIds (st ^. pRootsL)
+          & ensProtVerL
+            .~ (gePParams ^. ppProtocolVersionL)
