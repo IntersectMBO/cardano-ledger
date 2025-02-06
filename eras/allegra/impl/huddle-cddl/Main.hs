1bf339ea2c1d1d070774fce3ced9d78245044509
lehins <lehins@users.noreply.github.com>
2025-02-06 15:30:25 +0000

    Remove all existing files

--- eras/allegra/impl/huddle-cddl/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Paths_cardano_ledger_allegra
-import Test.Cardano.Ledger.Allegra.CDDL (allegraCDDL)
-import Test.Cardano.Ledger.Binary.Cuddle (writeSpec)
-
-main :: IO ()
-main = writeSpec allegraCDDL =<< getDataFileName "cddl-files/allegra.cddl"
