9d2befb7a5a7a6d25ab6dee9d500cb887fc04589
teodanciu <teodanciu@users.noreply.github.com>
2025-02-18 15:01:18 +0000

    Remove all existing files

--- eras/byron/ledger/impl/src/Cardano/Chain/Delegation.hs
+++
@@ -1,8 +0,0 @@
-module Cardano.Chain.Delegation (
-  module X,
-)
-where
-
-import Cardano.Chain.Delegation.Certificate as X
-import Cardano.Chain.Delegation.Map as X
-import Cardano.Chain.Delegation.Payload as X
