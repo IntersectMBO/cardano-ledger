e14f21f0d94815289e8c934ed9616d6283be557d
teodanciu <teodora.danciu@tweag.io>
2025-02-25 19:18:51 +0000

    Add missing huddle roundtrip tests in cardano-protocol-tpraos

--- libs/cardano-protocol-tpraos/test/Test/Cardano/Protocol/Binary/CddlSpec.hs
+++ libs/cardano-protocol-tpraos/test/Test/Cardano/Protocol/Binary/CddlSpec.hs
@@ -11,6 +11,7 @@ import Cardano.Ledger.Shelley
 import Cardano.Protocol.Crypto
 import Cardano.Protocol.TPraos.BHeader (BHBody, BHeader)
 import Cardano.Protocol.TPraos.OCert (OCert)
+import Codec.CBOR.Cuddle.Huddle (Huddle)
 import qualified Data.ByteString.Lazy as BSL
 import Test.Cardano.Ledger.Binary.Cddl (
   beforeAllCddlFile,
@@ -27,20 +28,25 @@ spec :: Spec
 spec =
   describe "CDDL" $ do
     let n = 3
-    specForEra @ShelleyEra readShelleyCddlFiles n
+    specForEra @ShelleyEra readShelleyCddlFiles shelleyCDDL n
 
-specForEra :: forall era. Era era => IO [BSL.ByteString] -> Int -> Spec
-specForEra readCddlFiles n = do
+specForEra :: forall era. Era era => IO [BSL.ByteString] -> Huddle -> Int -> Spec
+specForEra readCddlFiles cddlFile n = do
   describe (eraName @era) $ do
     let v = eraProtVerLow @era
-    beforeAllCddlFile n readCddlFiles $ do
-      cddlRoundTripAnnCborSpec @(BHeader StandardCrypto) v "header"
-      cddlRoundTripCborSpec @(BHeader StandardCrypto) v "header"
-      cddlRoundTripCborSpec @(BHBody StandardCrypto) v "header_body"
-      cddlRoundTripCborSpec @(CBORGroup (OCert StandardCrypto)) v "[ operational_cert ]"
-      -- TODO: add Huddle round trip tests
-      describe "DecCBOR instances equivalence via CDDL" $ do
-        cddlDecoderEquivalenceSpec @(BHeader StandardCrypto) v "header"
-    describe "DecCBOR instances equivalence via CDDL - Huddle" $ do
-      specWithHuddle shelleyCDDL 100 $ do
+    describe "Ruby-based" $
+      beforeAllCddlFile n readCddlFiles $ do
+        cddlRoundTripAnnCborSpec @(BHeader StandardCrypto) v "header"
+        cddlRoundTripCborSpec @(BHeader StandardCrypto) v "header"
+        cddlRoundTripCborSpec @(BHBody StandardCrypto) v "header_body"
+        cddlRoundTripCborSpec @(CBORGroup (OCert StandardCrypto)) v "[ operational_cert ]"
+        describe "DecCBOR instances equivalence via CDDL" $ do
+          cddlDecoderEquivalenceSpec @(BHeader StandardCrypto) v "header"
+    describe "Huddle" $ specWithHuddle cddlFile 100 $ do
+      huddleRoundTripAnnCborSpec @(BHeader StandardCrypto) v "header"
+      huddleRoundTripCborSpec @(BHeader StandardCrypto) v "header"
+      huddleRoundTripCborSpec @(BHBody StandardCrypto) v "header_body"
+      xdescribe "[ operational_cert ] doesn't roundtrip with huddle" $
+        huddleRoundTripCborSpec @(CBORGroup (OCert StandardCrypto)) v "[ operational_cert ]"
+      describe "DecCBOR instances equivalence via CDDL - Huddle" $ do
         huddleDecoderEquivalenceSpec @(BHeader StandardCrypto) v "header"
