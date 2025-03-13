9e798b767c2f6c46b4b31c68247bd29eebe3c3cf
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-03-13 18:19:30 +0100

    Remove `upgradeCertState` method

--- eras/babbage/impl/src/Cardano/Ledger/Babbage/State/CertState.hs
+++ eras/babbage/impl/src/Cardano/Ledger/Babbage/State/CertState.hs
@@ -6,12 +6,9 @@ module Cardano.Ledger.Babbage.State.CertState () where
 
 import Cardano.Ledger.Babbage.Era (BabbageEra)
 import Cardano.Ledger.Shelley.State
-import Data.Coerce (coerce)
 
 instance EraCertState BabbageEra where
   type CertState BabbageEra = ShelleyCertState BabbageEra
-
-  upgradeCertState = coerce
 
   certDStateL = shelleyCertDStateL
   {-# INLINE certDStateL #-}
