12b6418c082dc2c307b33ddfc87ae96cf0f421d7
neilmayhew <neilmayhew@users.noreply.github.com>
2025-03-21 08:27:04 +0000

    Remove all existing files

--- eras/conway/impl/src/Cardano/Ledger/Conway/State.hs
+++
@@ -1,8 +0,0 @@
-module Cardano.Ledger.Conway.State (
-  module Cardano.Ledger.Babbage.State,
-  module Cardano.Ledger.Conway.State.Stake,
-) where
-
-import Cardano.Ledger.Babbage.State
-
-import Cardano.Ledger.Conway.State.Stake
--- eras/shelley/impl/src/Cardano/Ledger/Shelley/State.hs
+++
@@ -1,8 +0,0 @@
-module Cardano.Ledger.Shelley.State (
-  module Cardano.Ledger.State,
-  module Cardano.Ledger.Shelley.State.Stake,
-)
-where
-
-import Cardano.Ledger.Shelley.State.Stake
-import Cardano.Ledger.State
