894e2aa658856d12436559d169f7dd3ff89a5599
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2025-04-04 23:02:10 +0530

    Redeundant constraints

--- eras/shelley/impl/src/Cardano/Ledger/Shelley/BlockChain.hs
+++ eras/shelley/impl/src/Cardano/Ledger/Shelley/BlockChain.hs
@@ -204,14 +204,7 @@ auxDataSeqDecoder bodiesLength auxDataMap lax = do
     indexLookupSeq :: Int -> IntMap a -> Seq (Maybe a)
     indexLookupSeq n ixMap = Seq.fromList [IntMap.lookup ix ixMap | ix <- [0 .. n - 1]]
 
-instance
-  ( EraTx era
-  , DecCBOR (TxBody era)
-  , DecCBOR (TxWits era)
-  , DecCBOR (TxAuxData era)
-  ) =>
-  DecCBOR (ShelleyTxSeq era)
-  where
+instance EraTx era => DecCBOR (ShelleyTxSeq era) where
   decCBOR = do
     Annotated bodies bodiesBytes <- decodeAnnotated decCBOR
     Annotated wits witsBytes <- decodeAnnotated decCBOR
