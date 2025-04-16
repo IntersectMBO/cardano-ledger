985799b3a5e7745a65d7cc90501e94f0403f25dc
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-04-16 16:34:41 +0200

    Add `Era` module to Alonzo `testlib`

---
+++ eras/alonzo/impl/testlib/Test/Cardano/Ledger/Alonzo/Era.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Cardano.Ledger.Alonzo.Era (
+  module Test.Cardano.Ledger.Mary.Era,
+  AlonzoEraTest,
+) where
+
+import Cardano.Ledger.Alonzo
+import Cardano.Ledger.Alonzo.Core
+import Cardano.Ledger.Alonzo.Plutus.Context
+import Cardano.Ledger.Alonzo.Scripts ()
+import Cardano.Ledger.Alonzo.UTxO
+import Data.TreeDiff
+import Test.Cardano.Ledger.Alonzo.Arbitrary ()
+import Test.Cardano.Ledger.Alonzo.TreeDiff ()
+import Test.Cardano.Ledger.Mary.Era
+
+class
+  ( MaryEraTest era
+  , EraPlutusContext era
+  , AlonzoEraTx era
+  , AlonzoEraTxAuxData era
+  , AlonzoEraUTxO era
+  , ToExpr (PlutusScript era)
+  , Script era ~ AlonzoScript era
+  ) =>
+  AlonzoEraTest era
+
+instance EraTest AlonzoEra
+
+instance ShelleyEraTest AlonzoEra
+
+instance AllegraEraTest AlonzoEra
+
+instance MaryEraTest AlonzoEra
+
+instance AlonzoEraTest AlonzoEra
