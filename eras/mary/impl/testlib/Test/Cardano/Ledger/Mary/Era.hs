6517b621be4ed5b7b52945126daeb19c55d3ba8d
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-04-18 10:43:50 +0200

    wip - Add era specific tfs to Mary

--- eras/mary/impl/testlib/Test/Cardano/Ledger/Mary/Era.hs
+++ eras/mary/impl/testlib/Test/Cardano/Ledger/Mary/Era.hs
@@ -2,18 +2,18 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Test.Cardano.Ledger.Mary.Era (
-  module Test.Cardano.Ledger.Shelley.Era,
+  module Test.Cardano.Ledger.Allegra.Era,
   MaryEraTest,
 ) where
 
 import Cardano.Ledger.Mary
 import Cardano.Ledger.Mary.Core
+import Test.Cardano.Ledger.Allegra.Era
 import Test.Cardano.Ledger.Mary.Arbitrary ()
 import Test.Cardano.Ledger.Mary.TreeDiff ()
-import Test.Cardano.Ledger.Shelley.Era
 
 class
-  ( ShelleyEraTest era
+  ( AllegraEraTest era
   , MaryEraTxBody era
   ) =>
   MaryEraTest era
@@ -21,5 +21,7 @@ class
 instance EraTest MaryEra
 
 instance ShelleyEraTest MaryEra
+
+instance AllegraEraTest MaryEra
 
 instance MaryEraTest MaryEra
