563aa915ea9f8ff1862808be4607f8661281db8e
teodanciu <teodora.danciu@tweag.io>
2025-04-25 17:33:21 +0100

    Add temporary tests to check the equivalence of the new serializers
    with the existing ones. These tests will be removed when the switch to the new serializers will be made

--- eras/conway/impl/test/Main.hs
+++ eras/conway/impl/test/Main.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Cardano.Ledger.Conway (ConwayEra)
+import Cardano.Ledger.Conway.PParams (ConwayEraPParams (..))
+import Cardano.Ledger.Conway.TxInfo ()
+import Cardano.Ledger.Core
+import Cardano.Ledger.Plutus.ToPlutusData (ToPlutusData (..))
 import qualified Test.Cardano.Ledger.Alonzo.Binary.CostModelsSpec as CostModelsSpec
 import qualified Test.Cardano.Ledger.Alonzo.Binary.TxWitsSpec as TxWitsSpec
 import Test.Cardano.Ledger.Common
+import Test.Cardano.Ledger.Conway.Arbitrary ()
 import qualified Test.Cardano.Ledger.Conway.Binary.CddlSpec as Cddl
 import qualified Test.Cardano.Ledger.Conway.Binary.Regression as Regression
 import qualified Test.Cardano.Ledger.Conway.BinarySpec as Binary
@@ -21,6 +27,7 @@ import qualified Test.Cardano.Ledger.Conway.Proposals as Proposals
 import qualified Test.Cardano.Ledger.Conway.SPORatifySpec as SPORatifySpec
 import qualified Test.Cardano.Ledger.Conway.Spec as Spec
 import qualified Test.Cardano.Ledger.Conway.TxInfoSpec as TxInfo
+import qualified Test.Cardano.Ledger.Core.EraPParamsSpec as EraPParams
 import Test.Cardano.Ledger.Core.JSON (roundTripJsonEraSpec)
 
 main :: IO ()
@@ -49,3 +56,15 @@ main =
         PlutusSpec.spec
       Regression.spec @ConwayEra
       TxInfo.spec
+      EraPParams.spec @ConwayEra
+      eraPParamsPlutusDataSpec
+
+eraPParamsPlutusDataSpec :: HasCallStack => Spec
+eraPParamsPlutusDataSpec = describe "EraPParams PlutusData" $ do
+  prop "PParams toPlutusData equivalence" $ \(ppu :: PParamsUpdate ConwayEra) -> do
+    let pd1 = toPlutusData ppu
+    let pd2 = toPlutusDataPParamsUpdate @ConwayEra ppu
+    pd1 `shouldBe` pd2
+
+    let deserialized = fromPlutusDataPParamsUpdate pd1
+    deserialized `shouldBe` Just ppu
