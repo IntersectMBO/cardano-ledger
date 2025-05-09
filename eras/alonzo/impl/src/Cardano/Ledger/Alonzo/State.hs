c75b4c950e79dd9d7bd5eeeabaafb71ee68531af
lehins <lehins@users.noreply.github.com>
2025-05-09 18:38:16 +0000

    Remove all existing files

--- eras/alonzo/impl/src/Cardano/Ledger/Alonzo/State.hs
+++
@@ -1,8 +0,0 @@
-module Cardano.Ledger.Alonzo.State (
-  module Cardano.Ledger.Mary.State,
-) where
-
-import Cardano.Ledger.Mary.State
-
-import Cardano.Ledger.Alonzo.State.CertState ()
-import Cardano.Ledger.Alonzo.State.Stake ()
