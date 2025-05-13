2429ac3038a8135c2d92327bb8ac267717666ce6
Neil Mayhew <neil.mayhew@iohk.io>
2025-05-13 09:54:15 -0600

    Add placeholders for tests that will be migrated from Examples.BabbageFeatures

--- eras/babbage/impl/testlib/Test/Cardano/Ledger/Babbage/Imp/PlutusSpec.hs
+++ eras/babbage/impl/testlib/Test/Cardano/Ledger/Babbage/Imp/PlutusSpec.hs
@@ -4,5 +4,9 @@ import Test.Cardano.Ledger.Alonzo.ImpTest
 import Test.Cardano.Ledger.Imp.Common
 
 spec :: SpecWith (ImpInit (LedgerSpec era))
-spec = describe "Plutus" $ do
-  pure ()
+spec = describe "Plutus V1 with references" $ do
+  it "fails with a reference script" $ do
+    const $ pendingWith "not implemented yet"
+
+  it "fails with a reference input" $ do
+    const $ pendingWith "not implemented yet"
