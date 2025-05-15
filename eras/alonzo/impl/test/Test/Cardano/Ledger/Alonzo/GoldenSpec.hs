04ddbb0080fb3170834b5538576232cc99770795
teodanciu <teodora.danciu@tweag.io>
2025-05-15 14:46:46 +0100

    Add JSON-serialization golden tests for `PParamsUpdate` in each era

--- eras/alonzo/impl/test/Test/Cardano/Ledger/Alonzo/GoldenSpec.hs
+++ eras/alonzo/impl/test/Test/Cardano/Ledger/Alonzo/GoldenSpec.hs
@@ -5,11 +5,14 @@ module Test.Cardano.Ledger.Alonzo.GoldenSpec (spec) where
 
 import Cardano.Ledger.Alonzo
 import Paths_cardano_ledger_alonzo (getDataFileName)
+import Test.Cardano.Ledger.Alonzo.Era ()
 import Test.Cardano.Ledger.Common
-import Test.Cardano.Ledger.Core.JSON (goldenJsonPParamsSpec)
+import Test.Cardano.Ledger.Core.JSON (goldenJsonPParamsSpec, goldenJsonPParamsUpdateSpec)
 
 spec :: Spec
 spec =
   describe "Golden" $ do
     beforeAll (getDataFileName "golden/pparams.json") $
       goldenJsonPParamsSpec @AlonzoEra
+    beforeAll (getDataFileName "golden/pparams-update.json") $
+      goldenJsonPParamsUpdateSpec @AlonzoEra
--- eras/babbage/impl/test/Test/Cardano/Ledger/Babbage/GoldenSpec.hs
+++ eras/babbage/impl/test/Test/Cardano/Ledger/Babbage/GoldenSpec.hs
@@ -5,11 +5,14 @@ module Test.Cardano.Ledger.Babbage.GoldenSpec (spec) where
 
 import Cardano.Ledger.Babbage
 import Paths_cardano_ledger_babbage (getDataFileName)
+import Test.Cardano.Ledger.Babbage.Era ()
 import Test.Cardano.Ledger.Common
-import Test.Cardano.Ledger.Core.JSON (goldenJsonPParamsSpec)
+import Test.Cardano.Ledger.Core.JSON (goldenJsonPParamsSpec, goldenJsonPParamsUpdateSpec)
 
 spec :: Spec
 spec =
   describe "Golden" $ do
     beforeAll (getDataFileName "golden/pparams.json") $
       goldenJsonPParamsSpec @BabbageEra
+    beforeAll (getDataFileName "golden/pparams-update.json") $
+      goldenJsonPParamsUpdateSpec @BabbageEra
