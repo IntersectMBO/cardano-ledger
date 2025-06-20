afdb201f4f53543ee9f7d2ca423677a20b3644a5
MaximilianAlgehed <MaximilianAlgehed@users.noreply.github.com>
2025-06-20 08:27:38 +0000

    Remove all existing files

--- eras/mary/impl/huddle-cddl/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Paths_cardano_ledger_mary
-import Test.Cardano.Ledger.Binary.Cuddle (writeSpec)
-import Test.Cardano.Ledger.Mary.CDDL (maryCDDL)
-
-main :: IO ()
-main = writeSpec maryCDDL =<< getDataFileName "cddl-files/mary.cddl"
