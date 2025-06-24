42be853017edf9b84449ba28e17fe5981a3e9092
teodanciu <teodora.danciu@tweag.io>
2025-06-24 14:28:08 +0100

    Refactor `BBodySpec` and `UtxoSpec` to reuse the existing abstractions

--- eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/BbodySpec.hs
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/BbodySpec.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -26,15 +25,14 @@ import Cardano.Ledger.Plutus (SLanguage (..))
 import Cardano.Ledger.Shelley.LedgerState
 import Cardano.Ledger.Shelley.Rules (
   BbodyEnv (..),
+  Event,
   ShelleyBbodyState (..),
  )
-import Cardano.Ledger.TxIn
 import Control.Monad (forM)
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Map as Map
 import qualified Data.Sequence.Strict as SSeq
-import qualified Data.Set as Set
-import Lens.Micro ((&), (.~), (^.))
+import Lens.Micro ((^.))
 import Lens.Micro.Mtl (use)
 import Test.Cardano.Ledger.Babbage.ImpTest
 import Test.Cardano.Ledger.Imp.Common
@@ -45,12 +43,13 @@ spec ::
   ( AlonzoEraImp era
   , BabbageEraTxBody era
   , InjectRuleFailure "BBODY" ConwayBbodyPredFailure era
+  , ToExpr (Event (EraRule "BBODY" era))
   ) =>
   SpecWith (ImpInit (LedgerSpec era))
-spec = describe "BBODY" $ do
+spec = do
   it "BodyRefScriptsSizeTooBig" $ do
-    Just (script :: Script era) <- pure largeScript
-    let scriptSize = originalBytesSize script
+    Just plutusScript <- pure $ mkPlutusScript @era $ purposeIsWellformedNoDatum SPlutusV2
+    let scriptSize = originalBytesSize plutusScript
 
     -- Determine a number of transactions and a number of times the reference script
     -- needs to be included as an input in each transaction,
@@ -62,36 +61,22 @@ spec = describe "BBODY" $ do
         maxRefScriptSizePerTx
         maxRefScriptSizePerBlock
 
-    let expectedTotalRefScriptSize = scriptSize * sum txScriptCounts
+    let mkTxWithNScripts n = do
+          -- Instead of using the rootTxIn, we are creating an input for each transaction
+          -- that we subsequently need to submit,
+          -- so that we can submit them independently of each other.
+          txIn <- freshKeyAddr_ >>= \addr -> sendCoinTo addr (Coin 8_000_000)
+          refIns <- replicateM n $ produceRefScript (fromPlutusScript plutusScript)
+          pure $ mkTxWithRefInputs txIn (NE.fromList refIns)
+
     txs <- do
-      -- Instead of using the rootTxIn, we are creating an input for each transaction
-      -- that we subsequently need to submit,
-      -- so that we can submit them independently of each other.
       forM txScriptCounts $ \n -> do
-        txIn <- mkTxIn
-        mkTxWithNScripts txIn script n
+        mkTxWithNScripts n
           >>= fixupFees
           >>= updateAddrTxWits
 
-    let txSeq = toTxSeq @era $ SSeq.fromList txs
-    nes <- use impNESL
-    let ls = nes ^. nesEsL . esLStateL
-        pp = nes ^. nesEsL . curPParamsEpochStateL
-    kh <- freshKeyHash
-    slotNo <- use impLastTickG
-    let bhView =
-          BHeaderView
-            { bhviewID = kh
-            , bhviewBSize = fromIntegral $ bBodySize (ProtVer (eraProtVerLow @era) 0) txSeq
-            , bhviewHSize = 0
-            , bhviewBHash = hashTxSeq txSeq
-            , bhviewSlot = slotNo
-            }
-    Left predFailures <-
-      tryRunImpRule @"BBODY"
-        (BbodyEnv pp (nes ^. chainAccountStateL))
-        (BbodyState ls (BlocksMade Map.empty))
-        (Block bhView txSeq)
+    let expectedTotalRefScriptSize = scriptSize * sum txScriptCounts
+    predFailures <- expectLeftExpr =<< tryRunBBODY txs
     predFailures
       `shouldBe` NE.fromList
         [ injectFailure
@@ -103,24 +88,25 @@ spec = describe "BBODY" $ do
             )
         ]
   where
-    mkTxIn :: ImpTestM era TxIn
-    mkTxIn = do
-      addr <- freshKeyAddr_
-      sendCoinTo addr (Coin 8_000_000)
-
-    largeScript :: Maybe (Script era)
-    largeScript = do
-      script <- mkPlutusScript @era $ purposeIsWellformedNoDatum SPlutusV2
-      pure $ fromPlutusScript script
-
-    mkTxWithNScripts :: TxIn -> Script era -> Int -> ImpTestM era (Tx era)
-    mkTxWithNScripts txIn script n = do
-      txIns <- replicateM n (produceRefScript script)
-      pure $
-        mkBasicTx $
-          mkBasicTxBody
-            & referenceInputsTxBodyL .~ Set.fromList txIns
-            & inputsTxBodyL .~ [txIn]
+    tryRunBBODY txs = do
+      let txSeq = toTxSeq @era $ SSeq.fromList txs
+      nes <- use impNESL
+      let ls = nes ^. nesEsL . esLStateL
+          pp = nes ^. nesEsL . curPParamsEpochStateL
+      kh <- freshKeyHash
+      slotNo <- use impLastTickG
+      let bhView =
+            BHeaderView
+              { bhviewID = kh
+              , bhviewBSize = fromIntegral $ bBodySize (ProtVer (eraProtVerLow @era) 0) txSeq
+              , bhviewHSize = 0
+              , bhviewBHash = hashTxSeq txSeq
+              , bhviewSlot = slotNo
+              }
+      tryRunImpRule @"BBODY"
+        (BbodyEnv pp (nes ^. chainAccountStateL))
+        (BbodyState ls (BlocksMade Map.empty))
+        (Block bhView txSeq)
 
 -- Generate a list of integers such that the sum of their multiples by scale is greater than toExceed
 -- and each individual value multiplied by the scale is smaller than maxSingle
--- eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/UtxoSpec.hs
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/UtxoSpec.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
@@ -24,30 +23,22 @@ import Cardano.Ledger.Conway.PParams (ppMinFeeRefScriptCostPerByteL)
 import Cardano.Ledger.Conway.TxCert
 import Cardano.Ledger.Conway.TxInfo (ConwayContextError (..))
 import Cardano.Ledger.Credential
-import Cardano.Ledger.MemoBytes (getMemoRawBytes)
 import Cardano.Ledger.Plutus.Language (
-  Plutus (..),
   SLanguage (..),
   hashPlutusScript,
  )
 import Cardano.Ledger.Shelley.LedgerState
 import Cardano.Ledger.Shelley.Scripts (
-  pattern RequireAllOf,
   pattern RequireSignature,
  )
 import Cardano.Ledger.Shelley.UTxO (getShelleyMinFeeTxUtxo)
 import Cardano.Ledger.State hiding (balance)
-import Cardano.Ledger.TxIn (TxIn (..))
 import Cardano.Ledger.Val
-import qualified Data.ByteString.Short as SBS (length)
-import Data.Functor ((<&>))
-import qualified Data.Map.Strict as Map
+import qualified Data.List.NonEmpty as NE
 import qualified Data.Sequence.Strict as SSeq
-import qualified Data.Set as Set
 import Lens.Micro ((&), (.~), (^.))
 import Test.Cardano.Ledger.Conway.ImpTest
 import Test.Cardano.Ledger.Core.Rational ((%!))
-import Test.Cardano.Ledger.Core.Utils (txInAt)
 import Test.Cardano.Ledger.Imp.Common
 import Test.Cardano.Ledger.Plutus.Examples (alwaysSucceedsNoDatum, inputsOverlapsWithRefInputs)
 
@@ -128,145 +119,63 @@ spec = do
       passEpoch
       -- Check for successfull pool refund
       getReward cred0 `shouldReturn` stakePoolDeposit
+
   describe "Reference scripts" $ do
+    let
+      nativeScript = RequireSignature @era <$> freshKeyHash
+      checkMinFeeUsingRefScripts refScripts = do
+        modifyPParams (ppMinFeeRefScriptCostPerByteL .~ (10 %! 1))
+        scriptTxIn <- nativeScript >>= impAddNativeScript >>= produceScript
+        refIns <- produceRefScripts (NE.fromList refScripts)
+        tx <- submitTxWithRefInputs scriptTxIn refIns
+        minFeeDiff <- do
+          utxo <- getUTxO
+          pp <- getsNES $ nesEsL . curPParamsEpochStateL
+          pure $ getMinFeeTxUtxo pp tx utxo <-> getShelleyMinFeeTxUtxo pp tx
+        refScriptFee <- getsPParams ppMinFeeRefScriptCostPerByteL
+        -- we check that the difference between conway and shelleyMinFee computation is exactly
+        -- the size of the sizes of the reference scripts
+        minFeeDiff
+          `shouldBe` Coin
+            ( floor $
+                fromIntegral @Int @Rational (sum $ originalBytesSize <$> refScripts)
+                  * unboundRational refScriptFee
+            )
+      distinctScripts = do
+        nativeScripts <- replicateM 3 (fromNativeScript <$> nativeScript)
+        Just plutusScriptV2 <- pure $ mkPlutusScript @era $ alwaysSucceedsNoDatum SPlutusV2
+        Just plutusScriptV3 <- pure $ mkPlutusScript @era $ alwaysSucceedsNoDatum SPlutusV3
+        pure $ nativeScripts ++ [fromPlutusScript plutusScriptV2, fromPlutusScript plutusScriptV3]
+
     it "required reference script counts towards the minFee calculation" $ do
       spendingScript <- nativeScript
-      checkMinFee spendingScript [fromNativeScript spendingScript]
+      checkMinFeeUsingRefScripts [fromNativeScript spendingScript]
 
     it "reference scripts not required for spending the input count towards the minFee calculation" $ do
       spendingScript <- nativeScript
       extraScripts <- distinctScripts
-      checkMinFee spendingScript $
-        fromNativeScript spendingScript : extraScripts
+      checkMinFeeUsingRefScripts $ fromNativeScript spendingScript : extraScripts
 
     it "a scripts referenced several times counts for each reference towards the minFee calculation" $ do
       spendingScript <- nativeScript
       extraScripts <- distinctScripts
-      checkMinFee spendingScript $
+      checkMinFeeUsingRefScripts $
         [fromNativeScript spendingScript, fromNativeScript spendingScript]
           ++ extraScripts
           ++ extraScripts
-    describe "disjoint inputs and reference inputs" $ do
-      let
-        scriptHash lang = hashPlutusScript $ inputsOverlapsWithRefInputs lang
-        mkTestTx :: TxIn -> Tx era
-        mkTestTx txIn =
-          mkBasicTx mkBasicTxBody
-            & bodyTxL . inputsTxBodyL .~ Set.singleton txIn
-            & bodyTxL . referenceInputsTxBodyL .~ Set.singleton txIn
 
-      it "Cannot run scripts that expect inputs and refInputs to overlap (PV 9/10)" $ do
-        whenMajorVersionAtMost @10 $ do
-          txIn <- produceScript $ scriptHash SPlutusV3
-          submitFailingTx @era
-            (mkTestTx txIn)
-            [ injectFailure $ BabbageNonDisjointRefInputs [txIn]
-            ]
-      it "Same script cannot appear in regular and reference inputs in PlutusV3 (PV 11)" $ whenMajorVersionAtLeast @11 $ do
+    let scriptHash lang = hashPlutusScript $ inputsOverlapsWithRefInputs lang
+    it "Cannot run scripts that expect inputs and refInputs to overlap (PV 9/10)" $ do
+      whenMajorVersionAtMost @10 $ do
         txIn <- produceScript $ scriptHash SPlutusV3
         submitFailingTx @era
-          (mkTestTx txIn)
-          [ injectFailure $
-              CollectErrors [BadTranslation . inject $ ReferenceInputsNotDisjointFromInputs @era [txIn]]
+          (mkTxWithRefInputs txIn (NE.fromList [txIn]))
+          [ injectFailure $ BabbageNonDisjointRefInputs [txIn]
           ]
-  where
-    checkMinFee :: HasCallStack => NativeScript era -> [Script era] -> ImpTestM era ()
-    checkMinFee scriptToSpend refScripts = do
-      refScriptFee <- setRefScriptFee
-      logString "lock an input with a script"
-      scriptSpendIn <- createScriptUtxo scriptToSpend
-      logString
-        "create outputs with reference scripts and the return them mapped to their corresponding inputs"
-      refScriptInToScripts <- createRefScriptsUtxos refScripts
-      logString "spend the initial input by passing the reference scripts"
-      tx <- spendScriptUsingRefScripts scriptSpendIn $ Map.keysSet refScriptInToScripts
-      logString
-        "compute the difference between the current-era minFee and that computed in pre-Conway eras"
-      minFeeDiff <- conwayDiffMinFee tx
-      logString "check that the difference is the sum of the sizes of the passed reference scripts"
-      minFeeDiff
-        `shouldBe` Coin
-          ( floor $
-              fromIntegral @Int @Rational (sum $ scriptSize <$> refScriptInToScripts)
-                * unboundRational refScriptFee
-          )
-
-    distinctScripts :: HasCallStack => ImpTestM era [Script era]
-    distinctScripts = do
-      nativeScripts <-
-        (fromNativeScript @era <$>)
-          <$> replicateM 3 nativeScript
-      let
-        psh1 = hashPlutusScript $ alwaysSucceedsNoDatum SPlutusV2
-      ps1 <- impAnn "Expecting Plutus script" . expectJust $ impLookupPlutusScript psh1
-      let
-        psh2 = hashPlutusScript $ alwaysSucceedsNoDatum SPlutusV3
-      ps2 <- impAnn "Expecting Plutus script" . expectJust $ impLookupPlutusScript psh2
-      let plutusScripts = [fromPlutusScript ps1, fromPlutusScript ps2]
-      pure $ nativeScripts ++ plutusScripts
-
-    conwayDiffMinFee :: Tx era -> ImpTestM era Coin
-    conwayDiffMinFee tx = do
-      utxo <- getUTxO
-      pp <- getsNES $ nesEsL . curPParamsEpochStateL
-      pure $ getMinFeeTxUtxo pp tx utxo <-> getShelleyMinFeeTxUtxo pp tx
-
-    createScriptUtxo :: HasCallStack => NativeScript era -> ImpTestM era TxIn
-    createScriptUtxo script = do
-      scriptAddr <- addScriptAddr script
-      tx <-
-        submitTx . mkBasicTx $
-          mkBasicTxBody
-            & outputsTxBodyL @era
-              .~ SSeq.fromList [mkBasicTxOut @era scriptAddr mempty]
-      pure $ txInAt 0 tx
-
-    createRefScriptsUtxos ::
-      HasCallStack => [Script era] -> ImpTestM era (Map.Map TxIn (Script era))
-    createRefScriptsUtxos scripts = do
-      rootOut <- snd <$> getImpRootTxOut
-      let outs =
-            scripts
-              <&> ( \s ->
-                      mkBasicTxOut @era (rootOut ^. addrTxOutL) mempty
-                        & referenceScriptTxOutL @era .~ SJust s
-                  )
-      tx <-
-        submitTx . mkBasicTx $
-          mkBasicTxBody
-            & outputsTxBodyL @era
-              .~ SSeq.fromList outs
-      let refIns = (`txInAt` tx) <$> [0 .. length scripts - 1]
-      pure $ Map.fromList $ refIns `zip` scripts
-
-    spendScriptUsingRefScripts ::
-      HasCallStack => TxIn -> Set.Set TxIn -> ImpTestM era (Tx era)
-    spendScriptUsingRefScripts scriptIn refIns =
-      submitTxAnn "spendScriptUsingRefScripts" . mkBasicTx $
-        mkBasicTxBody
-          & inputsTxBodyL @era .~ Set.singleton scriptIn
-          & referenceInputsTxBodyL @era .~ refIns
-
-    nativeScript :: ImpTestM era (NativeScript era)
-    nativeScript = do
-      requiredKeyHash <- freshKeyHash
-      let script = RequireAllOf (SSeq.singleton (RequireSignature @era requiredKeyHash))
-      _ <- impAddNativeScript script
-      pure script
-
-    addScriptAddr :: NativeScript era -> ImpTestM era Addr
-    addScriptAddr script = do
-      scriptHash <- impAddNativeScript script
-      stakingKeyHash <- freshKeyHash @'Staking
-      pure $ mkAddr scriptHash stakingKeyHash
-
-    scriptSize :: Script era -> Int
-    scriptSize = \case
-      TimelockScript tl -> SBS.length $ getMemoRawBytes tl
-      PlutusScript ps -> withPlutusScript ps (SBS.length . unPlutusBinary . plutusBinary)
-
-    setRefScriptFee :: ImpTestM era NonNegativeInterval
-    setRefScriptFee = do
-      let refScriptFee = 10 %! 1
-      modifyPParams $ ppMinFeeRefScriptCostPerByteL .~ refScriptFee
-      pure refScriptFee
+    it "Same script cannot appear in regular and reference inputs in PlutusV3 (PV 11)" $ whenMajorVersionAtLeast @11 $ do
+      txIn <- produceScript $ scriptHash SPlutusV3
+      submitFailingTx @era
+        (mkTxWithRefInputs txIn (NE.fromList [txIn]))
+        [ injectFailure $
+            CollectErrors [BadTranslation . inject $ ReferenceInputsNotDisjointFromInputs @era [txIn]]
+        ]
