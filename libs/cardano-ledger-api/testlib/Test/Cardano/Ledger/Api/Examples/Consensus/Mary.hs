87b18e4d3f3def1c50fc642360a16d1382a4bda0
teodanciu <teodora.danciu@tweag.io>
2025-07-02 13:58:43 +0100

    Complete defs of shelley-ma `Examples` in cardano-ledger-api:testlib

--- libs/cardano-ledger-api/testlib/Test/Cardano/Ledger/Api/Examples/Consensus/Mary.hs
+++ libs/cardano-ledger-api/testlib/Test/Cardano/Ledger/Api/Examples/Consensus/Mary.hs
@@ -11,16 +11,35 @@ module Test.Cardano.Ledger.Api.Examples.Consensus.Mary (
   exampleMultiAsset,
 ) where
 
+import Cardano.Ledger.Coin
+import Cardano.Ledger.Genesis (NoGenesis (..))
 import Cardano.Ledger.Mary (MaryEra)
+import Cardano.Ledger.Mary.Core
 import Cardano.Ledger.Mary.Value
+import qualified Data.Map.Strict as Map (singleton)
+import Data.Proxy
+import Lens.Micro
+import Test.Cardano.Ledger.Api.Examples.Consensus.Allegra
 import Test.Cardano.Ledger.Api.Examples.Consensus.Shelley
 
--- | ShelleyLedgerExamples for Mary era
+-- | ShelleyLedgerExamples for Allegra era
 ledgerExamplesMary :: ShelleyLedgerExamples MaryEra
-ledgerExamplesMary = undefined
+ledgerExamplesMary =
+  defaultShelleyLedgerExamples
+    (mkWitnessesPreAlonzo (Proxy @MaryEra))
+    id
+    (exampleMultiAssetValue 1)
+    ((exampleAllegraTxBody (exampleMultiAssetValue 1)) & mintTxBodyL .~ exampleMultiAsset 1)
+    exampleAllegraTxAuxData
+    NoGenesis
 
 exampleMultiAssetValue :: Int -> MaryValue
-exampleMultiAssetValue = undefined
+exampleMultiAssetValue x = MaryValue (Coin 100) $ exampleMultiAsset x
 
 exampleMultiAsset :: Int -> MultiAsset
-exampleMultiAsset = undefined
+exampleMultiAsset x =
+  MultiAsset (Map.singleton policyId $ Map.singleton couttsCoin 1000)
+  where
+    policyId = PolicyID $ mkScriptHash x
+    couttsCoin :: AssetName
+    couttsCoin = AssetName "couttsCoin"
