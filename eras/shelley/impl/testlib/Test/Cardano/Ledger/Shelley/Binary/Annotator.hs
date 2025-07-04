148e4e8d710eb590815745ec4da2d0f1c14c3306
Aniket Deshpande <git@aniketd.mailer.me>
2025-07-04 18:53:13 +0530

    Move Shelley.BlockChain to an Internal module.
    Keep it exposed, since testlib depends on the TxSeq' constructor in a
    DecCBOR instance.

--- eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Binary/Annotator.hs
+++ eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Binary/Annotator.hs
@@ -22,6 +22,7 @@ import Cardano.Ledger.Core
 import Cardano.Ledger.MemoBytes (decodeMemoized)
 import Cardano.Ledger.Shelley (ShelleyEra)
 import Cardano.Ledger.Shelley.BlockChain
+import Cardano.Ledger.Shelley.BlockChain.Internal
 import Cardano.Ledger.Shelley.Scripts
 import Cardano.Ledger.Shelley.Tx (ShelleyTx (..))
 import Cardano.Ledger.Shelley.TxAuxData
