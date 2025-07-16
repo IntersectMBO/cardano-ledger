dd6b537589e377fc1a2a87b43e89b7e05c3ec070
teodanciu <teodora.danciu@tweag.io>
2025-07-16 12:29:21 +0100

    Redefine mary Examples in cardano-ledger-api using testlib and tpraos
    Examples and re-export them

--- libs/cardano-ledger-api/testlib/Test/Cardano/Ledger/Api/Examples/Consensus/Mary.hs
+++ libs/cardano-ledger-api/testlib/Test/Cardano/Ledger/Api/Examples/Consensus/Mary.hs
@@ -10,34 +10,22 @@ module Test.Cardano.Ledger.Api.Examples.Consensus.Mary (
   exampleMultiAsset,
 ) where
 
-import Cardano.Ledger.Coin
-import Cardano.Ledger.Genesis (NoGenesis (..))
 import Cardano.Ledger.Mary (MaryEra)
-import Cardano.Ledger.Mary.Core
-import Cardano.Ledger.Mary.Value
-import qualified Data.Map.Strict as Map (singleton)
-import Data.Proxy
-import Lens.Micro
-import Test.Cardano.Ledger.Api.Examples.Consensus.Allegra
-import Test.Cardano.Ledger.Api.Examples.Consensus.Shelley
+import Cardano.Protocol.Crypto (StandardCrypto)
+import Cardano.Protocol.TPraos.API (ChainDepState)
+import Cardano.Protocol.TPraos.BHeader (BHeader, HashHeader)
+import Test.Cardano.Ledger.Mary.Examples
+import Test.Cardano.Ledger.Shelley.Examples (LedgerExamples)
+import Test.Cardano.Protocol.TPraos.Examples (
+  exampleBlockHeader,
+  exampleHashHeader,
+  exampleLedgerChainDepState,
+ )
 
--- | ShelleyLedgerExamples for Allegra era
-ledgerExamplesMary :: ShelleyLedgerExamples MaryEra
+ledgerExamplesMary ::
+  LedgerExamples (BHeader StandardCrypto) HashHeader ChainDepState MaryEra
 ledgerExamplesMary =
-  defaultShelleyLedgerExamples
-    (mkWitnessesPreAlonzo (Proxy @MaryEra))
-    (exampleMultiAssetValue 1)
-    (exampleAllegraTxBody (exampleMultiAssetValue 1) & mintTxBodyL .~ exampleMultiAsset 1)
-    exampleAllegraTxAuxData
-    NoGenesis
-
-exampleMultiAssetValue :: Int -> MaryValue
-exampleMultiAssetValue x = MaryValue (Coin 100) $ exampleMultiAsset x
-
-exampleMultiAsset :: Int -> MultiAsset
-exampleMultiAsset x =
-  MultiAsset (Map.singleton policyId $ Map.singleton couttsCoin 1000)
-  where
-    policyId = PolicyID $ mkScriptHash x
-    couttsCoin :: AssetName
-    couttsCoin = AssetName "couttsCoin"
+  ledgerExamples
+    (exampleBlockHeader [exampleTxMary])
+    exampleHashHeader
+    (exampleLedgerChainDepState 1)
