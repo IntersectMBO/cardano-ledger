7482c9ea43f2cef296eefc8fa68011c151949dec
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-07-17 17:55:34 +0200

    Add `registerRewardAccountWithDeposit` helper

--- eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/ImpTest.hs
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/ImpTest.hs
@@ -136,6 +136,7 @@ module Test.Cardano.Ledger.Conway.ImpTest (
   getCommittee,
   registerStakeCredentialWithDeposit,
   registerPoolWithDeposit,
+  registerRewardAccountWithDeposit,
 ) where
 
 import Cardano.Ledger.Address (RewardAccount (..))
@@ -1878,3 +1879,10 @@ registerPoolWithDeposit ::
 registerPoolWithDeposit khPool =
   (freshKeyHash >>= registerStakeCredentialWithDeposit . KeyHashObj)
     >>= registerPoolWithRewardAccount khPool
+
+registerRewardAccountWithDeposit ::
+  forall era.
+  ConwayEraImp era =>
+  ImpTestM era RewardAccount
+registerRewardAccountWithDeposit = do
+  freshKeyHash >>= registerStakeCredentialWithDeposit . KeyHashObj
