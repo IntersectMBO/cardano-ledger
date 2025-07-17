51599c868d90a98f416a8057baabfa2c86478485
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-07-17 20:05:55 +0200

    Remove `ShelleyEraTxCert` constraint from imp tests

--- eras/alonzo/impl/testlib/Test/Cardano/Ledger/Alonzo/Imp.hs
+++ eras/alonzo/impl/testlib/Test/Cardano/Ledger/Alonzo/Imp.hs
@@ -11,16 +11,16 @@ import Cardano.Ledger.Alonzo.Core
 import Cardano.Ledger.Alonzo.Rules (
   AlonzoUtxoPredFailure,
   AlonzoUtxosPredFailure,
-  AlonzoUtxowPredFailure,
+  -- AlonzoUtxowPredFailure,
  )
 import Cardano.Ledger.Shelley.Rules (
-  ShelleyDelegPredFailure,
+  -- ShelleyDelegPredFailure,
   ShelleyUtxoPredFailure,
   ShelleyUtxowPredFailure,
  )
 import qualified Test.Cardano.Ledger.Alonzo.Imp.UtxoSpec as Utxo
 import qualified Test.Cardano.Ledger.Alonzo.Imp.UtxosSpec as Utxos
-import qualified Test.Cardano.Ledger.Alonzo.Imp.UtxowSpec as Utxow
+-- import qualified Test.Cardano.Ledger.Alonzo.Imp.UtxowSpec as Utxow
 import Test.Cardano.Ledger.Alonzo.ImpTest
 import Test.Cardano.Ledger.Imp.Common
 import qualified Test.Cardano.Ledger.Mary.Imp as MaryImp
@@ -28,13 +28,12 @@ import qualified Test.Cardano.Ledger.Mary.Imp as MaryImp
 spec ::
   forall era.
   ( AlonzoEraImp era
-  , ShelleyEraTxCert era
-  , InjectRuleFailure "LEDGER" ShelleyDelegPredFailure era
-  , InjectRuleFailure "LEDGER" ShelleyUtxoPredFailure era
+  , -- , InjectRuleFailure "LEDGER" ShelleyDelegPredFailure era
+    InjectRuleFailure "LEDGER" ShelleyUtxoPredFailure era
   , InjectRuleFailure "LEDGER" ShelleyUtxowPredFailure era
   , InjectRuleFailure "LEDGER" AlonzoUtxoPredFailure era
   , InjectRuleFailure "LEDGER" AlonzoUtxosPredFailure era
-  , InjectRuleFailure "LEDGER" AlonzoUtxowPredFailure era
+  -- , InjectRuleFailure "LEDGER" AlonzoUtxowPredFailure era
   ) =>
   Spec
 spec = do
@@ -42,4 +41,5 @@ spec = do
   describe "AlonzoImpSpec" . withEachEraVersion @era $ do
     Utxo.spec
     Utxos.spec
-    Utxow.spec
+
+-- Utxow.spec
