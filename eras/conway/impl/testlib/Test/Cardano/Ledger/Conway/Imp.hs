807c980f6d16359ab2677fe5dca2eab2e5f17e4f
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-07-18 21:45:04 +0200

    WIP - `LedgerSpec`

--- eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp.hs
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp.hs
@@ -48,7 +48,7 @@ import qualified Test.Cardano.Ledger.Conway.Imp.EnactSpec as Enact
 import qualified Test.Cardano.Ledger.Conway.Imp.EpochSpec as Epoch
 import qualified Test.Cardano.Ledger.Conway.Imp.GovCertSpec as GovCert
 import qualified Test.Cardano.Ledger.Conway.Imp.GovSpec as Gov
--- import qualified Test.Cardano.Ledger.Conway.Imp.LedgerSpec as Ledger
+import qualified Test.Cardano.Ledger.Conway.Imp.LedgerSpec as Ledger
 -- import qualified Test.Cardano.Ledger.Conway.Imp.RatifySpec as Ratify
 -- import qualified Test.Cardano.Ledger.Conway.Imp.UtxoSpec as Utxo
 -- import qualified Test.Cardano.Ledger.Conway.Imp.UtxosSpec as Utxos
@@ -136,8 +136,8 @@ conwaySpec = do
   describe "EPOCH" Epoch.spec
   describe "GOV" Gov.spec
   describe "GOVCERT" GovCert.spec
+  describe "LEDGER" Ledger.spec
 
--- describe "LEDGER" Ledger.spec
 -- describe "RATIFY" Ratify.spec
 -- describe "UTXO" Utxo.spec
 -- describe "UTXOS" Utxos.spec
