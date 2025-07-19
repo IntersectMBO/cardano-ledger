331f635c307d24eccab6de0989b400a800b0894e
lehins <lehins@users.noreply.github.com>
2025-07-19 01:48:25 +0000

    Remove all existing files

--- libs/cardano-ledger-test/src/Test/Cardano/Ledger/Constrained/Conway/NewEpoch.hs
+++
@@ -1,9 +0,0 @@
--- | Specs necessary to generate state for the NEWEPOCH rule
-module Test.Cardano.Ledger.Constrained.Conway.NewEpoch where
-
-import Cardano.Ledger.Conway (ConwayEra)
-import Cardano.Ledger.Shelley.API.Types
-import Constrained.API
-
-newEpochStateSpec :: Specification (NewEpochState ConwayEra)
-newEpochStateSpec = trueSpec
