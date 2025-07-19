8edcaf190cebac84dfc0b131c69c5c339fe34490
Neil Mayhew <neil.mayhew@iohk.io>
2025-07-18 20:07:49 -0600

    fixup! Implement

--- eras/babbage/impl/testlib/Test/Cardano/Ledger/Babbage/Imp/UtxowSpec/Valid.hs
+++ eras/babbage/impl/testlib/Test/Cardano/Ledger/Babbage/Imp/UtxowSpec/Valid.hs
@@ -13,15 +13,18 @@ module Test.Cardano.Ledger.Babbage.Imp.UtxowSpec.Valid (spec) where
 import Cardano.Ledger.Babbage.Core
 import Cardano.Ledger.BaseTypes
 import Cardano.Ledger.Coin (Coin (..))
+import Cardano.Ledger.Credential (StakeReference (..))
 import Cardano.Ledger.Plutus (
   Language (..),
   hashPlutusScript,
   withSLanguage,
  )
-import Cardano.Ledger.Shelley.Scripts (pattern RequireAnyOf)
-import Cardano.Ledger.TxIn (mkTxInPartial)
+import Cardano.Ledger.Shelley.Scripts (
+  pattern RequireAllOf,
+ )
 import Lens.Micro
 import Test.Cardano.Ledger.Alonzo.ImpTest
+import Test.Cardano.Ledger.Core.Utils (txInAt)
 import Test.Cardano.Ledger.Imp.Common
 import Test.Cardano.Ledger.Plutus.Examples
 
@@ -35,16 +38,16 @@ spec = describe "Valid" $ do
   it "Native reference scripts must be witnessed" $ do
     addr <- freshKeyAddr_
     let
-      timelock = fromNativeScript @era $ RequireAnyOf []
+      nativeScript = fromNativeScript @era $ RequireAllOf []
       txOut =
-        mkCoinTxOut addr (inject $ Coin 15_000_000)
-          & referenceScriptTxOutL .~ SJust timelock
+        mkCoinTxOut addr (Coin 15_000_000)
+          & referenceScriptTxOutL .~ SJust nativeScript
     tx0 <-
       submitTx $
         mkBasicTx mkBasicTxBody
           & bodyTxL . outputsTxBodyL .~ [txOut]
     let
-      txIn = mkTxInPartial (txIdTx tx0) 0
+      txIn = txInAt 0 tx0
       tx1 =
         mkBasicTx mkBasicTxBody
           & bodyTxL . referenceInputsTxBodyL .~ [txIn]
@@ -60,23 +63,44 @@ spec = describe "Valid" $ do
             mkBasicTx mkBasicTxBody
               & bodyTxL . referenceInputsTxBodyL .~ [txIn]
 
-  it "Reference script" $ do
-    const $ pendingWith "not implemented yet"
+        it "Reference script" $ do
+          const $ pendingWith "not implemented yet"
 
-  it "Inline datum and ref script" $ do
-    const $ pendingWith "not implemented yet"
+        it "Inline datum and ref script" $ do
+          const $ pendingWith "not implemented yet"
 
-  it "Reference input with data hash, no data witness" $ do
-    const $ pendingWith "not implemented yet"
+        it "Reference input with data hash, no data witness" $ do
+          const $ pendingWith "not implemented yet"
 
-  it "Reference input with data hash, with data witness" $ do
-    const $ pendingWith "not implemented yet"
+        it "Reference input with data hash, with data witness" $ do
+          const $ pendingWith "not implemented yet"
 
-  it "Reference script to authorize delegation certificate" $ do
-    const $ pendingWith "not implemented yet"
+        it "Reference script to authorize delegation certificate" $ do
+          const $ pendingWith "not implemented yet"
 
-  it "Reference script in output" $ do
-    const $ pendingWith "not implemented yet"
+        it "Reference script in output" $ do
+          const $ pendingWith "not implemented yet"
 
-  it "Spend simple script output with reference script" $ do
-    const $ pendingWith "not implemented yet"
+        it "Spend simple script output with reference script" $ do
+          addr <- freshKeyAddr_
+          let
+            nativeScript = fromNativeScript @era $ RequireAllOf []
+            nativeScriptAddr = mkAddr (hashScript nativeScript) StakeRefNull
+            txOutScript =
+              mkCoinTxOut nativeScriptAddr (Coin 12_000_000)
+            txOutRef =
+              mkCoinTxOut addr (Coin 15_000_000)
+                & referenceScriptTxOutL .~ SJust nativeScript
+          tx0 <-
+            submitTx $
+              mkBasicTx mkBasicTxBody
+                & bodyTxL . outputsTxBodyL .~ [txOutScript, txOutRef]
+          let
+            txInScript = txInAt 0 tx0
+            txInRef = txInAt 1 tx0
+            tx1 =
+              mkBasicTx $
+                mkBasicTxBody
+                  & inputsTxBodyL .~ [txInScript]
+                  & referenceInputsTxBodyL .~ [txInRef]
+          submitTx_ tx1
