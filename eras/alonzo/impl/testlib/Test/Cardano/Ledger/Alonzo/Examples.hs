a8e9b67f28cccbad98d0459e670517c9edf164ee
teodanciu <teodora.danciu@tweag.io>
2025-07-21 15:08:23 +0100

    Create `LedgerExamples` for each era
    in the respective testlib library, and expose them as
    `Test.Cardano.Ledger.<era>.Examples.ledgerExamples`

---
+++ eras/alonzo/impl/testlib/Test/Cardano/Ledger/Alonzo/Examples.hs
@@ -0,0 +1,219 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Test.Cardano.Ledger.Alonzo.Examples (
+  ledgerExamples,
+  mkLedgerExamples,
+  exampleTx,
+  exampleDatum,
+  exampleAlonzoGenesis,
+) where
+
+import Cardano.Ledger.Allegra.Scripts (Timelock)
+import Cardano.Ledger.Alonzo (AlonzoEra)
+import Cardano.Ledger.Alonzo.Core
+import Cardano.Ledger.Alonzo.Genesis (AlonzoGenesis (..))
+import Cardano.Ledger.Alonzo.Plutus.Context (EraPlutusTxInfo)
+import Cardano.Ledger.Alonzo.Scripts (
+  AlonzoPlutusPurpose (..),
+  AlonzoScript (..),
+  ExUnits (..),
+  Prices (..),
+ )
+import Cardano.Ledger.Alonzo.TxAuxData (AlonzoTxAuxData, mkAlonzoTxAuxData)
+import Cardano.Ledger.Alonzo.TxBody (AlonzoTxOut (..), TxBody (..))
+import Cardano.Ledger.Alonzo.TxWits (Redeemers (..), TxDats (..))
+import Cardano.Ledger.BaseTypes (StrictMaybe (..))
+import Cardano.Ledger.Coin (Coin (..))
+import Cardano.Ledger.Keys (asWitness)
+import Cardano.Ledger.Mary.Value (MaryValue (..))
+import Cardano.Ledger.Plutus.CostModels (mkCostModels)
+import Cardano.Ledger.Plutus.Data (Data (..), hashData)
+import Cardano.Ledger.Plutus.Language (Language (..))
+import Cardano.Ledger.Shelley.API (
+  ApplyTxError (..),
+  Credential (..),
+  Network (..),
+  NewEpochState (..),
+  ProposedPPUpdates (..),
+  RewardAccount (..),
+  TxId (..),
+  Update (..),
+ )
+import Cardano.Ledger.Shelley.Rules (ShelleyDelegsPredFailure (..), ShelleyLedgerPredFailure (..))
+import Cardano.Ledger.Shelley.Scripts
+import Cardano.Ledger.TxIn (mkTxInPartial)
+import Cardano.Slotting.Slot (EpochNo (..), SlotNo (..))
+import Data.Default (def)
+import qualified Data.Map.Strict as Map
+import qualified Data.Sequence.Strict as StrictSeq
+import qualified Data.Set as Set
+import Lens.Micro
+import qualified PlutusLedgerApi.Common as P
+import Test.Cardano.Ledger.Alonzo.Arbitrary (alwaysFails, alwaysSucceeds)
+import Test.Cardano.Ledger.Core.KeyPair (mkAddr, mkWitnessesVKey)
+import Test.Cardano.Ledger.Core.Utils (mkDummySafeHash, unsafeBoundRational)
+import Test.Cardano.Ledger.Mary.Examples (exampleMultiAssetValue)
+import Test.Cardano.Ledger.Plutus (zeroTestingCostModelV1)
+import Test.Cardano.Ledger.Shelley.Examples (
+  LedgerExamples (..),
+  exampleAuxDataMap,
+  exampleCerts,
+  exampleNewEpochState,
+  exampleNonMyopicRewards,
+  examplePayKey,
+  examplePoolDistr,
+  exampleStakeKey,
+  keyToCredential,
+  mkKeyHash,
+  mkScriptHash,
+  testShelleyGenesis,
+ )
+
+ledgerExamples :: LedgerExamples AlonzoEra
+ledgerExamples =
+  mkLedgerExamples
+    ( ApplyTxError $
+        pure $
+          DelegsFailure $
+            DelegateeNotRegisteredDELEG @AlonzoEra (mkKeyHash 1)
+    )
+    exampleAlonzoNewEpochState
+    exampleTxAlonzo
+    exampleAlonzoGenesis
+
+mkLedgerExamples ::
+  forall era.
+  AlonzoEraPParams era =>
+  ApplyTxError era ->
+  NewEpochState era ->
+  Tx era ->
+  TranslationContext era ->
+  LedgerExamples era
+mkLedgerExamples
+  applyTxError
+  newEpochState
+  tx
+  translationContext =
+    LedgerExamples
+      { leTx = tx
+      , leApplyTxError = applyTxError
+      , lePParams = def
+      , leProposedPPUpdates =
+          ProposedPPUpdates $
+            Map.singleton
+              (mkKeyHash 0)
+              (emptyPParamsUpdate & ppuCollateralPercentageL .~ SJust 150)
+      , leNewEpochState = newEpochState
+      , lePoolDistr = examplePoolDistr
+      , leRewardsCredentials =
+          Set.fromList
+            [ Left (Coin 100)
+            , Right (ScriptHashObj (mkScriptHash 1))
+            , Right (KeyHashObj (mkKeyHash 2))
+            ]
+      , leNonMyopicRewards = exampleNonMyopicRewards
+      , leTranslationContext = translationContext
+      , leShelleyGenesis = testShelleyGenesis
+      }
+
+exampleAlonzoNewEpochState :: NewEpochState AlonzoEra
+exampleAlonzoNewEpochState =
+  exampleNewEpochState
+    (exampleMultiAssetValue 1)
+    emptyPParams
+    (emptyPParams & ppCoinsPerUTxOWordL .~ CoinPerWord (Coin 1))
+
+exampleTxAlonzo :: Tx AlonzoEra
+exampleTxAlonzo = exampleTx exampleTxBodyAlonzo (AlonzoSpending $ AsIx 0)
+
+exampleTx ::
+  forall era.
+  ( AlonzoEraTx era
+  , EraPlutusTxInfo 'PlutusV1 era
+  , TxAuxData era ~ AlonzoTxAuxData era
+  , Script era ~ AlonzoScript era
+  , NativeScript era ~ Timelock era
+  ) =>
+  TxBody era -> PlutusPurpose AsIx era -> Tx era
+exampleTx txBody scriptPurpose =
+  mkBasicTx @era txBody
+    & witsTxL
+      .~ ( mkBasicTxWits
+             & addrTxWitsL .~ mkWitnessesVKey (hashAnnotated txBody) [asWitness examplePayKey]
+             & bootAddrTxWitsL .~ mempty
+             & scriptTxWitsL
+               .~ Map.singleton
+                 (hashScript @era $ alwaysSucceeds @'PlutusV1 3)
+                 (alwaysSucceeds @'PlutusV1 3)
+             & datsTxWitsL .~ TxDats (Map.singleton (hashData (exampleDatum @era)) exampleDatum)
+             & rdmrsTxWitsL
+               .~ Redeemers (Map.singleton scriptPurpose (exampleRedeemer, ExUnits 5000 5000))
+         )
+    & isValidTxL .~ IsValid True
+    & auxDataTxL
+      .~ SJust
+        ( mkAlonzoTxAuxData
+            exampleAuxDataMap
+            [alwaysFails @'PlutusV1 2, TimelockScript $ RequireAllOf @era mempty]
+        )
+
+exampleTxBodyAlonzo :: TxBody AlonzoEra
+exampleTxBodyAlonzo =
+  AlonzoTxBody
+    (Set.fromList [mkTxInPartial (TxId (mkDummySafeHash 1)) 0]) -- inputs
+    (Set.fromList [mkTxInPartial (TxId (mkDummySafeHash 2)) 1]) -- collateral
+    ( StrictSeq.fromList
+        [ AlonzoTxOut
+            (mkAddr examplePayKey exampleStakeKey)
+            (exampleMultiAssetValue 2)
+            (SJust $ mkDummySafeHash 1) -- outputs
+        ]
+    )
+    exampleCerts -- txcerts
+    ( Withdrawals $
+        Map.singleton
+          (RewardAccount Testnet (keyToCredential exampleStakeKey))
+          (Coin 100) -- txwdrls
+    )
+    (Coin 999) -- txfee
+    (ValidityInterval (SJust (SlotNo 2)) (SJust (SlotNo 4))) -- txvldt
+    ( SJust $
+        Update
+          ( ProposedPPUpdates $
+              Map.singleton
+                (mkKeyHash 1)
+                (emptyPParamsUpdate & ppuMaxBHSizeL .~ SJust 4000)
+          )
+          (EpochNo 0)
+    ) -- txUpdates
+    (Set.singleton $ mkKeyHash 212) -- reqSignerHashes
+    exampleMultiAsset -- mint
+    (SJust $ mkDummySafeHash 42) -- scriptIntegrityHash
+    (SJust . TxAuxDataHash $ mkDummySafeHash 42) -- adHash
+    (SJust Mainnet) -- txnetworkid
+  where
+    MaryValue _ exampleMultiAsset = exampleMultiAssetValue 3
+
+exampleDatum :: Era era => Data era
+exampleDatum = Data (P.I 191)
+
+exampleRedeemer :: Era era => Data era
+exampleRedeemer = Data (P.I 919)
+
+exampleAlonzoGenesis :: AlonzoGenesis
+exampleAlonzoGenesis =
+  AlonzoGenesis
+    { agCoinsPerUTxOWord = CoinPerWord $ Coin 1
+    , agCostModels = mkCostModels (Map.fromList [(PlutusV1, zeroTestingCostModelV1)])
+    , agPrices = Prices (unsafeBoundRational 90) (unsafeBoundRational 91)
+    , agMaxTxExUnits = ExUnits 123 123
+    , agMaxBlockExUnits = ExUnits 223 223
+    , agMaxValSize = 1234
+    , agCollateralPercentage = 20
+    , agMaxCollateralInputs = 30
+    }
---
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Examples.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.Cardano.Ledger.Conway.Examples (
+  ledgerExamples,
+  exampleConwayCerts,
+) where
+
+import Cardano.Ledger.Babbage.TxBody (BabbageTxOut (..))
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.Binary (mkSized)
+import Cardano.Ledger.Coin (Coin (..))
+import Cardano.Ledger.Conway (ConwayEra)
+import Cardano.Ledger.Conway.Core
+import Cardano.Ledger.Conway.Genesis (ConwayGenesis (..))
+import Cardano.Ledger.Conway.Governance (VotingProcedures (..))
+import Cardano.Ledger.Conway.Rules (ConwayDELEG, ConwayDelegPredFailure (..), ConwayLEDGER)
+import Cardano.Ledger.Conway.Scripts (ConwayPlutusPurpose (..))
+import Cardano.Ledger.Conway.TxBody (TxBody (..))
+import Cardano.Ledger.Conway.TxCert
+import Cardano.Ledger.Mary.Value (MaryValue (..))
+import Cardano.Ledger.Plutus.Data (
+  Datum (..),
+  dataToBinaryData,
+ )
+import Cardano.Ledger.Plutus.Language (Language (..))
+import Cardano.Ledger.Shelley.API (
+  ApplyTxError (..),
+  RewardAccount (..),
+  TxId (..),
+ )
+import Cardano.Ledger.TxIn (mkTxInPartial)
+import Control.State.Transition.Extended (Embed (..))
+import qualified Data.Map.Strict as Map
+import qualified Data.OSet.Strict as OSet
+import qualified Data.Sequence.Strict as StrictSeq
+import qualified Data.Set as Set
+import Test.Cardano.Ledger.Alonzo.Arbitrary (alwaysSucceeds)
+import Test.Cardano.Ledger.Alonzo.Examples (
+  exampleDatum,
+  exampleTx,
+  mkLedgerExamples,
+ )
+import Test.Cardano.Ledger.Babbage.Examples (exampleBabbageNewEpochState, exampleCollateralOutput)
+import Test.Cardano.Ledger.Conway.Era ()
+import Test.Cardano.Ledger.Conway.Genesis (expectedConwayGenesis)
+import Test.Cardano.Ledger.Core.KeyPair (mkAddr)
+import Test.Cardano.Ledger.Core.Utils (mkDummySafeHash)
+import Test.Cardano.Ledger.Mary.Examples (exampleMultiAssetValue)
+import Test.Cardano.Ledger.Shelley.Examples (
+  LedgerExamples (..),
+  examplePayKey,
+  examplePoolParams,
+  exampleStakeKey,
+  keyToCredential,
+  mkKeyHash,
+ )
+
+ledgerExamples :: LedgerExamples ConwayEra
+ledgerExamples =
+  mkLedgerExamples
+    ( ApplyTxError $
+        pure $
+          wrapFailed @(ConwayDELEG ConwayEra) @(ConwayLEDGER ConwayEra) $
+            DelegateeStakePoolNotRegisteredDELEG @ConwayEra (mkKeyHash 1)
+    )
+    exampleBabbageNewEpochState
+    exampleTxConway
+    exampleConwayGenesis
+
+exampleTxConway :: Tx ConwayEra
+exampleTxConway = exampleTx exampleTxBodyConway (ConwaySpending $ AsIx 0)
+
+exampleTxBodyConway :: TxBody ConwayEra
+exampleTxBodyConway =
+  ConwayTxBody
+    (Set.fromList [mkTxInPartial (TxId (mkDummySafeHash 1)) 0]) -- spending inputs
+    (Set.fromList [mkTxInPartial (TxId (mkDummySafeHash 2)) 1]) -- collateral inputs
+    (Set.fromList [mkTxInPartial (TxId (mkDummySafeHash 1)) 3]) -- reference inputs
+    ( StrictSeq.fromList
+        [ mkSized (eraProtVerHigh @ConwayEra) $
+            BabbageTxOut
+              (mkAddr examplePayKey exampleStakeKey)
+              (exampleMultiAssetValue 2)
+              (Datum $ dataToBinaryData exampleDatum) -- inline datum
+              (SJust $ alwaysSucceeds @'PlutusV2 3) -- reference script
+        ]
+    )
+    (SJust $ mkSized (eraProtVerHigh @ConwayEra) exampleCollateralOutput) -- collateral return
+    (SJust $ Coin 8675309) -- collateral tot
+    exampleConwayCerts
+    ( Withdrawals $
+        Map.singleton
+          (RewardAccount Testnet (keyToCredential exampleStakeKey))
+          (Coin 100)
+    )
+    (Coin 999) -- txfee
+    (ValidityInterval (SJust (SlotNo 2)) (SJust (SlotNo 4))) -- txvldt
+    (Set.singleton $ mkKeyHash 212) -- reqSignerHashes
+    exampleMultiAsset -- mint
+    (SJust $ mkDummySafeHash 42) -- scriptIntegrityHash
+    (SJust . TxAuxDataHash $ mkDummySafeHash 42) -- adHash
+    (SJust Mainnet) -- txnetworkid
+    (VotingProcedures mempty)
+    mempty
+    (SJust $ Coin 867530900000) -- current treasury value
+    mempty
+  where
+    MaryValue _ exampleMultiAsset = exampleMultiAssetValue 3
+
+exampleConwayCerts :: OSet.OSet (ConwayTxCert era)
+exampleConwayCerts =
+  OSet.fromList -- TODO add all possible certificates
+    [ConwayTxCertPool (RegPool examplePoolParams)]
+
+exampleConwayGenesis :: ConwayGenesis
+exampleConwayGenesis = expectedConwayGenesis
