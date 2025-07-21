f7ac00b2404379a79634ab98dd641b80c4a481f9
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-07-21 19:59:48 +0200

    Adjust Conway `UtxosSpec` for Dijkstra era
    * Generalise compatible tests by using certificates with deposits
    * Move tests that are reliant on certs w/out deposits and only run them pre-Dijkstra

--- eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp.hs
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp.hs
@@ -56,7 +56,7 @@ import qualified Test.Cardano.Ledger.Conway.Imp.GovSpec as Gov
 import qualified Test.Cardano.Ledger.Conway.Imp.LedgerSpec as Ledger
 import qualified Test.Cardano.Ledger.Conway.Imp.RatifySpec as Ratify
 import qualified Test.Cardano.Ledger.Conway.Imp.UtxoSpec as Utxo
--- import qualified Test.Cardano.Ledger.Conway.Imp.UtxosSpec as Utxos
+import qualified Test.Cardano.Ledger.Conway.Imp.UtxosSpec as Utxos
 -- import qualified Test.Cardano.Ledger.Conway.Imp.UtxowSpec as Utxow
 import Test.Cardano.Ledger.Conway.ImpTest (
   ConwayEraImp,
@@ -149,6 +149,6 @@ conwaySpec = do
   describe "LEDGER" Ledger.spec
   describe "RATIFY" Ratify.spec
   describe "UTXO" Utxo.spec
+  describe "UTXOS" Utxos.spec
 
--- describe "UTXOS" Utxos.spec
 -- describe "UTXOW" Utxow.spec
