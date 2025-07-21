ec77789ac57c6f4d331335fa4201b4b4d83114af
teodanciu <teodora.danciu@tweag.io>
2025-07-21 21:34:41 +0100

    Move some tests from `cardano-ledger-alonzo-test` to alonzo testlib

--- eras/alonzo/test-suite/test/Tests.hs
+++ eras/alonzo/test-suite/test/Tests.hs
@@ -13,9 +13,6 @@ import System.Environment (lookupEnv)
 import qualified Test.Cardano.Ledger.Alonzo.ChainTrace as ChainTrace
 import qualified Test.Cardano.Ledger.Alonzo.Golden as Golden
 import Test.Cardano.Ledger.Alonzo.ImpTest ()
-import qualified Test.Cardano.Ledger.Alonzo.Serialisation.Canonical as Canonical
-import qualified Test.Cardano.Ledger.Alonzo.Translation as Translation
-import qualified Test.Cardano.Ledger.Alonzo.TxInfo as TxInfo
 import qualified Test.Cardano.Ledger.Shelley.PropertyTests as Shelley
 import qualified Test.Cardano.Ledger.Shelley.Rules.AdaPreservation as AdaPreservation
 import qualified Test.Cardano.Ledger.Shelley.Rules.IncrementalStake as IncrementalStake
@@ -33,10 +30,7 @@ defaultTests =
   testGroup
     "Alonzo tests"
     [ AdaPreservation.tests @AlonzoEra 50
-    , Translation.tests
-    , Canonical.tests
     , Golden.tests
-    , TxInfo.tests
     ]
 
 nightlyTests :: TestTree
