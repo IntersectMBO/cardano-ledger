99f582aca0934b93bb4d13c1ec446d00c987f476
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2025-08-01 13:09:36 -0600

    Update ghc to 9.6.7

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
         inherit (nixpkgs) lib;
 
         # see flake `variants` below for alternative compilers
-        defaultCompiler = "ghc966";
+        defaultCompiler = "ghc967";
         fourmoluVersion = "0.16.2.0";
         # We use cabalProject' to ensure we don't build the plan for
         # all systems.
