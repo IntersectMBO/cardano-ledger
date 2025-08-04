24335f7f0d316b1e2b98c02d73574473a28a6f29
Lucsanszky <daniel.lucsanszky@iohk.io>
2025-08-04 19:00:01 +0200

    Adjust Babbage `UtxowSpec` for Dijkstra era
    * Move tests that are reliant on certs w/out deposits and only run them pre-Dijkstra

--- eras/babbage/impl/testlib/Test/Cardano/Ledger/Babbage/Imp.hs
+++ eras/babbage/impl/testlib/Test/Cardano/Ledger/Babbage/Imp.hs
@@ -54,4 +54,6 @@ spec = do
       Utxos.spec @era
 
 instance EraSpecificSpec BabbageEra where
-  eraSpecific = AlonzoImp.alonzoEraSpecificSpec
+  eraSpecific =
+    AlonzoImp.alonzoEraSpecificSpec
+      >> Utxow.babbageEraSpecificSpec
