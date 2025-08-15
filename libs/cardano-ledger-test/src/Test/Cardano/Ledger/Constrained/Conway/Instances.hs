b58103822626d0b63e435359cbfa12583faebaa6
lehins <lehins@users.noreply.github.com>
2025-08-15 00:04:06 +0000

    Remove all existing files

--- libs/cardano-ledger-test/src/Test/Cardano/Ledger/Constrained/Conway/Instances.hs
+++
@@ -1,9 +0,0 @@
-module Test.Cardano.Ledger.Constrained.Conway.Instances (
-  module X,
-) where
-
-import Test.Cardano.Ledger.Constrained.Conway.Instances.Basic as X
-import Test.Cardano.Ledger.Constrained.Conway.Instances.Ledger as X
-import Test.Cardano.Ledger.Constrained.Conway.Instances.Ledgers as X ()
-import Test.Cardano.Ledger.Constrained.Conway.Instances.PParams as X
-import Test.Cardano.Ledger.Constrained.Conway.Instances.TxBody as X
