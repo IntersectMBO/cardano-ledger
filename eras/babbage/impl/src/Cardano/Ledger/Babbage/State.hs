2e568d552956f53cb0fd675d4ae9cc5a901ca95e
lehins <lehins@users.noreply.github.com>
2025-08-22 19:55:22 +0000

    Remove all existing files

--- eras/babbage/impl/src/Cardano/Ledger/Babbage/State.hs
+++
@@ -1,8 +0,0 @@
-module Cardano.Ledger.Babbage.State (
-  module Cardano.Ledger.Alonzo.State,
-) where
-
-import Cardano.Ledger.Alonzo.State
-import Cardano.Ledger.Babbage.State.Account ()
-import Cardano.Ledger.Babbage.State.CertState ()
-import Cardano.Ledger.Babbage.State.Stake ()
