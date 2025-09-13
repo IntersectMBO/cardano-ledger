58658de94819c8810f9f8608e41b8929387eee71
lehins <lehins@users.noreply.github.com>
2025-09-13 02:43:50 +0000

    Remove all existing files

--- eras/babbage/impl/huddle-cddl/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Paths_cardano_ledger_babbage
-import Test.Cardano.Ledger.Babbage.CDDL (babbageCDDL)
-import Test.Cardano.Ledger.Binary.Cuddle (writeSpec)
-
-main :: IO ()
-main = writeSpec babbageCDDL =<< getDataFileName "cddl-files/babbage.cddl"
--- eras/shelley/test-suite/src/Test/Cardano/Ledger/Shelley/Generator/Delegation.hs
+++
@@ -1,6 +0,0 @@
-module Test.Cardano.Ledger.Shelley.Generator.Delegation
-  {-# DEPRECATED "Use `Test.Cardano.Ledger.Shelley.Generator.TxCert` instead" #-} (
-  module Test.Cardano.Ledger.Shelley.Generator.TxCert,
-) where
-
-import Test.Cardano.Ledger.Shelley.Generator.TxCert
