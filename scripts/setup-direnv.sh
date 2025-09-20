39bbffd22679c565ed31d06e3a2c733406d8e89f
lehins <lehins@users.noreply.github.com>
2025-09-20 04:32:48 +0000

    Remove all existing files

--- scripts/setup-direnv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-######
-# Utility script for setting up `direnv`.
-# The script is extremely minimal and assumes you have `direnv` installed.
-
-echo "use flake .#pre-commit" > .envrc
-direnv allow
