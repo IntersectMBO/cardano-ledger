3062439935547392c9e7cb65e46bef9eb05f72de
lehins <lehins@users.noreply.github.com>
2025-09-26 23:00:33 +0000

    Remove all existing files

--- nix/pkgs.nix
+++
@@ -1,7 +0,0 @@
-# our packages overlay
-final: prev:
-import ./latex.nix {inherit (final) stdenv lib texlive;}
-// {
-  cddl = final.callPackage ./pkgs/cddl {};
-  cardano-ledger-release-tool = final.callPackage ./pkgs/cardano-ledger-release-tool.nix {};
-}
