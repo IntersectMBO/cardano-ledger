8f0d068070c6d40c48bb3891253a61613a59bf89
Neil Mayhew <neil.mayhew@iohk.io>
2025-09-30 15:14:29 -0600

    Obtain cardano-ledger-release-tool from flake instead of static binary
    This enables use on non-Linux platforms (eg Mac)

--- nix/pkgs/cardano-ledger-release-tool.nix
+++ nix/pkgs/cardano-ledger-release-tool.nix
@@ -1,24 +1,8 @@
-{
-  stdenv,
-  lib,
-  fetchzip,
-}:
-# Although `cardano-ledger-release-tool` is a flake, we choose to download
-# the released binary artifact for compatibility with the non-nix CI
+{ system }:
 let
-  pname = "cardano-ledger-release-tool";
-  version = "0.1.0.0";
-  hash = "sha256-Xge1/T5mtsOF5XiIDPoWU2f2HMJ0pCEbS+brfQyCZpw=";
-
-  repo = "input-output-hk/${pname}";
-  release = "${pname}-${version}";
-  url = "https://github.com/${repo}/releases/download/${release}/${pname}-Linux-x86_64.zip";
+  type = "github";
+  owner = "input-output-hk";
+  repo = "cardano-ledger-release-tool";
+  rev = "f52262644af02e6eae54cf5d6021ecebf7f5c1d1";
 in
-  stdenv.mkDerivation {
-    inherit pname version;
-    src = fetchzip {inherit url hash;};
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -a ${pname} $out/bin/
-    '';
-  }
+(builtins.getFlake "${type}:${owner}/${repo}/${rev}").packages.${system}.default
