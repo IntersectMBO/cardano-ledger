4733357729675fefa82bd558b04547a67f21eda1
Neil Mayhew <neil.mayhew@iohk.io>
2025-10-22 14:20:46 -0600

    Remove obsolete BabbageFeatures test module from cardano-ledger-test

--- libs/cardano-ledger-test/test/Tests.hs
+++ libs/cardano-ledger-test/test/Tests.hs
@@ -14,7 +14,6 @@ import qualified Test.Cardano.Ledger.Constrained.Conway.MiniTrace as MiniTrace
 import qualified Test.Cardano.Ledger.Examples.AlonzoAPI as AlonzoAPI (tests)
 import qualified Test.Cardano.Ledger.Examples.AlonzoBBODY as AlonzoBBODY (tests)
 import qualified Test.Cardano.Ledger.Examples.AlonzoCollectInputs as AlonzoCollectInputs (tests)
-import Test.Cardano.Ledger.Examples.BabbageFeatures (babbageFeatures)
 import Test.Cardano.Ledger.Generic.AggPropTests (aggTests, depositTests)
 import Test.Cardano.Ledger.Generic.GenState (defaultGenSize)
 import Test.Cardano.Ledger.Generic.Properties (genericProperties)
@@ -40,7 +39,6 @@ defaultTests = do
   depositTests
   calcPoolDistOldEqualsNew
   describe "STS Tests" $ do
-    babbageFeatures
     AlonzoBBODY.tests
     AlonzoAPI.tests
     AlonzoCollectInputs.tests
