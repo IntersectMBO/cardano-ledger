0f2f3be7aa642cf0033a7944b4777aace3350a8e
Neil Mayhew <neil.mayhew@iohk.io>
2025-10-23 15:19:13 -0600

    Remove tasty from vector-map

--- libs/vector-map/test/Test/Common.hs
+++ libs/vector-map/test/Test/Common.hs
@@ -9,21 +9,23 @@ module Test.Common (
 ) where
 
 import Control.Applicative
+import Data.Foldable (traverse_)
 import Data.Proxy as X
+import Test.Hspec as X
+import Test.Hspec.QuickCheck as X
+import Test.QuickCheck as X
 import Test.QuickCheck.Classes.Base as X
 import Test.QuickCheck.Property (mapTotalResult, maybeNumTests)
-import Test.Tasty as X
-import Test.Tasty.QuickCheck as X
 
 withMaxTimesSuccess :: Testable prop => Int -> prop -> Property
 withMaxTimesSuccess !n =
   mapTotalResult $ \res -> res {maybeNumTests = (n *) <$> (maybeNumTests res <|> Just 100)}
 
-testPropertyN :: Testable prop => Int -> TestName -> prop -> TestTree
-testPropertyN n name = testProperty name . withMaxTimesSuccess n
+testPropertyN :: Testable prop => Int -> String -> prop -> Spec
+testPropertyN n name = prop name . withMaxTimesSuccess n
 
-testLawsGroup :: TestName -> [Laws] -> TestTree
-testLawsGroup name = testGroup name . fmap testLaws
+testLawsGroup :: String -> [Laws] -> Spec
+testLawsGroup name = describe name . traverse_ testLaws
   where
     testLaws Laws {..} =
-      testGroup lawsTypeclass $ fmap (uncurry testProperty) lawsProperties
+      describe lawsTypeclass $ traverse_ (uncurry prop) lawsProperties
