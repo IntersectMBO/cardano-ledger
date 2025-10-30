130378c6f22c7db664704c42797ba82a0c4bb6c6
Joosep Jääger <joosep.jaager@iohk.io>
2025-10-30 18:59:08 +0200

    Switched to Enc

--- eras/dijkstra/impl/test/Test/Cardano/Ledger/Dijkstra/GoldenSpec.hs
+++ eras/dijkstra/impl/test/Test/Cardano/Ledger/Dijkstra/GoldenSpec.hs
@@ -5,22 +5,11 @@
 
 module Test.Cardano.Ledger.Dijkstra.GoldenSpec (spec) where
 
-import Cardano.Ledger.Binary (DeserialiseFailure (..))
-import Cardano.Ledger.Binary.Decoding (DecoderError (..), decodeFull')
 import Cardano.Ledger.Dijkstra (DijkstraEra)
-import Cardano.Ledger.Dijkstra.Core (EraTx (..), eraProtVerLow)
-import qualified Data.ByteString as BS
 import Paths_cardano_ledger_dijkstra (getDataFileName)
 import Test.Cardano.Ledger.Common
 import Test.Cardano.Ledger.Core.JSON (goldenJsonPParamsSpec, goldenJsonPParamsUpdateSpec)
 import Test.Cardano.Ledger.Dijkstra.Era ()
-
-expectDecoderFailure :: FilePath -> DecoderError -> SpecWith ()
-expectDecoderFailure path err =
-  it path $ do
-    bytes <- BS.readFile path
-    decodeFull' @(Tx DijkstraEra) (eraProtVerLow @DijkstraEra) bytes
-      `shouldBe` Left err
 
 spec :: Spec
 spec =
