439322d9be4b094a2ee16f9955f6f975807693e2
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-11-11 12:57:10 +0100

    Update fls; enable test

--- eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/RatifySpec.hs
+++ eras/conway/impl/testlib/Test/Cardano/Ledger/Conway/Imp/RatifySpec.hs
@@ -198,9 +198,7 @@ spoAndCCVotingSpec = do
         else do
           getLastEnactedParameterChange `shouldReturn` SNothing
           newRefScriptBaseFee `shouldBe` initialRefScriptBaseFee
-    -- https://github.com/IntersectMBO/cardano-ledger/issues/5170
-    -- TODO: Re-enable after issue is resolved, by removing this override
-    disableInConformanceIt "Constitution cannot be changed if active committee size is below min size"
+    it "Constitution cannot be changed if active committee size is below min size"
       . whenPostBootstrap
       $ do
         modifyPParams $ \pp ->
