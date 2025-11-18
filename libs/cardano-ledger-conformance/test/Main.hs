9d9b98775f09a6f3f449a7a55045f005535ebb58
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-11-18 14:41:58 +0100

    Cleanup

--- libs/cardano-ledger-conformance/test/Main.hs
+++ libs/cardano-ledger-conformance/test/Main.hs
@@ -1,9 +1,7 @@
-{-# LANGUAGE DataKinds #-}
-
 module Main (main) where
 
-import Test.Cardano.Ledger.Common
 import qualified Test.Cardano.Ledger.Conformance.ConformanceSpec as ConformanceSpec
+import Test.Cardano.Ledger.Common (describe , ledgerTestMain)
 import qualified Test.Cardano.Ledger.Conformance.Spec.Conway as SpecConway
 import qualified Test.Cardano.Ledger.Conformance.Imp.Conway as ImpConway
 
