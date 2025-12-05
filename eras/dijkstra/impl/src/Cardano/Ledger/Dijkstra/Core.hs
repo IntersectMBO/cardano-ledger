8d441221596f1a58c5410b93de0eddfa9b87351d
Agustin Mista <agustin.mista@moduscreate.com>
2025-12-05 09:57:09 +0100

    Wire up new Dijkstra block body
    This commit replaces the existing Dijkstra block body pointing to an
    Alonzo block body with its new implementation defined in
    Cardano.Ledger.Dijkstra.BlockBody.Internal.

--- eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Core.hs
+++ eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Core.hs
@@ -1,5 +1,7 @@
 module Cardano.Ledger.Dijkstra.Core (
+  DijkstraBlockBody (..),
   module Cardano.Ledger.Conway.Core,
 ) where
 
 import Cardano.Ledger.Conway.Core
+import Cardano.Ledger.Dijkstra.BlockBody (DijkstraBlockBody (..))
