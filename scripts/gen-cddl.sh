badd547b1a76a77479bd9ef75af6f86220fe001f
Aniket Deshpande <git@aniketd.mailer.me>
2025-12-09 00:48:53 +0530

    CDDL: Replace huddle-cddl with generate-cddl

--- scripts/gen-cddl.sh
+++ scripts/gen-cddl.sh
@@ -7,7 +7,7 @@ eras=("shelley" "allegra" "mary" "alonzo" "babbage" "conway" "dijkstra")
 for era in "${eras[@]}"; do
 
   echo "Generating cddl for $era..."
-  cabal run "cardano-ledger-$era:exe:huddle-cddl"
+  cabal run "cardano-ledger-$era:exe:generate-cddl"
   echo "Regenerated ${era}.cddl"
 
 done
