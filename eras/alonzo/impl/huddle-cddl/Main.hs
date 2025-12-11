0969c24e2222cbbdddec52ded2f87fde93157e8b
Aniket Deshpande <git@aniketd.mailer.me>
2025-12-11 17:49:03 +0530

    CDDL: Remove old CDDL modules from testlibs

--- eras/alonzo/impl/huddle-cddl/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Paths_cardano_ledger_alonzo
-import Test.Cardano.Ledger.Alonzo.CDDL (alonzoCDDL)
-import Test.Cardano.Ledger.Binary.Cuddle (writeSpec)
-
-main :: IO ()
-main = writeSpec alonzoCDDL =<< getDataFileName "cddl/data/alonzo.cddl"
--- eras/dijkstra/impl/huddle-cddl/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Paths_cardano_ledger_dijkstra
-import Test.Cardano.Ledger.Binary.Cuddle (writeSpec)
-import Test.Cardano.Ledger.Dijkstra.CDDL (dijkstraCDDL)
-
-main :: IO ()
-main = writeSpec dijkstraCDDL =<< getDataFileName "cddl/data/dijkstra.cddl"
