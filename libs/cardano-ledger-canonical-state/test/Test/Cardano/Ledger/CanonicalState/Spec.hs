014738729c3a07b852d80283c102896aa880965f
Alexander Vershilov <alexander.vershilov@tweag.io>
2026-02-03 14:56:29 +0100

    Introduce utxo/v0 namespace for canonical ledger state (#5548)
    * Introduce utxo/v0 namespace for canonical ledger state
    
    In this commit following changes were introduced:
    
    - new utxo/v0 namespace for namespaces that reuses on-wire
    types as much as possible.
    - in order to use these type new OnChain wrapper was introduced
      this type allows keep value of the type and binary representation
    
    For forward compatibility all era specific types are included in
    the internal library of the cardano-ledger-canonical-state package.
    
    * Apply suggestions from code review
    
    Co-authored-by: Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
    
    * Make code compile and simplify the structure.
    
    As it was possible to abstract code and make it depend only on era
    it was possible to remove era specific code. After this change
    'cardano-ledger-canonical-state:conway' may only keep the instaces
    for the era spefic types.
    
    * Apply suggestions from code review
    
    Co-authored-by: Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
    
    * Address comments and further simplify code
    
    * Remove redundant module
    
    * Format cabal file
    
    ---------
    
    Co-authored-by: Alexey Kuleshevich <alexey.kuleshevich@iohk.io>

--- libs/cardano-ledger-canonical-state/test/Test/Cardano/Ledger/CanonicalState/Spec.hs
+++ libs/cardano-ledger-canonical-state/test/Test/Cardano/Ledger/CanonicalState/Spec.hs
@@ -7,7 +7,10 @@
 
 module Test.Cardano.Ledger.CanonicalState.Spec (spec) where
 
+import Cardano.Ledger.CanonicalState.Conway ()
 import qualified Cardano.Ledger.CanonicalState.Namespace.Blocks.V0 as Blocks.V0
+import qualified Cardano.Ledger.CanonicalState.Namespace.UTxO.V0 as UTxO.V0
+import Cardano.Ledger.Conway (ConwayEra)
 import Cardano.SCLS.CBOR.Canonical.Encoder (ToCanonicalCBOR (..))
 import Cardano.SCLS.Testlib
 import Data.Typeable
@@ -21,8 +24,12 @@ spec = do
     describe "blocks/v0" $ do
       isCanonical @"blocks/v0" @Blocks.V0.BlockOut
       validateType @"blocks/v0" @Blocks.V0.BlockOut "record_entry"
+    describe "utxo/v0" $ do
+      isCanonical @"utxo/v0" @(UTxO.V0.UtxoOut ConwayEra)
+      validateType @"utxo/v0" @(UTxO.V0.UtxoOut ConwayEra) "record_entry"
   describe "namespaces" $ do
     testNS @"blocks/v0"
+    testNS @"utxo/v0"
 
 isCanonical ::
   forall ns a. (KnownSymbol ns, ToCanonicalCBOR ns a, Typeable a, Arbitrary a, Show a) => Spec
--- libs/cardano-ledger-canonical-state/testlib/Test/Cardano/Ledger/CanonicalState/Arbitrary.hs
+++ libs/cardano-ledger-canonical-state/testlib/Test/Cardano/Ledger/CanonicalState/Arbitrary.hs
@@ -1,13 +1,23 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Test.Cardano.Ledger.CanonicalState.Arbitrary () where
 
+import Cardano.Ledger.CanonicalState.Conway ()
 import qualified Cardano.Ledger.CanonicalState.Namespace.Blocks.V0 as Blocks.V0
+import qualified Cardano.Ledger.CanonicalState.Namespace.UTxO.V0 as UtxoOut.V0
+import Cardano.Ledger.Core (Era, EraTxOut, TxOut)
+import Test.Cardano.Ledger.Conway.Arbitrary ()
 import Test.QuickCheck (Arbitrary (..), Positive (..))
 
 instance Arbitrary Blocks.V0.BlockOut where
   -- starting form the QuickCheck-2.17 there is an arbirary instance for
   -- Natural, so it's possible to use genericArbitraryU directly.
   arbitrary = Blocks.V0.BlockOut . fromIntegral . getPositive @Integer <$> arbitrary
+
+instance (EraTxOut era, Arbitrary (TxOut era), Era era) => Arbitrary (UtxoOut.V0.UtxoOut era) where
+  arbitrary = UtxoOut.V0.mkUtxo <$> arbitrary
