2d8821dfb0a20930c4dcdcadecb30b9050d996ca
Alexander Vershilov <alexander.vershilov@gmail.com>
2026-02-16 17:28:02 +0100

    Support gov/pparams/v0 namespace

--- libs/cardano-ledger-canonical-state/testlib/Test/Cardano/Ledger/Conway/CanonicalState/Arbitrary.hs
+++ libs/cardano-ledger-canonical-state/testlib/Test/Cardano/Ledger/Conway/CanonicalState/Arbitrary.hs
@@ -9,6 +9,7 @@ module Test.Cardano.Ledger.Conway.CanonicalState.Arbitrary () where
 
 import Cardano.Ledger.CanonicalState.Conway (mkCanonicalConstitution)
 import qualified Cardano.Ledger.CanonicalState.Namespace.GovConstitution.V0 as GovConstitution.V0
+import qualified Cardano.Ledger.CanonicalState.Namespace.GovPParams.V0 as GovPParams.V0
 import Cardano.Ledger.Conway (ConwayEra)
 import Cardano.Ledger.Conway.Governance (Constitution)
 import Generic.Random (genericArbitraryU)
@@ -19,4 +20,7 @@ instance Arbitrary GovConstitution.V0.CanonicalConstitution where
   arbitrary = mkCanonicalConstitution <$> arbitrary @(Constitution ConwayEra)
 
 instance Arbitrary GovConstitution.V0.GovConstitutionOut where
+  arbitrary = genericArbitraryU
+
+instance Arbitrary (GovPParams.V0.GovPParamsOut ConwayEra) where
   arbitrary = genericArbitraryU
