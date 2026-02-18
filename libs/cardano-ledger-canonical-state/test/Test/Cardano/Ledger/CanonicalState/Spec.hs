2599212f25a19000b09f75667fa3e1990b78d792
Alexander Vershilov <alexander.vershilov@gmail.com>
2026-02-18 14:32:04 +0100

    Introduce new canonical namespace for the pool_stake

--- libs/cardano-ledger-canonical-state/test/Test/Cardano/Ledger/CanonicalState/Spec.hs
+++ libs/cardano-ledger-canonical-state/test/Test/Cardano/Ledger/CanonicalState/Spec.hs
@@ -20,6 +20,7 @@ import qualified Cardano.Ledger.CanonicalState.Namespace.Blocks.V0 as Blocks.V0
 import qualified Cardano.Ledger.CanonicalState.Namespace.GovCommittee.V0 as Committee.V0
 import qualified Cardano.Ledger.CanonicalState.Namespace.GovConstitution.V0 as GovConstitution.V0
 import qualified Cardano.Ledger.CanonicalState.Namespace.GovPParams.V0 as GovPParams.V0
+import qualified Cardano.Ledger.CanonicalState.Namespace.PoolStake.V0 as PoolStake.V0 ()
 import qualified Cardano.Ledger.CanonicalState.Namespace.UTxO.V0 as UTxO.V0
 import Cardano.Ledger.Conway (ConwayEra)
 import Cardano.Ledger.Core (PParams)
@@ -67,9 +68,10 @@ spec = do
     testNS @"gov/committee/v0"
     testNS @"gov/pparams/v0"
 
--- Is checked by the: isCanonical @"gov/pparams/v0" @(PParamsWithProtVer ConwayEra)
--- prop "canonical with regards to it's definition" $
---  propNamespaceEntryRoundTrip @"gov/pparams/v0"
+    -- Is checked by the: isCanonical @"gov/pparams/v0" @(PParamsWithProtVer ConwayEra)
+    -- prop "canonical with regards to it's definition" $
+    --  propNamespaceEntryRoundTrip @"gov/pparams/v0"
+    testNS @"pool_stake/v0"
 
 isCanonical ::
   forall ns a. (KnownSymbol ns, ToCanonicalCBOR ns a, Typeable a, Arbitrary a, Show a) => Spec
