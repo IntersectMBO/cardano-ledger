9b2e2cbb261e3270dbff7e70ec32eeaac879c187
teodanciu <teodora.danciu@tweag.io>
2026-03-12 13:32:15 +0000

    Run `validateWrongNetwork` checks in SUBUTXO

--- eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Rules/SubUtxo.hs
+++ eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Rules/SubUtxo.hs
@@ -33,6 +33,7 @@ import Cardano.Ledger.Alonzo.Rules (AlonzoUtxoPredFailure)
 import qualified Cardano.Ledger.Alonzo.Rules as Alonzo (
   validateOutputTooBigUTxO,
   validateOutsideForecast,
+  validateWrongNetworkInTxBody,
  )
 import Cardano.Ledger.Babbage.Rules (BabbageUtxoPredFailure)
 import qualified Cardano.Ledger.Babbage.Rules as Babbage (
@@ -70,6 +71,8 @@ import qualified Cardano.Ledger.Shelley.Rules as Shelley (
   validateInputSetEmptyUTxO,
   validateMaxTxSizeUTxO,
   validateOutputBootAddrAttrsTooBig,
+  validateWrongNetwork,
+  validateWrongNetworkWithdrawal,
  )
 import Cardano.Ledger.State (CertState, ScriptsProvided (..))
 import Cardano.Ledger.TxIn (TxIn)
@@ -273,6 +276,11 @@ dijkstraSubUtxoTransition = do
   runTestOnSignal $ Shelley.validateOutputBootAddrAttrsTooBig allOutputs
 
   runTestOnSignal $ Babbage.validateOutputTooSmallUTxO pp allSizedOutputs
+
+  netId <- liftSTS $ asks networkId
+  runTestOnSignal $ Shelley.validateWrongNetwork netId allOutputs
+  runTestOnSignal $ Shelley.validateWrongNetworkWithdrawal netId txBody
+  runTestOnSignal $ Alonzo.validateWrongNetworkInTxBody netId txBody
 
   pure utxoState
 
