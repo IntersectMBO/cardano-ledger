c8f4d4f081d549f6c6faeb38ff72baa8900a4af1
Alexander Vershilov <alexander.vershilov@gmail.com>
2026-03-18 17:32:34 +0100

    Introduce namespace accounts/v0

--- libs/cardano-ledger-canonical-state/conway/Cardano/Ledger/CanonicalState/Conway.hs
+++ libs/cardano-ledger-canonical-state/conway/Cardano/Ledger/CanonicalState/Conway.hs
@@ -61,6 +61,8 @@ import Data.Text (Text)
 import Data.Word (Word8)
 import Lens.Micro
 
+type instance NamespaceEra "accounts/v0" = ConwayEra
+
 type instance NamespaceEra "blocks/v0" = ConwayEra
 
 type instance NamespaceEra "gov/committee/v0" = ConwayEra
