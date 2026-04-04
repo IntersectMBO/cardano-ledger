fa447bd1b8817fc2663d1ebd78757aa1ccaa3442
Onyeka Obi <Onyeka.Obi@gmail.com>
2026-04-03 20:53:41 -0700

    Remove unused Timelock import in AlonzoEraGen
      The Timelock(..) import became redundant after removing the
      NativeScript era ~ Timelock era constraint from someLeaf.
      With -Werror, this caused the CI build to fail.

--- eras/alonzo/test-suite/src/Test/Cardano/Ledger/Alonzo/AlonzoEraGen.hs
+++ eras/alonzo/test-suite/src/Test/Cardano/Ledger/Alonzo/AlonzoEraGen.hs
@@ -15,7 +15,6 @@ module Test.Cardano.Ledger.Alonzo.AlonzoEraGen where
 
 import Cardano.Ledger.Allegra.Scripts (
   AllegraEraScript,
-  Timelock (..),
   translateTimelock,
   pattern RequireTimeExpire,
   pattern RequireTimeStart,
