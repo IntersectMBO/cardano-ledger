e766811fee1b29ea5b3855fa5200c7b0fa804268
Joosep Jääger <joosep.jaager@iohk.io>
2026-04-23 18:56:58 +0300

    Fixed the metadatum decoding test

--- eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Binary/RoundTrip.hs
+++ eras/shelley/impl/testlib/Test/Cardano/Ledger/Shelley/Binary/RoundTrip.hs
@@ -13,15 +13,16 @@ module Test.Cardano.Ledger.Shelley.Binary.RoundTrip (
 
 import Cardano.Ledger.Binary
 import Cardano.Ledger.Core
-import Cardano.Ledger.Metadata (Metadatum)
+import Cardano.Ledger.Metadata (Metadatum (I))
 import Cardano.Ledger.Shelley (ShelleyEra)
 import Cardano.Ledger.Shelley.LedgerState
 import Cardano.Ledger.Shelley.State
 import qualified Data.Text as T
 import Test.Cardano.Base.Bytes (genByteString)
 import Test.Cardano.Ledger.Binary.RoundTrip (
-  roundTripCborRangeExpectation,
-  roundTripCborRangeFailureExpectation,
+  embedTrip,
+  embedTripRangeFailureExpectation,
+  mkTrip,
  )
 import Test.Cardano.Ledger.Common
 import Test.Cardano.Ledger.Core.Binary.RoundTrip
@@ -107,17 +108,48 @@ metadatumIntRangeSpec fromVersion toVersion = do
     maxInt = 2 ^ (64 :: Int) - 1
     minInt :: Integer
     minInt = -(2 ^ (64 :: Int))
+    intToMetadatum = mkTrip (encCBOR @Integer) (decCBOR @Metadatum)
+    expectAccepted n =
+      forM_ [fromVersion .. toVersion] $ \v ->
+        case embedTrip v v intToMetadatum n of
+          Left err -> expectationFailure $ "Failed to deserialize: " ++ show err
+          Right m -> m `shouldBe` I n
+    expectRejected = embedTripRangeFailureExpectation intToMetadatum fromVersion toVersion
+    -- Test the binary decoder directly, bypassing FlatTerm.
+    -- This is needed because cborg's FlatTerm has an off-by-one bug in
+    -- tokenTypeOf that misclassifies -(2^64) as TypeInteger instead of
+    -- TypeNInt64 (https://github.com/well-typed/cborg/issues/377)
+    --
+    -- This bug only affect the tests, not the actual decoder, because the
+    -- actual decoder does not go through FlatTerm
+    expectBinaryAccepted n =
+      forM_ [fromVersion .. toVersion] $ \v ->
+        decodeFull @Metadatum v (serialize v n) `shouldBe` Right (I n)
+    expectBinaryRejected n =
+      forM_ [fromVersion .. toVersion] $ \v ->
+        case decodeFull @Metadatum v (serialize v n) of
+          Left _ -> pure ()
+          Right _ -> expectationFailure $ "Should not have deserialized: " ++ show n
   it "Accepts max int (2^64-1)" $
-    roundTripCborRangeExpectation fromVersion toVersion maxInt
-  it "Accepts min int (-(2^64))" $
-    roundTripCborRangeExpectation fromVersion toVersion minInt
+    expectAccepted maxInt
+  it "Accepts max int (2^64-1) (binary)" $
+    expectBinaryAccepted maxInt
+  -- TODO: enable this once the cborg FlatTerm bug is fixed
+  xit "Accepts min int (-(2^64))" $
+    expectAccepted minInt
+  it "Accepts min int (-(2^64)) (binary)" $
+    expectBinaryAccepted minInt
   it "Rejects positive big integer (2^64)" $
-    roundTripCborRangeFailureExpectation fromVersion toVersion (maxInt + 1)
+    expectRejected (maxInt + 1)
+  it "Rejects positive big integer (2^64) (binary)" $
+    expectBinaryRejected (maxInt + 1)
   it "Rejects negative big integer (-(2^64+1))" $
-    roundTripCborRangeFailureExpectation fromVersion toVersion (minInt - 1)
+    expectRejected (minInt - 1)
+  it "Rejects negative big integer (-(2^64+1)) (binary)" $
+    expectBinaryRejected (minInt - 1)
   prop "Accepts any int in CBOR range" $
     forAll (choose (minInt, maxInt)) $ \n ->
-      property $ roundTripCborRangeExpectation fromVersion toVersion n
+      property $ expectBinaryAccepted n
 
 instance RuleListEra ShelleyEra where
   type
