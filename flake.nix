d8082f1c17131d798674f71c9879473c8821e613
Drazen Popovic <bladyjoker@gmail.com>
2026-04-24 20:05:30 +0200

    leios: disable x86_64-darwin build

--- flake.nix
+++ flake.nix
@@ -34,7 +34,6 @@
   outputs = inputs: let
     supportedSystems = [
       "x86_64-linux"
-      "x86_64-darwin"
       # "aarch64-linux" - disable these temporarily because the build is broken
       "aarch64-darwin"
     ];
