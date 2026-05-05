8391071358e7fed7ea6ae1d9738a0842d90fd866
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2026-05-05 13:09:17 +0300

    Add new helpers with predicate failure injection:
    List below also shows direct mapping to older helpers without injection:
      - `?!.` -> `?!`
      - `?!#.` -> `?!#`
      - `checkFailOnLeftStatic` -> `?!#:`
      - `checkFailOnJustStatic` -> `failOnJustStatic`
      - `checkFailOnJust` -> `failOnJust`
      - `checkFailOnNonEmpty` -> `failOnNonEmpty`
      - `checkFailOnNonEmptySet` -> `failOnNonEmptySet`
      - `checkFailOnNonEmptyMap` -> `failOnNonEmptyMap`

--- libs/cardano-ledger-core/src/Cardano/Ledger/Rules/ValidationMode.hs
+++ libs/cardano-ledger-core/src/Cardano/Ledger/Rules/ValidationMode.hs
@@ -26,6 +26,18 @@ module Cardano.Ledger.Rules.ValidationMode (
   runTest,
   runTestOnSignal,
   failOnJustStatic,
+
+  -- * Validation with injection of predicate failures
+  (?!.),
+  checkFailOnJust,
+  checkFailOnNonEmpty,
+  checkFailOnNonEmptySet,
+  checkFailOnNonEmptyMap,
+
+  -- ** Static
+  (?!#.),
+  checkFailOnLeftStatic,
+  checkFailOnJustStatic,
 ) where
 
 import Cardano.Ledger.BaseTypes (Inject (..))
@@ -33,6 +45,9 @@ import Cardano.Ledger.Core
 import Control.State.Transition.Extended
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NE
+import qualified Data.Map.NonEmpty as NEM
+import Data.Map.Strict (Map)
+import qualified Data.Set.NonEmpty as NES
 import Validation
 
 --------------------------------------------------------------------------------
@@ -91,3 +106,64 @@ runTestOnSignal = validateTransLabeled injectFailure $ lblStatic NE.:| []
 failOnJustStatic :: Maybe a -> (a -> PredicateFailure sts) -> Rule sts ctx ()
 failOnJustStatic cond onJust =
   validateTransLabeled id (lblStatic NE.:| []) $ failureOnJust cond onJust
+{-# INLINE failOnJustStatic #-}
+
+-- =========== Validation with injection of predicate failures ===========
+
+-- | Same as `?!`, except accepts injectable predicate failure
+(?!.) ::
+  InjectRuleFailure rule t era =>
+  Bool -> t era -> Rule (EraRule rule era) ctx ()
+(?!.) cond predFailure = cond ?! injectFailure predFailure
+{-# INLINE (?!.) #-}
+
+-- | Same as `?!#`, except accepts injectable predicate failure
+(?!#.) ::
+  InjectRuleFailure rule t era =>
+  Bool -> t era -> Rule (EraRule rule era) ctx ()
+(?!#.) cond predFailure = cond ?!# injectFailure predFailure
+{-# INLINE (?!#.) #-}
+
+-- | Same as `failOnJust`, except accepts injectable predicate failure
+checkFailOnJust ::
+  InjectRuleFailure rule t era =>
+  Maybe a -> (a -> t era) -> Rule (EraRule rule era) ctx ()
+checkFailOnJust cond onJust = failOnJust cond (injectFailure . onJust)
+{-# INLINE checkFailOnJust #-}
+
+-- | Same as `failOnNonEmpty`, except accepts injectable predicate failure
+checkFailOnNonEmpty ::
+  InjectRuleFailure rule t era =>
+  Foldable f =>
+  f a -> (NonEmpty a -> t era) -> Rule (EraRule rule era) ctx ()
+checkFailOnNonEmpty cond onJust = failOnNonEmpty cond (injectFailure . onJust)
+{-# INLINE checkFailOnNonEmpty #-}
+
+-- | Same as `failOnNonEmptySet`, except accepts injectable predicate failure
+checkFailOnNonEmptySet ::
+  (InjectRuleFailure rule t era, Foldable f, Ord a) =>
+  f a -> (NES.NonEmptySet a -> t era) -> Rule (EraRule rule era) ctx ()
+checkFailOnNonEmptySet cond onJust = failOnNonEmptySet cond (injectFailure . onJust)
+{-# INLINE checkFailOnNonEmptySet #-}
+
+-- | Same as `failOnNonEmptyMap`, except accepts injectable predicate failure
+checkFailOnNonEmptyMap ::
+  InjectRuleFailure rule t era =>
+  Map k v -> (NEM.NonEmptyMap k v -> t era) -> Rule (EraRule rule era) ctx ()
+checkFailOnNonEmptyMap cond onJust = failOnNonEmptyMap cond (injectFailure . onJust)
+{-# INLINE checkFailOnNonEmptyMap #-}
+
+-- | Same as `?!#:`, except accepts injectable predicate failure and is not designed to be used as
+-- infix operator
+checkFailOnLeftStatic ::
+  InjectRuleFailure rule t era =>
+  Either e () -> (e -> t era) -> Rule (EraRule rule era) ctx ()
+checkFailOnLeftStatic cond predFailure = cond ?!#: (injectFailure . predFailure)
+{-# INLINE checkFailOnLeftStatic #-}
+
+-- | Same as `failOnJustStatic`, except accepts injectable predicate failure
+checkFailOnJustStatic ::
+  InjectRuleFailure rule t era =>
+  Maybe a -> (a -> t era) -> Rule (EraRule rule era) ctx ()
+checkFailOnJustStatic cond onJust = failOnJustStatic cond (injectFailure . onJust)
+{-# INLINE checkFailOnJustStatic #-}
