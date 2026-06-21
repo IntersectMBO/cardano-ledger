fa38c64b99a4051af8e99da2dc209b8ff6405e58
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-06-21 20:12:24 +0200

    Adapt TreeDiff usage of LeiosCert to new exports
    Not sure if this is better, but the upstream module has a smaller API
    surface now.

--- eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/TreeDiff.hs
+++ eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/TreeDiff.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -17,9 +18,10 @@ module Test.Cardano.Ledger.Dijkstra.TreeDiff (
 ) where
 
 import Cardano.Crypto.DSIGN (rawSerialiseSigDSIGN)
-import Cardano.Crypto.Leios (LeiosCert (..), bitFieldToBytes)
+import Cardano.Crypto.Leios (LeiosCert (..), encodeBitField)
 import Cardano.Ledger.Alonzo.Plutus.Context (ContextError)
 import Cardano.Ledger.BaseTypes (StrictMaybe)
+import Cardano.Ledger.Binary.Plain (serialize)
 import qualified Cardano.Ledger.Conway.Rules as Conway
 import Cardano.Ledger.Dijkstra (DijkstraEra)
 import Cardano.Ledger.Dijkstra.BlockBody (PerasCert)
@@ -132,13 +134,15 @@ instance ToExpr (TxBody l DijkstraEra)
 instance ToExpr PerasCert
 
 -- Manual ToExpr to avoid an orphan 'ToExpr (SigDSIGN BLS12381MinSigDSIGN)':
--- show the BLS signature as its raw byte representation.
+-- show the BLS signature as its raw byte representation, and the bitfield
+-- as its CBOR encoding (since the wire-format bytes aren't separately
+-- observable through the public API).
 instance ToExpr LeiosCert where
-  toExpr cert =
+  toExpr LeiosCert {leiosCertSigners, leiosCertSignature} =
     Rec "LeiosCert" $
       OMap.fromList
-        [ ("signers", toExpr (bitFieldToBytes $ signers cert))
-        , ("aggregatedSignature", toExpr (rawSerialiseSigDSIGN $ aggregatedSignature cert))
+        [ ("signers", toExpr (serialize (encodeBitField leiosCertSigners)))
+        , ("aggregatedSignature", toExpr (rawSerialiseSigDSIGN leiosCertSignature))
         ]
 
 instance ToExpr (Tx TopTx era) => ToExpr (DijkstraBlockBodyRaw era)
