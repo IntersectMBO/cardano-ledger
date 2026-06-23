3b910dc93f3c9e444c08f7ce407cf94676a3d86a
Neil Mayhew <neil.mayhew@iohk.io>
2026-06-22 18:11:35 -0600

    Avoid infinite loops with ghc >=9.10 and prevent building with 9.8

--- eras/conway/impl/src/Cardano/Ledger/Conway/Rules/Certs.hs
+++ eras/conway/impl/src/Cardano/Ledger/Conway/Rules/Certs.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
@@ -16,6 +17,10 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+#if __GLASGOW_HASKELL__ >= 910
+-- See https://gitlab.haskell.org/ghc/ghc/-/issues/27342
+{-# OPTIONS_GHC -fno-spec-eval #-}
+#endif
 
 module Cardano.Ledger.Conway.Rules.Certs (
   CERTS,
