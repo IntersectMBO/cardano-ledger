ce4c8107b365ad8c0dd78ccccbd368bd217b92c5
teodanciu <teodora.danciu@iohk.io>
2026-06-30 13:31:10 +0100

    Check and apply withdrawals in SUBENTITIES

--- eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Rules/SubEntities.hs
+++ eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Rules/SubEntities.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
@@ -17,23 +19,36 @@ module Cardano.Ledger.Dijkstra.Rules.SubEntities (
   SubEntitiesEvent (..),
 ) where
 
-import Cardano.Ledger.BaseTypes (ShelleyBase)
+import Cardano.Ledger.BaseTypes (Globals (networkId), Mismatch (..), Relation (..), ShelleyBase)
 import Cardano.Ledger.Binary (DecCBOR (..), EncCBOR (..))
+import Cardano.Ledger.Binary.Coders
+import Cardano.Ledger.Coin (Coin)
 import Cardano.Ledger.Conway.Core
+import qualified Cardano.Ledger.Conway.Rules as Conway
 import Cardano.Ledger.Conway.State
 import Cardano.Ledger.Dijkstra.Era (DijkstraEra, SUBCERTS, SUBENTITIES)
 import Cardano.Ledger.Dijkstra.Rules.SubCerts (
   DijkstraSubCertsEvent,
   DijkstraSubCertsPredFailure,
-  SubCertsEnv,
+  SubCertsEnv (..),
  )
+import Cardano.Ledger.Rules.ValidationMode (runTest)
 import Control.DeepSeq (NFData)
+import Control.Monad.Trans.Reader (asks)
 import Control.State.Transition.Extended
+import Data.List.NonEmpty (NonEmpty)
+import Data.Map.NonEmpty (NonEmptyMap)
+import qualified Data.Map.NonEmpty as NEM
+import qualified Data.Map.Strict as Map
 import Data.Sequence (Seq)
 import GHC.Generics (Generic)
+import Lens.Micro
 
-newtype SubEntitiesPredFailure era
+data SubEntitiesPredFailure era
   = SubCertsFailure (PredicateFailure (EraRule "SUBCERTS" era))
+  | SubWdrlNotDelegatedToDRep (NonEmpty (KeyHash Staking))
+  | SubWithdrawalsMissingAccounts Withdrawals
+  | SubWithdrawalAmountsExceedAccountBalances (NonEmptyMap AccountAddress (Mismatch RelLTEQ Coin))
   deriving (Generic)
 
 deriving stock instance
@@ -46,13 +61,31 @@ instance
   NFData (PredicateFailure (EraRule "SUBCERTS" era)) =>
   NFData (SubEntitiesPredFailure era)
 
-deriving newtype instance
-  (Era era, EncCBOR (PredicateFailure (EraRule "SUBCERTS" era))) =>
+instance
+  ( Era era
+  , EncCBOR (PredicateFailure (EraRule "SUBCERTS" era))
+  ) =>
   EncCBOR (SubEntitiesPredFailure era)
+  where
+  encCBOR =
+    encode . \case
+      SubCertsFailure x -> Sum (SubCertsFailure @era) 0 !> To x
+      SubWdrlNotDelegatedToDRep x -> Sum (SubWdrlNotDelegatedToDRep @era) 1 !> To x
+      SubWithdrawalsMissingAccounts x -> Sum (SubWithdrawalsMissingAccounts @era) 2 !> To x
+      SubWithdrawalAmountsExceedAccountBalances x -> Sum (SubWithdrawalAmountsExceedAccountBalances @era) 3 !> To x
 
-deriving newtype instance
-  (Era era, DecCBOR (PredicateFailure (EraRule "SUBCERTS" era))) =>
+instance
+  ( Era era
+  , DecCBOR (PredicateFailure (EraRule "SUBCERTS" era))
+  ) =>
   DecCBOR (SubEntitiesPredFailure era)
+  where
+  decCBOR = decode . Summands "SubEntitiesPredFailure" $ \case
+    0 -> SumD SubCertsFailure <! From
+    1 -> SumD SubWdrlNotDelegatedToDRep <! From
+    2 -> SumD SubWithdrawalsMissingAccounts <! From
+    3 -> SumD SubWithdrawalAmountsExceedAccountBalances <! From
+    n -> Invalid n
 
 newtype SubEntitiesEvent era = SubCertsEvent (Event (EraRule "SUBCERTS" era))
   deriving (Generic)
@@ -70,13 +103,20 @@ instance InjectRuleFailure "SUBENTITIES" SubEntitiesPredFailure DijkstraEra
 instance InjectRuleFailure "SUBENTITIES" DijkstraSubCertsPredFailure DijkstraEra where
   injectFailure = SubCertsFailure
 
+instance InjectRuleFailure "SUBENTITIES" Conway.ConwayLedgerPredFailure DijkstraEra where
+  injectFailure = conwayToDijkstraSubEntitiesPredFailure
+
 instance
   ( EraTx era
+  , ConwayEraTxBody era
+  , ConwayEraCertState era
   , Embed (EraRule "SUBCERTS" era) (SUBENTITIES era)
   , State (EraRule "SUBCERTS" era) ~ CertState era
   , Signal (EraRule "SUBCERTS" era) ~ Seq (TxCert era)
   , Environment (EraRule "SUBCERTS" era) ~ SubCertsEnv era
   , EraRule "SUBENTITIES" era ~ SUBENTITIES era
+  , InjectRuleFailure "SUBENTITIES" SubEntitiesPredFailure era
+  , InjectRuleFailure "SUBENTITIES" Conway.ConwayLedgerPredFailure era
   ) =>
   STS (SUBENTITIES era)
   where
@@ -92,15 +132,54 @@ instance
 
 dijkstraSubEntitiesTransition ::
   forall era.
-  ( Embed (EraRule "SUBCERTS" era) (SUBENTITIES era)
+  ( EraTx era
+  , ConwayEraTxBody era
+  , ConwayEraCertState era
+  , Embed (EraRule "SUBCERTS" era) (SUBENTITIES era)
   , State (EraRule "SUBCERTS" era) ~ CertState era
   , Signal (EraRule "SUBCERTS" era) ~ Seq (TxCert era)
   , Environment (EraRule "SUBCERTS" era) ~ SubCertsEnv era
+  , EraRule "SUBENTITIES" era ~ SUBENTITIES era
+  , InjectRuleFailure "SUBENTITIES" SubEntitiesPredFailure era
+  , InjectRuleFailure "SUBENTITIES" Conway.ConwayLedgerPredFailure era
   ) =>
   TransitionRule (SUBENTITIES era)
 dijkstraSubEntitiesTransition = do
-  TRC (env, certState, certificates) <- judgmentContext
-  trans @(EraRule "SUBCERTS" era) $ TRC (env, certState, certificates)
+  TRC (subCertsEnv, certState, certificates) <- judgmentContext
+  let tx = certsTx subCertsEnv
+      withdrawals = tx ^. bodyTxL . withdrawalsTxBodyL
+      accounts = certState ^. certDStateL . accountsL
+
+  runTest $ Conway.validateWithdrawalsDelegated accounts tx
+
+  network <- liftSTS $ asks networkId
+  let (missingWithdrawals, exceededWithdrawals) =
+        case withdrawalsThatExceedAccountBalance withdrawals network accounts of
+          Nothing -> (Map.empty, Map.empty)
+          Just (missing, exceeded) -> (unWithdrawals missing, exceeded)
+  failOnNonEmptyMap missingWithdrawals $
+    injectFailure . SubWithdrawalsMissingAccounts . Withdrawals . NEM.toMap
+  failOnNonEmptyMap exceededWithdrawals $ injectFailure . SubWithdrawalAmountsExceedAccountBalances
+
+  let certState' =
+        certState
+          & certDStateL . accountsL %~ applyWithdrawals withdrawals
+  trans @(EraRule "SUBCERTS" era) $ TRC (subCertsEnv, certState', certificates)
+
+conwayToDijkstraSubEntitiesPredFailure ::
+  forall era. Conway.ConwayLedgerPredFailure era -> SubEntitiesPredFailure era
+conwayToDijkstraSubEntitiesPredFailure = \case
+  Conway.ConwayWdrlNotDelegatedToDRep khs -> SubWdrlNotDelegatedToDRep khs
+  Conway.ConwayUtxowFailure _ -> impossible "ConwayUtxowFailure"
+  Conway.ConwayCertsFailure _ -> impossible "ConwayCertsFailure"
+  Conway.ConwayGovFailure _ -> impossible "ConwayGovFailure"
+  Conway.ConwayTreasuryValueMismatch _ -> impossible "ConwayTreasuryValueMismatch"
+  Conway.ConwayTxRefScriptsSizeTooBig _ -> impossible "ConwayTxRefScriptsSizeTooBig"
+  Conway.ConwayMempoolFailure _ -> impossible "ConwayMempoolFailure"
+  Conway.ConwayWithdrawalsMissingAccounts _ -> impossible "ConwayWithdrawalsMissingAccounts"
+  Conway.ConwayIncompleteWithdrawals _ -> impossible "ConwayIncompleteWithdrawals"
+  where
+    impossible name = error $ "Impossible: `" <> name <> "` for SUBENTITIES"
 
 instance
   ( STS (SUBCERTS era)
