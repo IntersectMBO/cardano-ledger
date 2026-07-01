4bc98a1c1f932c55a2e759ececa79f0e550693a6
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2026-07-01 13:37:00 +0200

    Update Conway conformance to PV 11; rename modules for consistency

--- libs/cardano-ledger-conformance/test/Test/Cardano/Ledger/Conformance/Imp/Conway.hs
+++ libs/cardano-ledger-conformance/test/Test/Cardano/Ledger/Conformance/Imp/Conway.hs
@@ -8,46 +8,39 @@ import Cardano.Ledger.BaseTypes
 import Cardano.Ledger.Conway (ConwayEra)
 import Test.Cardano.Ledger.Conformance.Imp.Conway.Ratify qualified as RatifySpec
 import Test.Cardano.Ledger.Conformance.Imp.Core
-import Test.Cardano.Ledger.Conway.Imp.BbodySpec qualified as Bbody
-import Test.Cardano.Ledger.Conway.Imp.CertsSpec qualified as Certs
-import Test.Cardano.Ledger.Conway.Imp.DelegSpec qualified as Deleg
-import Test.Cardano.Ledger.Conway.Imp.EnactSpec qualified as Enact
-import Test.Cardano.Ledger.Conway.Imp.EpochSpec qualified as Epoch
-import Test.Cardano.Ledger.Conway.Imp.GovCertSpec qualified as GovCert
-import Test.Cardano.Ledger.Conway.Imp.GovSpec qualified as Gov
-import Test.Cardano.Ledger.Conway.Imp.LedgerSpec qualified as Ledger
-import Test.Cardano.Ledger.Conway.Imp.RatifySpec qualified as Ratify
-import Test.Cardano.Ledger.Conway.Imp.UtxoSpec qualified as Utxo
-import Test.Cardano.Ledger.Conway.Imp.UtxosSpec qualified as Utxos
-import Test.Cardano.Ledger.Conway.Imp.UtxowSpec qualified as Utxow
+import Test.Cardano.Ledger.Conway.Imp.BbodySpec qualified as BBODY
+import Test.Cardano.Ledger.Conway.Imp.CertsSpec qualified as CERTS
+import Test.Cardano.Ledger.Conway.Imp.DelegSpec qualified as DELEG
+import Test.Cardano.Ledger.Conway.Imp.EnactSpec qualified as ENACT
+import Test.Cardano.Ledger.Conway.Imp.EpochSpec qualified as EPOCH
+import Test.Cardano.Ledger.Conway.Imp.GovCertSpec qualified as GOVCERT
+import Test.Cardano.Ledger.Conway.Imp.GovSpec qualified as GOV
+import Test.Cardano.Ledger.Conway.Imp.LedgerSpec qualified as LEDGER
+import Test.Cardano.Ledger.Conway.Imp.RatifySpec qualified as RATIFY
+import Test.Cardano.Ledger.Conway.Imp.UtxoSpec qualified as UTXO
+import Test.Cardano.Ledger.Conway.Imp.UtxosSpec qualified as UTXOS
+import Test.Cardano.Ledger.Conway.Imp.UtxowSpec qualified as UTXOW
 import Test.Cardano.Ledger.Conway.ImpTest
 import Test.Cardano.Ledger.Imp.Common hiding (Args)
-import UnliftIO (evaluateDeep)
 
 spec :: Spec
 spec = do
   describe "Imp" $ do
     withImpInit @(LedgerSpec ConwayEra) $
-      modifyImpInitProtVer @ConwayEra (natVersion @10) $
+      modifyImpInitProtVer @ConwayEra (natVersion @11) $
         modifyImpInitPostSubmitTxHook submitTxConformanceHook $ do
           modifyImpInitPostEpochBoundaryHook epochBoundaryConformanceHook $ do
-            describe "Basic" $ do
-              it "Submit constitution" $ do
-                _ <- submitConstitution @ConwayEra SNothing
-                passNEpochs 2
-              it "Can elect a basic committee" $ do
-                void $ evaluateDeep =<< electBasicCommittee
-            describe "BBODY" Bbody.spec
-            describe "CERTS" Certs.spec
-            describe "DELEG" Deleg.spec
-            describe "ENACT" Enact.spec
-            describe "EPOCH" Epoch.spec
-            describe "GOV" Gov.spec
-            describe "GOVCERT" GovCert.spec
-            describe "LEDGER" Ledger.spec
-            describe "RATIFY" Ratify.spec
-            describe "UTXO" Utxo.spec
-            describe "UTXOW" Utxow.spec
-            describe "UTXOS" Utxos.spec
+            BBODY.spec
+            CERTS.spec
+            DELEG.spec
+            ENACT.spec
+            EPOCH.spec
+            GOV.spec
+            GOVCERT.spec
+            LEDGER.spec
+            RATIFY.spec
+            UTXO.spec
+            UTXOW.spec
+            UTXOS.spec
   describe "Imp (only spec)" $ do
     RatifySpec.spec
