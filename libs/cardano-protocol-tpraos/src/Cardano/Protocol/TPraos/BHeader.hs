99a6facac7b094a90c52973d99a048a507f549c2
Lucsanszky <daniel.lucsanszky@iohk.io>
2026-07-03 17:08:37 +0200

    Deprecate `Cardano.Protocol.TPraos.BHeader`
    Adds the deprecation pragma here, separately from the previous commit
    where the header was moved, so we preserve the `git` history.

---
+++ libs/cardano-protocol-tpraos/src/Cardano/Protocol/TPraos/BHeader.hs
@@ -0,0 +1,6 @@
+module Cardano.Protocol.TPraos.BHeader
+  {-# DEPRECATED "Use Cardano.Protocol.TPraos.BlockHeader instead" #-} (
+  module Cardano.Protocol.TPraos.BlockHeader,
+) where
+
+import Cardano.Protocol.TPraos.BlockHeader
