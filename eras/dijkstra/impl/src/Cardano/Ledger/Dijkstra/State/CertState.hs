3e46b75fa5c9fb66b07eb9e921cb57345227f658
teodanciu <teodora.danciu@iohk.io>
2026-07-15 23:34:13 +0100

    Implement batch-aware `certsTotalRefundsTxBody` in Dijkstra
    which will create correct results in `AdaPots` as well as in the
    `deposited` field of `UTxOState`

--- eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/State/CertState.hs
+++ eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/State/CertState.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Cardano.Ledger.Dijkstra.State.CertState () where
+module Cardano.Ledger.Dijkstra.State.CertState (
+  dijkstraCertsTotalRefundsTxBody,
+) where
 
+import Cardano.Ledger.Coin (Coin)
 import Cardano.Ledger.Conway.State (
   ConwayCertState,
   ConwayEraCertState (..),
+  EraAccounts,
   EraCertState (..),
   conwayCertDStateL,
   conwayCertPStateL,
@@ -14,8 +21,14 @@ import Cardano.Ledger.Conway.State (
   conwayCertsTotalRefundsTxBody,
   conwayObligationCertState,
  )
+import Cardano.Ledger.Core
 import Cardano.Ledger.Dijkstra.Era (DijkstraEra)
 import Cardano.Ledger.Dijkstra.State.Account ()
+import Cardano.Ledger.Dijkstra.Tx ()
+import Cardano.Ledger.Dijkstra.TxBody (DijkstraEraTxBody (..))
+import Cardano.Ledger.Val ((<+>))
+import Data.Foldable (foldMap')
+import Lens.Micro ((^.))
 
 instance EraCertState DijkstraEra where
   type CertState DijkstraEra = ConwayCertState DijkstraEra
@@ -30,8 +43,32 @@ instance EraCertState DijkstraEra where
 
   certsTotalDepositsTxBody = conwayCertsTotalDepositsTxBody
 
-  certsTotalRefundsTxBody = conwayCertsTotalRefundsTxBody
+  certsTotalRefundsTxBody = dijkstraCertsTotalRefundsTxBody
 
 instance ConwayEraCertState DijkstraEra where
   certVStateL = conwayCertVStateL
   {-# INLINE certVStateL #-}
+
+-- | Batch-aware total refunds for a transaction
+dijkstraCertsTotalRefundsTxBody ::
+  forall era l.
+  ( EraTx era
+  , EraAccounts era
+  , DijkstraEraTxBody era
+  , STxLevel l era ~ STxBothLevels l era
+  ) =>
+  PParams era ->
+  ConwayCertState era ->
+  TxBody l era ->
+  Coin
+dijkstraCertsTotalRefundsTxBody pp certState txBody =
+  -- TODO: restrict to TopTx
+  withBothTxLevels
+    txBody
+    ( \topTxBody ->
+        conwayCertsTotalRefundsTxBody pp certState topTxBody
+          <+> foldMap'
+            (conwayCertsTotalRefundsTxBody pp certState . (^. bodyTxL))
+            (topTxBody ^. subTransactionsTxBodyL)
+    )
+    (conwayCertsTotalRefundsTxBody pp certState)
