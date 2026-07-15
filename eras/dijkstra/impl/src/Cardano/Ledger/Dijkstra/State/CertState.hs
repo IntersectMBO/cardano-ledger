991f46b4f4af5e11bb6e09105188c6e585029f6e
teodanciu <teodora.danciu@iohk.io>
2026-07-15 23:34:13 +0100

    Implement batch-aware `certsTotalRefundsTxBody` in Dijkstra
    which will create correct results in `AdaPots` as well as in the
    `deposited` field of `UTxOState`

--- eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/State/CertState.hs
+++ eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/State/CertState.hs
@@ -1,11 +1,19 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cardano.Ledger.Dijkstra.State.CertState () where
 
+import Cardano.Ledger.Coin (Coin)
 import Cardano.Ledger.Conway.State
+import Cardano.Ledger.Core
 import Cardano.Ledger.Dijkstra.Era (DijkstraEra)
 import Cardano.Ledger.Dijkstra.State.Account ()
+import Cardano.Ledger.Dijkstra.Tx ()
+import Cardano.Ledger.Dijkstra.TxBody (DijkstraEraTxBody (..))
+import Data.Foldable (foldMap')
+import Lens.Micro ((^.))
 
 instance EraCertState DijkstraEra where
   type CertState DijkstraEra = ConwayCertState DijkstraEra
@@ -20,8 +28,29 @@ instance EraCertState DijkstraEra where
 
   certsTotalDepositsTxBody = conwayCertsTotalDepositsTxBody
 
-  certsTotalRefundsTxBody = shelleyCertsTotalRefundsTxBody
+  certsTotalRefundsTxBody = dijkstraCertsTotalRefundsTxBody
 
 instance ConwayEraCertState DijkstraEra where
   certVStateL = conwayCertVStateL
   {-# INLINE certVStateL #-}
+
+-- | Total refunds for a transaction, summed across the top-level tx and its subtransactions
+dijkstraCertsTotalRefundsTxBody ::
+  forall era l.
+  ( EraTx era
+  , DijkstraEraTxBody era
+  , STxLevel l era ~ STxBothLevels l era
+  ) =>
+  PParams era ->
+  Accounts era ->
+  TxBody l era ->
+  Coin
+dijkstraCertsTotalRefundsTxBody pp _ txBody =
+  getTotalRefundsTxCerts pp (const Nothing) $
+    withBothTxLevels
+      txBody
+      ( \topTxBody ->
+          (topTxBody ^. certsTxBodyL)
+            <> foldMap' (^. bodyTxL . certsTxBodyL) (topTxBody ^. subTransactionsTxBodyL)
+      )
+      (^. certsTxBodyL)
