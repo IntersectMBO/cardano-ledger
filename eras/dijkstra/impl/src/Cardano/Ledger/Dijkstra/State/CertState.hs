ea750d93439109837826fc6f096da087fc6d6891
teodanciu <teodora.danciu@iohk.io>
2026-07-20 14:16:42 +0100

    Implement batch-aware `certsTotalRefundsTxBody` in Dijkstra

--- eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/State/CertState.hs
+++ eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/State/CertState.hs
@@ -32,7 +32,7 @@ instance EraCertState DijkstraEra where
 
   certsTotalDepositsTxBody = dijkstraCertsTotalDepositsTxBody
 
-  certsTotalRefundsTxBody = shelleyCertsTotalRefundsTxBody
+  certsTotalRefundsTxBody = dijkstraCertsTotalRefundsTxBody
 
 instance ConwayEraCertState DijkstraEra where
   certVStateL = conwayCertVStateL
@@ -65,3 +65,24 @@ dijkstraCertsTotalDepositsTxBody pp certState = \txBody ->
     (getTotalDepositsTxBody pp isPoolReg)
   where
     isPoolReg = (`Map.member` psStakePools (conwayCertPState certState))
+
+-- | Total refunds for a transaction, summed across the top-level tx and its subtransactions
+dijkstraCertsTotalRefundsTxBody ::
+  forall era l.
+  ( EraTx era
+  , DijkstraEraTxBody era
+  , STxLevel l era ~ STxBothLevels l era
+  ) =>
+  PParams era ->
+  Accounts era ->
+  TxBody l era ->
+  Coin
+dijkstraCertsTotalRefundsTxBody pp _ txBody =
+  getTotalRefundsTxCerts pp (const Nothing) $
+    withBothTxLevels
+      txBody
+      ( \topTxBody ->
+          (topTxBody ^. certsTxBodyL)
+            <> foldMap' (^. bodyTxL . certsTxBodyL) (topTxBody ^. subTransactionsTxBodyL)
+      )
+      (^. certsTxBodyL)
