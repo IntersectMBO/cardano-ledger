56baeb1ee96dbba978ed4038f448a7e31f04b800
Lucsanszky <daniel.lucsanszky@iohk.io>
2026-07-23 14:34:32 +0200

    Add per-era Block instances for Babbage era

--- eras/babbage/impl/src/Cardano/Ledger/Babbage/BlockBody.hs
+++ eras/babbage/impl/src/Cardano/Ledger/Babbage/BlockBody.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cardano.Ledger.Babbage.BlockBody where
@@ -7,9 +14,21 @@ import Cardano.Ledger.Alonzo.BlockBody
 import Cardano.Ledger.Babbage.Era
 import Cardano.Ledger.Babbage.Tx ()
 import Cardano.Ledger.BaseTypes (ProtVer (..))
-import Cardano.Ledger.Binary (EncCBORGroup (..), serialize')
+import Cardano.Ledger.Binary (
+  Annotator,
+  DecCBOR (decCBOR),
+  EncCBOR (..),
+  EncCBORGroup (..),
+  decodeRecordNamed,
+  encodeListLen,
+  serialize',
+  toPlainEncoding,
+ )
+import qualified Cardano.Ledger.Binary.Plain as Plain
+import Cardano.Ledger.Block (Block (..))
 import Cardano.Ledger.Core
 import qualified Data.ByteString as BS
+import Data.Typeable (Typeable)
 
 instance EraBlockBody BabbageEra where
   type BlockBody BabbageEra = AlonzoBlockBody BabbageEra
@@ -17,3 +36,20 @@ instance EraBlockBody BabbageEra where
   txSeqBlockBodyL = txSeqBlockBodyAlonzoL
   hashBlockBody = alonzoBlockBodyHash
   blockBodySize (ProtVer v _) = BS.length . serialize' v . encCBORGroup
+
+instance EncCBOR h => EncCBOR (Block h BabbageEra) where
+  encCBOR (Block h txns) =
+    encodeListLen 5 <> encCBOR h <> encCBORGroup txns
+
+instance (EncCBOR h, Typeable h) => Plain.ToCBOR (Block h BabbageEra) where
+  toCBOR = toPlainEncoding (eraProtVerLow @BabbageEra) . encCBOR
+
+instance
+  (DecCBOR (Annotator h), Typeable h) =>
+  DecCBOR (Annotator (Block h BabbageEra))
+  where
+  decCBOR =
+    decodeRecordNamed "Block" (const 5) $ do
+      header <- decCBOR
+      txns <- decCBOR
+      pure $ Block <$> header <*> txns
