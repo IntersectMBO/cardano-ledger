888f75951e9dd3963378e9398100cca62332f64b
Aniket Deshpande <git@aniketd.net>
2026-08-06 17:22:43 +0530

    Copy conway EPOCH rule over to dijkstra verbatim

--- eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Rules.hs
+++ eras/dijkstra/impl/src/Cardano/Ledger/Dijkstra/Rules.hs
@@ -35,6 +35,7 @@ import Cardano.Ledger.Dijkstra.Rules.Cert ()
 import Cardano.Ledger.Dijkstra.Rules.Certs ()
 import Cardano.Ledger.Dijkstra.Rules.Deleg ()
 import Cardano.Ledger.Dijkstra.Rules.Entities
+import Cardano.Ledger.Dijkstra.Rules.Epoch ()
 import Cardano.Ledger.Dijkstra.Rules.Gov
 import Cardano.Ledger.Dijkstra.Rules.GovCert
 import Cardano.Ledger.Dijkstra.Rules.Ledger
