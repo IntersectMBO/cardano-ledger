c99a715917a5e396866f4e33b15af99665582506
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2026-08-10 15:54:00 +0200

    Enable tests in Dijkstra

--- libs/cardano-ledger-conformance/test/Test/Cardano/Ledger/Conformance/Imp/Dijkstra.hs
+++ libs/cardano-ledger-conformance/test/Test/Cardano/Ledger/Conformance/Imp/Dijkstra.hs
@@ -24,17 +24,23 @@ import Test.Cardano.Ledger.Conway.Imp.GovCertSpec qualified as ConwayGOVCERT
 import Test.Cardano.Ledger.Conway.Imp.GovSpec qualified as ConwayGOV
 import Test.Cardano.Ledger.Conway.Imp.LedgerSpec qualified as ConwayLEDGER
 import Test.Cardano.Ledger.Conway.Imp.RatifySpec qualified as ConwayRATIFY
+import Test.Cardano.Ledger.Conway.Imp.SnapSpec qualified as ConwaySNAP
 import Test.Cardano.Ledger.Conway.Imp.UtxoSpec qualified as ConwayUTXO
 import Test.Cardano.Ledger.Conway.Imp.UtxosSpec qualified as ConwayUTXOS
 import Test.Cardano.Ledger.Conway.Imp.UtxowSpec qualified as ConwayUTXOW
 import Test.Cardano.Ledger.Dijkstra.Imp.CertSpec qualified as CERT
 import Test.Cardano.Ledger.Dijkstra.Imp.EntitiesSpec qualified as ENTITIES
 import Test.Cardano.Ledger.Dijkstra.Imp.LedgerSpec qualified as LEDGER
+import Test.Cardano.Ledger.Dijkstra.Imp.PoolSpec qualified as POOL
 import Test.Cardano.Ledger.Dijkstra.Imp.UtxoSpec qualified as UTXO
 import Test.Cardano.Ledger.Dijkstra.Imp.UtxowSpec qualified as UTXOW
 import Test.Cardano.Ledger.Dijkstra.ImpTest
 import Test.Cardano.Ledger.Imp.Common hiding (Args)
 import Test.Cardano.Ledger.Mary.Imp.UtxoSpec qualified as MaryUTXO
+import Test.Cardano.Ledger.Shelley.Imp.DelegSpec qualified as ShelleyDELEG
+import Test.Cardano.Ledger.Shelley.Imp.EpochSpec qualified as ShelleyEPOCH
+import Test.Cardano.Ledger.Shelley.Imp.LedgerSpec qualified as ShelleyLEDGER
+import Test.Cardano.Ledger.Shelley.Imp.PoolSpec qualified as ShelleyPOOL
 import Test.Cardano.Ledger.Shelley.Imp.UtxoSpec qualified as ShelleyUTXO
 import Test.Cardano.Ledger.Shelley.Imp.UtxowSpec qualified as ShelleyUTXOW
 
@@ -46,28 +52,49 @@ spec = do
         modifyImpInitPostSubmitTxHook submitTxConformanceHook $ do
           modifyImpInitPostEpochBoundaryHook epochBoundaryConformanceHook $ do
             ConwayBBODY.spec
+
             CERT.spec
+
             xdescribe "disabled" ConwayCERTS.spec
+
             ENTITIES.spec
+
+            ShelleyDELEG.spec
             ConwayDELEG.spec
+
             ConwayENACT.spec
+
+            ShelleyEPOCH.spec
             ConwayEPOCH.spec
+
             ConwayGOV.spec
+
             ConwayGOVCERT.spec
+
+            ShelleyLEDGER.spec
             ConwayLEDGER.spec
             LEDGER.spec
+
+            xdescribe "disabled" ShelleyPOOL.spec
+            POOL.spec
+
             ConwayRATIFY.spec
+
+            ConwaySNAP.spec
+
             ShelleyUTXO.spec
             MaryUTXO.spec
             AlonzoUTXO.spec
             BabbageUTXO.spec
             ConwayUTXO.spec
             UTXO.spec
+
             ShelleyUTXOW.spec
             AlonzoUTXOW.spec
             BabbageUTXOW.spec
             ConwayUTXOW.spec
             UTXOW.spec
+
             AlonzoUTXOS.spec
             BabbageUTXOS.spec
             ConwayUTXOS.spec
