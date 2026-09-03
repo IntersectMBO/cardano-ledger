3ec93f2ff1f968d57a93ebaadbe7f1ad6c0a2a12
Lucsanszky <daniel.lucsanszky@iohk.io>
2026-09-03 21:29:01 +0200

    Inject `CostModels` into test state in Dijkstra
    Introduce an `AlonzoEraTransition` class with a `tcAlonzoGenesisL` lens,
    generalize `alonzoInjectCostModels` over it and use it in a new
    `conwayInjectIntoTestState` helper for both Conway and Dijkstra, so that
    cost models supplied via `extraConfig.costModels` in the Alonzo genesis
    are applied when a testnet starts in Dijkstra.

--- eras/alonzo/impl/src/Cardano/Ledger/Alonzo/Transition.hs
+++ eras/alonzo/impl/src/Cardano/Ledger/Alonzo/Transition.hs
@@ -1,16 +1,20 @@
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cardano.Ledger.Alonzo.Transition (
+  AlonzoEraTransition (..),
   TransitionConfig (..),
   alonzoInjectCostModels,
 ) where
 
-import Cardano.Ledger.Alonzo.Core (AlonzoEraPParams, ppCostModelsL)
+import Cardano.Ledger.Alonzo.Core (AlonzoEraPParams, PreviousEra, ppCostModelsL)
 import Cardano.Ledger.Alonzo.Era
 import Cardano.Ledger.Alonzo.Genesis
 import Cardano.Ledger.Alonzo.Translation ()
@@ -23,6 +27,15 @@ import Cardano.Ledger.Shelley.Transition
 import GHC.Generics
 import Lens.Micro
 import NoThunks.Class (NoThunks (..))
+
+class (EraTransition era, AlonzoEraPParams era) => AlonzoEraTransition era where
+  -- | Lens for the `AlonzoGenesis` from the `TransitionConfig`. Default implementation
+  -- looks in the previous era's config
+  tcAlonzoGenesisL :: Lens' (TransitionConfig era) AlonzoGenesis
+  default tcAlonzoGenesisL ::
+    AlonzoEraTransition (PreviousEra era) =>
+    Lens' (TransitionConfig era) AlonzoGenesis
+  tcAlonzoGenesisL = tcPreviousEraConfigL . tcAlonzoGenesisL
 
 instance EraTransition AlonzoEra where
   data TransitionConfig AlonzoEra = AlonzoTransitionConfig
@@ -42,13 +55,16 @@ instance EraTransition AlonzoEra where
   tcTranslationContextL =
     lens atcAlonzoGenesis (\atc ag -> atc {atcAlonzoGenesis = ag})
 
+instance AlonzoEraTransition AlonzoEra where
+  tcAlonzoGenesisL = tcTranslationContextL
+
 instance NoThunks (TransitionConfig AlonzoEra)
 
 alonzoInjectCostModels ::
-  (EraTransition era, AlonzoEraPParams era) =>
-  TransitionConfig AlonzoEra -> NewEpochState era -> NewEpochState era
+  AlonzoEraTransition era =>
+  TransitionConfig era -> NewEpochState era -> NewEpochState era
 alonzoInjectCostModels cfg =
-  case agExtraConfig $ cfg ^. tcTranslationContextL of
+  case agExtraConfig $ cfg ^. tcAlonzoGenesisL of
     SNothing -> id
     SJust aec -> overrideCostModels (aecCostModels aec)
 
