af9053298cad5608903de3316208626ce69eaa56
Joosep Jääger <joosep.jaager@iohk.io>
2026-09-14 13:42:47 +0300

    WIP

--- eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/Imp.hs
+++ eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/Imp.hs
@@ -16,6 +16,7 @@ import qualified Test.Cardano.Ledger.Dijkstra.Imp.LedgerSpec as LEDGER
 import qualified Test.Cardano.Ledger.Dijkstra.Imp.PoolSpec as POOL
 import qualified Test.Cardano.Ledger.Dijkstra.Imp.SnapSpec as SNAP
 import qualified Test.Cardano.Ledger.Dijkstra.Imp.SubDelegSpec as SUBDELEGS
+import qualified Test.Cardano.Ledger.Dijkstra.Imp.SubPoolSpec as SUBPOOL
 import qualified Test.Cardano.Ledger.Dijkstra.Imp.SubUtxoSpec as SUBUTXO
 import qualified Test.Cardano.Ledger.Dijkstra.Imp.SubUtxowSpec as SUBUTXOW
 import qualified Test.Cardano.Ledger.Dijkstra.Imp.UtxoSpec as UTXO
@@ -44,3 +45,4 @@ spec era = do
     SUBUTXO.spec
     SUBUTXOW.spec
     SUBDELEGS.spec
+    SUBPOOL.spec
