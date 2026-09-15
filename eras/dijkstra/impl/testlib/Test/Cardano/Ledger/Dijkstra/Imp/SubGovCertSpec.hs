b247d6a340d0715bd199b4eaf09fa1ed0b12fdb6
Joosep Jääger <joosep.jaager@iohk.io>
2026-09-15 16:23:40 +0300

    Added more tests

--- eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/Imp/SubGovCertSpec.hs
+++ eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/Imp/SubGovCertSpec.hs
@@ -9,7 +9,7 @@
 
 module Test.Cardano.Ledger.Dijkstra.Imp.SubGovCertSpec (spec) where
 
-import Cardano.Ledger.BaseTypes (Mismatch (..))
+import Cardano.Ledger.BaseTypes (EpochInterval (..), Mismatch (..), StrictMaybe (..))
 import Cardano.Ledger.Credential (Credential (..))
 import Cardano.Ledger.Dijkstra.Core
 import Cardano.Ledger.Dijkstra.Rules (
@@ -18,6 +18,7 @@ import Cardano.Ledger.Dijkstra.Rules (
  )
 import Cardano.Ledger.Dijkstra.State (DRepState (..))
 import Lens.Micro ((&), (.~))
+import Test.Cardano.Ledger.Core.Rational (IsRatio (..))
 import Test.Cardano.Ledger.Dijkstra.ImpTest
 import Test.Cardano.Ledger.Imp.Common
 
@@ -38,6 +39,17 @@ spec = describe "SUBGOVCERT" $ do
       case dSt of
         Just DRepState {..} -> drepAnchor `shouldBeExpr` newAnchor
         Nothing -> expectationFailure "Expected DRep to exist, but it does not"
+    it "Fails to update an unregistered DRep" $ do
+      drepCred <- KeyHashObj <$> freshKeyHash
+      anchor <- arbitrary
+      submitFailingTx
+        ( mkTopTxWithSubTxs
+            [ mkBasicTx mkBasicTxBody
+                & bodyTxL . certsTxBodyL .~ [UpdateDRepTxCert drepCred anchor]
+            ]
+        )
+        [ injectFailure . DijkstraSubGovCertPredFailure $ DijkstraDRepNotRegistered drepCred
+        ]
     it "Fails to register with an invalid deposit" $ do
       drepCred <- KeyHashObj <$> freshKeyHash
       expectedDeposit <- getsPParams ppDRepDepositL
@@ -123,3 +135,97 @@ spec = describe "SUBGOVCERT" $ do
         [ injectFailure . DijkstraSubGovCertPredFailure . DijkstraDRepIncorrectRefund $
             Mismatch actualDeposit expectedDeposit
         ]
+  describe "Committee hot key authorization" $ do
+    it "Authorizing a hot key succeeds" $ do
+      void registerInitialCommittee
+      initialCommittee <- getCommitteeMembers
+      forM_ initialCommittee $ \ccColdCred ->
+        replicateM_ 2 $ do
+          ccHotCred <- KeyHashObj <$> freshKeyHash
+          submitTxAnn_ "Authorizing a hot key" $
+            mkTopTxWithSubTxs
+              [ mkBasicTx mkBasicTxBody
+                  & bodyTxL . certsTxBodyL .~ [AuthCommitteeHotKeyTxCert ccColdCred ccHotCred]
+              ]
+    it "Fails for an unknown cold credential until it is proposed as a member" $
+      whenPostBootstrap $ do
+        unknownColdCred <- KeyHashObj <$> freshKeyHash
+        ccHotCred <- KeyHashObj <$> freshKeyHash
+        let authTx =
+              mkTopTxWithSubTxs
+                [ mkBasicTx mkBasicTxBody
+                    & bodyTxL . certsTxBodyL .~ [AuthCommitteeHotKeyTxCert unknownColdCred ccHotCred]
+                ]
+        submitFailingTx
+          authTx
+          [ injectFailure . DijkstraSubGovCertPredFailure $ DijkstraCommitteeIsUnknown unknownColdCred
+          ]
+        void $ submitUpdateCommittee Nothing mempty [(unknownColdCred, EpochInterval 20)] (1 %! 2)
+        submitTx_ authTx
+    it "Fails for a resigned member" $ do
+      void registerInitialCommittee
+      initialCommittee <- getCommitteeMembers
+      forM_ initialCommittee $ \ccColdCred -> do
+        ccHotCred <- KeyHashObj <$> freshKeyHash
+        let authTx =
+              mkTopTxWithSubTxs
+                [ mkBasicTx mkBasicTxBody
+                    & bodyTxL . certsTxBodyL .~ [AuthCommitteeHotKeyTxCert ccColdCred ccHotCred]
+                ]
+        submitTxAnn_ "Authorizing a hot key" authTx
+        submitTxAnn_ "Resigning the cold key" $
+          mkTopTxWithSubTxs
+            [ mkBasicTx mkBasicTxBody
+                & bodyTxL . certsTxBodyL .~ [ResignCommitteeColdTxCert ccColdCred SNothing]
+            ]
+        submitFailingTx
+          authTx
+          [ injectFailure . DijkstraSubGovCertPredFailure $
+              DijkstraCommitteeHasPreviouslyResigned ccColdCred
+          ]
+  describe "Committee cold key resignation" $ do
+    it "Resigning with an anchor succeeds" $ do
+      void registerInitialCommittee
+      initialCommittee <- getCommitteeMembers
+      forM_ initialCommittee $ \ccColdCred -> do
+        anchor <- arbitrary
+        submitTxAnn_ "Resigning the cold key" $
+          mkTopTxWithSubTxs
+            [ mkBasicTx mkBasicTxBody
+                & bodyTxL . certsTxBodyL .~ [ResignCommitteeColdTxCert ccColdCred anchor]
+            ]
+    it "Resigning a proposed member succeeds" $
+      whenPostBootstrap $ do
+        ccColdCred <- KeyHashObj <$> freshKeyHash
+        void $ submitUpdateCommittee Nothing mempty [(ccColdCred, EpochInterval 20)] (1 %! 2)
+        submitTxAnn_ "Resigning the proposed cold key" $
+          mkTopTxWithSubTxs
+            [ mkBasicTx mkBasicTxBody
+                & bodyTxL . certsTxBodyL .~ [ResignCommitteeColdTxCert ccColdCred SNothing]
+            ]
+    it "Fails for an unknown cold credential" $ do
+      void registerInitialCommittee
+      unknownColdCred <- KeyHashObj <$> freshKeyHash
+      submitFailingTx
+        ( mkTopTxWithSubTxs
+            [ mkBasicTx mkBasicTxBody
+                & bodyTxL . certsTxBodyL .~ [ResignCommitteeColdTxCert unknownColdCred SNothing]
+            ]
+        )
+        [ injectFailure . DijkstraSubGovCertPredFailure $ DijkstraCommitteeIsUnknown unknownColdCred
+        ]
+    it "Fails for a member that has already resigned" $ do
+      void registerInitialCommittee
+      initialCommittee <- getCommitteeMembers
+      forM_ initialCommittee $ \ccColdCred -> do
+        let resignTx =
+              mkTopTxWithSubTxs
+                [ mkBasicTx mkBasicTxBody
+                    & bodyTxL . certsTxBodyL .~ [ResignCommitteeColdTxCert ccColdCred SNothing]
+                ]
+        submitTxAnn_ "Resigning the cold key" resignTx
+        submitFailingTx
+          resignTx
+          [ injectFailure . DijkstraSubGovCertPredFailure $
+              DijkstraCommitteeHasPreviouslyResigned ccColdCred
+          ]
