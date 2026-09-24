720a1cdfffbb3536e21217d752d5354080bce92f
Joosep Jääger <joosep.jaager@iohk.io>
2026-09-24 12:46:16 +0300

    Make submitFailingLegacySubTx accept a NonEmpty of predicate failures

--- eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/Imp/SubUtxowSpec.hs
+++ eras/dijkstra/impl/testlib/Test/Cardano/Ledger/Dijkstra/Imp/SubUtxowSpec.hs
@@ -27,7 +27,7 @@ import qualified Cardano.Ledger.Conway.Rules as Conway
 import Cardano.Ledger.Core
 import Cardano.Ledger.Credential (Credential (..), StakeReference (..), credKeyHashWitness)
 import Cardano.Ledger.Dijkstra.Core
-import Cardano.Ledger.Dijkstra.Rules (DijkstraSubUtxowPredFailure (..))
+import Cardano.Ledger.Dijkstra.Rules (DijkstraSubUtxowPredFailure (..), STS (..))
 import Cardano.Ledger.Dijkstra.TxInfo (DijkstraContextError (..))
 import Cardano.Ledger.Keys (asWitness, witVKeyHash)
 import Cardano.Ledger.Plutus (
@@ -267,7 +267,7 @@ spec = describe "SUBUTXOW" $ do
           submitFailingLegacySubTx
             lang
             (mkTopTxWithSubTxs [mkBasicTx $ mkBasicTxBody & inputsTxBodyL .~ [txIn]])
-            (SubUnspendableUTxONoDatumHash @era $ NES.singleton txIn)
+            [injectFailure . SubUnspendableUTxONoDatumHash @era $ NES.singleton txIn]
 
   forM_ (eraLanguages @era) $ \lang ->
     withSLanguage lang $ \slang ->
@@ -283,7 +283,7 @@ spec = describe "SUBUTXOW" $ do
             submitFailingLegacySubTx
               lang
               (mkTopTxWithSubTxs [scriptSpendingSubTx txIn])
-              (SubMissingRequiredDatums @era (NES.singleton missingDatum) [])
+              [injectFailure $ SubMissingRequiredDatums @era (NES.singleton missingDatum) []]
 
         it "SubNotAllowedSupplementalDatums" $ do
           txIn <- produceScript redeemerSameAsDatumHash
@@ -298,7 +298,7 @@ spec = describe "SUBUTXOW" $ do
             submitFailingLegacySubTx
               lang
               (mkTopTxWithSubTxs [scriptSpendingSubTx txIn])
-              (SubNotAllowedSupplementalDatums @era (NES.singleton extraDatumHash) [])
+              [injectFailure $ SubNotAllowedSupplementalDatums @era (NES.singleton extraDatumHash) []]
 
         it "SubMissingRedeemers" $ do
           txIn <- produceScript redeemerSameAsDatumHash
@@ -324,7 +324,7 @@ spec = describe "SUBUTXOW" $ do
             submitFailingLegacySubTx
               lang
               (mkTopTxWithSubTxs [scriptSpendingSubTx txIn])
-              (SubExtraRedeemers @era [extraPurpose])
+              [injectFailure $ SubExtraRedeemers @era [extraPurpose]]
 
         describe "SubScriptIntegrityHashMismatch" $ do
           let testHashMismatch badHash = do
@@ -344,10 +344,14 @@ spec = describe "SUBUTXOW" $ do
                   rederiveAddrTxWits $
                     fixedUpTx & bodyTxL . subTransactionsTxBodyL .~ OMap.singleton badSubTx
                 withNoFixup $
-                  submitFailingLegacySubTx lang badTopTx $
-                    SubScriptIntegrityHashMismatch @era
-                      Mismatch {mismatchSupplied = badHash, mismatchExpected = goodHash}
-                      (originalBytes <$> expectedIntegrity)
+                  submitFailingLegacySubTx
+                    lang
+                    badTopTx
+                    [ injectFailure $
+                        SubScriptIntegrityHashMismatch @era
+                          Mismatch {mismatchSupplied = badHash, mismatchExpected = goodHash}
+                          (originalBytes <$> expectedIntegrity)
+                    ]
           it "the supplied hash is wrong" $ testHashMismatch . SJust =<< arbitrary
           it "the supplied hash is missing" $ testHashMismatch SNothing
 
@@ -357,7 +361,7 @@ spec = describe "SUBUTXOW" $ do
           submitFailingLegacySubTx
             lang
             (mkTopTxWithSubTxs [scriptSpendingSubTx txIn])
-            (SubMalformedScriptWitnesses @era $ NES.singleton scriptHash)
+            [injectFailure . SubMalformedScriptWitnesses @era $ NES.singleton scriptHash]
 
         -- https://github.com/IntersectMBO/formal-ledger-specifications/issues/1287
         -- TODO: Re-enable after issue is resolved, by removing this override
@@ -397,19 +401,20 @@ submitFailingLegacySubTx ::
   DijkstraEraImp era =>
   Language ->
   Tx TopTx era ->
-  DijkstraSubUtxowPredFailure era ->
+  NE.NonEmpty (PredicateFailure (EraRule "LEDGER" era)) ->
   ImpTestM era ()
-submitFailingLegacySubTx lang tx expectedFailure =
+submitFailingLegacySubTx lang tx expectedFailures =
   submitFailingTxM tx $ \fixedUpTx ->
     case OMap.elems $ fixedUpTx ^. bodyTxL . subTransactionsTxBodyL of
       [subTx] ->
         pure $
-          injectFailure expectedFailure
-            NE.:| [ injectFailure $
-                      Conway.CollectErrors
-                        [BadTranslation . inject $ UnsupportedScriptInSubTx @era lang (txIdTx subTx)]
-                  | lang < PlutusV4
-                  ]
+          NE.appendList
+            expectedFailures
+            [ injectFailure $
+                Conway.CollectErrors
+                  [BadTranslation . inject $ UnsupportedScriptInSubTx @era lang (txIdTx subTx)]
+            | lang < PlutusV4
+            ]
       _ -> assertFailure "Expected exactly one sub-transaction"
 
 -- | Every distinct reason a sub-transaction requires a key witness,
